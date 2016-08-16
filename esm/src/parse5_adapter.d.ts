ad8fa117386c5620170e88c5eea1ca59f0834374
Hans Larsen <hans@hansl.ca>
2016-08-16 22:50:42 +0000

    501b9fe Rename of XHR - Moving files

--- esm/src/parse5_adapter.d.ts
+++ esm/src/parse5_adapter.d.ts
@@ -1,6 +1,6 @@
 import { DomAdapter } from '../platform_browser_private';
 import { Type } from '@angular/core';
-import { XHR } from '@angular/compiler';
+import { ResourceLoader } from '@angular/compiler';
 export declare class Parse5DomAdapter extends DomAdapter {
     static makeCurrent(): void;
     hasProperty(element: any, name: string): boolean;
@@ -10,7 +10,7 @@ export declare class Parse5DomAdapter extends DomAdapter {
     log(error: any): void;
     logGroup(error: any): void;
     logGroupEnd(): void;
-    getXHR(): Type<XHR>;
+    getResourceLoader(): Type<ResourceLoader>;
     readonly attrToPropMap: {
         [key: string]: string;
     };

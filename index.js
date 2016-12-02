92140a274c027819fb1bf3c263cd1ee2777ba6ec
GitHub <noreply@github.com>
2016-12-02 01:16:00 +0000

    6c0b65b docs(changelog): add instructions for people that depend on the `cache` operator

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-614a35d');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-6c0b65b');
 export { ServerModule, platformDynamicServer, platformServer, __platform_server_private__ } from './src/platform-server';
 //# sourceMappingURL=index.js.map

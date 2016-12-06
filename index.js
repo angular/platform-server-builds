4b81edefa04e2d688f9febf746f0fdcc9d457b9f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-06 18:54:10 +0000

    f31c947 fix(compiler): short-circut expressions with an index (#13263)

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-4bd8f58');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-f31c947');
 export { ServerModule, platformDynamicServer, platformServer, __platform_server_private__ } from './src/platform-server';
 //# sourceMappingURL=index.js.map

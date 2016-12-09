6b16101a1d03b32f17b6a889144f08cb1db6e5f9
Victor Berchet <victor@suumit.com>
2016-12-09 19:33:21 +0000

    c8baf51 style: clang-format the code

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-9a8423d
+ * @license Angular v2.3.0-c8baf51
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1970,7 +1970,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-9a8423d');
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-c8baf51');
 
   exports.ServerModule = ServerModule;
   exports.platformDynamicServer = platformDynamicServer;

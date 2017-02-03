2352bc8aae7a43223f55ded35614d4f74533f7da
Misko Hevery <misko@hevery.com>
2017-02-03 05:42:45 +0000

    559cf9d docs(changelog): add changelog for 4.0.0-beta.6

---
+++ src/private_import_platform-browser.js
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { __platform_browser_private__ as _ } from '@angular/platform-browser/index';
+export const /** @type {?} */ DomAdapter = _.DomAdapter;
+export const /** @type {?} */ setRootDomAdapter = _.setRootDomAdapter;
+export const /** @type {?} */ getDOM = _.getDOM;
+export const /** @type {?} */ SharedStylesHost = _.SharedStylesHost;
+export const /** @type {?} */ NAMESPACE_URIS = _.NAMESPACE_URIS;
+export const /** @type {?} */ shimContentAttribute = _.shimContentAttribute;
+export const /** @type {?} */ shimHostAttribute = _.shimHostAttribute;
+export const /** @type {?} */ flattenStyles = _.flattenStyles;
+export const /** @type {?} */ splitNamespace = _.splitNamespace;
+export const /** @type {?} */ isNamespaced = _.isNamespaced;
+//# sourceMappingURL=private_import_platform-browser.js.map

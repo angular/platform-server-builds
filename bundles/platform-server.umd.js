c2f3dfc7f860152b61667742c76982b7e71576d2
Miško Hevery <misko@hevery.com>
2017-02-07 19:56:06 +0000

    a378aab docs(compiler): incorrect method reference (#14314)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.6-7036e04
+ * @license Angular v4.0.0-beta.6-a378aab
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1975,7 +1975,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.6-7036e04');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-beta.6-a378aab');
 
     exports.ServerModule = ServerModule;
     exports.platformDynamicServer = platformDynamicServer;

c6d29c461e3618da92869c386d11f0d7f3f8a3b3
GitHub <noreply@github.com>
2017-02-11 06:37:45 +0000

    2ca93e1 doc: update Node.js and npm version requirements

---
+++ src/location.js
@@ -0,0 +1,124 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { Injectable } from '@angular/core/index';
+import { Subject } from 'rxjs/Subject';
+import * as url from 'url';
+import { scheduleMicroTask } from './facade/lang';
+import { getDOM } from './private_import_platform-browser';
+/**
+ * Server-side implementation of URL state. Implements `pathname`, `search`, and `hash`
+ * but not the state stack.
+ */
+export class ServerPlatformLocation {
+    constructor() {
+        this._path = '/';
+        this._search = '';
+        this._hash = '';
+        this._hashUpdate = new Subject();
+    }
+    /**
+     * @return {?}
+     */
+    getBaseHrefFromDOM() { return getDOM().getBaseHref(); }
+    /**
+     * @param {?} fn
+     * @return {?}
+     */
+    onPopState(fn) {
+        // No-op: a state stack is not implemented, so
+        // no events will ever come.
+    }
+    /**
+     * @param {?} fn
+     * @return {?}
+     */
+    onHashChange(fn) { this._hashUpdate.subscribe(fn); }
+    /**
+     * @return {?}
+     */
+    get pathname() { return this._path; }
+    /**
+     * @return {?}
+     */
+    get search() { return this._search; }
+    /**
+     * @return {?}
+     */
+    get hash() { return this._hash; }
+    /**
+     * @return {?}
+     */
+    get url() { return `${this.pathname}${this.search}${this.hash}`; }
+    /**
+     * @param {?} value
+     * @param {?} oldUrl
+     * @return {?}
+     */
+    setHash(value, oldUrl) {
+        if (this._hash === value) {
+            // Don't fire events if the hash has not changed.
+            return;
+        }
+        this._hash = value;
+        const /** @type {?} */ newUrl = this.url;
+        scheduleMicroTask(() => this._hashUpdate.next(/** @type {?} */ ({ type: 'hashchange', oldUrl, newUrl })));
+    }
+    /**
+     * @param {?} state
+     * @param {?} title
+     * @param {?} newUrl
+     * @return {?}
+     */
+    replaceState(state, title, newUrl) {
+        const /** @type {?} */ oldUrl = this.url;
+        const /** @type {?} */ parsedUrl = url.parse(newUrl, true);
+        this._path = parsedUrl.path;
+        this._search = parsedUrl.search;
+        this.setHash(parsedUrl.hash, oldUrl);
+    }
+    /**
+     * @param {?} state
+     * @param {?} title
+     * @param {?} newUrl
+     * @return {?}
+     */
+    pushState(state, title, newUrl) {
+        this.replaceState(state, title, newUrl);
+    }
+    /**
+     * @return {?}
+     */
+    forward() { throw new Error('Not implemented'); }
+    /**
+     * @return {?}
+     */
+    back() { throw new Error('Not implemented'); }
+}
+ServerPlatformLocation.decorators = [
+    { type: Injectable },
+];
+/** @nocollapse */
+ServerPlatformLocation.ctorParameters = () => [];
+function ServerPlatformLocation_tsickle_Closure_declarations() {
+    /** @type {?} */
+    ServerPlatformLocation.decorators;
+    /**
+     * @nocollapse
+     * @type {?}
+     */
+    ServerPlatformLocation.ctorParameters;
+    /** @type {?} */
+    ServerPlatformLocation.prototype._path;
+    /** @type {?} */
+    ServerPlatformLocation.prototype._search;
+    /** @type {?} */
+    ServerPlatformLocation.prototype._hash;
+    /** @type {?} */
+    ServerPlatformLocation.prototype._hashUpdate;
+}
+//# sourceMappingURL=location.js.map
---
+++ src/server.js
@@ -0,0 +1,81 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { PlatformLocation } from '@angular/common/index';
+import { platformCoreDynamic } from '@angular/compiler/index';
+import { NgModule, PLATFORM_INITIALIZER, RootRenderer, createPlatformFactory, isDevMode, platformCore } from '@angular/core/index';
+import { BrowserModule } from '@angular/platform-browser/index';
+import { ServerPlatformLocation } from './location';
+import { Parse5DomAdapter } from './parse5_adapter';
+import { DebugDomRootRenderer } from './private_import_core';
+import { SharedStylesHost } from './private_import_platform-browser';
+import { ServerRootRenderer } from './server_renderer';
+/**
+ * @param {?} feature
+ * @return {?}
+ */
+function notSupported(feature) {
+    throw new Error(`platform-server does not support '${feature}'.`);
+}
+export const /** @type {?} */ INTERNAL_SERVER_PLATFORM_PROVIDERS = [
+    { provide: PLATFORM_INITIALIZER, useValue: initParse5Adapter, multi: true },
+    { provide: PlatformLocation, useClass: ServerPlatformLocation },
+];
+/**
+ * @return {?}
+ */
+function initParse5Adapter() {
+    Parse5DomAdapter.makeCurrent();
+}
+/**
+ * @param {?} rootRenderer
+ * @return {?}
+ */
+export function _createConditionalRootRenderer(rootRenderer) {
+    if (isDevMode()) {
+        return new DebugDomRootRenderer(rootRenderer);
+    }
+    return rootRenderer;
+}
+export const /** @type {?} */ SERVER_RENDER_PROVIDERS = [
+    ServerRootRenderer,
+    { provide: RootRenderer, useFactory: _createConditionalRootRenderer, deps: [ServerRootRenderer] },
+    // use plain SharedStylesHost, not the DomSharedStylesHost
+    SharedStylesHost
+];
+/**
+ * The ng module for the server.
+ *
+ * \@experimental
+ */
+export class ServerModule {
+}
+ServerModule.decorators = [
+    { type: NgModule, args: [{ exports: [BrowserModule], providers: SERVER_RENDER_PROVIDERS },] },
+];
+/** @nocollapse */
+ServerModule.ctorParameters = () => [];
+function ServerModule_tsickle_Closure_declarations() {
+    /** @type {?} */
+    ServerModule.decorators;
+    /**
+     * @nocollapse
+     * @type {?}
+     */
+    ServerModule.ctorParameters;
+}
+/**
+ * @experimental
+ */
+export const /** @type {?} */ platformServer = createPlatformFactory(platformCore, 'server', INTERNAL_SERVER_PLATFORM_PROVIDERS);
+/**
+ * The server platform that supports the runtime compiler.
+ *
+ * @experimental
+ */
+export const /** @type {?} */ platformDynamicServer = createPlatformFactory(platformCoreDynamic, 'serverDynamic', INTERNAL_SERVER_PLATFORM_PROVIDERS);
+//# sourceMappingURL=server.js.map

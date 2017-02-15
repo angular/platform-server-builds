3d4ddc44eeb688181d8bc958cfb4d0f706021cb6
GitHub <noreply@github.com>
2017-02-15 16:57:47 +0000

    3a2e5f5 build: update tbosch/vicb in pullapprove

---
+++ src/platform-server.js
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+export { PlatformState } from './platform_state';
+export { INITIAL_CONFIG, ServerModule, platformDynamicServer, platformServer } from './server';
+export { renderModule, renderModuleFactory } from './utils';
+export { __platform_server_private__ } from './private_export';
+export { VERSION } from './version';
+//# sourceMappingURL=platform-server.js.map

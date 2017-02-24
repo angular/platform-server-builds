242b64f00a43d16cd65dc65e19c0eff7a336c989
Igor Minar <iminar@google.com>
2017-02-24 05:46:44 +0000

    be85103 fix(animations): microtasks should not be timeouts (#14687)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -2334,7 +2334,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-01907ba');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-beta.8-be85103');
 
     exports.PlatformState = PlatformState;
     exports.ServerModule = ServerModule;

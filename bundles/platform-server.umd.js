45a81775d1951365489dfe61a0079cb599d04464
Igor Minar <iminar@google.com>
2017-03-01 16:25:57 +0000

    d1182af refactor: change remaining `<template>` to `<ng-template>` (#14706)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1993,7 +1993,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-5753de5');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-d1182af');
 
     exports.PlatformState = PlatformState;
     exports.ServerModule = ServerModule;

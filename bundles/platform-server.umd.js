7e2a19ede0599e9641817bf5c72dc548ea8a3634
Igor Minar <iminar@google.com>
2017-03-01 16:34:05 +0000

    49b462e test: closure integration test for change detection (#14691)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1993,7 +1993,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-d1182af');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-49b462e');
 
     exports.PlatformState = PlatformState;
     exports.ServerModule = ServerModule;

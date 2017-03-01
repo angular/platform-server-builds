b6dbbd1a47d46a0c6e4f31b191e5a1926b53d49f
Igor Minar <iminar@google.com>
2017-03-01 23:05:04 +0000

    a6996a9 fix(tsc-wrapped): validate metadata in static members of a class (#14772)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1993,7 +1993,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-6bae737');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-a6996a9');
 
     exports.PlatformState = PlatformState;
     exports.ServerModule = ServerModule;

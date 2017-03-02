e170743f0922c6993920b0a3057aa92b0c014ed3
Igor Minar <igor@angularjs.org>
2017-03-02 07:20:31 +0000

    180b705 release: cut the 4.0.0-rc.2 release

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1993,7 +1993,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-9560ad8');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-180b705');
 
     exports.PlatformState = PlatformState;
     exports.ServerModule = ServerModule;

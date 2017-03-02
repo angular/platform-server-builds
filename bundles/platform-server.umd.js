b4b74c4b1e0b778715cc7a3a9539655391dbd437
Igor Minar <igor@angularjs.org>
2017-03-02 07:43:10 +0000

    49aa508 docs: add 2.4.9 release notes

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1993,7 +1993,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-180b705');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.2-49aa508');
 
     exports.PlatformState = PlatformState;
     exports.ServerModule = ServerModule;

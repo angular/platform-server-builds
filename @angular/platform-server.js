bca6e7523804e66069fa910e784bb5db77a19586
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-15 23:24:33 +0000

    bf98d9d fix(aio): scroll to hash fragment element on URL change

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-a9b1880
+ * @license Angular v4.0.0-rc.3-bf98d9d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2143,6 +2143,6 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-a9b1880');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-bf98d9d');
 
 export { PlatformState, ServerModule, platformDynamicServer, platformServer, INITIAL_CONFIG, renderModule, renderModuleFactory, VERSION, INTERNAL_SERVER_PLATFORM_PROVIDERS as ɵINTERNAL_SERVER_PLATFORM_PROVIDERS, SERVER_RENDER_PROVIDERS as ɵSERVER_RENDER_PROVIDERS, ServerRendererFactory2 as ɵServerRendererFactory2, SERVER_HTTP_PROVIDERS as ɵe, ServerXhr as ɵb, ServerXsrfStrategy as ɵc, httpFactory as ɵd, ServerStylesHost as ɵa };

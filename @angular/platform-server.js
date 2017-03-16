b96841927d7c7c45d7fdbb4581249fdebea75933
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 16:31:19 +0000

    41f61b0 test: integration test that Angular apps compile with TS 2.2 (#15153)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-f92d644
+ * @license Angular v4.0.0-rc.3-41f61b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2161,6 +2161,6 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-f92d644');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-41f61b0');
 
 export { PlatformState, ServerModule, platformDynamicServer, platformServer, INITIAL_CONFIG, renderModule, renderModuleFactory, VERSION, INTERNAL_SERVER_PLATFORM_PROVIDERS as ɵINTERNAL_SERVER_PLATFORM_PROVIDERS, SERVER_RENDER_PROVIDERS as ɵSERVER_RENDER_PROVIDERS, ServerRendererFactory2 as ɵServerRendererFactory2, SERVER_HTTP_PROVIDERS as ɵe, ServerXhr as ɵb, ServerXsrfStrategy as ɵc, httpFactory as ɵd, ServerStylesHost as ɵa };

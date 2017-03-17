e3df8ccdbd6fb710c5fbbbf3142e3938f719eb4c
Chuck Jazdzewski <chuckj@google.com>
2017-03-17 03:39:27 +0000

    b7212f5 docs: add changelog for 4.0.0-rc.4

---
+++ @angular/platform-server/testing.es5.js
@@ -0,0 +1,32 @@
+/**
+ * @license Angular v4.0.0-rc.4-b7212f5
+ * (c) 2010-2017 Google, Inc. https://angular.io/
+ * License: MIT
+ */
+import { platformCoreDynamicTesting } from '@angular/compiler/testing';
+import { createPlatformFactory, NgModule } from '@angular/core';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { ɵINTERNAL_SERVER_PLATFORM_PROVIDERS, ɵSERVER_RENDER_PROVIDERS } from '@angular/platform-server';
+/**
+ * Platform for testing
+ *
+ * @experimental API related to bootstrapping are still under review.
+ */
+var platformServerTesting = createPlatformFactory(platformCoreDynamicTesting, 'serverTesting', ɵINTERNAL_SERVER_PLATFORM_PROVIDERS);
+/**
+ * NgModule for testing.
+ *
+ * @experimental API related to bootstrapping are still under review.
+ */
+var ServerTestingModule = (function () {
+    function ServerTestingModule() {
+    }
+    return ServerTestingModule;
+}());
+ServerTestingModule.decorators = [
+    { type: NgModule, args: [{ exports: [BrowserDynamicTestingModule], providers: ɵSERVER_RENDER_PROVIDERS },] },
+];
+/** @nocollapse */
+ServerTestingModule.ctorParameters = function () { return []; };
+export { platformServerTesting, ServerTestingModule };
+//# sourceMappingURL=testing.es5.js.map
---
+++ @angular/platform-server/testing.js
@@ -0,0 +1,31 @@
+/**
+ * @license Angular v4.0.0-rc.4-b7212f5
+ * (c) 2010-2017 Google, Inc. https://angular.io/
+ * License: MIT
+ */
+import { platformCoreDynamicTesting } from '@angular/compiler/testing';
+import { createPlatformFactory, NgModule } from '@angular/core';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { ɵINTERNAL_SERVER_PLATFORM_PROVIDERS, ɵSERVER_RENDER_PROVIDERS } from '@angular/platform-server';
+
+/**
+ * Platform for testing
+ *
+ * @experimental API related to bootstrapping are still under review.
+ */
+const platformServerTesting = createPlatformFactory(platformCoreDynamicTesting, 'serverTesting', ɵINTERNAL_SERVER_PLATFORM_PROVIDERS);
+/**
+ * NgModule for testing.
+ *
+ * @experimental API related to bootstrapping are still under review.
+ */
+class ServerTestingModule {
+}
+ServerTestingModule.decorators = [
+    { type: NgModule, args: [{ exports: [BrowserDynamicTestingModule], providers: ɵSERVER_RENDER_PROVIDERS },] },
+];
+/** @nocollapse */
+ServerTestingModule.ctorParameters = () => [];
+
+export { platformServerTesting, ServerTestingModule };
+//# sourceMappingURL=testing.js.map

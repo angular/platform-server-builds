95c439371a6927e4f582b99aadd1583cea3862a1
Miško Hevery <misko@hevery.com>
2017-03-17 20:55:50 +0000

    d0bc83c build(aio): implement prerendering

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.4-fcaca45
+ * @license Angular v4.0.0-rc.5-d0bc83c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2228,7 +2228,7 @@
     /**
      * \@stable
      */
-    var VERSION = new _angular_core.Version('4.0.0-rc.4-fcaca45');
+    var VERSION = new _angular_core.Version('4.0.0-rc.5-d0bc83c');
 
     exports.PlatformState = PlatformState;
     exports.ServerModule = ServerModule;

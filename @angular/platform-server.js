3a5a6ead821df50240a769d4c16065bf27806012
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-20 17:32:46 +0000

    d5a6e69 build: update to angular 4.0.0-rc.4 and service-worker@1.0.0-beta.7

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-60c8368
+ * @license Angular v4.0.0-rc.5-d5a6e69
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2179,7 +2179,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-60c8368');
+const VERSION = new Version('4.0.0-rc.5-d5a6e69');
 
 export { PlatformState, ServerModule, platformDynamicServer, platformServer, INITIAL_CONFIG, renderModule, renderModuleFactory, VERSION, INTERNAL_SERVER_PLATFORM_PROVIDERS as ɵINTERNAL_SERVER_PLATFORM_PROVIDERS, SERVER_RENDER_PROVIDERS as ɵSERVER_RENDER_PROVIDERS, ServerRendererFactory2 as ɵServerRendererFactory2, SERVER_HTTP_PROVIDERS as ɵe, ServerXhr as ɵb, ServerXsrfStrategy as ɵc, httpFactory as ɵd, ServerStylesHost as ɵa };
 //# sourceMappingURL=platform-server.js.map

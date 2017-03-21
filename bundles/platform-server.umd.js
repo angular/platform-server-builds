3b5a2abff048743c64af6d0abe3668a15b84993a
Miško Hevery <misko@hevery.com>
2017-03-21 20:40:48 +0000

    e59e5e2 build(aio): upgrade to rxjs@5.2.0

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-64beae9
+ * @license Angular v4.0.0-rc.5-e59e5e2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.5-64beae9
+ * @license Angular v4.0.0-rc.5-e59e5e2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2319,7 +2319,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.0.0-rc.5-64beae9');
+var VERSION = new _angular_core.Version('4.0.0-rc.5-e59e5e2');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

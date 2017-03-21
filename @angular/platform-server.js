81d3f90d9fbf53d1557b2cdd9ac0862535fab353
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-21 15:53:46 +0000

    11d5309 build(aio): fix paths to "index" pages

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-81b6a5f
+ * @license Angular v4.0.0-rc.5-11d5309
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2258,7 +2258,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-81b6a5f');
+const VERSION = new Version('4.0.0-rc.5-11d5309');
 
 /**
  * @license

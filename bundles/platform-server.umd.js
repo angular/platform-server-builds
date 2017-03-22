51511c8be895714d8f2cc1f16f1465c0e08914a5
Igor Minar <iminar@google.com>
2017-03-22 22:29:47 +0000

    a50d79d fix(compiler-cli): adding missing format xliff for the extractor (#15386)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-64285a2
+ * @license Angular v4.0.0-rc.5-a50d79d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -15,7 +15,7 @@ var __extends = (undefined && undefined.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.5-64285a2
+ * @license Angular v4.0.0-rc.5-a50d79d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2319,7 +2319,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.0.0-rc.5-64285a2');
+var VERSION = new _angular_core.Version('4.0.0-rc.5-a50d79d');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

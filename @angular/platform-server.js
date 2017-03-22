f75c79703c1dbca52773bd61591f1bac9e1522bd
Igor Minar <iminar@google.com>
2017-03-22 20:44:59 +0000

    941f194 docs: spelling errors (#15375)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-8b4edcc
+ * @license Angular v4.0.0-rc.5-941f194
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-8b4edcc');
+const VERSION = new Version('4.0.0-rc.5-941f194');
 
 /**
  * @license

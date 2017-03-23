74ec9df9205367f6fbfbe69fb02343589ee4b9b9
Victor Berchet <victor@suumit.com>
2017-03-23 17:44:39 +0000

    376088d build(aio): delete content folder before doc-gen (#15414)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-73808dd
+ * @license Angular v4.0.0-rc.6-376088d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.6-73808dd');
+const VERSION = new Version('4.0.0-rc.6-376088d');
 
 /**
  * @license

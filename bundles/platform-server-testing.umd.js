adc0bdbcb01c564dd05869c6f1e4377ffa9591cb
Igor Minar <iminar@google.com>
2017-03-23 00:31:28 +0000

    26f6bd4 ci(travis): update excluded branch to g3 (#15391)

--- bundles/platform-server-testing.umd.js
+++ bundles/platform-server-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-ea49a95
+ * @license Angular v4.0.0-rc.5-26f6bd4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_compiler_testing,_angular_core,_angular_platformBrowserDynamic_testing,_angular_platformBrowser_animations,_angular_platformServer) { 'use strict';
 
 /**
- * @license Angular v4.0.0-rc.5-ea49a95
+ * @license Angular v4.0.0-rc.5-26f6bd4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */

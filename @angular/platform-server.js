8e54e4eda903f38a18213a4b11ceb69c36a33ef1
Igor Minar <igor@angularjs.org>
2017-03-24 04:14:19 +0000

    c2892da docs: revert the move of CONTRIBUTING.md to docs/CONTRIBUTING.md

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-e1b09e3
+ * @license Angular v4.0.0-c2892da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-e1b09e3');
+const VERSION = new Version('4.0.0-c2892da');
 
 /**
  * @license

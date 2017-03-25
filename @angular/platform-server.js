b17cd0e9ecea1e59c1cadfc3c7f827299d110867
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-25 07:12:28 +0000

    afb17a5 feat(aio): block code examples style fix

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-53b89ec
+ * @license Angular v4.0.0-afb17a5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-53b89ec');
+const VERSION = new Version('4.0.0-afb17a5');
 
 /**
  * @license

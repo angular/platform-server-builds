da03dba99e732241ef637d75c1b0288f566f4442
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-25 14:39:43 +0000

    c9e51d9 feat(aio): add new classes, table and images styling (#15478)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-b283038
+ * @license Angular v4.0.0-c9e51d9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-b283038');
+const VERSION = new Version('4.0.0-c9e51d9');
 
 /**
  * @license

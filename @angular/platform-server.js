4cf23911670c10e9c7d220ad5a4f3256cfc29869
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 09:31:06 +0000

    52ea193 build(aio): left align code regions

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-228238e
+ * @license Angular v4.0.0-52ea193
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-228238e');
+const VERSION = new Version('4.0.0-52ea193');
 
 /**
  * @license

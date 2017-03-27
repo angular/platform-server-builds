0d3842dfcda98f8c4bdc07fd724c7f514be4e4ff
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 08:37:54 +0000

    a6fd22c docs(aio): fix typo

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-1616cae
+ * @license Angular v4.0.0-a6fd22c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-1616cae');
+const VERSION = new Version('4.0.0-a6fd22c');
 
 /**
  * @license

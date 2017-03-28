34330242641c43d7f06a6acf193830d6d14a7ead
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-28 09:41:26 +0000

    fd72fad docs(aio): update migrated content from anguar.io

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-920bf37
+ * @license Angular v4.0.0-fd72fad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-920bf37');
+const VERSION = new Version('4.0.0-fd72fad');
 
 /**
  * @license

3c04b3de772464cbbb60af07fe66bcd6de4d5a1e
Victor Berchet <victor@suumit.com>
2017-03-28 21:59:03 +0000

    861953c feat(aio): add LiveExampleComponent (#15544)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-aa16ccd
+ * @license Angular v4.0.0-861953c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-aa16ccd');
+const VERSION = new Version('4.0.0-861953c');
 
 /**
  * @license

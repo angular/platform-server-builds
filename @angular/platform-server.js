38557dfff5d131a1e985cb55190a7d8208450e30
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-28 11:12:04 +0000

    b7a89ce feat(aio): show top-nav with same styling as side-nav when window is narrow

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-9995191
+ * @license Angular v4.0.0-b7a89ce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-9995191');
+const VERSION = new Version('4.0.0-b7a89ce');
 
 /**
  * @license

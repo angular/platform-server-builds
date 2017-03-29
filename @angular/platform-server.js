a9e325c63f15531f7ee7d189d72203b321d58446
Victor Berchet <victor@suumit.com>
2017-03-29 23:31:27 +0000

    14fd78f fix(core): fix inheritance in JIT mode for TS 2.1 (#15599)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-a9321b1
+ * @license Angular v4.0.0-14fd78f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-a9321b1');
+const VERSION = new Version('4.0.0-14fd78f');
 
 /**
  * @license

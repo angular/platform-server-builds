ffd331a045bb10cc84dcf17890eaa561c8947321
Victor Berchet <victor@suumit.com>
2017-03-29 18:03:22 +0000

    902bb2f fix(compiler): throw when a component defines both template and templateUrl (#15572)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-23bf348
+ * @license Angular v4.0.0-902bb2f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-23bf348');
+const VERSION = new Version('4.0.0-902bb2f');
 
 /**
  * @license

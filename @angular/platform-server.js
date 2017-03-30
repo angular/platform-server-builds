c4a794ac3878f5c6ef6c77b371be2525469f4e05
Igor Minar <iminar@google.com>
2017-03-30 07:11:28 +0000

    3ccbe28 feat(aio): enable activating available ServiceWorker updates (#15561)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-67719f2
+ * @license Angular v4.1.0-beta.0-3ccbe28
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-67719f2');
+const VERSION = new Version('4.1.0-beta.0-3ccbe28');
 
 /**
  * @license

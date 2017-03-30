e49bd892533be1f8f0d239e2b4e1effe8a929bd5
Victor Berchet <victor@suumit.com>
2017-03-30 00:00:27 +0000

    6b79ab5 docs: add changelog for 4.0.1

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-ca66530
+ * @license Angular v4.0.1-6b79ab5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-ca66530');
+const VERSION = new Version('4.0.1-6b79ab5');
 
 /**
  * @license

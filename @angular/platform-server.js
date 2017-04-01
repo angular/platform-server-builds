c0edc44a3de28995e9236501b69899ca5eae8b14
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-01 00:47:03 +0000

    08941aa build(aio): process marketing docs that have .md extension

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-03fb766
+ * @license Angular v4.1.0-beta.0-08941aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-03fb766');
+const VERSION = new Version('4.1.0-beta.0-08941aa');
 
 /**
  * @license

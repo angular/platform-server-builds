f60b2a68ec1394cde46d3d34b41516b84e06f998
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 06:32:38 +0000

    e1c2e50 docs(aio): move bio images into content folder

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-08941aa
+ * @license Angular v4.1.0-beta.0-e1c2e50
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-08941aa');
+const VERSION = new Version('4.1.0-beta.0-e1c2e50');
 
 /**
  * @license

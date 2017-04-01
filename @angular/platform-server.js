18fdd534879d9e793445f00c72b8a89e948bf49a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 07:01:27 +0000

    ad639d7 feat(aio): add contribute marketing page

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-e1c2e50
+ * @license Angular v4.1.0-beta.0-ad639d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-e1c2e50');
+const VERSION = new Version('4.1.0-beta.0-ad639d7');
 
 /**
  * @license

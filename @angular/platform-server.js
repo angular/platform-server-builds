46b3b85d9c36e1635fc22287d0e0f90d1b2edf40
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-03 22:57:14 +0000

    0e38bf9 build(aio): new migration

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-309bae0
+ * @license Angular v4.1.0-beta.0-0e38bf9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-309bae0');
+const VERSION = new Version('4.1.0-beta.0-0e38bf9');
 
 /**
  * @license

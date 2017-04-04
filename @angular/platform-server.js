521dfef34c747ddc8b9f92a106c87124742d5d22
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-04 20:31:18 +0000

    7b005aa style(router): fix typo in router preloader method

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-7764c5c
+ * @license Angular v4.1.0-beta.0-7b005aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-7764c5c');
+const VERSION = new Version('4.1.0-beta.0-7b005aa');
 
 /**
  * @license

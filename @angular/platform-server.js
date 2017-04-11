0f6379a97e830a5258cf74e228a21ecc486cc14b
Hans Larsen <hans@hansl.ca>
2017-04-11 23:03:48 +0000

    14a2d1a docs: add changelog for 4.0.2

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.1-6b79ab5
+ * @license Angular v4.0.2-14a2d1a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.1-6b79ab5');
+const VERSION = new Version('4.0.2-14a2d1a');
 
 /**
  * @license

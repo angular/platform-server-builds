f1288f6f96f58c7a00a2feb28d6454d20acfc5d9
Matias Niemelä <matias@yearofmoo.com>
2017-04-13 05:24:53 +0000

    c664486 feat(animations): wip: introduce a wave of new animation features

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-f4b5784
+ * @license Angular v4.1.0-beta.1-c664486
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2269,7 +2269,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-f4b5784');
+const VERSION = new Version('4.1.0-beta.1-c664486');
 
 /**
  * @license

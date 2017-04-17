d68d4ab0ff31073726015bf8efb55b6cb9592e5f
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 21:52:31 +0000

    38a7e0d fix(compiler): ignore calls to unresolved symbols in metadata

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-a77b126
+ * @license Angular v4.1.0-beta.1-38a7e0d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2272,7 +2272,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-a77b126');
+const VERSION = new Version('4.1.0-beta.1-38a7e0d');
 
 /**
  * @license

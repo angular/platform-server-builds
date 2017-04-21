895db689687575e5a1730abc591a602d6211f1da
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-21 15:21:14 +0000

    cbe5f78 docs(aio): fix old jade on DI

--- bundles/platform-server-testing.umd.js
+++ bundles/platform-server-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-4598af2
+ * @license Angular v4.1.0-rc.0-cbe5f78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_compiler_testing,_angular_core,_angular_platformBrowserDynamic_testing,_angular_platformBrowser_animations,_angular_platformServer) { 'use strict';
 
 /**
- * @license Angular v4.1.0-rc.0-4598af2
+ * @license Angular v4.1.0-rc.0-cbe5f78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */

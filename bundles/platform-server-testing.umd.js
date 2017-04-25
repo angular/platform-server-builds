44067279e8563e4f9c14b5c9af6c6150c311aa63
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-25 06:23:30 +0000

    ff0f53c build(aio): include missing packages in the docs

--- bundles/platform-server-testing.umd.js
+++ bundles/platform-server-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-64ef69f
+ * @license Angular v4.1.0-rc.0-ff0f53c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_compiler_testing,_angular_core,_angular_platformBrowserDynamic_testing,_angular_platformBrowser_animations,_angular_platformServer) { 'use strict';
 
 /**
- * @license Angular v4.1.0-rc.0-64ef69f
+ * @license Angular v4.1.0-rc.0-ff0f53c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */

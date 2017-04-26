a04c22f923834cc15ad531be9b8c2225390d2696
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-26 09:09:06 +0000

    35a2dfc fix(aio): resource nav

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-d8e2829
+ * @license Angular v4.1.0-rc.0-35a2dfc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2272,7 +2272,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-d8e2829');
+const VERSION = new Version('4.1.0-rc.0-35a2dfc');
 
 /**
  * @license

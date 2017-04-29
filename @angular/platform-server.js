07a31260d78cfcc0e1778aef0f7dff69b9674c3e
Miško Hevery <misko@hevery.com>
2017-04-29 04:12:01 +0000

    065b76d feat(core): upgrade dep on zone.js to 0.8.9 (#16401)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-270d694
+ * @license Angular v4.1.0-065b76d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2272,7 +2272,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-270d694');
+const VERSION = new Version('4.1.0-065b76d');
 
 /**
  * @license

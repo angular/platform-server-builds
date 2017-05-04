e25df15b4a590e023afa71de0c3c244b44f60a40
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 23:37:56 +0000

    04197ee feat: add .ngsummary.ts files to support AOT unit tests

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-1092292
+ * @license Angular v4.2.0-beta.0-04197ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2280,7 +2280,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-1092292');
+const VERSION = new Version('4.2.0-beta.0-04197ee');
 
 /**
  * @license

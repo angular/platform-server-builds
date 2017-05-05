f53fc0c8c3ca4d6d69967cd52f1314feb9b9e873
Matias Niemelä <matias@yearofmoo.com>
2017-05-05 20:41:12 +0000

    9da6340 fix(http): flatten metadata for @angular/http/testing

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-547c363
+ * @license Angular v4.2.0-beta.0-9da6340
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2280,7 +2280,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-547c363');
+const VERSION = new Version('4.2.0-beta.0-9da6340');
 
 /**
  * @license

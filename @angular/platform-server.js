4889136fc8cbc7ed0e909777f411ae0f0a9c82e6
Jason Aden <jasonaden@google.com>
2017-05-10 00:58:14 +0000

    e46a65f build: update key for pushing to PACKAGE-builds repos (#16667)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.1-5f6d0f2
+ * @license Angular v4.1.1-e46a65f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2272,7 +2272,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.1-5f6d0f2');
+const VERSION = new Version('4.1.1-e46a65f');
 
 /**
  * @license

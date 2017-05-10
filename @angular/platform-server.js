f6d5998203616f70158abcbbe12383d7a5d3cd52
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 11:12:47 +0000

    efa2928 docs(aio): add a progress reporter for the webpack guide

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-f5335d1
+ * @license Angular v4.2.0-beta.0-efa2928
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2280,7 +2280,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-f5335d1');
+const VERSION = new Version('4.2.0-beta.0-efa2928');
 
 /**
  * @license

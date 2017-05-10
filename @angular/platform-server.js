5a7741348731fa8a2677f7c8f994e9ae8baabfe4
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 07:09:33 +0000

    412ab3f fix(aio): basic fix to the TOC styling

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-954c08e
+ * @license Angular v4.2.0-beta.0-412ab3f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2280,7 +2280,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-954c08e');
+const VERSION = new Version('4.2.0-beta.0-412ab3f');
 
 /**
  * @license

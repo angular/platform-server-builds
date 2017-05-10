171bdfc8cf1aaad2cd5654b0baee7d11c9ad491e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 12:10:23 +0000

    56833a6 docs(aio): more content fixes

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-efa2928
+ * @license Angular v4.2.0-beta.0-56833a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2280,7 +2280,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-efa2928');
+const VERSION = new Version('4.2.0-beta.0-56833a6');
 
 /**
  * @license

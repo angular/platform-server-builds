26bf4d65f99c3c69c20c9bc96e250fcd3ba0e397
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-18 15:10:31 +0000

    6531806 build(aio): do not ignore `is-on-https` in lighthouse check

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-0e13a59
+ * @license Angular v4.2.0-beta.1-6531806
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ var __extends = (undefined && undefined.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-0e13a59
+ * @license Angular v4.2.0-beta.1-6531806
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2334,7 +2334,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-beta.1-0e13a59');
+var VERSION = new _angular_core.Version('4.2.0-beta.1-6531806');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

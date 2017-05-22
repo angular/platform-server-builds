069ea59a102af87df914ad194388afa9edfe25aa
Chuck Jazdzewski <chuckj@google.com>
2017-05-22 17:26:22 +0000

    98849de build: fix public-api:update gulp task (#16859)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-afd703d
+ * @license Angular v4.2.0-rc.0-98849de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -20,7 +20,7 @@ var __extends = (undefined && undefined.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-rc.0-afd703d
+ * @license Angular v4.2.0-rc.0-98849de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2334,7 +2334,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.0-afd703d');
+var VERSION = new _angular_core.Version('4.2.0-rc.0-98849de');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

e2e1532554f63c38e52fa179569f4f2959cf81de
Chuck Jazdzewski <chuckj@google.com>
2017-05-22 18:40:43 +0000

    08dfe91 feat(compiler): add location note to extracted xliff2 files (#16791)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-85d4c4b
+ * @license Angular v4.2.0-rc.0-08dfe91
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2280,7 +2280,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.0-85d4c4b');
+const VERSION = new Version('4.2.0-rc.0-08dfe91');
 
 /**
  * @license

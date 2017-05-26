731b2ad53b5a07e206444ea2f0f6de4b69f026df
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 00:01:13 +0000

    4c5e28e build(aio): less verbose boilerplate generator (#16970)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-b37a048
+ * @license Angular v4.2.0-rc.0-4c5e28e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2280,7 +2280,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.0-b37a048');
+const VERSION = new Version('4.2.0-rc.0-4c5e28e');
 
 /**
  * @license

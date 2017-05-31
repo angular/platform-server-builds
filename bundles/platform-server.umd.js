bc16d241ba82433d7d81a4c5090cdfead8b9767e
Victor Berchet <victor@suumit.com>
2017-05-31 22:00:00 +0000

    c20f60b fix(router): make remove trailing slash consistent with URL params

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-1408357
+ * @license Angular v4.2.0-rc.1-c20f60b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.1-1408357
+ * @license Angular v4.2.0-rc.1-c20f60b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2350,7 +2350,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.1-1408357');
+var VERSION = new _angular_core.Version('4.2.0-rc.1-c20f60b');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

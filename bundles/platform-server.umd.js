d5a066ec821995fd036b95ce828ba1bbe032e55f
Victor Berchet <victor@suumit.com>
2017-06-01 21:24:08 +0000

    7822187 fix(aio): fix scrolling to top (#17102)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.1-068133e
+ * @license Angular v4.2.0-rc.1-7822187
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.1-068133e
+ * @license Angular v4.2.0-rc.1-7822187
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2350,7 +2350,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.1-068133e');
+var VERSION = new _angular_core.Version('4.2.0-rc.1-7822187');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

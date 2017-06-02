5aa0b0f019f66a23d84c50ce9ac0e54c7893dda1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-02 21:50:08 +0000

    4c32cb9 fix(router): opening links in new window

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-d5ce086
+ * @license Angular v4.2.0-rc.2-4c32cb9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2280,7 +2280,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-d5ce086');
+const VERSION = new Version('4.2.0-rc.2-4c32cb9');
 
 /**
  * @license

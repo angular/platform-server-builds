3bc5368f5b4042c615e538f083583a3d8cc48da7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-03 00:23:03 +0000

    819514a fix(animations): ensure web-animations understands a numeric CSS perspective value

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-b55adee
+ * @license Angular v4.2.0-rc.2-819514a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2280,7 +2280,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-b55adee');
+const VERSION = new Version('4.2.0-rc.2-819514a');
 
 /**
  * @license

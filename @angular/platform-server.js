29116cfe4f85c807f185a84915dd90b2d32df658
Igor Minar <iminar@google.com>
2017-06-06 07:07:48 +0000

    784347f fix(aio): ensure all views can indicate the active node (#17194)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-42176a7
+ * @license Angular v4.2.0-rc.2-784347f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2280,7 +2280,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-42176a7');
+const VERSION = new Version('4.2.0-rc.2-784347f');
 
 /**
  * @license

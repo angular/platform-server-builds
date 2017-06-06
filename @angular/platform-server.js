a8d13265b2ee02409739db306bddab98f3fb8c2b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-06 15:59:02 +0000

    3a121a6 build(aio): migrate contribute page to html

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-c06f4fc
+ * @license Angular v4.2.0-rc.2-3a121a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2280,7 +2280,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-c06f4fc');
+const VERSION = new Version('4.2.0-rc.2-3a121a6');
 
 /**
  * @license

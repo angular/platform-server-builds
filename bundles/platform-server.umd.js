a7e5ab83552adcca284f31b67f0824ca11f5851b
GitHub <noreply@github.com>
2017-06-08 22:47:39 +0000

    576d6d8 feat(aio): update the home page and docs landing page content (#17325)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-a222c3e
+ * @license Angular v4.2.0-rc.2-576d6d8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-rc.2-a222c3e
+ * @license Angular v4.2.0-rc.2-576d6d8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2344,7 +2344,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-rc.2-a222c3e');
+var VERSION = new _angular_core.Version('4.2.0-rc.2-576d6d8');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

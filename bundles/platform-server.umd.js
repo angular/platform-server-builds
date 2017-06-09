26d4a28d785111e8859c4e7f70c5c4d5917ac7d2
Igor Minar <iminar@google.com>
2017-06-09 20:56:25 +0000

    3a99af2 docs(packaging): update homepage in @angular/tsc-wrapped (#17352)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-503fd1f
+ * @license Angular v4.2.0-3a99af2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-503fd1f
+ * @license Angular v4.2.0-3a99af2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2344,7 +2344,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-503fd1f');
+var VERSION = new _angular_core.Version('4.2.0-3a99af2');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

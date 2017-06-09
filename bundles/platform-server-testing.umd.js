f0fd806a0d50d3aa6e9d5925e008ec738861860f
Igor Minar <iminar@google.com>
2017-06-09 20:50:54 +0000

    503fd1f docs(aio): fix image width on the homepage

--- bundles/platform-server-testing.umd.js
+++ bundles/platform-server-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-ce18c68
+ * @license Angular v4.2.0-503fd1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_compiler_testing,_angular_core,_angular_platformBrowserDynamic_testing,_angular_platformBrowser_animations,_angular_platformServer) { 'use strict';
 
 /**
- * @license Angular v4.2.0-ce18c68
+ * @license Angular v4.2.0-503fd1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-ce18c68
+ * @license Angular v4.2.0-503fd1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-ce18c68
+ * @license Angular v4.2.0-503fd1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2344,7 +2344,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-ce18c68');
+var VERSION = new _angular_core.Version('4.2.0-503fd1f');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

b689950c72cb5ffa1cd5a8245761e80246aa11ba
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-09 16:15:35 +0000

    2d56239 fix(aio): sidebar folder state after select item

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-a0b30e5
+ * @license Angular v4.2.0-2d56239
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-a0b30e5
+ * @license Angular v4.2.0-2d56239
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2344,7 +2344,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-a0b30e5');
+var VERSION = new _angular_core.Version('4.2.0-2d56239');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

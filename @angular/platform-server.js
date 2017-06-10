e2d8c0e23380fa70b392405b665940d6166605c7
GitHub <noreply@github.com>
2017-06-10 01:52:16 +0000

    c8c0cee build(bazel): fix the typescript path in the BUILD file (#17316)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-afbc2b0
+ * @license Angular v4.2.1-c8c0cee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2274,7 +2274,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.1-afbc2b0');
+const VERSION = new Version('4.2.1-c8c0cee');
 
 /**
  * @license

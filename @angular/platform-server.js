9b1a92efb8847aca4b9f3ac3b27187bc9a25b55c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-11 18:06:11 +0000

    d961911 docs(aio): fix broken example in upgrade guide

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-e543272
+ * @license Angular v4.2.1-d961911
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2274,7 +2274,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.1-e543272');
+const VERSION = new Version('4.2.1-d961911');
 
 /**
  * @license

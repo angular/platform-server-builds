39b2fb64c37e77f65e19b67dfe37189687ecaf5a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 19:40:29 +0000

    451257a fix(animations): do not treat a `0` animation state as `void`

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-185075d
+ * @license Angular v4.2.1-451257a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2274,7 +2274,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.1-185075d');
+const VERSION = new Version('4.2.1-451257a');
 
 /**
  * @license

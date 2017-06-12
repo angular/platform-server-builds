70e60d18e9e6a78c274aa9fcb8d70652008483dd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 23:18:54 +0000

    234268e ci(aio): deploy commits on the stable branch to production

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-7a2d2ef
+ * @license Angular v4.2.1-234268e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2274,7 +2274,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-7a2d2ef');
+const VERSION = new Version('4.2.1-234268e');
 
 /**
  * @license

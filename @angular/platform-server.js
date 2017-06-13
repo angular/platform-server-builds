8c024f27d39c97b5516a63822b5ef72415ed4999
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 10:54:20 +0000

    bffccf4 fix(aio): fix buttons in "Home" and "Features"

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-7caa0a8
+ * @license Angular v4.2.2-bffccf4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2274,7 +2274,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-7caa0a8');
+const VERSION = new Version('4.2.2-bffccf4');
 
 /**
  * @license

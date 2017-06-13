81206c743b4d5dd13186ddf91598a878396b4c9d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-13 19:02:50 +0000

    4164369 docs(aio): update typescript for `examples/webpack` to same as cli

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-dc084a5
+ * @license Angular v4.2.2-4164369
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-dc084a5
+ * @license Angular v4.2.2-4164369
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2344,7 +2344,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-dc084a5');
+var VERSION = new _angular_core.Version('4.2.2-4164369');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

668d0124710a320f9fdcd41418349acba2b9dea4
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-13 11:24:22 +0000

    709a3f6 fix(aio): fix scrolling to elements near the bottom of the page

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-bffccf4
+ * @license Angular v4.2.2-709a3f6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2274,7 +2274,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-bffccf4');
+const VERSION = new Version('4.2.2-709a3f6');
 
 /**
  * @license

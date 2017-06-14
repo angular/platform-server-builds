8e6196c619d44baf3996363ecc52483230e45485
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-14 15:04:15 +0000

    2bdf2fe test(aio): add async beforeEach to prevent Chrome disconnects

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-607fb1f
+ * @license Angular v4.2.2-2bdf2fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-607fb1f
+ * @license Angular v4.2.2-2bdf2fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2344,7 +2344,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-607fb1f');
+var VERSION = new _angular_core.Version('4.2.2-2bdf2fe');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

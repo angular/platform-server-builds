1ed5127eeeb2d7e1e7a96edd582d78fdb93cdeb9
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-15 07:05:48 +0000

    01173b9 fix(aio): do not log messages in production

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-06a0cf2
+ * @license Angular v4.2.2-01173b9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-06a0cf2
+ * @license Angular v4.2.2-01173b9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2344,7 +2344,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-06a0cf2');
+var VERSION = new _angular_core.Version('4.2.2-01173b9');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

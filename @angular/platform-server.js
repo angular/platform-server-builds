fcd8b469aae0694e9dfc60694dceee415d27889b
Hans Larsen <hans@hansl.ca>
2017-06-16 17:20:26 +0000

    1ddbddb docs: add changelog for 4.2.3

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-232bd93
+ * @license Angular v4.2.2-1ddbddb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2274,7 +2274,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-232bd93');
+const VERSION = new Version('4.2.2-1ddbddb');
 
 /**
  * @license
--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-232bd93
+ * @license Angular v4.2.2-1ddbddb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-232bd93
+ * @license Angular v4.2.2-1ddbddb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2344,7 +2344,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-232bd93');
+var VERSION = new _angular_core.Version('4.2.2-1ddbddb');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

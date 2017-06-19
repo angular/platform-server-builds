96b5015dc56e9eb6f92424e49ff3a6e8b648eb15
Hans Larsen <hans@hansl.ca>
2017-06-19 18:47:03 +0000

    afbb6bb docs: remove aio from changelog

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-4f37f86
+ * @license Angular v4.2.2-afbb6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2274,7 +2274,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-4f37f86');
+const VERSION = new Version('4.2.2-afbb6bb');
 
 /**
  * @license
--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-4f37f86
+ * @license Angular v4.2.2-afbb6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-4f37f86
+ * @license Angular v4.2.2-afbb6bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2344,7 +2344,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-4f37f86');
+var VERSION = new _angular_core.Version('4.2.2-afbb6bb');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

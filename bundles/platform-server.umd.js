d3d3c3d256b9c22dd995146bacac9e9db9ce2518
Hans <hans@hansl.ca>
2017-06-20 20:19:56 +0000

    c59c390 fix: argument destructuring sometimes breaks strictNullChecks

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-f194f18
+ * @license Angular v4.2.2-c59c390
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-f194f18
+ * @license Angular v4.2.2-c59c390
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2344,7 +2344,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-f194f18');
+var VERSION = new _angular_core.Version('4.2.2-c59c390');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

f26537fccef72ed1525746942b13cfebd83af445
Hans <hans@hansl.ca>
2017-06-20 20:30:05 +0000

    00874c2 docs(aio): update resource listing

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-009651e
+ * @license Angular v4.2.2-00874c2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.2-009651e
+ * @license Angular v4.2.2-00874c2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2344,7 +2344,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.2-009651e');
+var VERSION = new _angular_core.Version('4.2.2-00874c2');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

fd2110b3e05d1637b11e3f72649c0a39994f776e
Matias Niemelä <matias@yearofmoo.com>
2017-06-22 22:18:39 +0000

    133b5e6 docs(aio): added link to Russian Angular Courses

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-92bcfef
+ * @license Angular v4.3.0-beta.0-133b5e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.0-92bcfef
+ * @license Angular v4.3.0-beta.0-133b5e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2344,7 +2344,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.0-92bcfef');
+var VERSION = new _angular_core.Version('4.3.0-beta.0-133b5e6');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

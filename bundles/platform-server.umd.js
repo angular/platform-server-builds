bccf1db1de51ee410f3b6999bdaaa32317e91583
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 20:09:38 +0000

    ffbdf74 docs(aio): cleanup rollup-config script

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-9344c86
+ * @license Angular v4.2.4-ffbdf74
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.4-9344c86
+ * @license Angular v4.2.4-ffbdf74
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2344,7 +2344,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.4-9344c86');
+var VERSION = new _angular_core.Version('4.2.4-ffbdf74');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

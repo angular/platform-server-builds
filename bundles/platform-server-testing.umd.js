20e081744b54f55fdda10e46358bd579f530510d
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 17:06:02 +0000

    96b1703 feat(aio): cross reference readme and docs styleguide

--- bundles/platform-server-testing.umd.js
+++ bundles/platform-server-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-e47a77f
+ * @license Angular v4.3.0-beta.0-96b1703
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_compiler_testing,_angular_core,_angular_platformBrowserDynamic_testing,_angular_platformBrowser_animations,_angular_platformServer) { 'use strict';
 
 /**
- * @license Angular v4.3.0-beta.0-e47a77f
+ * @license Angular v4.3.0-beta.0-96b1703
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */

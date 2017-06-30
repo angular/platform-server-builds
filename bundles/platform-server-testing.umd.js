b1e4cde7e39cc318f15ed737bfc73bf5c38d2b9b
Matias Niemelä <matias@yearofmoo.com>
2017-06-30 00:36:58 +0000

    40921bb docs: add changelog for 4.2.5

--- bundles/platform-server-testing.umd.js
+++ bundles/platform-server-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-dfcca66
+ * @license Angular v4.3.0-beta.1-40921bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_compiler_testing,_angular_core,_angular_platformBrowserDynamic_testing,_angular_platformBrowser_animations,_angular_platformServer) { 'use strict';
 
 /**
- * @license Angular v4.3.0-beta.1-dfcca66
+ * @license Angular v4.3.0-beta.1-40921bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */

6f987e1266fc15ff622b9bd6ccfeb350c15c448b
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-05 22:05:54 +0000

    92d7ecf docs: shefali edits to Authors Style Guide (#17853)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-c6b75b0
+ * @license Angular v4.3.0-beta.1-92d7ecf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2274,7 +2274,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-c6b75b0');
+const VERSION = new Version('4.3.0-beta.1-92d7ecf');
 
 /**
  * @license

a1352338469f9ea2b11967aa917857438051e246
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 23:49:06 +0000

    2ba3ada revert: "refactor(compiler-cli): remove the dependency on fs in codegen.ts (#17738)"

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-c1474f3
+ * @license Angular v4.3.0-beta.1-2ba3ada
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2391,7 +2391,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-c1474f3');
+var VERSION = new Version('4.3.0-beta.1-2ba3ada');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

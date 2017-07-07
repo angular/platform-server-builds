ac62dbe47987a072cd2827caaa96e110032f7257
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 23:35:32 +0000

    0ede642 docs(aio): Update `NgStyle` and `NgClass` to use quoted keys

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-8e28382
+ * @license Angular v4.3.0-beta.1-0ede642
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2391,7 +2391,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-8e28382');
+var VERSION = new Version('4.3.0-beta.1-0ede642');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

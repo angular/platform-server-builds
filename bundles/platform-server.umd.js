f6df663bce9a987e69a764d0b73697308ef3c877
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 22:17:09 +0000

    8e28382 feat(animations): support disabling animations for sub elements

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-c81ad9d
+ * @license Angular v4.3.0-beta.1-8e28382
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-c81ad9d
+ * @license Angular v4.3.0-beta.1-8e28382
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2413,7 +2413,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-c81ad9d');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-8e28382');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

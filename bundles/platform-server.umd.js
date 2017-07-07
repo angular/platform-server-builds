619ddc6be978ae8cb5722f9d00b3314b6057f364
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 18:36:29 +0000

    e1174f3 fix(aio): home & marketing styles cleanup (#17926)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-cb16e9c
+ * @license Angular v4.3.0-beta.1-e1174f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-cb16e9c
+ * @license Angular v4.3.0-beta.1-e1174f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2344,7 +2344,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-cb16e9c');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-e1174f3');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

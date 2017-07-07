5351929cd391fe6e03ffde5f9c8d3e3df41d00c5
Jason Aden <jasonaden@angular.io>
2017-07-07 16:13:21 +0000

    11db3bd fix(upgrade): bring the dynamic version closer to the static one (#17971)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-3b2d2c4
+ * @license Angular v4.3.0-beta.1-11db3bd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-beta.1-3b2d2c4
+ * @license Angular v4.3.0-beta.1-11db3bd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2344,7 +2344,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-beta.1-3b2d2c4');
+var VERSION = new _angular_core.Version('4.3.0-beta.1-11db3bd');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

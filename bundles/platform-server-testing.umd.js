6c846a01da8489adbed618f2acd7c981d92abaf0
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-10 13:14:31 +0000

    bd4ec5c revert:build(aio): upgrade to dgeni-packages v0.20.0-rc.1

--- bundles/platform-server-testing.umd.js
+++ bundles/platform-server-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-406dd45
+ * @license Angular v4.2.6-bd4ec5c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_compiler_testing,_angular_core,_angular_platformBrowserDynamic_testing,_angular_platformBrowser_animations,_angular_platformServer) { 'use strict';
 
 /**
- * @license Angular v4.2.6-406dd45
+ * @license Angular v4.2.6-bd4ec5c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */

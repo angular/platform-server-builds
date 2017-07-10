8783acfa6bb45a00e7c6566e263770f800e7c18f
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-10 11:54:01 +0000

    ab13f1f build(aio): upgrade to dgeni-packages v0.20.0-rc.1

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-b69f0fa
+ * @license Angular v4.2.6-ab13f1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.6-b69f0fa
+ * @license Angular v4.2.6-ab13f1f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2344,7 +2344,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.6-b69f0fa');
+var VERSION = new _angular_core.Version('4.2.6-ab13f1f');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

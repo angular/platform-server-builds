ff0bfa29c46a1f7fbd89dca0fddfa4e0e76ab230
Pete Bacon Darwin <pete@bacondarwin.com>
2017-07-10 12:02:31 +0000

    2055634 feat(aio): add API endpoint for notifying about PR updates

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-rc.0-b3d9036
+ * @license Angular v4.3.0-rc.0-2055634
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2391,7 +2391,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-rc.0-b3d9036');
+var VERSION = new Version('4.3.0-rc.0-2055634');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

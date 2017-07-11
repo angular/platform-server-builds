0b6fe1664f42b2ab32c660fd3cbce434c9c49956
Igor Minar <iminar@google.com>
2017-07-11 20:36:29 +0000

    20127c1 docs: fix typos and grammar (#17818)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-a50d935
+ * @license Angular v4.3.0-rc.0-20127c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2336,7 +2336,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-a50d935');
+const VERSION = new Version('4.3.0-rc.0-20127c1');
 
 /**
  * @license

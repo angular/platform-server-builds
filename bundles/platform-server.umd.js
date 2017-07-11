6c0c768225cf0e81906c36e4eeb16f6191538de8
Igor Minar <iminar@google.com>
2017-07-11 19:17:34 +0000

    d9b03be test: git ignore e2e test folders (#17984)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-b6aad07
+ * @license Angular v4.3.0-rc.0-d9b03be
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-b6aad07
+ * @license Angular v4.3.0-rc.0-d9b03be
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2413,7 +2413,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-b6aad07');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-d9b03be');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

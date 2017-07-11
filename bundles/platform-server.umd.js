5875527691e45438d91b52e52b8c67527d9247e7
Igor Minar <iminar@google.com>
2017-07-11 19:43:19 +0000

    1b1a6ba docs: fix 404 link to SFEIR website (#17913)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-df06e8b
+ * @license Angular v4.3.0-rc.0-1b1a6ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-df06e8b
+ * @license Angular v4.3.0-rc.0-1b1a6ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2413,7 +2413,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-df06e8b');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-1b1a6ba');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

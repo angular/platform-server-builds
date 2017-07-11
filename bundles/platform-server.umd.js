8cbd38e912bba54b1be4115de5307a9d746c4f31
Igor Minar <iminar@google.com>
2017-07-11 19:14:44 +0000

    b6aad07 build: remove duplicate dependency on TypeScript

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-4d45fe6
+ * @license Angular v4.3.0-rc.0-b6aad07
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-4d45fe6
+ * @license Angular v4.3.0-rc.0-b6aad07
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2413,7 +2413,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-4d45fe6');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-b6aad07');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

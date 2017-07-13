914ee8d95968f5bca991d38007d30e423f0e7e6c
Igor Minar <igor@angularjs.org>
2017-07-13 20:09:42 +0000

    e8b98a7 docs(aio): add Dart as a language option (#18080)

--- bundles/platform-server-testing.umd.js
+++ bundles/platform-server-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.6-afd5d72
+ * @license Angular v4.2.6-e8b98a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_compiler_testing,_angular_core,_angular_platformBrowserDynamic_testing,_angular_platformBrowser_animations,_angular_platformServer) { 'use strict';
 
 /**
- * @license Angular v4.2.6-afd5d72
+ * @license Angular v4.2.6-e8b98a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */

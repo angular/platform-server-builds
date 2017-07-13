63e820f882065d140d83c7d4748a43d1b153f436
Igor Minar <igor@angularjs.org>
2017-07-13 23:45:13 +0000

    3d85f72 revert: revert: ci(aio): exclude changes in aio/content folder

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-9f28e83
+ * @license Angular v4.3.0-rc.0-3d85f72
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-9f28e83
+ * @license Angular v4.3.0-rc.0-3d85f72
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2413,7 +2413,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-9f28e83');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-3d85f72');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

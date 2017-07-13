5ad700dce4c229d3efd81cd1610113cba4577b4a
Igor Minar <iminar@google.com>
2017-07-13 23:34:52 +0000

    ddb766e feat(compiler): do not evaluate metadata expressions that can use references (#18001)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-72143e8
+ * @license Angular v4.3.0-rc.0-ddb766e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-rc.0-72143e8
+ * @license Angular v4.3.0-rc.0-ddb766e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2413,7 +2413,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-rc.0-72143e8');
+var VERSION = new _angular_core.Version('4.3.0-rc.0-ddb766e');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

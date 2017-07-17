b6cc30682e9d38974a3c314e03b532a5e68ba1c4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 18:13:04 +0000

    0bb8423 ci: add GK and PBD to aio content and marketing groups

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-95698d9
+ * @license Angular v4.3.0-0bb8423
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2391,7 +2391,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-95698d9');
+var VERSION = new Version('4.3.0-0bb8423');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

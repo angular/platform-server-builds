97edd7d537c6c40f651b8c467a9062568f517f74
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 21:34:05 +0000

    47b3ecd docs(http): fix "Expecting and answering requests" example mistake

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-7e72317
+ * @license Angular v4.3.0-47b3ecd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2391,7 +2391,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-7e72317');
+var VERSION = new Version('4.3.0-47b3ecd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

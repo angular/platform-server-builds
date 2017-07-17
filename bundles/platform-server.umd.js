c08a04d638f90a8e797e792dfcddaf4ebeb5a63e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 22:08:53 +0000

    375d598 docs(aio): add George's fixes

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-9f99f4f
+ * @license Angular v4.3.0-375d598
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-9f99f4f
+ * @license Angular v4.3.0-375d598
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2413,7 +2413,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-9f99f4f');
+var VERSION = new _angular_core.Version('4.3.0-375d598');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

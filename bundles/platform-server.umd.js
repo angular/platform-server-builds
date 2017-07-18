c6b358d7e20fe48ac13e87af1bf63a2b70a191f8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 17:37:07 +0000

    ad3029e fix(router): should run resolvers for the same route concurrently

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-2a2fe11
+ * @license Angular v4.3.0-ad3029e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-2a2fe11
+ * @license Angular v4.3.0-ad3029e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2413,7 +2413,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-2a2fe11');
+var VERSION = new _angular_core.Version('4.3.0-ad3029e');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

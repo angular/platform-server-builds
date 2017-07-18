a9633d733fd4a796f4d2fd6a1052990ee07a651e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 23:54:19 +0000

    5344be5 fix(animations): make sure @.disabled works in non-animation components

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-23146c9
+ * @license Angular v4.3.0-5344be5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2336,7 +2336,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-23146c9');
+const VERSION = new Version('4.3.0-5344be5');
 
 /**
  * @license

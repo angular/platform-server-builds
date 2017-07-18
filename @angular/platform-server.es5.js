b07c53f4b493ee33217509df34c8dda9bd8d465d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:18:24 +0000

    3b588fe docs: fix typo

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-e03adb9
+ * @license Angular v4.3.0-3b588fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2391,7 +2391,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-e03adb9');
+var VERSION = new Version('4.3.0-3b588fe');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

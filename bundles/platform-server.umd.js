ddb58f59edd47f2909c252ed7932909360c27a73
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 21:30:53 +0000

    8076482 fix(platform-server): provide XhrFactory for HttpClient

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-a5205c6
+ * @license Angular v4.3.0-8076482
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-a5205c6
+ * @license Angular v4.3.0-8076482
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2413,7 +2413,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-a5205c6');
+var VERSION = new _angular_core.Version('4.3.0-8076482');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

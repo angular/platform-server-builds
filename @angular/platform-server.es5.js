56c1dd406871b90fef5e0383b63c903ae0735112
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 17:49:22 +0000

    e20cfe1 fix(router): canDeactivate guards should run from bottom to top

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-eb6fb5f
+ * @license Angular v4.3.0-e20cfe1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2391,7 +2391,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-eb6fb5f');
+var VERSION = new Version('4.3.0-e20cfe1');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

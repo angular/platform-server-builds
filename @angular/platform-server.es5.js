75ea98e686c4d08d9cbb2b4176b2937eb44ca7b4
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-18 11:04:23 +0000

    fda607c build(aio): fail doc-gen if referenced images are missing

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-1390afe
+ * @license Angular v4.3.0-fda607c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2391,7 +2391,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-1390afe');
+var VERSION = new Version('4.3.0-fda607c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

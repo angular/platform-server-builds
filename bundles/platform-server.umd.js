4f654e130f0c0d1e91272f13e6fde6520768ba4b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 22:40:30 +0000

    5db6f38 fix(animations): do not crash animations if a nested component fires CD during CD

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-d22f8f5
+ * @license Angular v4.3.0-5db6f38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.0-d22f8f5
+ * @license Angular v4.3.0-5db6f38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2413,7 +2413,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.0-d22f8f5');
+var VERSION = new _angular_core.Version('4.3.0-5db6f38');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

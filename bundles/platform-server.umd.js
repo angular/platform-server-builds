b4944735b40f3bb890ab3cbd01855778eb69c2b4
Miško Hevery <misko@hevery.com>
2017-07-20 23:27:50 +0000

    54e0244 Revert "docs: Remove unneeded file (#18106)"

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-8d28191
+ * @license Angular v5.0.0-beta.0-54e0244
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.0-8d28191
+ * @license Angular v5.0.0-beta.0-54e0244
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2413,7 +2413,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.0-8d28191');
+var VERSION = new _angular_core.Version('5.0.0-beta.0-54e0244');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

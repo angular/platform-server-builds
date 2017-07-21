757d726b07b9f40d52b6969dda1196f81e1b3dd8
Miško Hevery <misko@hevery.com>
2017-07-21 18:34:39 +0000

    abee785 refactor(tsc-wrapped): update tsc-wrapped to pass strictNullCheck (#18160)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-e0a9625
+ * @license Angular v5.0.0-beta.0-abee785
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2341,7 +2341,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.0-e0a9625');
+const VERSION = new Version('5.0.0-beta.0-abee785');
 
 /**
  * @license

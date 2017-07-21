12ea61ed0ef8b16072a451ad1d4889bbc35bdbd4
Miško Hevery <misko@hevery.com>
2017-07-21 17:48:32 +0000

    e0a9625 test: fix bad merge (#18267)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-a6c635e
+ * @license Angular v5.0.0-beta.0-e0a9625
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.0-a6c635e
+ * @license Angular v5.0.0-beta.0-e0a9625
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2418,7 +2418,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.0-a6c635e');
+var VERSION = new _angular_core.Version('5.0.0-beta.0-e0a9625');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

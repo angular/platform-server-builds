d33c842a78f5cce3db76cbe6fd509a3580681ce4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 21:35:52 +0000

    cc2a4c4 build(aio): fix warning about missing `<h1>`

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-a11542a
+ * @license Angular v5.0.0-beta.1-cc2a4c4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2373,7 +2373,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.1-a11542a');
+const VERSION = new Version('5.0.0-beta.1-cc2a4c4');
 
 /**
  * @fileoverview added by tsickle

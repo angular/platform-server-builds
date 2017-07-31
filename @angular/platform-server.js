c0b66532dc453ef95b1756a0e555c2b53844a09e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 23:15:03 +0000

    9479a10 build: enable TSLint on the packages folder

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-e64b54b
+ * @license Angular v5.0.0-beta.1-9479a10
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2373,7 +2373,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.1-e64b54b');
+const VERSION = new Version('5.0.0-beta.1-9479a10');
 
 /**
  * @fileoverview added by tsickle

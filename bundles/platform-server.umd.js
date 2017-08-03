12f37a20b8ca95659eee44c636af7d8c8ca9f012
Victor Berchet <victor@suumit.com>
2017-08-03 01:05:19 +0000

    81cb5bc docs(router): fix typo (#18479)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-1640d2a
+ * @license Angular v5.0.0-beta.2-81cb5bc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.2-1640d2a
+ * @license Angular v5.0.0-beta.2-81cb5bc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2450,7 +2450,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.2-1640d2a');
+var VERSION = new _angular_core.Version('5.0.0-beta.2-81cb5bc');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

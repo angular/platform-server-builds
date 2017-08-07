e70dc45f980f01ccd90c55b9618c6b3fc233a267
Victor Berchet <victor@suumit.com>
2017-08-07 18:04:51 +0000

    3212f8c docs(aio): typo & update the bio (#18559)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-c421cca
+ * @license Angular v4.3.3-3212f8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.3-c421cca
+ * @license Angular v4.3.3-3212f8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2426,7 +2426,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.3-c421cca');
+var VERSION = new _angular_core.Version('4.3.3-3212f8c');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

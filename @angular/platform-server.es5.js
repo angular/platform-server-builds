2fd07c6c8caf2dc543b5fe752a892f6ff3003bc6
Victor Berchet <victor@suumit.com>
2017-08-08 21:32:13 +0000

    f0c3ed0 ci(aio): fix deploying to firebase (#18590)

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.3-e0660b1
+ * @license Angular v4.3.3-f0c3ed0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2404,7 +2404,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.3-e0660b1');
+var VERSION = new Version('4.3.3-f0c3ed0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

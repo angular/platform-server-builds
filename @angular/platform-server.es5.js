943d1b152374ed2fbfdf08c5ece60db94e77c593
Victor Berchet <victor@suumit.com>
2017-08-09 21:44:25 +0000

    71f5e78 test(animations): disable buggy test in Chrome 39 (#18483)

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.3-f0c3ed0
+ * @license Angular v4.3.3-71f5e78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2404,7 +2404,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.3-f0c3ed0');
+var VERSION = new Version('4.3.3-71f5e78');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

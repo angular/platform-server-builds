965da6fe0fc41d37310ac2401b1b20aafc011ddc
Victor Berchet <victor@suumit.com>
2017-08-10 03:48:59 +0000

    77fa3c3 test(aio): fix tests for example-boilerplate (#18619)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-f4cb453
+ * @license Angular v4.3.3-77fa3c3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.3-f4cb453
+ * @license Angular v4.3.3-77fa3c3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2426,7 +2426,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.3-f4cb453');
+var VERSION = new _angular_core.Version('4.3.3-77fa3c3');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

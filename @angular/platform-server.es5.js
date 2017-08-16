b51ec4c75c390aa8be47d4b975159031fc2364e2
Hans Larsen <hans@hansl.ca>
2017-08-16 00:33:27 +0000

    7662cef docs: remove TypeScript to JavaScript guide & sample

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.4-1cb6076
+ * @license Angular v4.3.4-7662cef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2404,7 +2404,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.4-1cb6076');
+var VERSION = new Version('4.3.4-7662cef');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.4-1cb6076
+ * @license Angular v4.3.4-7662cef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.4-1cb6076
+ * @license Angular v4.3.4-7662cef
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2426,7 +2426,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.4-1cb6076');
+var VERSION = new _angular_core.Version('4.3.4-7662cef');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

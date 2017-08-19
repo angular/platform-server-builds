f1cfb3de29a69a83b5044e96b14fbd30daf8c7a3
Miško Hevery <misko@hevery.com>
2017-08-19 14:34:08 +0000

    e1f45a3 fix(animations): restore auto-style support for removed DOM nodes (#18787)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.5-77ebd2b
+ * @license Angular v4.3.5-e1f45a3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.5-77ebd2b
+ * @license Angular v4.3.5-e1f45a3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2426,7 +2426,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.5-77ebd2b');
+var VERSION = new _angular_core.Version('4.3.5-e1f45a3');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

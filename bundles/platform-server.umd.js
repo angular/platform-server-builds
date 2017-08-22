8631ec41a49aaefc8985efd0ae04dcf4ad7611d5
Miško Hevery <misko@hevery.com>
2017-08-22 01:53:46 +0000

    1f43713 fix(aio): do not redirect API pages on archive and next deployments (#18791)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.5-14d34c9
+ * @license Angular v4.3.5-1f43713
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.5-14d34c9
+ * @license Angular v4.3.5-1f43713
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2426,7 +2426,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.5-14d34c9');
+var VERSION = new _angular_core.Version('4.3.5-1f43713');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

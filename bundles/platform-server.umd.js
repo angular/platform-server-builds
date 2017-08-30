74d51eacb99e07e4de01401974b2bca80c12674f
Jason Aden <jasonaden@google.com>
2017-08-30 00:47:30 +0000

    a203a95 fix(common): fix improper packaging for @angular/common/http (#18613)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-8821723
+ * @license Angular v4.3.6-a203a95
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.3.6-8821723
+ * @license Angular v4.3.6-a203a95
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2426,7 +2426,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.3.6-8821723');
+var VERSION = new _angular_core.Version('4.3.6-a203a95');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

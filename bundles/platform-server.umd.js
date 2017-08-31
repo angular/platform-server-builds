a00b9d46ce6b85d26f934bc8468110b48d73cba8
Jason Aden <jasonaden@google.com>
2017-08-31 17:17:24 +0000

    cf7d47d feat(compiler-cli): add watch mode to `ngc` (#18818)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-c7e1bda
+ * @license Angular v5.0.0-beta.5-cf7d47d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.5-c7e1bda
+ * @license Angular v5.0.0-beta.5-cf7d47d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1428,7 +1428,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.5-c7e1bda');
+var VERSION = new _angular_core.Version('5.0.0-beta.5-cf7d47d');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

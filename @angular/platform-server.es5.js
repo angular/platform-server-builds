1e1022a3ee452792621203eca0482a30ed907728
Miško Hevery <misko@hevery.com>
2017-09-01 19:00:14 +0000

    b8b551c perf(core): add option to remove blank text nodes from compiled templates (#18823)

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.6-1cc3fe2
+ * @license Angular v4.3.6-b8b551c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2404,7 +2404,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.6-1cc3fe2');
+var VERSION = new Version('4.3.6-b8b551c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

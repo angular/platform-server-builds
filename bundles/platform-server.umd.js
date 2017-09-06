b7257c45beb82331ab01bed5d09841b6afb5a1df
Miško Hevery <misko@hevery.com>
2017-09-06 16:35:45 +0000

    29762f1 test(aio): fix docs example e2e test (#19070)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-d2707f1
+ * @license Angular v5.0.0-beta.6-29762f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.6-d2707f1
+ * @license Angular v5.0.0-beta.6-29762f1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1460,7 +1460,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.6-d2707f1');
+var VERSION = new _angular_core.Version('5.0.0-beta.6-29762f1');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

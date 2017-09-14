7b0130fb44d49fb524d17f835c6d784f08e64733
Matias Niemelä <matias@yearofmoo.com>
2017-09-14 23:06:41 +0000

    b6431c6 refactor: core, http & platform-webworker to remove public private class separation (#19143)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-4695c69
+ * @license Angular v5.0.0-beta.7-b6431c6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.7-4695c69
+ * @license Angular v5.0.0-beta.7-b6431c6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1804,7 +1804,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-4695c69');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-b6431c6');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

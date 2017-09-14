b6040a69b92afab005be341767a1e2c0190f3f93
Matias Niemelä <matias@yearofmoo.com>
2017-09-14 03:08:21 +0000

    4695c69 refactor(compiler): remove all source-level traces to tsc-wrapped (#18966)

--- esm15/index.js
+++ esm15/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-42c69d3
+ * @license Angular v5.0.0-beta.7-4695c69
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1385,7 +1385,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-42c69d3');
+const VERSION = new Version('5.0.0-beta.7-4695c69');
 
 /**
  * @fileoverview added by tsickle

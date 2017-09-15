e3605b92cddd3368e1781821d549886c50c4bc5e
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 23:06:50 +0000

    818f4a7 release: cut the 4.4.1 release

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.0-RC.0-b20c5d2
+ * @license Angular v4.4.1-818f4a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.0-RC.0-b20c5d2
+ * @license Angular v4.4.1-818f4a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2426,7 +2426,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.0-RC.0-b20c5d2');
+var VERSION = new _angular_core.Version('4.4.1-818f4a7');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

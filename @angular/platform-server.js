3008110ee49782e5f746889b027f9912fbd21717
Matias Niemelä <matias@yearofmoo.com>
2017-09-15 21:11:16 +0000

    0bafd03 revert: test(packaging): added test for source map correctness

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.0-RC.0-e8d1858
+ * @license Angular v4.4.0-RC.0-0bafd03
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2349,7 +2349,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.0-RC.0-e8d1858');
+const VERSION = new Version('4.4.0-RC.0-0bafd03');
 
 /**
  * @license

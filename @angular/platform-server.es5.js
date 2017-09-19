304f35273713c1479d061bb4609ec22b61f2be38
Igor Minar <igor@angularjs.org>
2017-09-19 04:36:33 +0000

    49d122e release: cut the 4.4.2 release

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.1-ff87294
+ * @license Angular v4.4.2-49d122e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2404,7 +2404,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.1-ff87294');
+var VERSION = new Version('4.4.2-49d122e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

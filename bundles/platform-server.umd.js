49acaf499b31c5af4c02a7e51ae9dc9822112434
Igor Minar <igor@angularjs.org>
2017-09-19 23:32:49 +0000

    9d2236a ci: update yarn to 1.0.2 (#19270)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-e17128e
+ * @license Angular v5.0.0-beta.7-9d2236a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.7-e17128e
+ * @license Angular v5.0.0-beta.7-9d2236a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1804,7 +1804,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-e17128e');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-9d2236a');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

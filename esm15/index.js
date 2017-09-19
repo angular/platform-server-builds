a4bbb414bacb9a03fed39cb3a8d91f5ac2ad7db7
Igor Minar <igor@angularjs.org>
2017-09-19 04:35:26 +0000

    b6b18c1 fix(tsc-wrapped): deduplicate metadata only when the module is the same (#19249)

--- esm15/index.js
+++ esm15/index.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-2373186
+ * @license Angular v5.0.0-beta.7-b6b18c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1385,7 +1385,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.7-2373186');
+const VERSION = new Version('5.0.0-beta.7-b6b18c1');
 
 /**
  * @fileoverview added by tsickle

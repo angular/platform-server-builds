c7184679ee1746991174ce9fd3c9b9239f85bc2e
Igor Minar <igor@angularjs.org>
2017-09-21 00:13:45 +0000

    4586fcc refactor(upgrade): use correct property name (#19180)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-9e84719
+ * @license Angular v5.0.0-beta.7-4586fcc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.7-9e84719
+ * @license Angular v5.0.0-beta.7-4586fcc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1780,7 +1780,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-9e84719');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-4586fcc');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

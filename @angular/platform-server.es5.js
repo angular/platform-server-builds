cb4da8e5b692412dc2cc325e33f05b3f997e7e5e
Igor Minar <igor@angularjs.org>
2017-09-21 18:48:36 +0000

    04bc525 docs(aio): tidy up tooling documentation (#18151)

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.3-e7eb0b8
+ * @license Angular v4.4.3-04bc525
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2404,7 +2404,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.3-e7eb0b8');
+var VERSION = new Version('4.4.3-04bc525');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

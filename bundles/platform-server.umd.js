5fec36396574d275b5e512df95eb3bc90c4c83c3
Igor Minar <igor@angularjs.org>
2017-09-22 01:21:06 +0000

    9bbf009 build(aio): change examples to CLI (#19248)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-6e1896b
+ * @license Angular v5.0.0-beta.7-9bbf009
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.7-6e1896b
+ * @license Angular v5.0.0-beta.7-9bbf009
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1832,7 +1832,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-6e1896b');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-9bbf009');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

fc7f1a554aac9d7bc62cac577dc9c33cf37fe022
Victor Berchet <victor@suumit.com>
2017-09-26 17:04:21 +0000

    2cf52c1 test: Enable sass compiler for bazel integration test (#19357)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-27c6638
+ * @license Angular v5.0.0-beta.7-2cf52c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.7-27c6638
+ * @license Angular v5.0.0-beta.7-2cf52c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-27c6638');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-2cf52c1');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

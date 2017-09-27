47a6076f83a5af56c658371102bd5cd7993bf8f7
Victor Berchet <victor@suumit.com>
2017-09-27 22:53:05 +0000

    0cefb0b docs(aio): add copy about NgForOf (#18686)

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.3-55a7443
+ * @license Angular v4.4.3-0cefb0b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2404,7 +2404,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.3-55a7443');
+var VERSION = new Version('4.4.3-0cefb0b');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

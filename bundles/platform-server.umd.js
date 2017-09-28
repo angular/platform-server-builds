b39aa5a52e14b116b068c12d2774808ee48152fe
Victor Berchet <victor@suumit.com>
2017-09-28 19:26:08 +0000

    7fc2dce fix(compiler): do not consider a reference with members as a reference (#19466)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-c3b39ba
+ * @license Angular v4.4.3-7fc2dce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.3-c3b39ba
+ * @license Angular v4.4.3-7fc2dce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2426,7 +2426,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.3-c3b39ba');
+var VERSION = new _angular_core.Version('4.4.3-7fc2dce');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

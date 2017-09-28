962d6241c4ac33fd65ebb89262c73f8f033ae768
Victor Berchet <victor@suumit.com>
2017-09-28 17:01:16 +0000

    2cd88bf docs(animations): document usage of negative limit values for query (#19451)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-0cefb0b
+ * @license Angular v4.4.3-2cd88bf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.3-0cefb0b
+ * @license Angular v4.4.3-2cd88bf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2426,7 +2426,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.3-0cefb0b');
+var VERSION = new _angular_core.Version('4.4.3-2cd88bf');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

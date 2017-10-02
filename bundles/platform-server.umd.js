3eab128f76baac63db9732e050faf166d0dd73ee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-02 23:24:15 +0000

    eef7d8a fix(upgrade): call `ngOnInit()` after `ngOnChanges()` (on components with inputs)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-745b59f
+ * @license Angular v5.0.0-rc.0-eef7d8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.0-745b59f
+ * @license Angular v5.0.0-rc.0-eef7d8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.0-745b59f');
+var VERSION = new _angular_core.Version('5.0.0-rc.0-eef7d8a');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

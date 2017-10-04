0b03fb610f1f73c11dbb3af5a05437f8793b33f3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 22:33:29 +0000

    f98eb35 docs(aio): document Custom Async validators

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-95f3b1d
+ * @license Angular v4.4.4-f98eb35
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.4-95f3b1d
+ * @license Angular v4.4.4-f98eb35
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2425,7 +2425,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.4-95f3b1d');
+var VERSION = new _angular_core.Version('4.4.4-f98eb35');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

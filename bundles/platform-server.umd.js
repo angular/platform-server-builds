2e9b3d75422ff1be9bda6f85f82ab188dd79bab8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-04 20:02:37 +0000

    9ae3742 docs(aio): fix typo in preview server config file comment

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-7c5ecb5
+ * @license Angular v5.0.0-rc.0-9ae3742
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.0-7c5ecb5
+ * @license Angular v5.0.0-rc.0-9ae3742
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.0-7c5ecb5');
+var VERSION = new _angular_core.Version('5.0.0-rc.0-9ae3742');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

646dd129d56a1d40ea0e01cb358c50c1506aaf0a
Chuck Jazdzewski <chuckj@google.com>
2017-10-07 01:37:50 +0000

    b1ca5d4 Revert "perf(animations): reduce size of bundle by removing AST classes (#19539)"

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-a8920eb
+ * @license Angular v5.0.0-rc.1-b1ca5d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.1-a8920eb
+ * @license Angular v5.0.0-rc.1-b1ca5d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.1-a8920eb');
+var VERSION = new _angular_core.Version('5.0.0-rc.1-b1ca5d4');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

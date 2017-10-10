e33135b6e6bbd8738fc1585682ce03bc7bf68506
Chuck Jazdzewski <chuckj@google.com>
2017-10-10 23:19:24 +0000

    f89ac92 test(aio): temporarily disable failing examples e2e tests (#19600)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-ccc25ee
+ * @license Angular v4.4.4-f89ac92
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2348,7 +2348,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.4-ccc25ee');
+const VERSION = new Version('4.4.4-f89ac92');
 
 /**
  * @license

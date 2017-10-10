2319b0089ac98c7a9751bfbf7c153675da9a3038
Chuck Jazdzewski <chuckj@google.com>
2017-10-10 21:09:58 +0000

    dfa0973 docs(aio): update animations for CLI (#19551)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-ac50bd6
+ * @license Angular v5.0.0-rc.1-dfa0973
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.1-ac50bd6');
+var VERSION = new Version('5.0.0-rc.1-dfa0973');
 
 /**
  * @fileoverview added by tsickle

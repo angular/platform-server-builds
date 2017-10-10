f0a05ecb73fd74ceca334532ccb51924222fc1cb
Chuck Jazdzewski <chuckj@google.com>
2017-10-10 22:00:35 +0000

    b0befd7 fix(compiler): `TestBed.overrideProvider` should keep imported `NgModule`s eager (#19624)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-dfa0973
+ * @license Angular v5.0.0-rc.1-b0befd7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.1-dfa0973
+ * @license Angular v5.0.0-rc.1-b0befd7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.1-dfa0973');
+var VERSION = new _angular_core.Version('5.0.0-rc.1-b0befd7');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

20311d8877b9cab7f268f99980aa7f894409bbc6
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 22:07:22 +0000

    65ca7fd docs(aio): update banner ad (#19669)

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-3e3f918
+ * @license Angular v4.4.4-65ca7fd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2348,7 +2348,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.4-3e3f918');
+const VERSION = new Version('4.4.4-65ca7fd');
 
 /**
  * @license

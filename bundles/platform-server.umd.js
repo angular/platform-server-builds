03f6945ca1f5a00884b91ac0fec5c7e171a1134f
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 20:41:37 +0000

    3acf9c7 docs(aio): update banner ad (#19669)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-566606e
+ * @license Angular v5.0.0-rc.1-3acf9c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.1-566606e
+ * @license Angular v5.0.0-rc.1-3acf9c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.1-566606e');
+var VERSION = new _angular_core.Version('5.0.0-rc.1-3acf9c7');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;
--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-566606e
+ * @license Angular v5.0.0-rc.1-3acf9c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1424,7 +1424,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.1-566606e');
+const VERSION = new Version('5.0.0-rc.1-3acf9c7');
 
 /**
  * @fileoverview added by tsickle

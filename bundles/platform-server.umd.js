cbb46207f74830a62ca9ad87f88317365686621f
Chuck Jazdzewski <chuckj@google.com>
2017-10-11 00:29:41 +0000

    8b57130 build(aio): append information about links in and out of docs (#19583)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.1-b0befd7
+ * @license Angular v5.0.0-rc.1-8b57130
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.1-b0befd7
+ * @license Angular v5.0.0-rc.1-8b57130
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.1-b0befd7');
+var VERSION = new _angular_core.Version('5.0.0-rc.1-8b57130');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

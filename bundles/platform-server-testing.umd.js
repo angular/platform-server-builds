ae485ede67cc31afb071ed15d8846def61d3a4cc
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 00:15:10 +0000

    a15abbb docs(aio): change Material 2 to Material (#19663)

--- bundles/platform-server-testing.umd.js
+++ bundles/platform-server-testing.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-28c29d5
+ * @license Angular v4.4.4-a15abbb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -10,7 +10,7 @@
 }(this, (function (exports,_angular_compiler_testing,_angular_core,_angular_platformBrowserDynamic_testing,_angular_platformBrowser_animations,_angular_platformServer) { 'use strict';
 
 /**
- * @license Angular v4.4.4-28c29d5
+ * @license Angular v4.4.4-a15abbb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */

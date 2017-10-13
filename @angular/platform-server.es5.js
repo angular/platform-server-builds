363528ca33bcd087fda7a7204b4ddc284b39b254
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 16:30:36 +0000

    4acd322 fix(core): don't refer to `hydration` in docs anymore.

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.5-32a814b
+ * @license Angular v4.4.5-4acd322
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2403,7 +2403,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.5-32a814b');
+var VERSION = new Version('4.4.5-4acd322');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

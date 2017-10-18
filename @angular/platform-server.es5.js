684bf606aca949c420c0c3286070e4e13f5a4d47
Tobias Bosch <tbosch1009@gmail.com>
2017-10-18 20:36:32 +0000

    269f5ac fix(common): attempt to JSON.parse errors for JSON responses (#19773)

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.5-3342a82
+ * @license Angular v4.4.5-269f5ac
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2403,7 +2403,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.5-3342a82');
+var VERSION = new Version('4.4.5-269f5ac');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

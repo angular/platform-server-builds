31bc62fa8315fc5e5ddd35ece048c35167ca23eb
Tobias Bosch <tbosch1009@gmail.com>
2017-10-18 17:48:13 +0000

    3342a82 fix(aio): make tests less flaky (#19784) PR Close #19784

--- @angular/platform-server.js
+++ @angular/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-630c19f
+ * @license Angular v4.4.5-3342a82
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2348,7 +2348,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.5-630c19f');
+const VERSION = new Version('4.4.5-3342a82');
 
 /**
  * @license

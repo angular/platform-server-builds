0197e69d5dc383ff4162a19c26c6100dbfbf6834
Tobias Bosch <tbosch1009@gmail.com>
2017-10-19 00:46:41 +0000

    7231f5e docs: add changelog for 4.4.6

--- @angular/platform-server.es5.js
+++ @angular/platform-server.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.4.5-269f5ac
+ * @license Angular v4.4.6-7231f5e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2403,7 +2403,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.4.5-269f5ac');
+var VERSION = new Version('4.4.6-7231f5e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.

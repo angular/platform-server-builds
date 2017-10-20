6aebe905eb1e21f22e05fd3a2954127b78d2480c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:41:11 +0000

    910735d build: fix `yarn install` command (`--freeze-lockfile` --> `--frozen-lockfile`)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-820bb7b
+ * @license Angular v5.0.0-rc.3-910735d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.3-820bb7b
+ * @license Angular v5.0.0-rc.3-910735d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.3-820bb7b');
+var VERSION = new _angular_core.Version('5.0.0-rc.3-910735d');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

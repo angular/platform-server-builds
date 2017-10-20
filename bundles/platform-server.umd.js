6a19f606c414e0fd4480d7919bf64246d21df629
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-20 17:03:58 +0000

    820bb7b revert: ci: use chrome stable (#18307)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-14380ff
+ * @license Angular v5.0.0-rc.3-820bb7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.3-14380ff
+ * @license Angular v5.0.0-rc.3-820bb7b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.3-14380ff');
+var VERSION = new _angular_core.Version('5.0.0-rc.3-820bb7b');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

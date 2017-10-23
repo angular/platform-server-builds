c07843fb794457b660c0a231373f7165d67fd8ad
Matias Niemelä <matias@yearofmoo.com>
2017-10-23 23:06:01 +0000

    5da96c7 fix(bazel): don't console.error from the compile helper (#19879)

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-90d1423
+ * @license Angular v5.0.0-rc.3-5da96c7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1424,7 +1424,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.3-90d1423');
+const VERSION = new Version('5.0.0-rc.3-5da96c7');
 
 /**
  * @fileoverview added by tsickle

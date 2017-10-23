72279fd361d403b69953de5130f47845e85d7a36
Matias Niemelä <matias@yearofmoo.com>
2017-10-23 23:52:26 +0000

    fc0b1d5 fix(compiler): correctly calculate the outDir if it repeats a parts of the `rootDir`. (#19836)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.3-5da96c7
+ * @license Angular v5.0.0-rc.3-fc0b1d5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.3-5da96c7
+ * @license Angular v5.0.0-rc.3-fc0b1d5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.3-5da96c7');
+var VERSION = new _angular_core.Version('5.0.0-rc.3-fc0b1d5');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

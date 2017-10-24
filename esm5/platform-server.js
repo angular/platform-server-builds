ff150cc5e056a01bd27dbbce964a1ae9dfe87757
Matias Niemelä <matias@yearofmoo.com>
2017-10-24 23:44:19 +0000

    6cc042e fix(compiler-cli): produce correct paths for windows output (#19915)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-f1108fe
+ * @license Angular v5.0.0-rc.5-6cc042e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.5-f1108fe');
+var VERSION = new Version('5.0.0-rc.5-6cc042e');
 
 /**
  * @fileoverview added by tsickle

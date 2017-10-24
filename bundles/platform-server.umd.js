016dce223295e1eb8b8309c966f2ad9fa3aab3a8
Matias Niemelä <matias@yearofmoo.com>
2017-10-24 22:19:31 +0000

    7bfeac7 fix(compiler-cli): only use error collector when needed. (#19912)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-b51d57d
+ * @license Angular v5.0.0-rc.5-7bfeac7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.5-b51d57d
+ * @license Angular v5.0.0-rc.5-7bfeac7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.5-b51d57d');
+var VERSION = new _angular_core.Version('5.0.0-rc.5-7bfeac7');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

b257648fe9e9cc122216964d16988537db84233c
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 18:51:38 +0000

    ef08330 fix(compiler): automatically set `emitDecoratorMetadata` when `"annotationsAs": "static fields”` (#19927)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-6cc042e
+ * @license Angular v5.0.0-rc.5-ef08330
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.5-6cc042e
+ * @license Angular v5.0.0-rc.5-ef08330
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.5-6cc042e');
+var VERSION = new _angular_core.Version('5.0.0-rc.5-ef08330');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;
--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-6cc042e
+ * @license Angular v5.0.0-rc.5-ef08330
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.5-6cc042e');
+var VERSION = new Version('5.0.0-rc.5-ef08330');
 
 /**
  * @fileoverview added by tsickle

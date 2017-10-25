7004d1a4f9c5438ebad5fbbc819079ae23ead22a
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 20:11:05 +0000

    680bcf7 build: update to rxjs@5.5.2 (#19931)

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-ef08330
+ * @license Angular v5.0.0-rc.5-680bcf7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1424,7 +1424,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.5-ef08330');
+const VERSION = new Version('5.0.0-rc.5-680bcf7');
 
 /**
  * @fileoverview added by tsickle

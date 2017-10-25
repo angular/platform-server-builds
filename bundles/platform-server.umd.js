6dc4507c4966450f8d48dd2e4bdd361c5020e86c
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 20:01:54 +0000

    fde5f2f build: update to rxjs@5.5.2 (#19931)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-d567246
+ * @license Angular v5.0.0-rc.5-fde5f2f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.5-d567246
+ * @license Angular v5.0.0-rc.5-fde5f2f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.5-d567246');
+var VERSION = new _angular_core.Version('5.0.0-rc.5-fde5f2f');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

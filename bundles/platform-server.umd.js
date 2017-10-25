e9bd1d2053a35aba70c3c7f45d52b7eceb450fe2
Matias Niemelä <matias@yearofmoo.com>
2017-10-25 22:15:57 +0000

    703fcda docs: add changelog for 5.0.0-rc.6

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.5-fde5f2f
+ * @license Angular v5.0.0-rc.6-703fcda
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.5-fde5f2f
+ * @license Angular v5.0.0-rc.6-703fcda
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.5-fde5f2f');
+var VERSION = new _angular_core.Version('5.0.0-rc.6-703fcda');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

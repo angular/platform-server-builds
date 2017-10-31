7cef9be562bf088b3d5ff9ff125cd7fcc68d34f6
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 04:45:17 +0000

    7553ce9 docs: add changelog for 5.0.0-rc.9

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-3bdbb18
+ * @license Angular v5.0.0-rc.9-7553ce9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-rc.8-3bdbb18');
+var VERSION = new Version('5.0.0-rc.9-7553ce9');
 
 /**
  * @fileoverview added by tsickle

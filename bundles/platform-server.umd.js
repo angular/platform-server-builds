45abc1a7de1065ddd902515ff9bd6c0150275048
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 04:47:28 +0000

    f5b7f2b docs: add changelog for 5.0.0-rc.9

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-cf5fce8
+ * @license Angular v5.0.0-rc.9-f5b7f2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.8-cf5fce8
+ * @license Angular v5.0.0-rc.9-f5b7f2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.8-cf5fce8');
+var VERSION = new _angular_core.Version('5.0.0-rc.9-f5b7f2b');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

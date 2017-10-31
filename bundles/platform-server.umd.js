f65db0402fc585df041f4381d2d6de08bdbf4f8a
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 01:44:35 +0000

    bf57df3 build: temporarily increase the commit msg limit to 120

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-951bd33
+ * @license Angular v5.0.0-rc.8-bf57df3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.8-951bd33
+ * @license Angular v5.0.0-rc.8-bf57df3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.8-951bd33');
+var VERSION = new _angular_core.Version('5.0.0-rc.8-bf57df3');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

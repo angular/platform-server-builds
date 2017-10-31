2e6c1fb7fa6e74fe01cfb2b05156ea43dac23175
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 00:52:21 +0000

    04eb80c fix(compiler): always use relative paths to refer to generated code

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-308fc8e
+ * @license Angular v5.0.0-rc.8-04eb80c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.8-308fc8e
+ * @license Angular v5.0.0-rc.8-04eb80c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.8-308fc8e');
+var VERSION = new _angular_core.Version('5.0.0-rc.8-04eb80c');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;
--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-308fc8e
+ * @license Angular v5.0.0-rc.8-04eb80c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1424,7 +1424,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-rc.8-308fc8e');
+const VERSION = new Version('5.0.0-rc.8-04eb80c');
 
 /**
  * @fileoverview added by tsickle

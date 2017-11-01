2e8ecfadf2267382f7b0230e485e2a091d44536d
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 22:43:25 +0000

    c30eff8 docs(aio): update universal for CLI

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-ca129ba
+ * @license Angular v5.0.0-c30eff8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1424,7 +1424,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-ca129ba');
+const VERSION = new Version('5.0.0-c30eff8');
 
 /**
  * @fileoverview added by tsickle

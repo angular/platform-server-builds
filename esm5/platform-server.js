34ac2cda8402ef6b5336b48949d2a5d603753891
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 22:54:07 +0000

    a1ec65b docs(aio): fix typo in attribute-directives

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-851e1cf
+ * @license Angular v5.0.0-a1ec65b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-851e1cf');
+var VERSION = new Version('5.0.0-a1ec65b');
 
 /**
  * @fileoverview added by tsickle

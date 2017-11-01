747060cc1a56a5d10dd1115dfd9fb3ece5247f2d
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 23:06:04 +0000

    16d7258 build(aio): upgrade to @angular/material@2.0.0-beta.12 (#19702)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-a1ec65b
+ * @license Angular v5.0.0-16d7258
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-a1ec65b
+ * @license Angular v5.0.0-16d7258
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-a1ec65b');
+var VERSION = new _angular_core.Version('5.0.0-16d7258');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

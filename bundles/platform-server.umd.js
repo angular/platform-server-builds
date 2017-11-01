71ea884b1000e1484a5dde7b25e2b3f601e6561a
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 22:48:25 +0000

    851e1cf docs(aio): update universal for CLI

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-a363be6
+ * @license Angular v5.0.0-851e1cf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-a363be6
+ * @license Angular v5.0.0-851e1cf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-a363be6');
+var VERSION = new _angular_core.Version('5.0.0-851e1cf');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

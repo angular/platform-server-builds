58d14afc08660b7ab89b17bbb16d30b5f2d97770
Victor Berchet <victor@suumit.com>
2017-11-02 23:36:38 +0000

    a28d616 fix(core): __symbol__ should return __zone_symbol__ without zone.js loaded (#19541)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-613a9e3
+ * @license Angular v5.1.0-beta.0-a28d616
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-613a9e3');
+var VERSION = new Version('5.1.0-beta.0-a28d616');
 
 /**
  * @fileoverview added by tsickle

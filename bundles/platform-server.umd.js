5a3a7e5b4d1f104577967bcba53abd85f3d7ce60
Victor Berchet <victor@suumit.com>
2017-11-02 21:55:47 +0000

    a91252a docs: Npm Packages guide for CLI (#19850)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-a41558e
+ * @license Angular v5.1.0-beta.0-a91252a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.0-a41558e
+ * @license Angular v5.1.0-beta.0-a91252a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-a41558e');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-a91252a');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

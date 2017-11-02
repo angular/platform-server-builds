ccd48e25f35761801d20bacffbef51ada12ee6ef
Victor Berchet <victor@suumit.com>
2017-11-02 22:39:44 +0000

    068348e build(aio): do not fail the initial doc-gen when running docs-watch

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-5b16ce9
+ * @license Angular v5.1.0-beta.0-068348e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-5b16ce9');
+var VERSION = new Version('5.1.0-beta.0-068348e');
 
 /**
  * @fileoverview added by tsickle

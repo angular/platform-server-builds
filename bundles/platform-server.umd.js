63face6d6757d8765592d9f5f02782d71509c3e2
Igor Minar <igor@angular.io>
2017-11-02 00:26:28 +0000

    3db7112 docs(aio): fix linting for universal (#20086)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-2ea76cc
+ * @license Angular v5.1.0-beta.0-3db7112
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.0-2ea76cc
+ * @license Angular v5.1.0-beta.0-3db7112
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-2ea76cc');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-3db7112');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

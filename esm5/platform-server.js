288fa9e6975c79a20f2dcad7cd66e8cc4a32b057
Victor Berchet <victor@suumit.com>
2017-11-02 23:23:29 +0000

    65d57a0 build(aio): avoid building before running the local PWA tests

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-9975486
+ * @license Angular v5.1.0-beta.0-65d57a0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-9975486');
+var VERSION = new Version('5.1.0-beta.0-65d57a0');
 
 /**
  * @fileoverview added by tsickle

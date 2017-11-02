99164e2601794a5d489735fc50a356c951ad55a3
Victor Berchet <victor@suumit.com>
2017-11-02 22:28:08 +0000

    8639995 fix(compiler-cli): don't report emit diagnostics when --noEmitOnError is off (#20063)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-d550b68
+ * @license Angular v5.0.0-8639995
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-d550b68
+ * @license Angular v5.0.0-8639995
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-d550b68');
+var VERSION = new _angular_core.Version('5.0.0-8639995');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

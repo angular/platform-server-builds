9740c62b7f958bc55145b3fb342a1cc21f26f78f
Victor Berchet <victor@suumit.com>
2017-11-02 21:53:46 +0000

    a41558e docs: BrowserSupport guide for CLI (#19843)

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-132c071
+ * @license Angular v5.1.0-beta.0-a41558e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1424,7 +1424,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-132c071');
+const VERSION = new Version('5.1.0-beta.0-a41558e');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-132c071
+ * @license Angular v5.1.0-beta.0-a41558e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-132c071');
+var VERSION = new Version('5.1.0-beta.0-a41558e');
 
 /**
  * @fileoverview added by tsickle

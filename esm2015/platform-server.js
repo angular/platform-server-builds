97afb67d22dd314cf3a9cf97d8447284d9eb4050
Victor Berchet <victor@suumit.com>
2017-11-03 23:14:26 +0000

    3997d97 revert: feat(elements): implement `@angular/elements` #19469 (#20152)

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-200d92d
+ * @license Angular v5.1.0-beta.0-3997d97
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1424,7 +1424,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-200d92d');
+const VERSION = new Version('5.1.0-beta.0-3997d97');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-200d92d
+ * @license Angular v5.1.0-beta.0-3997d97
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-200d92d');
+var VERSION = new Version('5.1.0-beta.0-3997d97');
 
 /**
  * @fileoverview added by tsickle

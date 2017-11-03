791cac6a4c5438a03adf4743a040d496aef7c98e
Victor Berchet <victor@suumit.com>
2017-11-03 18:32:59 +0000

    5079d2d docs(aio): Add v4 doc link to navigation (#20089)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-dcf8840
+ * @license Angular v5.1.0-beta.0-5079d2d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-dcf8840');
+var VERSION = new Version('5.1.0-beta.0-5079d2d');
 
 /**
  * @fileoverview added by tsickle

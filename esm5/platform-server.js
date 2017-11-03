fa840acb87ecc210b7df154a52509d19e0ad79e0
Victor Berchet <victor@suumit.com>
2017-11-03 19:03:35 +0000

    e6f16a7 docs(aio): Fix typo in Architecture Overview. (#20073)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-678d1cf
+ * @license Angular v5.0.0-e6f16a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-678d1cf');
+var VERSION = new Version('5.0.0-e6f16a7');
 
 /**
  * @fileoverview added by tsickle

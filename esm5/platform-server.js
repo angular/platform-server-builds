2f5156a3bdb9d23def09393ed1ef1dc4619d6c1a
Victor Berchet <victor@suumit.com>
2017-11-03 22:39:41 +0000

    f7c9b94 ci(aio): Add payload size percentage changes check to payload size task (#19908)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-f076401
+ * @license Angular v5.1.0-beta.0-f7c9b94
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-f076401');
+var VERSION = new Version('5.1.0-beta.0-f7c9b94');
 
 /**
  * @fileoverview added by tsickle

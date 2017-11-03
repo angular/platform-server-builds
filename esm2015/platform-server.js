7fbf0c54d0a25c5d40e646178c52ce7c38cf29ca
Victor Berchet <victor@suumit.com>
2017-11-03 22:40:41 +0000

    dbec3ca docs(aio): update pipes for CLI (#19553)

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-f7c9b94
+ * @license Angular v5.1.0-beta.0-dbec3ca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1424,7 +1424,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-f7c9b94');
+const VERSION = new Version('5.1.0-beta.0-dbec3ca');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-f7c9b94
+ * @license Angular v5.1.0-beta.0-dbec3ca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-f7c9b94');
+var VERSION = new Version('5.1.0-beta.0-dbec3ca');
 
 /**
  * @fileoverview added by tsickle

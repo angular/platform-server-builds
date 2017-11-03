8f7c6b5b5557bb5b1b6923fd51c36d5b24e6435d
Victor Berchet <victor@suumit.com>
2017-11-03 19:14:33 +0000

    03f080b docs: Deployment guide for CLI (#19839)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-26f8299
+ * @license Angular v5.1.0-beta.0-03f080b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-26f8299');
+var VERSION = new Version('5.1.0-beta.0-03f080b');
 
 /**
  * @fileoverview added by tsickle

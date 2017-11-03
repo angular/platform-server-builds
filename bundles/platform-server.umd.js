691ba925090e785548f992e1a54a025300b54490
Victor Berchet <victor@suumit.com>
2017-11-03 22:54:54 +0000

    7083791 fix(core): should support event.stopImmediatePropagation (#19222)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-e4cd3b0
+ * @license Angular v5.0.0-7083791
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-e4cd3b0
+ * @license Angular v5.0.0-7083791
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-e4cd3b0');
+var VERSION = new _angular_core.Version('5.0.0-7083791');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

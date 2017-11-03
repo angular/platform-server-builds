f6d1744bb339f795fbe05fa64cb686b9f3b93370
Victor Berchet <victor@suumit.com>
2017-11-03 19:57:34 +0000

    e4cd3b0 test(aio): make e2e tests less flakey

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-9811bef
+ * @license Angular v5.0.0-e4cd3b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-9811bef
+ * @license Angular v5.0.0-e4cd3b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-9811bef');
+var VERSION = new _angular_core.Version('5.0.0-e4cd3b0');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

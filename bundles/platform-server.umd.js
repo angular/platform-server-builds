adaccddb2601c9b12e3cf0fa1e9199ef4a3ec68d
Victor Berchet <victor@suumit.com>
2017-11-06 18:33:19 +0000

    049c896 docs(aio): update ToH for CLI (#19811)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-8802016
+ * @license Angular v5.1.0-beta.0-049c896
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.0-8802016
+ * @license Angular v5.1.0-beta.0-049c896
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-8802016');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-049c896');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

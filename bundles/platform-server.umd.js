286bc93d792ead762cb829eb7e9d11a9aa425506
Victor Berchet <victor@suumit.com>
2017-11-06 18:42:43 +0000

    6b74883 build(aio): upgrade to @anglar/core@5.0.0 and cli@1.5.0

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-049c896
+ * @license Angular v5.1.0-beta.0-6b74883
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.0-049c896
+ * @license Angular v5.1.0-beta.0-6b74883
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-049c896');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-6b74883');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

b672a11238751386e9cb813a940dd14a36586ec9
Jason Aden <jasonaden@google.com>
2017-11-10 23:05:52 +0000

    612f508 refactor(animations): uses a loop instead Array.map() which creates and (#19910) returns a new array that is discarded.

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-aa3d75b
+ * @license Angular v5.0.1-612f508
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.1-aa3d75b
+ * @license Angular v5.0.1-612f508
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.1-aa3d75b');
+var VERSION = new _angular_core.Version('5.0.1-612f508');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

d06b6eaa8509a12a1ef63f83423411037c7c233a
Jason Aden <jasonaden@google.com>
2017-11-14 22:07:54 +0000

    91efc7f docs(core): change from deprecated renderer to renderer2 (#19920)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-7acbc19
+ * @license Angular v5.0.1-91efc7f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.1-7acbc19
+ * @license Angular v5.0.1-91efc7f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.1-7acbc19');
+var VERSION = new _angular_core.Version('5.0.1-91efc7f');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;
--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-7acbc19
+ * @license Angular v5.0.1-91efc7f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.1-7acbc19');
+var VERSION = new Version('5.0.1-91efc7f');
 
 /**
  * @fileoverview added by tsickle

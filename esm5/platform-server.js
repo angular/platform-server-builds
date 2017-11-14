db40744ac3f14cc81a662145fab7aba7ed875844
Jason Aden <jasonaden@google.com>
2017-11-14 18:27:10 +0000

    7acbc19 docs(aio): fix broken link in guide/component-interaction (#20411)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-612f508
+ * @license Angular v5.0.1-7acbc19
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.1-612f508');
+var VERSION = new Version('5.0.1-7acbc19');
 
 /**
  * @fileoverview added by tsickle

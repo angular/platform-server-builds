26d022b744e7ebf8b39147c39fd3f08c8a51524a
Jason Aden <jasonaden@google.com>
2017-11-14 22:06:23 +0000

    12af6d3 docs(core): change from deprecated renderer to renderer2 (#19920)

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-b9bd320
+ * @license Angular v5.1.0-beta.0-12af6d3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1424,7 +1424,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.0-b9bd320');
+const VERSION = new Version('5.1.0-beta.0-12af6d3');
 
 /**
  * @fileoverview added by tsickle

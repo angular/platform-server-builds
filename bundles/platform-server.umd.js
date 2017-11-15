98314b30a1431c89bb3b982b96f92a7427a25daa
Jason Aden <jasonaden@google.com>
2017-11-15 20:21:08 +0000

    043e408 style(core): fix comment format for linter

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-d47b2a6
+ * @license Angular v5.1.0-beta.0-043e408
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.0-d47b2a6
+ * @license Angular v5.1.0-beta.0-043e408
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-d47b2a6');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-043e408');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

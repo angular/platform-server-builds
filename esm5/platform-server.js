796e343026a7ba76c1fe0ea3e2e0a120e044ed5d
Miško Hevery <misko@hevery.com>
2017-11-16 04:50:52 +0000

    1285792 fix(aio): fix window title on Home page (#20440)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-2d6126e
+ * @license Angular v5.0.1-1285792
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.1-2d6126e');
+var VERSION = new Version('5.0.1-1285792');
 
 /**
  * @fileoverview added by tsickle

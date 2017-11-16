4407130750753feb2cbdcfe2ac92f4587bf23076
Miško Hevery <misko@hevery.com>
2017-11-16 06:59:30 +0000

    dcfffbf build: fix build failures (#20470)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-336041a
+ * @license Angular v5.1.0-beta.0-dcfffbf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.0-336041a');
+var VERSION = new Version('5.1.0-beta.0-dcfffbf');
 
 /**
  * @fileoverview added by tsickle

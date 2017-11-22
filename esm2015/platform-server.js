d4d43c306d0e68dc2316294262c2dce472469dfd
Miško Hevery <misko@hevery.com>
2017-11-22 18:01:55 +0000

    0e012c9 fix(animations): always fire inner trigger callbacks even if blocked by parent animations (#19753)

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-a0819d3
+ * @license Angular v5.1.0-beta.1-0e012c9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1431,7 +1431,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.1-a0819d3');
+const VERSION = new Version('5.1.0-beta.1-0e012c9');
 
 /**
  * @fileoverview added by tsickle

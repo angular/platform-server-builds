1b48a4031d854cf444b135d90d09b3d1616aed7f
Miško Hevery <misko@hevery.com>
2017-11-22 20:10:05 +0000

    ffb6dbe fix(animations): always fire start and done callbacks in order for noop animations (#20570)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-0e012c9
+ * @license Angular v5.1.0-beta.1-ffb6dbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.1-0e012c9
+ * @license Angular v5.1.0-beta.1-ffb6dbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.1-0e012c9');
+var VERSION = new _angular_core.Version('5.1.0-beta.1-ffb6dbe');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

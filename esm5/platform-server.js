65837c03b3d88e11df9403ef01a19e8fe80b1c70
Miško Hevery <misko@hevery.com>
2017-11-22 04:45:19 +0000

    7cf5e95 ci: update pullapprove (#20540)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-2a9d2ba
+ * @license Angular v5.1.0-beta.1-7cf5e95
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.1-2a9d2ba');
+var VERSION = new Version('5.1.0-beta.1-7cf5e95');
 
 /**
  * @fileoverview added by tsickle

58dfebd68665402823647dffb87d5dff7f5cd94a
Miško Hevery <misko@hevery.com>
2017-11-22 21:45:15 +0000

    65a40e6 docs: add changelog for 5.0.3

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.2-686c4ef
+ * @license Angular v5.0.3-65a40e6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.2-686c4ef');
+var VERSION = new Version('5.0.3-65a40e6');
 
 /**
  * @fileoverview added by tsickle

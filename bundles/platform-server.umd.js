26d74fc61f31f0749c69e7440474f58c85fb2ae5
Miško Hevery <misko@hevery.com>
2017-11-30 18:24:55 +0000

    47addd1 revert: docs(aio): add service worker guide content and update nav (#20021) (#20716)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-ba6af2a
+ * @license Angular v5.1.0-beta.2-47addd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.2-ba6af2a
+ * @license Angular v5.1.0-beta.2-47addd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.2-ba6af2a');
+var VERSION = new _angular_core.Version('5.1.0-beta.2-47addd1');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;
--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-ba6af2a
+ * @license Angular v5.1.0-beta.2-47addd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-beta.2-ba6af2a');
+var VERSION = new Version('5.1.0-beta.2-47addd1');
 
 /**
  * @fileoverview added by tsickle

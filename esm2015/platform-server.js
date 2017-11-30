b4c9551288dcd526d6e59a7fd441a2d9d7e89a4c
Miško Hevery <misko@hevery.com>
2017-11-30 03:56:07 +0000

    b9e4d62 docs(aio): remove services plurality (#20696)

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-ba850b3
+ * @license Angular v5.1.0-beta.2-b9e4d62
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1431,7 +1431,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.2-ba850b3');
+const VERSION = new Version('5.1.0-beta.2-b9e4d62');
 
 /**
  * @fileoverview added by tsickle

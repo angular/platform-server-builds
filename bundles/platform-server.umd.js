049d321e1af0fe9e85f84b0816f0660aeee5f02a
Miško Hevery <misko@hevery.com>
2017-11-30 03:59:45 +0000

    b450c83 docs(aio): remove services plurality (#20696)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-647ca64
+ * @license Angular v5.0.3-b450c83
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.3-647ca64
+ * @license Angular v5.0.3-b450c83
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1833,7 +1833,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.3-647ca64');
+var VERSION = new _angular_core.Version('5.0.3-b450c83');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

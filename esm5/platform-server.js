2b3ee96e37437490d1b662136fed0524c53c8be9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 22:45:59 +0000

    f582620 fix(service-worker): use relative path for ngsw.json

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-f841fbe
+ * @license Angular v5.1.0-rc.0-f582620
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-rc.0-f841fbe');
+var VERSION = new Version('5.1.0-rc.0-f582620');
 
 /**
  * @fileoverview added by tsickle

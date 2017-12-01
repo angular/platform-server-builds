d9b1d1d243181831e3d42f40aadcb67b0aa06f83
Misko Hevery <misko@hevery.com>
2017-12-01 06:15:39 +0000

    60a3081 docs: add changelog for 5.1.0-rc.0

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-47addd1
+ * @license Angular v5.1.0-rc.0-60a3081
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1431,7 +1431,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-beta.2-47addd1');
+const VERSION = new Version('5.1.0-rc.0-60a3081');
 
 /**
  * @fileoverview added by tsickle

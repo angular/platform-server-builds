123b897e5d5b1e18a685fd0eaeb63058b64803c5
Misko Hevery <misko@hevery.com>
2017-12-01 05:55:44 +0000

    cbd93fe docs: add changelog for 5.0.4

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-e95b5d7
+ * @license Angular v5.0.4-cbd93fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1424,7 +1424,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.3-e95b5d7');
+const VERSION = new Version('5.0.4-cbd93fe');
 
 /**
  * @fileoverview added by tsickle

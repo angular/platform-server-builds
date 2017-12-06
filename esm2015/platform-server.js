05dc255450f6e73caf5cf2d1e03052b4cca41eaa
Igor Minar <igor@angularjs.org>
2017-12-06 06:20:54 +0000

    9efb5da docs(aio): Fix typo (#20732)

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-860ef8c
+ * @license Angular v5.0.5-9efb5da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1424,7 +1424,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.5-860ef8c');
+const VERSION = new Version('5.0.5-9efb5da');
 
 /**
  * @fileoverview added by tsickle

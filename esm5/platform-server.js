ac67d909065f1a810314adb84b0cd26a2558edba
Igor Minar <igor@angularjs.org>
2017-12-06 15:15:46 +0000

    c91c0d4 fix(service-worker): esm2015 points to wrong path (#20800)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-9efb5da
+ * @license Angular v5.0.5-c91c0d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1813,7 +1813,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.5-9efb5da');
+var VERSION = new Version('5.0.5-c91c0d4');
 
 /**
  * @fileoverview added by tsickle

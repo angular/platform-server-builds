0f5b3bdeb5cbace4076b46991c33a049077f528a
Jason Aden <jasonaden@google.com>
2017-12-07 02:01:28 +0000

    6727336 fix(compiler-cli): Fix swallowed Error messages (#20846)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-c2dbc55
+ * @license Angular v5.1.0-6727336
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-c2dbc55
+ * @license Angular v5.1.0-6727336
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-c2dbc55');
+var VERSION = new _angular_core.Version('5.1.0-6727336');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

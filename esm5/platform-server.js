a2bf6766c14b7aa30b49eaaac655636390da0cd5
Jason Aden <jasonaden@google.com>
2017-12-07 21:53:43 +0000

    ef534c0 build: upgrade bazel rules to latest (#20768)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-073f485
+ * @license Angular v5.1.0-ef534c0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-073f485');
+var VERSION = new Version('5.1.0-ef534c0');
 
 /**
  * @fileoverview added by tsickle

18ce2009aa9d71e761ffd5aca044ce2e837e430b
Jason Aden <jasonaden@google.com>
2017-12-08 18:31:48 +0000

    ddada6e build(aio): upgrade to latest `@angular/cli` (#18428)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-225baf4
+ * @license Angular v5.1.0-ddada6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-225baf4
+ * @license Angular v5.1.0-ddada6e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-225baf4');
+var VERSION = new _angular_core.Version('5.1.0-ddada6e');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

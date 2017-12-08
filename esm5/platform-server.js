773e6c363a817d99754f0e2fa3ba4e3c0eb33ff0
Jason Aden <jasonaden@google.com>
2017-12-08 20:32:23 +0000

    3ce3b4d refactor(common): update i18n locale data to CLDR v32 (#20830)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-ddada6e
+ * @license Angular v5.1.0-3ce3b4d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-ddada6e');
+var VERSION = new Version('5.1.0-3ce3b4d');
 
 /**
  * @fileoverview added by tsickle

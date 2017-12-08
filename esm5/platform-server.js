032de744f5035b404414cebf706840000834d256
Jason Aden <jasonaden@google.com>
2017-12-08 18:35:59 +0000

    affa54d build(aio): upgrade to latest `@angular/cli` (#18428)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-53b9de3
+ * @license Angular v5.1.0-affa54d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-53b9de3');
+var VERSION = new Version('5.1.0-affa54d');
 
 /**
  * @fileoverview added by tsickle

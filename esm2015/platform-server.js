efddf4cc91dec9dfc5a11e777f9a55750a54b1d5
Jason Aden <jasonaden@google.com>
2017-12-12 00:24:49 +0000

    8a5f0f7 build: pin ChromeDriver version (#20940)

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-8bcb093
+ * @license Angular v5.1.0-8a5f0f7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1431,7 +1431,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-8bcb093');
+const VERSION = new Version('5.1.0-8a5f0f7');
 
 /**
  * @fileoverview added by tsickle

9579bc96531c3c05b0b8c2e44023c76e0cbca80c
Jason Aden <jasonaden@google.com>
2017-12-12 20:03:25 +0000

    be9a737 fix(compiler-cli): merge @fileoverview comments. (#20870)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-1f46949
+ * @license Angular v5.1.0-be9a737
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-1f46949
+ * @license Angular v5.1.0-be9a737
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-1f46949');
+var VERSION = new _angular_core.Version('5.1.0-be9a737');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;
--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-1f46949
+ * @license Angular v5.1.0-be9a737
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1431,7 +1431,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-1f46949');
+const VERSION = new Version('5.1.0-be9a737');
 
 /**
  * @fileoverview added by tsickle

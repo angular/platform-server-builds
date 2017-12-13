e6ea185e368a9e4629246b6ec8fff7ff498955fe
Jason Aden <jasonaden@google.com>
2017-12-13 17:59:58 +0000

    b3eb1db build: update node version number in .nvmrc (#20832)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-8c52088
+ * @license Angular v5.1.0-b3eb1db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-8c52088
+ * @license Angular v5.1.0-b3eb1db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-8c52088');
+var VERSION = new _angular_core.Version('5.1.0-b3eb1db');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

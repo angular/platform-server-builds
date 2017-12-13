4dcaac72f6a8b110998d48aa2f3afd13981e0b9c
Jason Aden <jasonaden@google.com>
2017-12-13 19:56:50 +0000

    7671417 docs: add changelog for 5.1.1

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-b3eb1db
+ * @license Angular v5.1.0-7671417
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-b3eb1db
+ * @license Angular v5.1.0-7671417
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-b3eb1db');
+var VERSION = new _angular_core.Version('5.1.0-7671417');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

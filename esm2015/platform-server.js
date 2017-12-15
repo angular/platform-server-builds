8081dbe5a25ae17eb95948856ab7a8b1f3104e5c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 16:21:13 +0000

    756dd34 fix(compiler): make tsx file aot compatible

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.1-267ebf3
+ * @license Angular v5.1.1-756dd34
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1431,7 +1431,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.1-267ebf3');
+const VERSION = new Version('5.1.1-756dd34');
 
 /**
  * @fileoverview added by tsickle

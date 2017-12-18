74142815a615f7edb0aca11d96473398d7cda122
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-18 20:29:49 +0000

    3846f19 docs(core): move core examples into `examples/core/` directory

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-82bcd83
+ * @license Angular v5.2.0-beta.0-3846f19
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-beta.0-82bcd83
+ * @license Angular v5.2.0-beta.0-3846f19
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-beta.0-82bcd83');
+var VERSION = new _angular_core.Version('5.2.0-beta.0-3846f19');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;
--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.0-82bcd83
+ * @license Angular v5.2.0-beta.0-3846f19
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1431,7 +1431,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-beta.0-82bcd83');
+const VERSION = new Version('5.2.0-beta.0-3846f19');
 
 /**
  * @fileoverview added by tsickle

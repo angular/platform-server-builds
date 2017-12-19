a2865a9c9d65512b8b83403f0b79462167a73883
Alex Eagle <alexeagle@google.com>
2017-12-19 17:54:42 +0000

    057b357 docs: Update BAZEL.md

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-43868d2
+ * @license Angular v5.2.0-beta.0-057b357
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.0-43868d2');
+var VERSION = new Version('5.2.0-beta.0-057b357');
 
 /**
  * @fileoverview added by tsickle

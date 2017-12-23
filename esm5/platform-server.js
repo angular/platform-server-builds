f00ac7a46b894453d442e1c92de50166107bb1a0
Igor Minar <igor@angular.io>
2017-12-23 05:57:14 +0000

    5de91fe docs(forms): add text about min() and max() as functions (#21110)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-d787b55
+ * @license Angular v5.1.2-5de91fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.2-d787b55');
+var VERSION = new Version('5.1.2-5de91fe');
 
 /**
  * @fileoverview added by tsickle

707a8f9904d88e44ced0d26215d865f35c9bf1a7
Igor Minar <igor@angular.io>
2017-12-23 05:58:47 +0000

    12702b2 docs(changelog): fix typo in 5.1.1 (#21007)

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-5de91fe
+ * @license Angular v5.1.2-12702b2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1431,7 +1431,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.2-5de91fe');
+const VERSION = new Version('5.1.2-12702b2');
 
 /**
  * @fileoverview added by tsickle

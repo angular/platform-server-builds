76cc526c7636c99469494f62bd763ca55b752aba
Igor Minar <igor@angular.io>
2017-12-27 21:43:40 +0000

    d8abf70 aio: Sort in the api type dropdown (#21176)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-efd9c09
+ * @license Angular v5.2.0-beta.1-d8abf70
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-beta.1-efd9c09
+ * @license Angular v5.2.0-beta.1-d8abf70
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-beta.1-efd9c09');
+var VERSION = new _angular_core.Version('5.2.0-beta.1-d8abf70');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

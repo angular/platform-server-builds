f904e0f96fe085fa8e816e3ef9d7944f3bf06dbf
Miško Hevery <misko@hevery.com>
2017-12-28 00:03:54 +0000

    bc7a6d7 build: force upstream fetch before merge (#21192)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-9f538a6
+ * @license Angular v5.2.0-beta.1-bc7a6d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-beta.1-9f538a6
+ * @license Angular v5.2.0-beta.1-bc7a6d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-beta.1-9f538a6');
+var VERSION = new _angular_core.Version('5.2.0-beta.1-bc7a6d7');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

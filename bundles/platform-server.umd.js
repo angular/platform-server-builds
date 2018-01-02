d30077ea4a6a1bb541fb3d95d548e581bdb73d97
Miško Hevery <misko@hevery.com>
2018-01-02 21:10:33 +0000

    48a1f32 fix(language-service): ignore null metadatas (#20557)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-66043e0
+ * @license Angular v5.1.2-48a1f32
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.2-66043e0
+ * @license Angular v5.1.2-48a1f32
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.2-66043e0');
+var VERSION = new _angular_core.Version('5.1.2-48a1f32');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

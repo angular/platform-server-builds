d5054b643abb3af8fa68807ad8b6b34a8703e344
Kara Erickson <karakara@google.com>
2018-01-04 01:29:11 +0000

    fc217f5 refactor(aio): use one argument for `DocViewer` error reporting (#21293)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-d138b38
+ * @license Angular v5.1.3-fc217f5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.3-d138b38
+ * @license Angular v5.1.3-fc217f5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.3-d138b38');
+var VERSION = new _angular_core.Version('5.1.3-fc217f5');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

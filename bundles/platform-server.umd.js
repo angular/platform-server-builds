d1f0ff1db36ec300416eb52a6be265161411f554
Kara Erickson <karakara@google.com>
2018-01-04 19:40:35 +0000

    ccea372 refactor(aio): use one argument for `DocViewer` error reporting (#21293)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-cdc66f6
+ * @license Angular v5.2.0-rc.0-ccea372
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-rc.0-cdc66f6
+ * @license Angular v5.2.0-rc.0-ccea372
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-rc.0-cdc66f6');
+var VERSION = new _angular_core.Version('5.2.0-rc.0-ccea372');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

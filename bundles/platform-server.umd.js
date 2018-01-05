b363a1d059a497b6202ba2cedfd85ae12a48f330
Kara Erickson <karakara@google.com>
2018-01-05 20:14:07 +0000

    17e85cd build(aio): fix plunker for custom plunkers (#21291)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-c809545
+ * @license Angular v5.1.3-17e85cd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.3-c809545
+ * @license Angular v5.1.3-17e85cd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1840,7 +1840,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.3-c809545');
+var VERSION = new _angular_core.Version('5.1.3-17e85cd');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

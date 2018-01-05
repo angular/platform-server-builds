26278f74a10e83b29eee8948d45655e607b5604e
Kara Erickson <karakara@google.com>
2018-01-05 20:19:53 +0000

    52cb79c build(aio): add `@nodoc` alias to the `@internal` tag (#21024)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-8dd93f8
+ * @license Angular v5.1.3-52cb79c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-8dd93f8');
+var VERSION = new Version('5.1.3-52cb79c');
 
 /**
  * @fileoverview added by tsickle

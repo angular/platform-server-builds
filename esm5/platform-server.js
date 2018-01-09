7c25f9c731c16871236223858c71e69a3ee10fd6
Kara Erickson <karakara@google.com>
2018-01-09 20:43:03 +0000

    96efb4b ci: temporary work-around for Travis issue (#21422)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-7c34807
+ * @license Angular v5.1.3-96efb4b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.1.3-7c34807');
+var VERSION = new Version('5.1.3-96efb4b');
 
 /**
  * @fileoverview added by tsickle

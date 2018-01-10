a089e4c2e8a0c0653ecc0652cc4cd2cb07c9f32c
Alex Eagle <alexeagle@google.com>
2018-01-10 20:46:13 +0000

    a76ab1f docs(router): remove extra brackets (#21294)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-72246d4
+ * @license Angular v5.2.0-a76ab1f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-72246d4
+ * @license Angular v5.2.0-a76ab1f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-72246d4');
+var VERSION = new _angular_core.Version('5.2.0-a76ab1f');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

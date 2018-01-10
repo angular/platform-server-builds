dae1c18d33f5eaf21e0c729c0300a7d5cf5a19c3
Alex Eagle <alexeagle@google.com>
2018-01-10 21:12:21 +0000

    f15ca6c fix(aio): support multibyte character in heading (#21414)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-0de219a
+ * @license Angular v5.2.0-f15ca6c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-0de219a
+ * @license Angular v5.2.0-f15ca6c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-0de219a');
+var VERSION = new _angular_core.Version('5.2.0-f15ca6c');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

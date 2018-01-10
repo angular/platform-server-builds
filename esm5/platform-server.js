f45b3883290ed4bbd3386f284d2803c893a97261
Alex Eagle <alexeagle@google.com>
2018-01-10 21:41:54 +0000

    7bdd9ae build: upgrade yarn to 1.3.2 (#21406)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-5ede67c
+ * @license Angular v5.2.0-7bdd9ae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1815,7 +1815,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-5ede67c');
+var VERSION = new Version('5.2.0-7bdd9ae');
 
 /**
  * @fileoverview added by tsickle

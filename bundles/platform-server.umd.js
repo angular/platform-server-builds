dd3a1322536777b0e9e9cf14c79dd8c8026b0771
Alex Eagle <alexeagle@google.com>
2018-01-10 23:50:58 +0000

    a33ff2c docs(ivy): add Ivy as a recognized label (#21428)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-92984ba
+ * @license Angular v5.2.0-a33ff2c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-92984ba
+ * @license Angular v5.2.0-a33ff2c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-92984ba');
+var VERSION = new _angular_core.Version('5.2.0-a33ff2c');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

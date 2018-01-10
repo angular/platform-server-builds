666cf09d830124c86d6276f1167e7441a37d6e89
Alex Eagle <alexeagle@google.com>
2018-01-10 22:16:50 +0000

    650f5fb build(bazel): fix merge conflict between c4f02e2 and ef956a2 (#21453)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-c32e833
+ * @license Angular v5.2.0-650f5fb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-c32e833
+ * @license Angular v5.2.0-650f5fb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-c32e833');
+var VERSION = new _angular_core.Version('5.2.0-650f5fb');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

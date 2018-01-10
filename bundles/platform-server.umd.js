764d7402ce4caf9fa80bb3d553c101c5e7bbac4a
Alex Eagle <alexeagle@google.com>
2018-01-10 21:30:42 +0000

    7493b8a build: upgrade yarn to 1.3.2 (#21406)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-c4f02e2
+ * @license Angular v5.2.0-7493b8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-c4f02e2
+ * @license Angular v5.2.0-7493b8a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-c4f02e2');
+var VERSION = new _angular_core.Version('5.2.0-7493b8a');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

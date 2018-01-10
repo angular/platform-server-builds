8bf99b3cba506b894e9fabd7142e4fe71f6afaeb
Alex Eagle <alexeagle@google.com>
2018-01-10 20:21:35 +0000

    167cbed build: add bazel rulse for benchmarks (#21436)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-70e8802
+ * @license Angular v5.2.0-167cbed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1815,7 +1815,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-70e8802');
+var VERSION = new Version('5.2.0-167cbed');
 
 /**
  * @fileoverview added by tsickle

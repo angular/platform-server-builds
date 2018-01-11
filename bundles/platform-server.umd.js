a13b8bf4aac95aa0afddb42d4af7ad09a8d0226f
Alex Eagle <alexeagle@google.com>
2018-01-11 15:22:58 +0000

    b49d54e docs(forms): clarify note in reactive forms doc (#21134)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-55fd82e
+ * @license Angular v5.2.0-b49d54e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-55fd82e
+ * @license Angular v5.2.0-b49d54e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-55fd82e');
+var VERSION = new _angular_core.Version('5.2.0-b49d54e');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

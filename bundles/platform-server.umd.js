778432961ffc7bdadb0232f1b487818a7bf647c7
Alex Eagle <alexeagle@google.com>
2018-01-12 22:20:31 +0000

    977978e test(common): make date pipe tests work in more timezones (#21379)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-eb70966
+ * @license Angular v5.2.0-977978e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-eb70966
+ * @license Angular v5.2.0-977978e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-eb70966');
+var VERSION = new _angular_core.Version('5.2.0-977978e');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

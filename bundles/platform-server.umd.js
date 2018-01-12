74c1e78bbdd241e66ea853973cc19743ed928c07
Alex Eagle <alexeagle@google.com>
2018-01-12 22:09:14 +0000

    3e3c22f fix(aio): correctly handle redirects (#21416)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-f6db952
+ * @license Angular v5.2.0-3e3c22f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-f6db952
+ * @license Angular v5.2.0-3e3c22f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-f6db952');
+var VERSION = new _angular_core.Version('5.2.0-3e3c22f');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;
--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-f6db952
+ * @license Angular v5.2.0-3e3c22f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1815,7 +1815,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.0-f6db952');
+var VERSION = new Version('5.2.0-3e3c22f');
 
 /**
  * @fileoverview added by tsickle

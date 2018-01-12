2c26897f137c797268da3aa16157dbd0749772f9
Alex Eagle <alexeagle@google.com>
2018-01-12 20:28:09 +0000

    f23896f docs: fix release schedule date (#21474)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-0e59d18
+ * @license Angular v5.2.0-f23896f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-0e59d18
+ * @license Angular v5.2.0-f23896f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-0e59d18');
+var VERSION = new _angular_core.Version('5.2.0-f23896f');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

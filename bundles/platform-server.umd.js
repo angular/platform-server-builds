4a08100172fcf6612b7de04d107ab68766b0a478
Alex Eagle <alexeagle@google.com>
2018-01-17 16:14:36 +0000

    6e8cf63 docs(aio): add description and docs links for code samples (#21561)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-d3bf54b
+ * @license Angular v5.2.0-6e8cf63
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-d3bf54b
+ * @license Angular v5.2.0-6e8cf63
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-d3bf54b');
+var VERSION = new _angular_core.Version('5.2.0-6e8cf63');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

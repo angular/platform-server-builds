48bd89f8e37a5c8d24781b5124b7895953eb7000
Alex Eagle <alexeagle@google.com>
2018-01-17 16:51:04 +0000

    b773a4a docs(aio): change `df-question` to `app-question` (#21438)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-55f15c5
+ * @license Angular v5.2.0-b773a4a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-55f15c5
+ * @license Angular v5.2.0-b773a4a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-55f15c5');
+var VERSION = new _angular_core.Version('5.2.0-b773a4a');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

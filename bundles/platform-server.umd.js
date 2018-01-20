f63d76b7a7f59becc67688f618449d2ee24184cd
Misko Hevery <misko@hevery.com>
2018-01-20 04:52:26 +0000

    554129d feat(aio): update metatags to control search engine crawling (#21665)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-c828e56
+ * @license Angular v5.2.1-554129d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.1-c828e56
+ * @license Angular v5.2.1-554129d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.1-c828e56');
+var VERSION = new _angular_core.Version('5.2.1-554129d');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

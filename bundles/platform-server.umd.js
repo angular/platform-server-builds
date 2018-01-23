c30fcf7db9c4f3370b65b644a4e87e5c4cc09bf1
Miško Hevery <misko@hevery.com>
2018-01-23 21:58:12 +0000

    8baff18 fix(ivy): add names to function expressions (#21714)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-95fbb7d
+ * @license Angular v5.9.9-6-beta.0-8baff18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.9.9-6-beta.0-95fbb7d
+ * @license Angular v5.9.9-6-beta.0-8baff18
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.9.9-6-beta.0-95fbb7d');
+var VERSION = new _angular_core.Version('5.9.9-6-beta.0-8baff18');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

e420f46588c207e33eabc1b3129770fff251dc99
Misko Hevery <misko@hevery.com>
2018-01-25 05:58:51 +0000

    3aaf43f docs: add changelog for 5.2.2

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-da9e57b
+ * @license Angular v5.2.2-3aaf43f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.1-da9e57b
+ * @license Angular v5.2.2-3aaf43f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.1-da9e57b');
+var VERSION = new _angular_core.Version('5.2.2-3aaf43f');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

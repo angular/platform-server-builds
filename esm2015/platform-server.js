0904c65d5d464e04a69fac9104695eb634e5b390
Miško Hevery <misko@hevery.com>
2018-01-26 18:52:32 +0000

    3b7bab7 feat(router): add navigationSource and restoredState to NavigationStart event (#21728)

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-108fa15
+ * @license Angular v6.0.0-beta.1-3b7bab7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -736,7 +736,9 @@ class ServerPlatformLocation {
         }
         (/** @type {?} */ (this)).hash = value;
         const /** @type {?} */ newUrl = this.url;
-        scheduleMicroTask(() => this._hashUpdate.next(/** @type {?} */ ({ type: 'hashchange', oldUrl, newUrl })));
+        scheduleMicroTask(() => this._hashUpdate.next(/** @type {?} */ ({
+            type: 'hashchange', state: null, oldUrl, newUrl
+        })));
     }
     /**
      * @param {?} state
@@ -1426,7 +1428,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.1-108fa15');
+const VERSION = new Version('6.0.0-beta.1-3b7bab7');
 
 /**
  * @fileoverview added by tsickle

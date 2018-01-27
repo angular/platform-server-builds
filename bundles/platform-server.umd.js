f8d74580d0e470a1530a29f6389ff44aae18e9a6
Misko Hevery <misko@hevery.com>
2018-01-27 22:44:08 +0000

    5bd93b1 build: fix broken build (#21835)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-a1cc02f
+ * @license Angular v6.0.0-beta.1-5bd93b1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.1-a1cc02f
+ * @license Angular v6.0.0-beta.1-5bd93b1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-a1cc02f');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-5bd93b1');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

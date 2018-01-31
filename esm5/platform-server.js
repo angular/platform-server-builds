036390e47758e8e4ee55dd350537bef3c86248d8
Jason Aden <jasonaden@google.com>
2018-01-31 19:01:08 +0000

    e0022ae docs: Fix platform-detection example for Universal (#21796)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.2-f2e923e
+ * @license Angular v5.2.2-e0022ae
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1815,7 +1815,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.2-f2e923e');
+var VERSION = new Version('5.2.2-e0022ae');
 
 /**
  * @fileoverview added by tsickle

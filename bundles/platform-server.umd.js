942e84deb059e17f6f037d4df03d51d2a7d59b9d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:56:44 +0000

    102d06b docs: consistency fix in describing a custom tag (#21747)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-aa9ba7f
+ * @license Angular v5.2.3-102d06b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.3-aa9ba7f
+ * @license Angular v5.2.3-102d06b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.3-aa9ba7f');
+var VERSION = new _angular_core.Version('5.2.3-102d06b');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

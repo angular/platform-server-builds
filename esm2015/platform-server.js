c9d6d6e311a7c6910406669aea37716f6c1d2232
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 21:20:19 +0000

    ce51ea9 fix(core): fix #20582, don't need to wrap zone in location change listener (#22007)

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-102d06b
+ * @license Angular v5.2.3-ce51ea9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1426,7 +1426,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.3-102d06b');
+const VERSION = new Version('5.2.3-ce51ea9');
 
 /**
  * @fileoverview added by tsickle

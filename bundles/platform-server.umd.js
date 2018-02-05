dc418013eee733c62003588ad771229faac42b75
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 22:00:39 +0000

    664f7fa build(aio): add API static members to search index (#21988)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-973607f
+ * @license Angular v5.2.3-664f7fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.3-973607f
+ * @license Angular v5.2.3-664f7fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.3-973607f');
+var VERSION = new _angular_core.Version('5.2.3-664f7fa');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

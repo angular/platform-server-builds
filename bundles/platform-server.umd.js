ebdaf15916006eef4a99a385eab90c0df531b524
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-06 16:31:38 +0000

    4d62be6 feat(ivy): memoize array literals in render3 (#21973)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-2707012
+ * @license Angular v6.0.0-beta.2-4d62be6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.2-2707012
+ * @license Angular v6.0.0-beta.2-4d62be6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1839,7 +1839,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.2-2707012');
+var VERSION = new _angular_core.Version('6.0.0-beta.2-4d62be6');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

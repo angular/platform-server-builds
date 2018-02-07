f08541dedb7206bafe68f4eafc7edd00b35c6e4d
Miško Hevery <misko@hevery.com>
2018-02-07 21:00:15 +0000

    9744a1c fix(forms): set state before emitting a value from ngModelChange (#21514)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-0bcfae7
+ * @license Angular v6.0.0-beta.3-9744a1c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.3-0bcfae7
+ * @license Angular v6.0.0-beta.3-9744a1c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1839,7 +1839,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.3-0bcfae7');
+var VERSION = new _angular_core.Version('6.0.0-beta.3-9744a1c');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

a4d4862ba84694eae42a19445dbcc53410d23135
Miško Hevery <misko@hevery.com>
2018-02-07 21:09:08 +0000

    9d02db3 fix(aio): ignore `.header-link` when selecting the heading text (#21695)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-56b9591
+ * @license Angular v5.2.4-9d02db3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.4-56b9591
+ * @license Angular v5.2.4-9d02db3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.4-56b9591');
+var VERSION = new _angular_core.Version('5.2.4-9d02db3');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

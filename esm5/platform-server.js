bc3ee587e7a38dd31cee05dbebabc625b5541456
Miško Hevery <misko@hevery.com>
2018-02-07 20:31:37 +0000

    b0ddb5a build(aio): blacklist unwanted URLs from the generated sitemap.xml (#22061)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-44ea80b
+ * @license Angular v5.2.4-b0ddb5a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1815,7 +1815,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.4-44ea80b');
+var VERSION = new Version('5.2.4-b0ddb5a');
 
 /**
  * @fileoverview added by tsickle

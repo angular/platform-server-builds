bbe4fb5990f6f109ddc86cb484bae5ed9d2d5c0b
Miško Hevery <misko@pangular.mtv.corp.google.com>
2018-02-08 00:30:07 +0000

    ffcb5dd build(aio): update examples to CLI to 1.6.5 (#21222)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-75897d4
+ * @license Angular v5.2.4-ffcb5dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.4-75897d4
+ * @license Angular v5.2.4-ffcb5dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.4-75897d4');
+var VERSION = new _angular_core.Version('5.2.4-ffcb5dd');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

0d30a4f9a5672e52bb3f83855a18005d5e890f00
Miško Hevery <misko@hevery.com>
2018-02-14 05:22:09 +0000

    c4f841f fix(core): add stacktrace in log when error during cleanup component in TestBed (#22162)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-1b8ea10
+ * @license Angular v5.2.4-c4f841f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.4-1b8ea10
+ * @license Angular v5.2.4-c4f841f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.4-1b8ea10');
+var VERSION = new _angular_core.Version('5.2.4-c4f841f');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

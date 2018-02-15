9ddab8d6cca467a90d121bab9577a4a32ef62bef
Victor Berchet <victor@suumit.com>
2018-02-15 22:27:47 +0000

    6a57264 Revert: "build: allow bazel build ... (#22168)"

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-1e3e0fa
+ * @license Angular v5.2.5-6a57264
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-1e3e0fa
+ * @license Angular v5.2.5-6a57264
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-1e3e0fa');
+var VERSION = new _angular_core.Version('5.2.5-6a57264');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

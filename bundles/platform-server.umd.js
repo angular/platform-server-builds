34622de4a31360f5aca14962812ee1b42f919c59
Victor Berchet <victor@suumit.com>
2018-02-17 02:25:56 +0000

    861250b docs: fix changelog errors (#22228)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-81c1e0a
+ * @license Angular v5.2.5-861250b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-81c1e0a
+ * @license Angular v5.2.5-861250b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-81c1e0a');
+var VERSION = new _angular_core.Version('5.2.5-861250b');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

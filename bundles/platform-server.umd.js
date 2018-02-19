d5052f72a1429689dcdc2a1b9c05b7cbc7fade21
Victor Berchet <victor@suumit.com>
2018-02-19 05:18:08 +0000

    bf07837 test(common): fix ngIf tests

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-cdfedc1
+ * @license Angular v5.2.5-bf07837
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-cdfedc1
+ * @license Angular v5.2.5-bf07837
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-cdfedc1');
+var VERSION = new _angular_core.Version('5.2.5-bf07837');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

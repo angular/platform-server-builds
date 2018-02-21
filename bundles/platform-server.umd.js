4bf968d8cce64b2f7ba2d8fd22c8f0d6f17b469e
Victor Berchet <victor@suumit.com>
2018-02-21 00:30:42 +0000

    94756eb docs(aio): fix incorrect quote mark usage (#22335) PR Close #22335

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-23b0707
+ * @license Angular v5.2.5-94756eb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-23b0707
+ * @license Angular v5.2.5-94756eb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-23b0707');
+var VERSION = new _angular_core.Version('5.2.5-94756eb');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

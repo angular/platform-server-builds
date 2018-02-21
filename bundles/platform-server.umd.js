aa23f7ae0b2a25fbcda2916f9e5617c355167f68
Victor Berchet <victor@suumit.com>
2018-02-21 00:03:53 +0000

    d20a08b docs(aio): update installed mobile tool list (#22331) PR Close #22331

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-5bdb3ac
+ * @license Angular v5.2.5-d20a08b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-5bdb3ac
+ * @license Angular v5.2.5-d20a08b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1835,7 +1835,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-5bdb3ac');
+var VERSION = new _angular_core.Version('5.2.5-d20a08b');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

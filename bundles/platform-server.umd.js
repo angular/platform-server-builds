b6a4d72db9c26892a2f1453bf3988b070d752e4e
Victor Berchet <victor@suumit.com>
2018-02-21 19:41:12 +0000

    722dec1 docs(aio): Wrong code example. Form status field was added later in the guide. (#21275)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-435f6ee
+ * @license Angular v6.0.0-beta.4-722dec1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.4-435f6ee
+ * @license Angular v6.0.0-beta.4-722dec1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1839,7 +1839,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-435f6ee');
+var VERSION = new _angular_core.Version('6.0.0-beta.4-722dec1');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

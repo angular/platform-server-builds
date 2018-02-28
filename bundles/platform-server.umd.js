060fc11d36cbff1831399dd84eeec566ac3a5875
Alex Eagle <alexeagle@google.com>
2018-02-28 02:57:03 +0000

    83b32a0 build(aio): render heading anchors on the right

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-8d34364
+ * @license Angular v6.0.0-beta.5-83b32a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.5-8d34364
+ * @license Angular v6.0.0-beta.5-83b32a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1789,7 +1789,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-8d34364');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-83b32a0');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

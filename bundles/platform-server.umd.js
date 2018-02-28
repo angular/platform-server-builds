d93e82f52d401fcc0ca0eb650d20c8ad16fb637f
Alex Eagle <alexeagle@google.com>
2018-02-28 19:28:11 +0000

    c82cef8 docs: fix dynamic component loader example (#22181)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-7d65356
+ * @license Angular v6.0.0-beta.5-c82cef8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.5-7d65356
+ * @license Angular v6.0.0-beta.5-c82cef8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1789,7 +1789,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-7d65356');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-c82cef8');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

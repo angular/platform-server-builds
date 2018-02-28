d4988d120cf72c4577b24e3cf3fa200ec4691916
Alex Eagle <alexeagle@google.com>
2018-02-28 23:40:11 +0000

    697d31a docs: add changelog for 5.2.7

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-0ec11e3
+ * @license Angular v5.2.7-697d31a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.6-0ec11e3
+ * @license Angular v5.2.7-697d31a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1785,7 +1785,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.6-0ec11e3');
+var VERSION = new _angular_core.Version('5.2.7-697d31a');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

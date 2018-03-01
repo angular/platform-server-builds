a97bb457a8b8b09c584408384d150dc176090a9e
Alex Eagle <alexeagle@google.com>
2018-03-01 16:47:00 +0000

    8bb2f5c docs(compiler): ivy separate compilation design document (#22480)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-94707fe
+ * @license Angular v6.0.0-beta.6-8bb2f5c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.6-94707fe
+ * @license Angular v6.0.0-beta.6-8bb2f5c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1789,7 +1789,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.6-94707fe');
+var VERSION = new _angular_core.Version('6.0.0-beta.6-8bb2f5c');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

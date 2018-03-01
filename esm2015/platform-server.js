bb51b879892568facd4fc2f5825a3620e78c99f3
Alex Eagle <alexeagle@google.com>
2018-03-01 21:39:31 +0000

    0451fd9 feat(ivy): support generating view and content queries (#22330)

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-49f074f
+ * @license Angular v6.0.0-beta.6-0451fd9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1391,7 +1391,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-49f074f');
+const VERSION = new Version('6.0.0-beta.6-0451fd9');
 
 /**
  * @fileoverview added by tsickle

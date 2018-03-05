c4675a63d5273ddb0273dd566db6678f7a6ca68a
Alex Eagle <alexeagle@google.com>
2018-03-05 18:33:05 +0000

    51ca643 test(ivy): add injectAttribute spec (#22510)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-4f60968
+ * @license Angular v6.0.0-beta.6-51ca643
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.6-4f60968
+ * @license Angular v6.0.0-beta.6-51ca643
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1789,7 +1789,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.6-4f60968');
+var VERSION = new _angular_core.Version('6.0.0-beta.6-51ca643');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

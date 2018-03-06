5de98081e70f22beeab5692c871f46e721f05e70
Alex Eagle <alexeagle@google.com>
2018-03-06 18:06:07 +0000

    5b7b208 docs: fix cli-quickstart doc and specs (#22338)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-789a47e
+ * @license Angular v5.2.7-5b7b208
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.7-789a47e
+ * @license Angular v5.2.7-5b7b208
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1785,7 +1785,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.7-789a47e');
+var VERSION = new _angular_core.Version('5.2.7-5b7b208');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

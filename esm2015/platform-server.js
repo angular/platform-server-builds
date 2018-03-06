f9fee0983480af519651d0f9ee4b4195c7067080
Alex Eagle <alexeagle@google.com>
2018-03-06 05:45:33 +0000

    94bc277 fix(aio): tidy up embedded ToC styling (#22570)

--- esm2015/platform-server.js
+++ esm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-2a1e3d1
+ * @license Angular v6.0.0-beta.6-94bc277
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1391,7 +1391,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-const VERSION = new Version('6.0.0-beta.6-2a1e3d1');
+const VERSION = new Version('6.0.0-beta.6-94bc277');
 
 /**
  * @fileoverview added by tsickle

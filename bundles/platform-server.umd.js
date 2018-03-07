2ba3e41e6404a0e669e290453fafc081ebb293ca
Alex Eagle <alexeagle@google.com>
2018-03-07 19:13:08 +0000

    4aeb04d docs: update RELEASE_SCHEDULE.md by pushing out v6 rc by one week

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-5876fb0
+ * @license Angular v5.2.7-4aeb04d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.7-5876fb0
+ * @license Angular v5.2.7-4aeb04d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1785,7 +1785,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.7-5876fb0');
+var VERSION = new _angular_core.Version('5.2.7-4aeb04d');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

26b98126d617f81e91130835de592b931891a10c
Kara Erickson <karakara@google.com>
2018-03-08 20:02:00 +0000

    f13f4db refactor(ivy): fix rebase error (#22661)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-2ca77d8
+ * @license Angular v6.0.0-beta.7-f13f4db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-2ca77d8
+ * @license Angular v6.0.0-beta.7-f13f4db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1789,7 +1789,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-2ca77d8');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-f13f4db');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

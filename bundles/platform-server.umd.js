6e9e759a76a5f36bbad3f6d2c8b56a37dd29e4b7
Kara Erickson <karakara@google.com>
2018-03-08 05:21:45 +0000

    a1efc27 ci: double our cores on CircleCI (#22641)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.8-3112320
+ * @license Angular v5.2.8-a1efc27
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.8-3112320
+ * @license Angular v5.2.8-a1efc27
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1785,7 +1785,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.8-3112320');
+var VERSION = new _angular_core.Version('5.2.8-a1efc27');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

9238c849e259d09cf76ea62480d192d73a221ac4
Kara Erickson <karakara@google.com>
2018-03-09 21:31:11 +0000

    0d8deb0 fix(compiler-cli): generate proper exports.* identifiers in cjs output (#22564)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-e8326e6
+ * @license Angular v6.0.0-beta.7-0d8deb0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-e8326e6
+ * @license Angular v6.0.0-beta.7-0d8deb0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1789,7 +1789,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-e8326e6');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-0d8deb0');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

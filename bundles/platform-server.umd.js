ef8cb6f5dbeacb553287dc7c3d69775836f6a736
Kara Erickson <karakara@google.com>
2018-03-12 17:32:54 +0000

    3f70aba fix(compiler-cli): disableTypeScriptVersionCheck should be applied even for older tsc versions (#22669)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-0bf6fa5
+ * @license Angular v6.0.0-beta.7-3f70aba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-0bf6fa5
+ * @license Angular v6.0.0-beta.7-3f70aba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1789,7 +1789,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-0bf6fa5');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-3f70aba');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

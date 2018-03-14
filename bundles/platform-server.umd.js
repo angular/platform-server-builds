feabfce3e22fcd5f0e0e4247663d7512573ccf10
Kara Erickson <karakara@google.com>
2018-03-14 17:45:32 +0000

    ad674da docs: testing - highlight dispatchEvent (#22726)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.8-86517f2
+ * @license Angular v5.2.8-ad674da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.8-86517f2
+ * @license Angular v5.2.8-ad674da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1785,7 +1785,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.8-86517f2');
+var VERSION = new _angular_core.Version('5.2.8-ad674da');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

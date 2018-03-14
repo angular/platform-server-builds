8e476117cd19fe9c32085b6044950229837ace53
Kara Erickson <karakara@google.com>
2018-03-14 18:25:07 +0000

    c0670ef docs(aio): add ng-japan 2018 to events (#22750)

--- esm5/platform-server.js
+++ esm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.8-fe96caf
+ * @license Angular v5.2.8-c0670ef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1765,7 +1765,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.8-fe96caf');
+var VERSION = new Version('5.2.8-c0670ef');
 
 /**
  * @fileoverview added by tsickle

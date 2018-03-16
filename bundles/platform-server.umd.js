4fe57c86c3cea3393b2a2512d5e4bd58b162e892
Miško Hevery <misko@hevery.com>
2018-03-16 04:42:52 +0000

    269c3a1 build(compiler-cli): fix tsconfig.json circularity issue (#22722)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-b149424
+ * @license Angular v6.0.0-beta.7-269c3a1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-b149424
+ * @license Angular v6.0.0-beta.7-269c3a1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-b149424');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-269c3a1');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

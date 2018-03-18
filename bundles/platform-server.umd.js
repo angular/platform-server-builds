3252716f305ca761cad1b5797a33f96261cdb1ce
Misko Hevery <misko@hevery.com>
2018-03-18 19:19:18 +0000

    e27cfd6 refactor(ivy): split up directiveCreate for tree shaking (#22838)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-4f21d37
+ * @license Angular v6.0.0-beta.7-e27cfd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-4f21d37
+ * @license Angular v6.0.0-beta.7-e27cfd6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-4f21d37');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-e27cfd6');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

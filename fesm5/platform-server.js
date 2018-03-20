6c37898c69a76ae094e15cfa643de572daa753c4
Igor Minar <igor@angular.io>
2018-03-20 21:02:21 +0000

    e6c731f fix(router): don't use spread operator to workaround an issue in closure compiler (#22884)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-7d095b9
+ * @license Angular v6.0.0-beta.7-e6c731f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-beta.7-7d095b9');
+var VERSION = new Version('6.0.0-beta.7-e6c731f');
 
 /**
  * @fileoverview added by tsickle

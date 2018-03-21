b8f7abfa233c482a6b3e0cd74a34dc81daed0b0a
Igor Minar <igor@angular.io>
2018-03-21 21:14:05 +0000

    f461f43 docs: fix a typo in aot compiler guide (#22876)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-64efcf1
+ * @license Angular v6.0.0-rc.0-f461f43
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-64efcf1');
+var VERSION = new Version('6.0.0-rc.0-f461f43');
 
 /**
  * @fileoverview added by tsickle

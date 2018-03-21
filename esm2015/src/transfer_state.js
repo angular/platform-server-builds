00b84995ad3db572dcb3cd975a0f4cfc968dbcf1
Igor Minar <igor@angular.io>
2018-03-21 06:31:08 +0000

    73261a8 docs: add changelog for 6.0.0-rc.0

---
+++ esm2015/src/transfer_state.js
@@ -0,0 +1,61 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { APP_ID, NgModule } from '@angular/core';
+import { DOCUMENT, TransferState, ɵescapeHtml as escapeHtml } from '@angular/platform-browser';
+import { BEFORE_APP_SERIALIZED } from './tokens';
+/**
+ * @param {?} doc
+ * @param {?} appId
+ * @param {?} transferStore
+ * @return {?}
+ */
+export function serializeTransferStateFactory(doc, appId, transferStore) {
+    return () => {
+        const /** @type {?} */ script = doc.createElement('script');
+        script.id = appId + '-state';
+        script.setAttribute('type', 'application/json');
+        script.textContent = escapeHtml(transferStore.toJson());
+        doc.body.appendChild(script);
+    };
+}
+/**
+ * NgModule to install on the server side while using the `TransferState` to transfer state from
+ * server to client.
+ *
+ * \@experimental
+ */
+export class ServerTransferStateModule {
+}
+ServerTransferStateModule.decorators = [
+    { type: NgModule, args: [{
+                providers: [
+                    TransferState, {
+                        provide: BEFORE_APP_SERIALIZED,
+                        useFactory: serializeTransferStateFactory,
+                        deps: [DOCUMENT, APP_ID, TransferState],
+                        multi: true,
+                    }
+                ]
+            },] },
+];
+/** @nocollapse */
+ServerTransferStateModule.ctorParameters = () => [];
+function ServerTransferStateModule_tsickle_Closure_declarations() {
+    /** @type {!Array<{type: !Function, args: (undefined|!Array<?>)}>} */
+    ServerTransferStateModule.decorators;
+    /**
+     * @nocollapse
+     * @type {function(): !Array<(null|{type: ?, decorators: (undefined|!Array<{type: !Function, args: (undefined|!Array<?>)}>)})>}
+     */
+    ServerTransferStateModule.ctorParameters;
+}
+//# sourceMappingURL=transfer_state.js.map

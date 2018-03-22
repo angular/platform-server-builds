d0c8810180ce27110ac559f1da061c97dd22fa34
Matias Niemelä <matsko@matskocloud.c.googlers.com>
2018-03-22 22:43:44 +0000

    49f6d1d build: rm --noimplicit_deps from bazel query (#22912)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-641cc49
+ * @license Angular v5.2.9-49f6d1d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.9-641cc49
+ * @license Angular v5.2.9-49f6d1d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1842,7 +1842,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-641cc49');
+var VERSION = new _angular_core.Version('5.2.9-49f6d1d');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

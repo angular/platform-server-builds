3be4db542dc176b7d76569a325c6d8e097cb8157
Matias Niemelä <matias@yearofmoo.com>
2018-03-22 23:49:37 +0000

    3498106 build: ts-api-guardian reuses root node_modules (#22894)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-e44f69c
+ * @license Angular v6.0.0-rc.0-3498106
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-e44f69c');
+var VERSION = new Version('6.0.0-rc.0-3498106');
 
 /**
  * @fileoverview added by tsickle

95a2c84873f5382a4758abf2adf7d45c74ecb5bc
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 01:50:45 +0000

    5ab9d4d test(compiler): allow asserting matching identifier names (#22835)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-49396ca
+ * @license Angular v6.0.0-rc.0-5ab9d4d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-49396ca
+ * @license Angular v6.0.0-rc.0-5ab9d4d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-49396ca');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-5ab9d4d');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

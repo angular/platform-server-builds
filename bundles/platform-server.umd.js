ee124e4fee65bd0c892a83f89fb6e1e04ea4050b
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 19:28:24 +0000

    b9cbe83 docs(ivy): update STATUS.md with compiler work breakdown (#22874)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-60d9983
+ * @license Angular v6.0.0-rc.0-b9cbe83
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-60d9983
+ * @license Angular v6.0.0-rc.0-b9cbe83
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-60d9983');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-b9cbe83');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

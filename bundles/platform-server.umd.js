946dbb7f9e1556cb19096838db219d46af4028ef
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 01:11:46 +0000

    8ca26a9 ci(compiler): run compiler tests in bazel (#22900)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-3498106
+ * @license Angular v6.0.0-rc.0-8ca26a9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-3498106
+ * @license Angular v6.0.0-rc.0-8ca26a9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-3498106');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-8ca26a9');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

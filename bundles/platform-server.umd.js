8ed2cb967f5aa30886b831cc54df9584309f9f94
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 18:35:08 +0000

    4be8b3f docs: update available platforms for `test.sh` (#22958)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-8d1e640
+ * @license Angular v5.2.9-4be8b3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.9-8d1e640
+ * @license Angular v5.2.9-4be8b3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1842,7 +1842,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-8d1e640');
+var VERSION = new _angular_core.Version('5.2.9-4be8b3f');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

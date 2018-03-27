db11563249068e57ff43cd98fdc0cc52e8b3d1d9
Matias Niemelä <matias@yearofmoo.com>
2018-03-27 19:34:37 +0000

    776bb82 build: update browserstack key (#23026)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-d7f4aa6
+ * @license Angular v5.2.9-776bb82
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.9-d7f4aa6
+ * @license Angular v5.2.9-776bb82
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1842,7 +1842,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-d7f4aa6');
+var VERSION = new _angular_core.Version('5.2.9-776bb82');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

74cf90d569e6cbe97e97efc7823ac3ac4ece36ef
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-29 16:27:39 +0000

    28058b7 fix(compiler): fix support for html-like text in translatable attributes (#23053)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-0049743
+ * @license Angular v6.0.0-rc.0-28058b7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-0049743');
+var VERSION = new Version('6.0.0-rc.0-28058b7');
 
 /**
  * @fileoverview added by tsickle

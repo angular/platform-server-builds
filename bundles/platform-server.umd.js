dfc0796580a9bf879a7390b56e24510e74d32357
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-30 15:26:38 +0000

    a5f0939 build(compiler-cli): include new test files in bazel config (#22705)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-be10bf5
+ * @license Angular v6.0.0-rc.0-a5f0939
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-be10bf5
+ * @license Angular v6.0.0-rc.0-a5f0939
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-be10bf5');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-a5f0939');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

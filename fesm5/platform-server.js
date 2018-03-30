27814ba4585a8af775690c53c0b6385d76b353ba
Igor Minar <igor@angular.io>
2018-03-30 20:36:57 +0000

    22cb2c9 build: add missing dependencies on @rxjs//operators needed for rxjs@6.0.0-beta.4 (#23084)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-a5f0939
+ * @license Angular v6.0.0-rc.0-22cb2c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-a5f0939');
+var VERSION = new Version('6.0.0-rc.0-22cb2c9');
 
 /**
  * @fileoverview added by tsickle

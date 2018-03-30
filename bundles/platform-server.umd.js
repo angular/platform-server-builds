b66b38e94eec37d548bd7948a443521580365242
GitHub <noreply@github.com>
2018-03-30 23:23:41 +0000

    d6bf71a docs(compiler): initial outline of Ivy Compiler Architecture design doc (#22916)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-9cd4465
+ * @license Angular v6.0.0-rc.1-d6bf71a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-9cd4465
+ * @license Angular v6.0.0-rc.1-d6bf71a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-9cd4465');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-d6bf71a');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

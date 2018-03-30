f7537dbb400dd5fa4d3c0e62750da3665a8ee8b9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-30 23:03:47 +0000

    4473da7 ci(aio): add monitoring for angular.io (#23093)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-4f7c369
+ * @license Angular v5.2.9-4473da7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.9-4f7c369
+ * @license Angular v5.2.9-4473da7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1842,7 +1842,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-4f7c369');
+var VERSION = new _angular_core.Version('5.2.9-4473da7');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

13436e706b850a7e6e4d76437f9e6fd5a9e9855a
Igor Minar <igor@angular.io>
2018-03-30 02:17:59 +0000

    481b22e fix(bazel): downlevel decorators in fesm5 files (#23078)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-45e090b
+ * @license Angular v6.0.0-rc.0-481b22e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-45e090b
+ * @license Angular v6.0.0-rc.0-481b22e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-45e090b');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-481b22e');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;
--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-45e090b
+ * @license Angular v6.0.0-rc.0-481b22e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.0-45e090b');
+var VERSION = new Version('6.0.0-rc.0-481b22e');
 
 /**
  * @fileoverview added by tsickle

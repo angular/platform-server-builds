0ce876bd2518362bae72dc289225f9391b74f12f
Igor Minar <igor@angular.io>
2018-03-31 00:52:40 +0000

    43d6202 build(aio): update to angular@6.0.0-rc.1 (#23101)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-d6bf71a
+ * @license Angular v6.0.0-rc.1-43d6202
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-d6bf71a
+ * @license Angular v6.0.0-rc.1-43d6202
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-d6bf71a');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-43d6202');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

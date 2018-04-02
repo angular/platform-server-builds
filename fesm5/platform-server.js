286eeb23d73d8c82446f90439eb4068bdf4facd4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-02 23:11:59 +0000

    55c9fb2 test(ivy): create todo app in ivy (#22921)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-a2330ff
+ * @license Angular v6.0.0-rc.1-55c9fb2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-a2330ff');
+var VERSION = new Version('6.0.0-rc.1-55c9fb2');
 
 /**
  * @fileoverview added by tsickle

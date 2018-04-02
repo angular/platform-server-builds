a84a5e4e7122d4dd2cec46fe563715b874c4e45e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-02 21:47:25 +0000

    a2330ff fix(upgrade): propagate return value of resumeBootstrap (#22754)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-ff34d5e
+ * @license Angular v6.0.0-rc.1-a2330ff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-ff34d5e
+ * @license Angular v6.0.0-rc.1-a2330ff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-ff34d5e');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-a2330ff');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

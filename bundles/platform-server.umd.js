93ca06d7588d6674d0a749c611dfedf01a0aa9d6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-03 18:14:38 +0000

    7ca7720 fix(common): `locales` are not being shipped (#23136)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-580f05b
+ * @license Angular v6.0.0-rc.1-7ca7720
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-580f05b
+ * @license Angular v6.0.0-rc.1-7ca7720
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-580f05b');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-7ca7720');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;
--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-580f05b
+ * @license Angular v6.0.0-rc.1-7ca7720
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-580f05b');
+var VERSION = new Version('6.0.0-rc.1-7ca7720');
 
 /**
  * @fileoverview added by tsickle

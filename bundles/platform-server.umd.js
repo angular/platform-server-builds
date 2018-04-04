fd5574faa7e0b8505d5b45d2f9078d9217b8b203
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 15:54:28 +0000

    23cc3ef fix(forms): improve error message for invalid value accessors (#22731)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-550433a
+ * @license Angular v6.0.0-rc.1-23cc3ef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-550433a
+ * @license Angular v6.0.0-rc.1-23cc3ef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-550433a');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-23cc3ef');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

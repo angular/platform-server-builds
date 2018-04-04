e8a9876aea0b801c82838fcbfc04cd135600a30d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 16:02:48 +0000

    d37064c test(router): fix typo in expectation (#23137)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-23cc3ef
+ * @license Angular v6.0.0-rc.1-d37064c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-23cc3ef
+ * @license Angular v6.0.0-rc.1-d37064c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-23cc3ef');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-d37064c');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;
--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-23cc3ef
+ * @license Angular v6.0.0-rc.1-d37064c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-23cc3ef');
+var VERSION = new Version('6.0.0-rc.1-d37064c');
 
 /**
  * @fileoverview added by tsickle

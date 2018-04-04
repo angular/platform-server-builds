4b56c6b4c1cc8e71a7e0f90e9e47decf9cf9c518
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 17:55:29 +0000

    32a41bc test(ivy): fixes in the TodoMVC example (#23161)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-f48e215
+ * @license Angular v6.0.0-rc.1-32a41bc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-f48e215
+ * @license Angular v6.0.0-rc.1-32a41bc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-f48e215');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-32a41bc');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

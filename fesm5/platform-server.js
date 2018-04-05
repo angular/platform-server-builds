ab499a4682dcbf016fc221b7b652de739cd4880b
Igor Minar <igor@angular.io>
2018-04-05 23:54:10 +0000

    b5f41f2 docs: fixed live example for the lifecycle hooks. (#23201)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-629629d
+ * @license Angular v6.0.0-rc.2-b5f41f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.2-629629d');
+var VERSION = new Version('6.0.0-rc.2-b5f41f2');
 
 /**
  * @fileoverview added by tsickle

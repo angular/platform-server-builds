77f229e8d0b196452b57fb330555abdc5a6eb9d3
Igor Minar <igor@angular.io>
2018-04-05 00:12:36 +0000

    6699fb5 build: language-service package built by bazel (#23155)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-6dd8f6e
+ * @license Angular v6.0.0-rc.1-6699fb5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-6dd8f6e');
+var VERSION = new Version('6.0.0-rc.1-6699fb5');
 
 /**
  * @fileoverview added by tsickle

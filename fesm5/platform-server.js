4b2ede3c59b64327ec8fb1ed45fd951923266d7e
Igor Minar <igor@angular.io>
2018-04-05 22:28:33 +0000

    ae8a009 build(bazel): use ng_package entry_point_name for umd bundles (#23132)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-628303d
+ * @license Angular v6.0.0-rc.2-ae8a009
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.2-628303d');
+var VERSION = new Version('6.0.0-rc.2-ae8a009');
 
 /**
  * @fileoverview added by tsickle

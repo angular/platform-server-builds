d2f88360b521797bb17a3f87b5115344f315bd21
Igor Minar <igor@angular.io>
2018-04-05 22:27:05 +0000

    628303d fix(ivy): instantiate dirs in correct order (#23178)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-d80e930
+ * @license Angular v6.0.0-rc.2-628303d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.2-d80e930');
+var VERSION = new Version('6.0.0-rc.2-628303d');
 
 /**
  * @fileoverview added by tsickle

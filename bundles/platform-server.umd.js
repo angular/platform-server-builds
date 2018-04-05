49e23a95d3f758bba293880e81cd1534893d879a
Igor Minar <igor@angular.io>
2018-04-05 23:54:40 +0000

    eb9968a docs(elements): use Angular Elements naming in nav (#23213)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-b5f41f2
+ * @license Angular v6.0.0-rc.2-eb9968a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.2-b5f41f2
+ * @license Angular v6.0.0-rc.2-eb9968a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.2-b5f41f2');
+var VERSION = new _angular_core.Version('6.0.0-rc.2-eb9968a');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

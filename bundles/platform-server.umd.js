464e8ada1d29c1f6fe6cc099ebb696eed009ea2f
Igor Minar <igor@angular.io>
2018-04-05 17:27:59 +0000

    ac316be build: remove unnecessary stability check (#23176)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-b8053f1
+ * @license Angular v6.0.0-rc.1-ac316be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-b8053f1
+ * @license Angular v6.0.0-rc.1-ac316be
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-b8053f1');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-ac316be');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

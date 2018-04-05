192efbcbb7c5d3a04c432d22192203947c1a6b47
Igor Minar <igor@angular.io>
2018-04-05 17:29:54 +0000

    8aa49ac fix(aio): update trusted GitHub teams (angular-core --> team) (#23181)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-3900c36
+ * @license Angular v5.2.9-8aa49ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.9-3900c36
+ * @license Angular v5.2.9-8aa49ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1842,7 +1842,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-3900c36');
+var VERSION = new _angular_core.Version('5.2.9-8aa49ac');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

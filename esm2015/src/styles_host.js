e07d38f8aca683dd93c072c22a0304b8191b3900
Igor Minar <igor@angular.io>
2018-04-07 00:13:36 +0000

    33f8120 docs: add changelog for 6.0.0-rc.3

---
+++ esm2015/src/styles_host.js
@@ -0,0 +1,68 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { Inject, Injectable, Optional } from '@angular/core';
+import { DOCUMENT, ɵSharedStylesHost as SharedStylesHost, ɵTRANSITION_ID, ɵgetDOM as getDOM } from '@angular/platform-browser';
+export class ServerStylesHost extends SharedStylesHost {
+    /**
+     * @param {?} doc
+     * @param {?} transitionId
+     */
+    constructor(doc, transitionId) {
+        super();
+        this.doc = doc;
+        this.transitionId = transitionId;
+        this.head = null;
+        this.head = getDOM().getElementsByTagName(doc, 'head')[0];
+    }
+    /**
+     * @param {?} style
+     * @return {?}
+     */
+    _addStyle(style) {
+        let /** @type {?} */ adapter = getDOM();
+        const /** @type {?} */ el = adapter.createElement('style');
+        adapter.setText(el, style);
+        if (!!this.transitionId) {
+            adapter.setAttribute(el, 'ng-transition', this.transitionId);
+        }
+        adapter.appendChild(this.head, el);
+    }
+    /**
+     * @param {?} additions
+     * @return {?}
+     */
+    onStylesAdded(additions) { additions.forEach(style => this._addStyle(style)); }
+}
+ServerStylesHost.decorators = [
+    { type: Injectable },
+];
+/** @nocollapse */
+ServerStylesHost.ctorParameters = () => [
+    { type: undefined, decorators: [{ type: Inject, args: [DOCUMENT,] },] },
+    { type: undefined, decorators: [{ type: Optional }, { type: Inject, args: [ɵTRANSITION_ID,] },] },
+];
+function ServerStylesHost_tsickle_Closure_declarations() {
+    /** @type {!Array<{type: !Function, args: (undefined|!Array<?>)}>} */
+    ServerStylesHost.decorators;
+    /**
+     * @nocollapse
+     * @type {function(): !Array<(null|{type: ?, decorators: (undefined|!Array<{type: !Function, args: (undefined|!Array<?>)}>)})>}
+     */
+    ServerStylesHost.ctorParameters;
+    /** @type {?} */
+    ServerStylesHost.prototype.head;
+    /** @type {?} */
+    ServerStylesHost.prototype.doc;
+    /** @type {?} */
+    ServerStylesHost.prototype.transitionId;
+}
+//# sourceMappingURL=styles_host.js.map

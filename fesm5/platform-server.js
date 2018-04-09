4604e095814ce1bd8182ff12dadcc9a36af56833
Igor Minar <igor@angular.io>
2018-04-09 22:40:46 +0000

    3e8eef6 fix(bazel): set rollup output.name and amd.id (#23274)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-1fac5f4
+ * @license Angular v6.0.0-rc.3-3e8eef6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.3-1fac5f4');
+var VERSION = new Version('6.0.0-rc.3-3e8eef6');
 
 /**
  * @fileoverview added by tsickle

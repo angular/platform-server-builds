db1bae95fb2efe6ea26b881084c6ed2dc32e938c
Igor Minar <igor@angular.io>
2018-04-10 20:38:20 +0000

    8555a3a fix(compiler): Pretty print object instead of [Object object] (#22689)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-10ecdb1
+ * @license Angular v6.0.0-rc.3-8555a3a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.3-10ecdb1
+ * @license Angular v6.0.0-rc.3-8555a3a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.3-10ecdb1');
+var VERSION = new _angular_core.Version('6.0.0-rc.3-8555a3a');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

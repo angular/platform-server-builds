1f24d08b52491c2714964aff915604a498c0de41
Igor Minar <igor@angular.io>
2018-04-10 20:41:51 +0000

    c973830 refactor(ivy): clean projection support (#23287)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-f1db789
+ * @license Angular v6.0.0-rc.3-c973830
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.3-f1db789');
+var VERSION = new Version('6.0.0-rc.3-c973830');
 
 /**
  * @fileoverview added by tsickle

6099cb1072777cc652be9b410069df5f0bed7681
Igor Minar <igor@angular.io>
2018-04-12 08:46:43 +0000

    01975ff docs: add changelog for 6.0.0-rc.4

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-1fc72e5
+ * @license Angular v6.0.0-rc.4-01975ff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.3-1fc72e5');
+var VERSION = new Version('6.0.0-rc.4-01975ff');
 
 /**
  * @fileoverview added by tsickle

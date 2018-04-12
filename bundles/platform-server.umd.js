c11bb98ac0e7d9828a223e74d8a95447c6173ada
Igor Minar <igor@angular.io>
2018-04-12 06:40:21 +0000

    7e8cee6 build: fix aio size tracking, we need to use node_modules local to aio (#23328)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-37d2cb4
+ * @license Angular v6.0.0-rc.3-7e8cee6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.3-37d2cb4
+ * @license Angular v6.0.0-rc.3-7e8cee6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.3-37d2cb4');
+var VERSION = new _angular_core.Version('6.0.0-rc.3-7e8cee6');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

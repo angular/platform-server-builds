4c02cad6a5fb7cc2a1ca67dade2c622ca816277f
Igor Minar <igor@angular.io>
2018-04-13 15:39:17 +0000

    eac36d7 test(aio): fix `DocViewerComponent` tests (#23359)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-74b203a
+ * @license Angular v6.0.0-rc.4-eac36d7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.4-74b203a
+ * @license Angular v6.0.0-rc.4-eac36d7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.4-74b203a');
+var VERSION = new _angular_core.Version('6.0.0-rc.4-eac36d7');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

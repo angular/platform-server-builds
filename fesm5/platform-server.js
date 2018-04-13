4512bec659f28e1278dedf985bd3ee74f1b85cd0
Igor Minar <igor@angular.io>
2018-04-13 07:52:36 +0000

    3bd682f docs(aio): Add link to Japanese localization (#20630) PR Close #20630

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-2bb7838
+ * @license Angular v6.0.0-rc.4-3bd682f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.4-2bb7838');
+var VERSION = new Version('6.0.0-rc.4-3bd682f');
 
 /**
  * @fileoverview added by tsickle

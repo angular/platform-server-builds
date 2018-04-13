260a027384424715a3df487c68e600b04cf9a540
Igor Minar <igor@angular.io>
2018-04-13 07:30:50 +0000

    a113330 refactor(language-service): fix typo on type.ts language-service

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-3cf5719
+ * @license Angular v5.2.9-a113330
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.9-3cf5719
+ * @license Angular v5.2.9-a113330
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1842,7 +1842,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-3cf5719');
+var VERSION = new _angular_core.Version('5.2.9-a113330');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

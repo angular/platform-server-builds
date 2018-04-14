8763953346c57d812948c2818c5538fe53eefd3f
Igor Minar <igor@angular.io>
2018-04-14 04:54:12 +0000

    33630dd fix(ivy): workaround for tsickle bug (#23379)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-d1177c7
+ * @license Angular v6.0.0-rc.4-33630dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.4-d1177c7
+ * @license Angular v6.0.0-rc.4-33630dd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.4-d1177c7');
+var VERSION = new _angular_core.Version('6.0.0-rc.4-33630dd');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

528dce206813e2ffcd60eab4cc276500161bf0cb
Igor Minar <igor@angular.io>
2018-04-16 09:32:59 +0000

    4361560 docs: add changelog for the v4.4.7 release

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-d979230
+ * @license Angular v6.0.0-rc.5-4361560
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.5-d979230
+ * @license Angular v6.0.0-rc.5-4361560
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.5-d979230');
+var VERSION = new _angular_core.Version('6.0.0-rc.5-4361560');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

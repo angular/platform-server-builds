f922202bcb2575b4439ff81f0f69f5b641a8754a
Igor Minar <igor@angular.io>
2018-04-16 09:36:51 +0000

    4361560 docs: add changelog for the v4.4.7 release

---
+++ esm2015/src/platform_state.js
@@ -0,0 +1,56 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { Inject, Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
+import { serializeDocument } from './domino_adapter';
+/**
+ * Representation of the current platform state.
+ *
+ * \@experimental
+ */
+export class PlatformState {
+    /**
+     * @param {?} _doc
+     */
+    constructor(_doc) {
+        this._doc = _doc;
+    }
+    /**
+     * Renders the current state of the platform to string.
+     * @return {?}
+     */
+    renderToString() { return serializeDocument(this._doc); }
+    /**
+     * Returns the current DOM state.
+     * @return {?}
+     */
+    getDocument() { return this._doc; }
+}
+PlatformState.decorators = [
+    { type: Injectable },
+];
+/** @nocollapse */
+PlatformState.ctorParameters = () => [
+    { type: undefined, decorators: [{ type: Inject, args: [DOCUMENT,] },] },
+];
+function PlatformState_tsickle_Closure_declarations() {
+    /** @type {!Array<{type: !Function, args: (undefined|!Array<?>)}>} */
+    PlatformState.decorators;
+    /**
+     * @nocollapse
+     * @type {function(): !Array<(null|{type: ?, decorators: (undefined|!Array<{type: !Function, args: (undefined|!Array<?>)}>)})>}
+     */
+    PlatformState.ctorParameters;
+    /** @type {?} */
+    PlatformState.prototype._doc;
+}
+//# sourceMappingURL=platform_state.js.map

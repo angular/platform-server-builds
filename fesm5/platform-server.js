689b34fe180ac6fa85eb7388f32b799e43dad46c
Victor Berchet <victor@suumit.com>
2018-04-24 22:31:30 +0000

    adf6235 docs: corrected spelling of "ambient".

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-04c18ac
+ * @license Angular v6.0.0-rc.5-adf6235
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1823,7 +1823,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5-04c18ac');
+var VERSION = new Version('6.0.0-rc.5-adf6235');
 
 /**
  * @fileoverview added by tsickle

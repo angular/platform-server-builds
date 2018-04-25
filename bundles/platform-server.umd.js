c2c10478fe487172d1e123161c203a48e1d6bf03
Igor Minar <igor@angular.io>
2018-04-25 20:46:33 +0000

    64bf6ed docs: update glossary architectural terms (#23045)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-adf6235
+ * @license Angular v6.0.0-rc.5-64bf6ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.5-adf6235
+ * @license Angular v6.0.0-rc.5-64bf6ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.5-adf6235');
+var VERSION = new _angular_core.Version('6.0.0-rc.5-64bf6ed');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

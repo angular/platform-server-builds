b19a30cdc5a195a0841977ddc613014e34525ae0
Igor Minar <igor@angular.io>
2018-05-02 22:40:53 +0000

    22eb8e2 build(aio): align stackblitz files with Angular CLI V6 (#23521)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.6-14138f6
+ * @license Angular v6.0.0-rc.6-22eb8e2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.6-14138f6
+ * @license Angular v6.0.0-rc.6-22eb8e2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1846,7 +1846,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.6-14138f6');
+var VERSION = new _angular_core.Version('6.0.0-rc.6-22eb8e2');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

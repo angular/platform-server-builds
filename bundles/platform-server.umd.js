b03f07380aebc3618698a19d6b9ed4a295c5f988
Igor Minar <igor@angular.io>
2018-05-04 22:32:53 +0000

    65dba9d test: fix firebase deployment script test

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.10-402f452
+ * @license Angular v5.2.10-65dba9d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.10-402f452
+ * @license Angular v5.2.10-65dba9d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1842,7 +1842,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.10-402f452');
+var VERSION = new _angular_core.Version('5.2.10-65dba9d');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

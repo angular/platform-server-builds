986c83ad7a6c1977e1a34043b3b067ad6e37de69
Igor Minar <igor@angular.io>
2018-05-10 15:28:39 +0000

    89f64e5 fix(router): avoid freezing queryParams in-place (#22663)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+156.sha-858e48a
+ * @license Angular v6.0.0-rc.5+158.sha-89f64e5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1038,7 +1038,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  *
  */
-var VERSION = new core.Version('6.0.0-rc.5+156.sha-858e48a');
+var VERSION = new core.Version('6.0.0-rc.5+158.sha-89f64e5');
 
 /**
  * @license

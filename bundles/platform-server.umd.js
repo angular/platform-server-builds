ea4dd4e1560ec51f30626816da06b60d3d43209a
Victor Berchet <victor@suumit.com>
2018-06-06 19:29:52 +0000

    47814b4 release: cut the v6.0.4 release

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+337.sha-700e55c
+ * @license Angular v6.0.0-rc.5+338.sha-47814b4
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1064,7 +1064,7 @@ function renderModuleFactory(moduleFactory, options) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.0-rc.5+337.sha-700e55c');
+var VERSION = new core.Version('6.0.0-rc.5+338.sha-47814b4');
 
 /**
  * @license

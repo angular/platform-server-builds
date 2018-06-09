db26193429270aac728db5626856aedf3b0a89c2
Miško Hevery <misko@hevery.com>
2018-06-09 00:15:50 +0000

    dc4a3d0 fix(animations): always render end-state styles for orphaned DOM nodes (#24236)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+28.sha-8aa70c2
+ * @license Angular v6.1.0-beta.0+29.sha-dc4a3d0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1020,7 +1020,7 @@ function renderModuleFactory(moduleFactory, options) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.0+28.sha-8aa70c2');
+var VERSION = new Version('6.1.0-beta.0+29.sha-dc4a3d0');
 
 /**
  * @license

20a382744e2f3bb329ee3cd03230cdca59f4eb07
Miško Hevery <misko@hevery.com>
2018-06-21 20:45:27 +0000

    84272e2 feat(ivy): runtime i18n (#24037)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.2+6.sha-cb31381
+ * @license Angular v6.1.0-beta.2+7.sha-84272e2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1020,7 +1020,7 @@ function renderModuleFactory(moduleFactory, options) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.2+6.sha-cb31381');
+var VERSION = new Version('6.1.0-beta.2+7.sha-84272e2');
 
 /**
  * @license

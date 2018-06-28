5a843b696d90d041ee62bf86fb425bc6f993674f
Miško Hevery <misko@hevery.com>
2018-06-28 22:40:35 +0000

    ac5b69f fix(docs-infra): use clean package.json template when generating zips (#24691)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.7+1.sha-ad68332
+ * @license Angular v6.0.7+3.sha-ac5b69f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1019,7 +1019,7 @@ function renderModuleFactory(moduleFactory, options) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.0.7+1.sha-ad68332');
+var VERSION = new Version('6.0.7+3.sha-ac5b69f');
 
 /**
  * @license

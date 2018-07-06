64fb9aee1b5e99c1870f7b9768b770f0bd6655db
Miško Hevery <misko@hevery.com>
2018-07-06 19:10:18 +0000

    f62876b fix(ivy): pipes are pure by default (#24750)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+35.sha-b6af870
+ * @license Angular v6.1.0-beta.3+46.sha-f62876b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1006,7 +1006,7 @@ function renderModuleFactory(moduleFactory, options) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new Version('6.1.0-beta.3+35.sha-b6af870');
+var VERSION = new Version('6.1.0-beta.3+46.sha-f62876b');
 
 /**
  * @license

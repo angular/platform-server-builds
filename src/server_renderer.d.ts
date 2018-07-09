b690fc274dda9cca8a1b4273e58d46b00f15be92
Greg Magolan <gmagolan@gmail.com>
2018-07-09 21:13:10 +0000

    ee2af9b feat(bazel): protractor_web_test_suite for release

---
+++ src/server_renderer.d.ts
@@ -0,0 +1,15 @@
+import { NgZone, Renderer2, RendererFactory2, RendererType2 } from '@angular/core';
+import { EventManager, ɵSharedStylesHost as SharedStylesHost } from '@angular/platform-browser';
+export declare class ServerRendererFactory2 implements RendererFactory2 {
+    private eventManager;
+    private ngZone;
+    private document;
+    private sharedStylesHost;
+    private rendererByCompId;
+    private defaultRenderer;
+    private schema;
+    constructor(eventManager: EventManager, ngZone: NgZone, document: any, sharedStylesHost: SharedStylesHost);
+    createRenderer(element: any, type: RendererType2 | null): Renderer2;
+    begin(): void;
+    end(): void;
+}

633c9a9d19de2544ef4db2deaf0c20fd1ef341db
Kara Erickson <karakara@google.com>
2018-08-03 21:51:26 +0000

    3f43dbb style(common): fix short param names (#23667)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1
+ * @license Angular v6.1.1+2.sha-3f43dbb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1483,7 +1483,7 @@ function renderModuleFactory(moduleFactory, options) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.1');
+const VERSION = new Version('6.1.1+2.sha-3f43dbb');
 
 /**
  * @fileoverview added by tsickle

52e473988f3ad5613415b26c26c04eb7ce245131
Ben Lesh <ben@benlesh.com>
2018-09-17 19:22:18 +0000

    47f4412 refactor(ivy): LContainers should store views not nodes (#25933)

--- fesm5/testing.js
+++ fesm5/testing.js
@@ -1,9 +1,10 @@
 /**
- * @license Angular v7.0.0-beta.2+28.sha-21a1440
+ * @license Angular v7.0.0-beta.5+32.sha-47f4412
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
+import { __decorate } from 'tslib';
 import { NgModule, createPlatformFactory } from '@angular/core';
 import { BrowserDynamicTestingModule, ɵplatformCoreDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
@@ -30,13 +31,13 @@ var platformServerTesting = createPlatformFactory(ɵplatformCoreDynamicTesting, 
 var ServerTestingModule = /** @class */ (function () {
     function ServerTestingModule() {
     }
-    ServerTestingModule.decorators = [
-        { type: NgModule, args: [{
-                    exports: [BrowserDynamicTestingModule],
-                    imports: [NoopAnimationsModule],
-                    providers: ɵSERVER_RENDER_PROVIDERS
-                },] }
-    ];
+    ServerTestingModule = __decorate([
+        NgModule({
+            exports: [BrowserDynamicTestingModule],
+            imports: [NoopAnimationsModule],
+            providers: ɵSERVER_RENDER_PROVIDERS
+        })
+    ], ServerTestingModule);
     return ServerTestingModule;
 }());
 

915208fd4c076051a8e61df282b4ef75c8e7351a
Andrew Kushnir <akushnir@google.com>
2018-11-12 23:57:23 +0000

    095b6e8 refactor(ivy): abstract all styling-related compiler logic into a shared class (#27043)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+27.sha-8b9249a
+ * @license Angular v7.1.0-beta.2+28.sha-095b6e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1082,7 +1082,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.2+27.sha-8b9249a');
+    var VERSION = new i0.Version('7.1.0-beta.2+28.sha-095b6e8');
 
     /**
      * @license

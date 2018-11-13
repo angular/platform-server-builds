dc4fb5bc5f0aead5a5267563ffd9e73c05ce6eff
Andrew Kushnir <akushnir@google.com>
2018-11-13 22:23:43 +0000

    d97994b fix(ivy): clone ts.SourceFile in ivy_switch when triggered (#27032)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0-beta.2+41.sha-c31e78f
+ * @license Angular v7.1.0-beta.2+42.sha-d97994b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { __decorate, __param, __metadata, __extends, __values } from 'tslib';
+import { __extends, __decorate, __param, __metadata, __values } from 'tslib';
 import { ɵBrowserDomAdapter, ɵsetRootDomAdapter, DOCUMENT, ɵgetDOM, EventManager, ɵNAMESPACE_URIS, ɵSharedStylesHost, ɵflattenStyles, ɵshimContentAttribute, ɵshimHostAttribute, ɵTRANSITION_ID, BrowserModule, EVENT_MANAGER_PLUGINS, TransferState, ɵescapeHtml } from '@angular/platform-browser';
 import { Inject, Injectable, Injector, InjectionToken, Optional, NgZone, ViewEncapsulation, NgModule, PLATFORM_ID, PLATFORM_INITIALIZER, RendererFactory2, Testability, createPlatformFactory, platformCore, ɵALLOW_MULTIPLE_PLATFORMS, APP_ID, ApplicationRef, Version } from '@angular/core';
 import { BrowserXhr, Http, ReadyState, RequestOptions, XHRBackend, XSRFStrategy, HttpModule } from '@angular/http';
@@ -1004,7 +1004,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+41.sha-c31e78f');
+var VERSION = new Version('7.1.0-beta.2+42.sha-d97994b');
 
 /**
  * @license

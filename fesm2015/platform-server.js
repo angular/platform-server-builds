f8e3f9f5b115c435719f8d9c805350bc4611eeb4
Misko Hevery <misko@hevery.com>
2018-11-20 20:08:31 +0000

    e56c8bf fix(ivy): align discovery methods for consistency (#27117)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.1.0-rc.0+28.sha-ca40565.with-local-changes
+ * @license Angular v7.1.0-rc.0+29.sha-e56c8bf.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { ɵBrowserDomAdapter, ɵsetRootDomAdapter, DOCUMENT, ɵgetDOM, EventManager, ɵNAMESPACE_URIS, ɵSharedStylesHost, ɵflattenStyles, ɵshimContentAttribute, ɵshimHostAttribute, ɵTRANSITION_ID, BrowserModule, EVENT_MANAGER_PLUGINS, TransferState, ɵescapeHtml } from '@angular/platform-browser';
-import { Inject, Injectable, APP_ID, NgModule, InjectionToken, Version, ApplicationRef, Optional, Injector, defineInjectable, inject, ɵsetClassMetadata, ɵdefineNgModule, defineInjector, PLATFORM_ID, PLATFORM_INITIALIZER, ɵALLOW_MULTIPLE_PLATFORMS, RendererFactory2, NgZone, Testability, createPlatformFactory, platformCore, ViewEncapsulation } from '@angular/core';
+import { Inject, Injectable, InjectionToken, APP_ID, NgModule, Version, ApplicationRef, Optional, Injector, defineInjectable, inject, ɵsetClassMetadata, ɵdefineNgModule, defineInjector, PLATFORM_ID, PLATFORM_INITIALIZER, ɵALLOW_MULTIPLE_PLATFORMS, RendererFactory2, NgZone, Testability, createPlatformFactory, platformCore, ViewEncapsulation } from '@angular/core';
 import { BrowserXhr, Http, ReadyState, RequestOptions, XHRBackend, XSRFStrategy, HttpModule } from '@angular/http';
 import { HttpHandler, HttpBackend, XhrFactory, ɵHttpInterceptingHandler, HttpClientModule } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
@@ -1646,7 +1646,7 @@ function renderModuleFactory(moduleFactory, options) {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.1.0-rc.0+28.sha-ca40565.with-local-changes');
+const VERSION = new Version('7.1.0-rc.0+29.sha-e56c8bf.with-local-changes');
 
 /**
  * @fileoverview added by tsickle

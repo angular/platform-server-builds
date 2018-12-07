9a855265945a034e0e8d7a5e3e6bcb39a44deed2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 18:03:42 +0000

    2a35471 test(upgrade): add diagnosis to failing ivy tests (#27542)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.2.0-beta.1+9.sha-15ca1bb
+ * @license Angular v7.2.0-beta.1+11.sha-2a35471
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { ɵBrowserDomAdapter, ɵsetRootDomAdapter, DOCUMENT, ɵgetDOM, EventManager, ɵNAMESPACE_URIS, ɵSharedStylesHost, ɵflattenStyles, ɵshimContentAttribute, ɵshimHostAttribute, ɵTRANSITION_ID, BrowserModule, EVENT_MANAGER_PLUGINS, TransferState, ɵescapeHtml } from '@angular/platform-browser';
-import { Inject, Injectable, APP_ID, NgModule, InjectionToken, Version, ApplicationRef, Optional, Injector, defineInjectable, inject, ɵsetClassMetadata, ɵdefineNgModule, defineInjector, PLATFORM_ID, PLATFORM_INITIALIZER, ɵALLOW_MULTIPLE_PLATFORMS, RendererFactory2, NgZone, Testability, createPlatformFactory, platformCore, ViewEncapsulation } from '@angular/core';
+import { Inject, Injectable, InjectionToken, APP_ID, NgModule, Version, ApplicationRef, Optional, Injector, defineInjectable, inject, ɵsetClassMetadata, ɵdefineNgModule, defineInjector, PLATFORM_ID, PLATFORM_INITIALIZER, ɵALLOW_MULTIPLE_PLATFORMS, RendererFactory2, NgZone, Testability, createPlatformFactory, platformCore, ViewEncapsulation } from '@angular/core';
 import { BrowserXhr, Http, ReadyState, RequestOptions, XHRBackend, XSRFStrategy, HttpModule } from '@angular/http';
 import { HttpHandler, HttpBackend, XhrFactory, ɵHttpInterceptingHandler, HttpClientModule } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
@@ -1646,7 +1646,7 @@ function renderModuleFactory(moduleFactory, options) {
 /** *
  * \@publicApi
   @type {?} */
-const VERSION = new Version('7.2.0-beta.1+9.sha-15ca1bb');
+const VERSION = new Version('7.2.0-beta.1+11.sha-2a35471');
 
 /**
  * @fileoverview added by tsickle

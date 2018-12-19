dc1ab5913c90e745c22279a2c164614888ece675
Igor Minar <igor@angular.io>
2018-12-19 18:04:12 +0000

    4d71c4c wip: try to use teams for @angular/animations

---
+++ fesm5/testing.js
@@ -0,0 +1,74 @@
+/**
+ * @license Angular v7.2.0-beta.2+77.sha-4d71c4c
+ * (c) 2010-2018 Google, Inc. https://angular.io/
+ * License: MIT
+ */
+
+import { __decorate } from 'tslib';
+import { NgModule, createPlatformFactory } from '@angular/core';
+import { BrowserDynamicTestingModule, ɵplatformCoreDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ɵINTERNAL_SERVER_PLATFORM_PROVIDERS, ɵSERVER_RENDER_PROVIDERS } from '@angular/platform-server';
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+/**
+ * Platform for testing
+ *
+ * @publicApi
+ */
+var platformServerTesting = createPlatformFactory(ɵplatformCoreDynamicTesting, 'serverTesting', ɵINTERNAL_SERVER_PLATFORM_PROVIDERS);
+/**
+ * NgModule for testing.
+ *
+ * @publicApi
+ */
+var ServerTestingModule = /** @class */ (function () {
+    function ServerTestingModule() {
+    }
+    ServerTestingModule = __decorate([
+        NgModule({
+            exports: [BrowserDynamicTestingModule],
+            imports: [NoopAnimationsModule],
+            providers: ɵSERVER_RENDER_PROVIDERS
+        })
+    ], ServerTestingModule);
+    return ServerTestingModule;
+}());
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+// This file only reexports content of the `src` folder. Keep it that way.
+
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { platformServerTesting, ServerTestingModule };
+//# sourceMappingURL=testing.js.map

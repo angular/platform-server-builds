412312bb7196c60ac88cff792fb25213b9969cf2
Kara Erickson <karakara@google.com>
2019-01-09 01:06:34 +0000

    1c39ad3 feat(ivy): reference external classes by their exported name (#27743)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+47.sha-b621a69
+ * @license Angular v7.2.0+54.sha-1c39ad3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1512,7 +1512,7 @@ function renderModuleFactory(moduleFactory, options) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+47.sha-b621a69');
+const VERSION = new Version('7.2.0+54.sha-1c39ad3');
 
 /**
  * @fileoverview added by tsickle

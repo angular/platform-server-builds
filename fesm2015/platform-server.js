320a1b019fedb79fb0807b1da9a2a6c52470c227
Andrew Kushnir <akushnir@google.com>
2019-01-09 23:52:24 +0000

    e1e4887 refactor(ivy): merge canInsertNativeNode and getParentNative (#28011)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+73.sha-91a8a4f
+ * @license Angular v7.2.0+78.sha-e1e4887
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1673,7 +1673,7 @@ function renderModuleFactory(moduleFactory, options) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.0+73.sha-91a8a4f');
+const VERSION = new Version('7.2.0+78.sha-e1e4887');
 
 /**
  * @fileoverview added by tsickle

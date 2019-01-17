9a3e8ac45f92d9390162721f31d8bb2518a333f5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 22:07:28 +0000

    d8c75f1 docs: fix comments in services example (#26194)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+4.sha-6d40ef2
+ * @license Angular v7.2.1+5.sha-d8c75f1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.1+4.sha-6d40ef2');
+var VERSION = new Version('7.2.1+5.sha-d8c75f1');
 
 /**
  * @license

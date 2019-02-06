14a46cdae66121463d4f6db2a84fb2bf23e44772
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 04:55:31 +0000

    d491a20 fix(router): set href when routerLink is used on an 'area' element (#28441)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4
+ * @license Angular v7.2.4+1.sha-d491a20
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1512,7 +1512,7 @@ function renderModuleFactory(moduleFactory, options) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.4');
+const VERSION = new Version('7.2.4+1.sha-d491a20');
 
 /**
  * @fileoverview added by tsickle

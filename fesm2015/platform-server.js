7100f43a0137aec5285efdba867333238a88ab30
Miško Hevery <misko@hevery.com>
2019-02-12 18:48:39 +0000

    2bf526b docs: add a README for third_party usage (#28651)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+46.sha-8bd7d5b
+ * @license Angular v7.2.4+47.sha-2bf526b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1512,7 +1512,7 @@ function renderModuleFactory(moduleFactory, options) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.4+46.sha-8bd7d5b');
+const VERSION = new Version('7.2.4+47.sha-2bf526b');
 
 /**
  * @fileoverview added by tsickle

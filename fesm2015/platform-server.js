7afb6a54e57193c825dd73e97e809dfb6577d4e3
Miško Hevery <misko@hevery.com>
2019-02-14 19:58:51 +0000

    80a5934 fix(ivy): support schemas at runtime (#28637)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+177.sha-63e5d27
+ * @license Angular v8.0.0-beta.3+179.sha-80a5934
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1673,7 +1673,7 @@ function renderModuleFactory(moduleFactory, options) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.3+177.sha-63e5d27');
+const VERSION = new Version('8.0.0-beta.3+179.sha-80a5934');
 
 /**
  * @fileoverview added by tsickle

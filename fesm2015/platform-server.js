b8cfcd0a4b716d9c3a24caba8521feddd3d473d4
Igor Minar <igor@angular.io>
2019-02-19 21:05:56 +0000

    6788d86 build: fix `validate-commit-message` on Windows (#28780)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+17.sha-af8d58c
+ * @license Angular v8.0.0-beta.4+19.sha-6788d86
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1512,7 +1512,7 @@ function renderModuleFactory(moduleFactory, options) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.4+17.sha-af8d58c');
+const VERSION = new Version('8.0.0-beta.4+19.sha-6788d86');
 
 /**
  * @fileoverview added by tsickle

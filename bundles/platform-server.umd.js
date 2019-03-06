fdffdef3c16ab04250c80a8281a22d2c1cb37e92
George Kalpakas <kalpakas.g@gmail.com>
2019-03-06 23:26:49 +0000

    2fa61e8 build(docs-infra): use pinned dependencies when possible in `ng-packages-installer` (#28510)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.11-d8b63d0
+ * @license Angular v5.2.11-2fa61e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.11-d8b63d0
+ * @license Angular v5.2.11-2fa61e8
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1842,7 +1842,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.11-d8b63d0');
+var VERSION = new _angular_core.Version('5.2.11-2fa61e8');
 
 exports.PlatformState = PlatformState;
 exports.ServerModule = ServerModule;

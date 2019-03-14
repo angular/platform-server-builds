775873559a45260547223500ea1be0c19c2290ad
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 22:40:32 +0000

    da78b02 docs: clarify commands to integrate routing (#26530)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+17.sha-d787925.with-local-changes
+ * @license Angular v7.2.9+18.sha-da78b02.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1512,7 +1512,7 @@ function renderModuleFactory(moduleFactory, options) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('7.2.9+17.sha-d787925.with-local-changes');
+const VERSION = new Version('7.2.9+18.sha-da78b02.with-local-changes');
 
 /**
  * @fileoverview added by tsickle

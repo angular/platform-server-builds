752f32cf289ac317cc2a54414f6448bad0101e32
Miško Hevery <misko@hevery.com>
2019-03-27 20:02:43 +0000

    c981006 ci: lock chrome and firefox versions for saucelabs (#29529)

--- src/http.d.ts
+++ src/http.d.ts
@@ -1,14 +1,33 @@
 import { Injector, Provider } from '@angular/core';
-import { HttpEvent, HttpRequest, HttpBackend, XhrFactory } from '@angular/common/http';
+import { BrowserXhr, Connection, ConnectionBackend, Http, ReadyState, Request, RequestOptions, Response, XHRBackend, XSRFStrategy } from '@angular/http';
+import { HttpEvent, HttpRequest, HttpBackend } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import * as i0 from "@angular/core";
-export declare class ServerXhr implements XhrFactory {
+export declare class ServerXhr implements BrowserXhr {
     build(): XMLHttpRequest;
     static ngInjectableDef: i0.ɵInjectableDef<ServerXhr>;
+}
+export declare class ServerXsrfStrategy implements XSRFStrategy {
+    configureRequest(req: Request): void;
+    static ngInjectableDef: i0.ɵInjectableDef<ServerXsrfStrategy>;
 }
 export declare abstract class ZoneMacroTaskWrapper<S, R> {
     wrap(request: S): Observable<R>;
     protected abstract delegate(request: S): Observable<R>;
+}
+export declare class ZoneMacroTaskConnection extends ZoneMacroTaskWrapper<Request, Response> implements Connection {
+    request: Request;
+    private backend;
+    response: Observable<Response>;
+    lastConnection: Connection;
+    constructor(request: Request, backend: XHRBackend);
+    delegate(request: Request): Observable<Response>;
+    readonly readyState: ReadyState;
+}
+export declare class ZoneMacroTaskBackend implements ConnectionBackend {
+    private backend;
+    constructor(backend: XHRBackend);
+    createConnection(request: any): ZoneMacroTaskConnection;
 }
 export declare class ZoneClientBackend extends ZoneMacroTaskWrapper<HttpRequest<any>, HttpEvent<any>> implements HttpBackend {
     private backend;
@@ -16,5 +35,6 @@ export declare class ZoneClientBackend extends ZoneMacroTaskWrapper<HttpRequest<
     handle(request: HttpRequest<any>): Observable<HttpEvent<any>>;
     protected delegate(request: HttpRequest<any>): Observable<HttpEvent<any>>;
 }
+export declare function httpFactory(xhrBackend: XHRBackend, options: RequestOptions): Http;
 export declare function zoneWrappedInterceptingHandler(backend: HttpBackend, injector: Injector): ZoneClientBackend;
 export declare const SERVER_HTTP_PROVIDERS: Provider[];

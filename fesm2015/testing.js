bf027324c01a5451694d86059727936b7d4b6387
GitHub <noreply@github.com>
2019-04-16 19:27:22 +0000

    ce95d97 Update aio/content/examples/getting-started/src/app/product-alerts/product-alerts.component.1.ts

---
+++ fesm2015/testing.js
@@ -0,0 +1,58 @@
+/**
+ * @license Angular v8.0.0-beta.12+18.sha-ce95d97.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { createPlatformFactory, NgModule } from '@angular/core';
+import { ɵplatformCoreDynamicTesting, BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ɵINTERNAL_SERVER_PLATFORM_PROVIDERS, ɵSERVER_RENDER_PROVIDERS } from '@angular/platform-server';
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+/**
+ * Platform for testing
+ *
+ * \@publicApi
+ * @type {?}
+ */
+const platformServerTesting = createPlatformFactory(ɵplatformCoreDynamicTesting, 'serverTesting', ɵINTERNAL_SERVER_PLATFORM_PROVIDERS);
+/**
+ * NgModule for testing.
+ *
+ * \@publicApi
+ */
+class ServerTestingModule {
+}
+ServerTestingModule.decorators = [
+    { type: NgModule, args: [{
+                exports: [BrowserDynamicTestingModule],
+                imports: [NoopAnimationsModule],
+                providers: ɵSERVER_RENDER_PROVIDERS
+            },] }
+];
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { platformServerTesting, ServerTestingModule };
+//# sourceMappingURL=testing.js.map

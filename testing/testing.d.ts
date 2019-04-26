dcee5520c7fb088edbf93d250af43b0d52f0f67e
Andrew Kushnir <akushnir@google.com>
2019-04-26 01:20:49 +0000

    fcacb2a test: update payload size limit for cli-hello-world app (#30134)

---
+++ testing/testing.d.ts
@@ -0,0 +1,25 @@
+/**
+ * @license Angular v8.0.0-rc.0+2.sha-fcacb2a.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+
+/**
+ * Platform for testing
+ *
+ * @publicApi
+ */
+export declare const platformServerTesting: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * NgModule for testing.
+ *
+ * @publicApi
+ */
+export declare class ServerTestingModule {
+}
+
+export { }

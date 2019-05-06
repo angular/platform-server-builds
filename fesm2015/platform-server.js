295dd6dd09b6443ae180e4d82536d3e7fb42a74b
Kara Erickson <karakara@google.com>
2019-05-06 16:53:53 +0000

    60a8888 fix(compiler-cli): log ngcc skipping messages as debug instead of info (#30232)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+85.sha-39f2c9f.with-local-changes
+ * @license Angular v8.0.0-rc.0+84.sha-60a8888.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1677,7 +1677,7 @@ function renderModuleFactory(moduleFactory, options) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+85.sha-39f2c9f.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+84.sha-60a8888.with-local-changes');
 
 /**
  * @fileoverview added by tsickle

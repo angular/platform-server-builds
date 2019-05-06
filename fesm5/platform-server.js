9b6af36a718eea50715365cd7fc7da582cfaac57
Kara Erickson <karakara@google.com>
2019-05-06 16:52:09 +0000

    fed07c7 docs(forms): fix ControlValueAccessor registerOnChange code example (#30276)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.2+17.sha-390cac6.with-local-changes
+ * @license Angular v8.0.0-rc.2+18.sha-fed07c7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -975,7 +975,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.2+17.sha-390cac6.with-local-changes');
+var VERSION = new Version('8.0.0-rc.2+18.sha-fed07c7.with-local-changes');
 
 /**
  * @license

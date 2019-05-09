5ac23d62c42cbe55131e2e7e4fed3be1af6636dc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 20:13:51 +0000

    8cec8f5 test(core): update core_all size-tracking golden file (#30257)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+123.sha-3aff79c.with-local-changes
+ * @license Angular v8.0.0-rc.0+134.sha-8cec8f5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1677,7 +1677,7 @@ function renderModuleFactory(moduleFactory, options) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+123.sha-3aff79c.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+134.sha-8cec8f5.with-local-changes');
 
 /**
  * @fileoverview added by tsickle

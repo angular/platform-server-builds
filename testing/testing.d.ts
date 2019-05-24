a170c6f34619528715c05329c14e273c35acfc01
GitHub <noreply@github.com>
2019-05-24 10:40:45 +0000

    05479f8 chore: static-query migration should use permalink for migration guide

---
+++ testing/testing.d.ts
@@ -0,0 +1,25 @@
+/**
+ * @license Angular v8.0.0-rc.0+336.sha-05479f8.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+
+/**
+ * Platform for testing
+ *
+ * @publicApi
+ */
+export declare const platformServerTesting: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * NgModule for testing.
+ *
+ * @publicApi
+ */
+export declare class ServerTestingModule {
+}
+
+export { }

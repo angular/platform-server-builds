1077ff7e34c82500fb2a867dcbec9885e1a25bb6
JiaLiPassion <JiaLi.Passion@gmail.com>
2019-06-09 14:40:47 +0000

    99331c0 test: remove rsync

---
+++ testing/testing.d.ts
@@ -0,0 +1,25 @@
+/**
+ * @license Angular v8.1.0-beta.0+32.sha-99331c0.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+
+/**
+ * Platform for testing
+ *
+ * @publicApi
+ */
+export declare const platformServerTesting: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * NgModule for testing.
+ *
+ * @publicApi
+ */
+export declare class ServerTestingModule {
+}
+
+export { }

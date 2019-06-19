9f90609302b5658fe732ceb4cbb90b062b6ce09a
Andrew Kushnir <akushnir@google.com>
2019-06-19 16:09:54 +0000

    f1c08c691 docs: add core and cli version alignment note (#30976) docs: add core and cli version alignment note PR Close #30976

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+35.sha-bf9de8c.with-local-changes
+ * @license Angular v8.0.1+36.sha-f1c08c6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -975,7 +975,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.1+35.sha-bf9de8c.with-local-changes');
+var VERSION = new Version('8.0.1+36.sha-f1c08c6.with-local-changes');
 
 /**
  * @license

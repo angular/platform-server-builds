e2005033e9b668fbb62973beb37fb6c9e3183bad
Miško Hevery <misko@hevery.com>
2019-07-26 17:40:31 +0000

    7a62530ed release: cut the v8.2.0-rc.0 release

---
+++ testing/testing.d.ts
@@ -0,0 +1,25 @@
+/**
+ * @license Angular v8.2.0-next.2+95.sha-7a62530.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+
+/**
+ * Platform for testing
+ *
+ * @publicApi
+ */
+export declare const platformServerTesting: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * NgModule for testing.
+ *
+ * @publicApi
+ */
+export declare class ServerTestingModule {
+}
+
+export { }

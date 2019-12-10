c732b9faa887f5c6b42cb0396ea5b1b735172c2e
Miško Hevery <misko@hevery.com>
2019-12-10 22:06:01 +0000

    ed486b53f8 perf(ivy): styling algorithm benchmark

---
+++ testing/testing.d.ts
@@ -0,0 +1,25 @@
+/**
+ * @license Angular v9.0.0-rc.1+420.sha-ed486b5.with-local-changes
+ * (c) 2010-2019 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+
+/**
+ * Platform for testing
+ *
+ * @publicApi
+ */
+export declare const platformServerTesting: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * NgModule for testing.
+ *
+ * @publicApi
+ */
+export declare class ServerTestingModule {
+}
+
+export { }

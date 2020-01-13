5448e4a2dc18d8fca07a04cc2506dc6fd6ea9ba0
atscott <atscott@google.com>
2020-01-13 15:46:35 +0000

    55037d1107 build: bump year (#34651)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,6 +1,6 @@
 /**
- * @license Angular v9.0.0-rc.1+632.sha-6d534f1
- * (c) 2010-2019 Google LLC. https://angular.io/
+ * @license Angular v9.0.0-rc.1+640.sha-55037d1
+ * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
 
@@ -1775,7 +1775,7 @@ function renderModuleFactory(moduleFactory, options) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+632.sha-6d534f1');
+const VERSION = new Version('9.0.0-rc.1+640.sha-55037d1');
 
 /**
  * @fileoverview added by tsickle

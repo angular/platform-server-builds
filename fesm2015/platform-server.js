31bde00566604b4fe9c8f94cdb687ec50303f267
Andrew Kushnir <akushnir@google.com>
2020-01-30 01:01:39 +0000

    36b8c0320b refactor(ivy): clean of  #34804 from previous merge (#35022)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+61.sha-19c4895
+ * @license Angular v9.0.0-rc.11+62.sha-36b8c03
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1627,7 +1627,7 @@ function renderModuleFactory(moduleFactory, options) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.11+61.sha-19c4895');
+const VERSION = new Version('9.0.0-rc.11+62.sha-36b8c03');
 
 /**
  * @fileoverview added by tsickle

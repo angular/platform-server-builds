a6816e83455f949e4f376fe1d333ad926c1133f8
Andrew Kushnir <akushnir@google.com>
2020-01-30 00:02:46 +0000

    19c489524f feat(ivy): Change static priority resolution to be same level as directive it belongs to (#34938)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+59.sha-0390d20
+ * @license Angular v9.0.0-rc.11+61.sha-19c4895
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -939,7 +939,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.0.0-rc.11+59.sha-0390d20');
+var VERSION = new Version('9.0.0-rc.11+61.sha-19c4895');
 
 /**
  * @license

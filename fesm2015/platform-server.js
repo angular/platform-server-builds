a98d70407e7807f2d71bc925677e5ec9ccfa7c31
Kara Erickson <karakara@google.com>
2020-02-11 00:54:58 +0000

    a92d97cda7 fix(compiler): report errors for missing binding names (#34595)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+981.sha-e1160f1
+ * @license Angular v9.0.0-rc.1+982.sha-a92d97c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1633,7 +1633,7 @@ function renderModuleFactory(moduleFactory, options) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+981.sha-e1160f1');
+const VERSION = new Version('9.0.0-rc.1+982.sha-a92d97c');
 
 /**
  * @fileoverview added by tsickle

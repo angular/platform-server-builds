f8948e2d58765fc555fe789f66fb67f04bdc50fc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 19:38:20 +0000

    ce85cbf2d3 Revert "feat(ngcc): pause async ngcc processing if another process has the lockfile (#35131)"

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.1+43.sha-c305b5c
+ * @license Angular v9.0.1+44.sha-ce85cbf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1633,7 +1633,7 @@ function renderModuleFactory(moduleFactory, options) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.1+43.sha-c305b5c');
+const VERSION = new Version('9.0.1+44.sha-ce85cbf');
 
 /**
  * @fileoverview added by tsickle

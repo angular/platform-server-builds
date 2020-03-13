794296ee8ada7d7674a7726deecad83c4d767d3b
Andrew Kushnir <akushnir@google.com>
2020-03-13 19:57:39 +0000

    a24eddbcfd test(core): Update transplanted views benchpress test to support VE (#36058)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+37.sha-9e698b7
+ * @license Angular v9.0.6+38.sha-a24eddb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1633,7 +1633,7 @@ function renderModuleFactory(moduleFactory, options) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.6+37.sha-9e698b7');
+const VERSION = new Version('9.0.6+38.sha-a24eddb');
 
 /**
  * @fileoverview added by tsickle

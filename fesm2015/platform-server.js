54fd992bbffe6a3879582474ef9bbfee71c9a5a9
Andrew Kushnir <akushnir@google.com>
2020-03-17 00:07:02 +0000

    ec750caff0 refactor(core): misc refactoring to support upcoming changes to improve synthetic host bindings (#35884)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+41.sha-a52b5680
+ * @license Angular v9.0.6+42.sha-ec750ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1633,7 +1633,7 @@ function renderModuleFactory(moduleFactory, options) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('9.0.6+41.sha-a52b5680');
+const VERSION = new Version('9.0.6+42.sha-ec750ca');
 
 /**
  * @fileoverview added by tsickle

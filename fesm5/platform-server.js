dc7213130897ed3263f93ae935cf3534a59b70a3
atscott <atscott@google.com>
2020-04-08 21:53:15 +0000

    717df13207 fix(ngcc): do not warn if `paths` mapping does not exist (#36525)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1
+ * @license Angular v10.0.0-next.1+1.sha-717df13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -945,7 +945,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-var VERSION = new Version('10.0.0-next.1');
+var VERSION = new Version('10.0.0-next.1+1.sha-717df13');
 
 /**
  * @license

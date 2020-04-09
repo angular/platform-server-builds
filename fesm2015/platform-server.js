45d1f394f2dc633c44db139a436fb0c062906027
atscott <atscott@google.com>
2020-04-09 19:07:19 +0000

    4aa4e6fd03 fix(compiler): handle type references to namespaced symbols correctly (#36106)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+17.sha-3bedfda
+ * @license Angular v10.0.0-next.1+14.sha-4aa4e6f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1633,7 +1633,7 @@ function renderModuleFactory(moduleFactory, options) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+17.sha-3bedfda');
+const VERSION = new Version('10.0.0-next.1+14.sha-4aa4e6f');
 
 /**
  * @fileoverview added by tsickle

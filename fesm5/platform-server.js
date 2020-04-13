4c588910cb5905bc9b9fb889df559bde1dbc70b9
atscott <atscott@google.com>
2020-04-13 15:40:33 +0000

    fd7698253e docs: move ng-conf 2020 to the already presented section (#36413)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+29.sha-3992341
+ * @license Angular v9.1.1+34.sha-fd76982
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -945,7 +945,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.1+29.sha-3992341');
+var VERSION = new Version('9.1.1+34.sha-fd76982');
 
 /**
  * @license

83067b32b52305993e0a2ad33b14a0ab85b3047d
atscott <atscott@google.com>
2020-04-14 00:56:01 +0000

    c8f2ca2349 docs: edit to setup-local (#36168)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+34.sha-fd76982
+ * @license Angular v9.1.1+36.sha-c8f2ca2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -945,7 +945,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.1+34.sha-fd76982');
+var VERSION = new Version('9.1.1+36.sha-c8f2ca2');
 
 /**
  * @license

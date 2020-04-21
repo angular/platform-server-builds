717978b8100e56088ac1da3fa02b1188702639fb
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:24:12 +0000

    0bd50e2e50 fix(core): missing-injectable migration should not migrate `@NgModule` classes (#36369)

--- fesm5/platform-server.js
+++ fesm5/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+47.sha-254b9ea
+ * @license Angular v9.1.2+51.sha-0bd50e2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -998,7 +998,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-var VERSION = new Version('9.1.2+47.sha-254b9ea');
+var VERSION = new Version('9.1.2+51.sha-0bd50e2');
 
 /**
  * @license

570301211e9662f0ab2ff65faf3cced7f6329af8
Matias Niemelä <matias@yearofmoo.com>
2020-06-02 18:15:50 +0000

    1b55da10b1 build: fix integration payload sizes

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+39.sha-c3651ce
+ * @license Angular v10.0.0-rc.0+44.sha-1b55da1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+39.sha-c3651ce');
+const VERSION = new Version('10.0.0-rc.0+44.sha-1b55da1');
 
 /**
  * @license

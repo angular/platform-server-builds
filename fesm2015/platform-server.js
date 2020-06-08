188d9dd066d5fbcc965b9f118d282547b7ed44fa
atscott <atscott@google.com>
2020-06-08 18:47:52 +0000

    779344012a refactor(core): assert TNode is not a container when setting attribute on element (#37111)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+92.sha-650974e
+ * @license Angular v10.0.0-rc.0+93.sha-7793440
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+92.sha-650974e');
+const VERSION = new Version('10.0.0-rc.0+93.sha-7793440');
 
 /**
  * @license

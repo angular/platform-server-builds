83e8f3d3fc875d2a6091cbccabdb21f16e420d3c
atscott <atscott@google.com>
2020-06-09 00:44:32 +0000

    bf2cb6fa48 feat(language-service): TS references from template items (#37437)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+71.sha-a1616ce
+ * @license Angular v10.0.0-rc.3+3.sha-bf2cb6f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.2+71.sha-a1616ce');
+const VERSION = new Version('10.0.0-rc.3+3.sha-bf2cb6f');
 
 /**
  * @license

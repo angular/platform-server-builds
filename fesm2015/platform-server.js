e1f7c001cc71e3043cd23f44162b3abd38d685d4
atscott <atscott@google.com>
2020-06-09 15:48:00 +0000

    a3313709ea fix(dev-infra): local changes check not working (#37489)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+104.sha-7301e70
+ * @license Angular v10.0.0-rc.0+107.sha-a331370
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1002,7 +1002,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+104.sha-7301e70');
+const VERSION = new Version('10.0.0-rc.0+107.sha-a331370');
 
 /**
  * @license

c60a142fa910f97cfb62653d66abc2acd3d80a29
Misko Hevery <misko@hevery.com>
2020-06-12 22:26:43 +0000

    1bc807c81a style(common): enforce format on newly included files (#36940)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+161.sha-8dcf618
+ * @license Angular v10.0.0-rc.0+164.sha-1bc807c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -973,7 +973,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+161.sha-8dcf618');
+const VERSION = new Version('10.0.0-rc.0+164.sha-1bc807c');
 
 /**
  * @license

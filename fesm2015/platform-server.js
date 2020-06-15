8947e4e254f90801044f44d8eb4c40909957c98c
Misko Hevery <misko@hevery.com>
2020-06-15 21:52:41 +0000

    2a145f2463 perf(forms): optimize internal method _anyControls in FormGroup (#32534)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+184.sha-e0dfa42
+ * @license Angular v10.0.0-rc.0+185.sha-2a145f2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -973,7 +973,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+184.sha-e0dfa42');
+const VERSION = new Version('10.0.0-rc.0+185.sha-2a145f2');
 
 /**
  * @license

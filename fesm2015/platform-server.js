6da78485628f6c3c68d6d652009fb8f994e7901e
Misko Hevery <misko@hevery.com>
2020-06-24 23:24:38 +0000

    96891a076f release: sort the v10.0.0 release in `CHANGELOG.md`

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+23.sha-c49507b
+ * @license Angular v10.0.0+2.sha-96891a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -958,7 +958,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.6+23.sha-c49507b');
+const VERSION = new Version('10.0.0+2.sha-96891a0');
 
 /**
  * @license

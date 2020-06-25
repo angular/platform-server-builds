f19a12d57025a8b2b2975f69b91b8bb77ffff705
Andrew Kushnir <akushnir@google.com>
2020-06-25 18:58:48 +0000

    bedc0451a0 docs(ngcc): add additional next steps to an error (#37672)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+228.sha-4481beb
+ * @license Angular v10.0.0-rc.0+231.sha-bedc045
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -973,7 +973,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+228.sha-4481beb');
+const VERSION = new Version('10.0.0-rc.0+231.sha-bedc045');
 
 /**
  * @license

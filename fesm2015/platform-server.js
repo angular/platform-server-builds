150f84e6de9c50b81e9da2e8a7cf8c03bf69efad
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:22:44 +0000

    dbc2364d16 fix(dev-infra): support running scripts from within a detached head (#37737)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+275.sha-c942662
+ * @license Angular v10.0.0-rc.0+271.sha-dbc2364
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -975,7 +975,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+275.sha-c942662');
+const VERSION = new Version('10.0.0-rc.0+271.sha-dbc2364');
 
 /**
  * @license

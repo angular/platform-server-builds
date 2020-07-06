ce382e7dd1f29cbab81c2ca3bbde6553df3eb7b2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 21:04:11 +0000

    a5c30733cb docs: mention for depreciation for `Testbed.get()` (#37815)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+313.sha-a5ffca0
+ * @license Angular v10.0.0-rc.0+315.sha-a5c3073
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -975,7 +975,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.0-rc.0+313.sha-a5ffca0');
+const VERSION = new Version('10.0.0-rc.0+315.sha-a5c3073');
 
 /**
  * @license

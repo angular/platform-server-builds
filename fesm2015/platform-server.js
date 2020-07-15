77c60ab974213be98c1e9f8aeb6ae5b323a58810
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:11:34 +0000

    17ddab98fb fix(core): incorrectly validating properties on ng-content and ng-container (#37773)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4
+ * @license Angular v10.0.4+9.sha-17ddab9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -958,7 +958,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.4');
+const VERSION = new Version('10.0.4+9.sha-17ddab9');
 
 /**
  * @license

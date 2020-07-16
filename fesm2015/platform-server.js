4087dcf92584831e14b4f28b11c4cdbbde598cdc
Andrew Kushnir <akushnir@google.com>
2020-07-16 21:27:37 +0000

    6c9401c338 docs: update router api documentation (#37980)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+23.sha-e382632
+ * @license Angular v10.1.0-next.1+20.sha-6c9401c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1011,7 +1011,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+23.sha-e382632');
+const VERSION = new Version('10.1.0-next.1+20.sha-6c9401c');
 
 /**
  * @license

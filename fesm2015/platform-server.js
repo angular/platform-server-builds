61d17e86333c5ce5175d2e29a2c2c84f8ea82668
Andrew Kushnir <akushnir@google.com>
2020-07-17 19:11:29 +0000

    fd45f7485d docs(docs-infra): reformat redundant sentence (#38109)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+24.sha-58f6079
+ * @license Angular v10.1.0-next.1+25.sha-fd45f74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1011,7 +1011,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.1+24.sha-58f6079');
+const VERSION = new Version('10.1.0-next.1+25.sha-fd45f74');
 
 /**
  * @license

9b071b0f76b3a5e0bf2c57467f8fa99bffb68f1a
Andrew Kushnir <akushnir@google.com>
2020-07-21 00:33:07 +0000

    4664acce50 docs: add routing terms to glossary (#38053)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+34.sha-3797861
+ * @license Angular v10.0.4+35.sha-4664acc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -958,7 +958,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.4+34.sha-3797861');
+const VERSION = new Version('10.0.4+35.sha-4664acc');
 
 /**
  * @license

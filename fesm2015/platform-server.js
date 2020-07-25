9b0478c7a4f52282848f8225162174cee1e93bf3
Misko Hevery <misko@hevery.com>
2020-07-25 01:23:28 +0000

    67e3ecc7e3 fix(dev-infra): Ensure conditions with groups do not fail verification (#37798)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+27.sha-6a3e68b
+ * @license Angular v10.1.0-next.2+30.sha-67e3ecc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1011,7 +1011,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+27.sha-6a3e68b');
+const VERSION = new Version('10.1.0-next.2+30.sha-67e3ecc');
 
 /**
  * @license

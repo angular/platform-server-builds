f110965b9584ea406e3b77fc24ed1daf509ed7f0
Misko Hevery <misko@hevery.com>
2020-07-27 22:49:28 +0000

    b1e7775a8a fix(compiler-cli): Add support for string literal class members (#38226)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+32.sha-776067c
+ * @license Angular v10.0.5+38.sha-b1e7775
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -958,7 +958,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.5+32.sha-776067c');
+const VERSION = new Version('10.0.5+38.sha-b1e7775');
 
 /**
  * @license

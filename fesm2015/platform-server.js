fc23be3bbd060be3735f582781ac2ec1a2046b45
Misko Hevery <misko@hevery.com>
2020-07-28 23:03:57 +0000

    57575e379f release: cut the v10.1.0-next.3 release

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+59.sha-28c3e2d
+ * @license Angular v10.1.0-next.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1011,7 +1011,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.2+59.sha-28c3e2d');
+const VERSION = new Version('10.1.0-next.3');
 
 /**
  * @license

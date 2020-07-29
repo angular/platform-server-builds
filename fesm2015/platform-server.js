47305032878389ed6fcd92e9783f1d2db9fb9b6c
Misko Hevery <misko@hevery.com>
2020-07-29 17:53:40 +0000

    9c8bc4a239 fix(common): narrow `NgIf` context variables in template type checker (#36627)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+1.sha-cf4da82
+ * @license Angular v10.1.0-next.3+2.sha-9c8bc4a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -975,7 +975,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.3+1.sha-cf4da82');
+const VERSION = new Version('10.1.0-next.3+2.sha-9c8bc4a');
 
 /**
  * @license

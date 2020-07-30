8f133e830e4c87cd677ededf68b7ddd3a42fca34
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 01:26:57 +0000

    4a6abbdfc6 fix(compiler-cli): correct type of makeDiagnostic()

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6+1.sha-0469d92
+ * @license Angular v10.0.6+19.sha-4a6abbd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -958,7 +958,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.6+1.sha-0469d92');
+const VERSION = new Version('10.0.6+19.sha-4a6abbd');
 
 /**
  * @license

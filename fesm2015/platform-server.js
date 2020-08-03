3f2a1ae13dee0dbf8cda16d5373cda3ea111d55a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-03 20:24:33 +0000

    8fbf40bf40 feat(core): update reference and doc to change `async` to `waitAsync`. (#37583)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+40.sha-87baa06
+ * @license Angular v10.1.0-next.3+43.sha-8fbf40b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1011,7 +1011,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.3+40.sha-87baa06');
+const VERSION = new Version('10.1.0-next.3+43.sha-8fbf40b');
 
 /**
  * @license

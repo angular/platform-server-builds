c9b25f5d3fb48518470aecbb68722b54dba4822f
Andrew Kushnir <akushnir@google.com>
2020-08-05 18:12:13 +0000

    585ef2bdd7 docs: fix typo in glossary (#38318)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4
+ * @license Angular v10.1.0-next.4+1.sha-585ef2b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -975,7 +975,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.4');
+const VERSION = new Version('10.1.0-next.4+1.sha-585ef2b');
 
 /**
  * @license

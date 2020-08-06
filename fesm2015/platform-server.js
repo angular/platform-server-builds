9a6331691da9cd89be5330f2100d22ddc55d22c5
Andrew Kushnir <akushnir@google.com>
2020-08-06 01:15:03 +0000

    58f4b3ad80 fix(common): ensure scrollRestoration is writable (#30630) (#38357)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+8.sha-db897f4
+ * @license Angular v10.0.8+10.sha-58f4b3a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -958,7 +958,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+8.sha-db897f4');
+const VERSION = new Version('10.0.8+10.sha-58f4b3a');
 
 /**
  * @license

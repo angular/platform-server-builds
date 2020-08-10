96ee9ca0f06edbd54fe68465a3eb099f27485252
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:04:38 +0000

    ee5123f5dc fix(core): Store the currently selected ICU in `LView` (#38345)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+26.sha-3dfaf56
+ * @license Angular v10.0.8+27.sha-ee5123f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -958,7 +958,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.8+26.sha-3dfaf56');
+const VERSION = new Version('10.0.8+27.sha-ee5123f');
 
 /**
  * @license

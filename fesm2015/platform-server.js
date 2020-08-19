93a60f608f55c22d8d1db9e5e44679729eb1f528
atscott <atscott01@gmail.com>
2020-08-19 16:41:18 +0000

    0270020ac2 docs: release notes for the v10.0.11 release

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+10.sha-55fd725
+ * @license Angular v10.1.0-next.6+12.sha-0270020
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.6+10.sha-55fd725');
+const VERSION = new Version('10.1.0-next.6+12.sha-0270020');
 
 /**
  * @license

99746c09301fc423fcb655d5987e1ca907ea5eef
Misko Hevery <misko@hevery.com>
2020-08-21 19:43:23 +0000

    2a643e1ab6 docs: change function name from async -> waitForAsync (#38548)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+14.sha-956b25a
+ * @license Angular v10.1.0-next.7+16.sha-2a643e1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.7+14.sha-956b25a');
+const VERSION = new Version('10.1.0-next.7+16.sha-2a643e1');
 
 /**
  * @license

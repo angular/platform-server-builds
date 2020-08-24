dd67edda051447da11f724d21328c5b416ac5745
Misko Hevery <misko@hevery.com>
2020-08-24 20:39:32 +0000

    375f0a6f67 build: update tslint to 6.1.3 (#38076)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+19.sha-b48cc6e
+ * @license Angular v10.1.0-next.7+25.sha-375f0a6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.7+19.sha-b48cc6e');
+const VERSION = new Version('10.1.0-next.7+25.sha-375f0a6');
 
 /**
  * @license

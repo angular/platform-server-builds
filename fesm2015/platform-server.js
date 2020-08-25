ed82f44eb749f2305e4db9c073f9f90054af74e7
Misko Hevery <misko@hevery.com>
2020-08-25 22:37:10 +0000

    14e90bef58 fix(localize): render text of extracted placeholders (#38536)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+31.sha-94a3e0e
+ * @license Angular v10.1.0-next.8+10.sha-14e90be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.0-next.7+31.sha-94a3e0e');
+const VERSION = new Version('10.1.0-next.8+10.sha-14e90be');
 
 /**
  * @license

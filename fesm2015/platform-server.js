64cc7e96d6441d4a71ecbdedf252e6fdc2b36da7
Misko Hevery <misko@hevery.com>
2020-08-25 17:17:49 +0000

    d06636848e docs: add correct path for karma.conf.js file (#38571)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.12
+ * @license Angular v10.0.12+3.sha-d066368
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -963,7 +963,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.0.12');
+const VERSION = new Version('10.0.12+3.sha-d066368');
 
 /**
  * @license

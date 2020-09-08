7d1f00c4447388e302f14459dfa8dcf8cccb3832
atscott <atscott01@gmail.com>
2020-09-08 17:45:43 +0000

    926ffcd8ac fix(router): support lazy loading for empty path named outlets (#38379)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+21.sha-261f689
+ * @license Angular v11.0.0-next.0+25.sha-926ffcd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.0+21.sha-261f689');
+const VERSION = new Version('11.0.0-next.0+25.sha-926ffcd');
 
 /**
  * @license

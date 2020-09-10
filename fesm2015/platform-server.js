5a17dd2e6dd6aa11f6ed18f476e63984f81d0419
Andrew Kushnir <akushnir@google.com>
2020-09-10 20:10:38 +0000

    19598b47ca feat(compiler-cli): add ability to get symbol of reference or variable (#38618)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+13.sha-26f2820
+ * @license Angular v11.0.0-next.1+19.sha-19598b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+13.sha-26f2820');
+const VERSION = new Version('11.0.0-next.1+19.sha-19598b4');
 
 /**
  * @license

b4d03358236a31f130dc5438e00c0adb16a17d4c
Andrew Kushnir <akushnir@google.com>
2020-09-14 22:10:42 +0000

    78e1ecb161 feat(dev-infra): allow local ng-dev configuration to error on invalid commit messages (#38784)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+35.sha-3934f0a
+ * @license Angular v11.0.0-next.1+36.sha-78e1ecb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+35.sha-3934f0a');
+const VERSION = new Version('11.0.0-next.1+36.sha-78e1ecb');
 
 /**
  * @license

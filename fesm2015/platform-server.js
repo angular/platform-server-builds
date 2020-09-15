817ce8f230114e3997402e5cd973b2c5b9ba40f5
Andrew Kushnir <akushnir@google.com>
2020-09-15 23:30:11 +0000

    d192c87f6a refactor(dev-infra): refactor commit-message files (#38845)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+45.sha-3817e5f
+ * @license Angular v11.0.0-next.1+46.sha-d192c87
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.1+45.sha-3817e5f');
+const VERSION = new Version('11.0.0-next.1+46.sha-d192c87');
 
 /**
  * @license

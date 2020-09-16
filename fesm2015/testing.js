1ad098bafcb8f1a3b00045dd4ffcbf77e75b62c8
GitHub <noreply@github.com>
2020-09-16 19:46:14 +0000

    956e4f2bab docs: add upcoming angular conferences

---
+++ fesm2015/testing.js
@@ -0,0 +1,70 @@
+/**
+ * @license Angular v11.0.0-next.1+47.sha-956e4f2
+ * (c) 2010-2020 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { createPlatformFactory, NgModule } from '@angular/core';
+import { ɵplatformCoreDynamicTesting, BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ɵINTERNAL_SERVER_PLATFORM_PROVIDERS, ɵSERVER_RENDER_PROVIDERS } from '@angular/platform-server';
+
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+/**
+ * Platform for testing
+ *
+ * @publicApi
+ */
+const platformServerTesting = createPlatformFactory(ɵplatformCoreDynamicTesting, 'serverTesting', ɵINTERNAL_SERVER_PLATFORM_PROVIDERS);
+/**
+ * NgModule for testing.
+ *
+ * @publicApi
+ */
+class ServerTestingModule {
+}
+ServerTestingModule.decorators = [
+    { type: NgModule, args: [{
+                exports: [BrowserDynamicTestingModule],
+                imports: [NoopAnimationsModule],
+                providers: ɵSERVER_RENDER_PROVIDERS
+            },] }
+];
+
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+// This file only reexports content of the `src` folder. Keep it that way.
+
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { ServerTestingModule, platformServerTesting };
+//# sourceMappingURL=testing.js.map

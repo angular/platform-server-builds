f286e95f05d4c68448bd1617f5b5438880cd79ce
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:53:46 +0000

    dd8d8c8289 fix(common): add `params` and `reportProgress` options to `HttpClient.put()` overload (#37873)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+11.sha-722699f
+ * @license Angular v11.0.0-next.2+13.sha-dd8d8c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+11.sha-722699f');
+const VERSION = new Version('11.0.0-next.2+13.sha-dd8d8c8');
 
 /**
  * @license

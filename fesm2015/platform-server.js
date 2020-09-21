f2d164e3e46d80b74f48b1859e471ec1b0f6e2a6
Misko Hevery <misko@hevery.com>
2020-09-21 20:00:33 +0000

    297b123151 refactor(compiler-cli): make the output AST translator generic (#38775)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+40.sha-6ae3b68
+ * @license Angular v11.0.0-next.2+47.sha-297b123
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.2+40.sha-6ae3b68');
+const VERSION = new Version('11.0.0-next.2+47.sha-297b123');
 
 /**
  * @license

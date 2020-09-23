aa1fb16caba69a2f710a5d6a06d45b4edf3c86f7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-23 19:42:17 +0000

    73c7882629 release: cut the v10.1.3 release

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.2+33.sha-32e32e5
+ * @license Angular v10.1.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -980,7 +980,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.2+33.sha-32e32e5');
+const VERSION = new Version('10.1.3');
 
 /**
  * @license

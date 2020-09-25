f49c11e5fc7a572f44c8c0e8d8589258fca73de6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:57:03 +0000

    cf9176eb46 docs(docs-infra): remove usage of whitelist in aio-builds-setup docs (#38964)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+18.sha-2f6777d
+ * @license Angular v11.0.0-next.3+19.sha-cf9176e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+18.sha-2f6777d');
+const VERSION = new Version('11.0.0-next.3+19.sha-cf9176e');
 
 /**
  * @license

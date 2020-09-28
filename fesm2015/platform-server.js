6491e153c7e17c94c65844da485735dc7fc9d180
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:42:42 +0000

    f96dcc5ce0 feat(dev-infra): tool for staging and publishing releases (#38656)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+29.sha-4744c22
+ * @license Angular v11.0.0-next.3+46.sha-f96dcc5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+29.sha-4744c22');
+const VERSION = new Version('11.0.0-next.3+46.sha-f96dcc5');
 
 /**
  * @license

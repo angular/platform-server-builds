e9f3743d8383706f26b03c0be1551d32fd7ce032
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 21:02:47 +0000

    e2997ed5ee build(docs-infra): update TypeScript and other deps to align with latest CLI (#39017)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+72.sha-06525cf
+ * @license Angular v11.0.0-next.3+76.sha-e2997ed
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+72.sha-06525cf');
+const VERSION = new Version('11.0.0-next.3+76.sha-e2997ed');
 
 /**
  * @license

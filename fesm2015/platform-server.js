6b5868efea3e7aaf53e8482dd291f31056a07c02
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:44:38 +0000

    c8958d76db build(docs-infra): simplify updating dependencies in docs examples boilerplate (#38992)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+81.sha-b16a69c
+ * @license Angular v11.0.0-next.3+86.sha-c8958d7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+81.sha-b16a69c');
+const VERSION = new Version('11.0.0-next.3+86.sha-c8958d7');
 
 /**
  * @license

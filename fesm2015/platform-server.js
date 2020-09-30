7619757600927a28809c65e653be41aa2e820469
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:54:31 +0000

    8f11b516f8 refactor(compiler-cli): API for getting components from a template file (#39002)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+90.sha-393ce55
+ * @license Angular v11.0.0-next.3+87.sha-8f11b51
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+90.sha-393ce55');
+const VERSION = new Version('11.0.0-next.3+87.sha-8f11b51');
 
 /**
  * @license

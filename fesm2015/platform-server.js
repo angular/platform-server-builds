cd565deee85fcbd4fcaf933ffd1f83da5c89d3f0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 14:00:46 +0000

    827245fdb6 docs(core): fix typo (#39041)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+91.sha-8b7acc4
+ * @license Angular v11.0.0-next.3+92.sha-827245f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.3+91.sha-8b7acc4');
+const VERSION = new Version('11.0.0-next.3+92.sha-827245f');
 
 /**
  * @license

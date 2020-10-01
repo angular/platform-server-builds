004ff00400d46960010bc48d91d59c9e7455eed2
Joey Perrott <josephperrott@gmail.com>
2020-10-01 22:53:21 +0000

    f2341e2372 ci: update g3 synced file list (#39084)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+26.sha-4bce213
+ * @license Angular v11.0.0-next.4+28.sha-f2341e2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+26.sha-4bce213');
+const VERSION = new Version('11.0.0-next.4+28.sha-f2341e2');
 
 /**
  * @license

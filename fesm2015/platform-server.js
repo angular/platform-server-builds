d5646fe5cf1ac2e7a0e997f7c78e240d34050c7f
Joey Perrott <josephperrott@gmail.com>
2020-10-06 16:03:39 +0000

    07a66fc4c2 docs: remove explicit boolean type in examples as TS infers it automatically (#39081)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.4+52.sha-6e0a8eb
+ * @license Angular v11.0.0-next.4+53.sha-07a66fc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1016,7 +1016,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.4+52.sha-6e0a8eb');
+const VERSION = new Version('11.0.0-next.4+53.sha-07a66fc');
 
 /**
  * @license

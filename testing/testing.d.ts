cbded7b2c95dacd1be3800e6549242f81835241a
Joey Perrott <josephperrott@gmail.com>
2020-10-08 16:35:15 +0000

    b635f25da5 build: upgrade angular build, integration/bazel and @angular/bazel package to rule_nodejs 2.2.0 (#37727)

---
+++ testing/testing.d.ts
@@ -0,0 +1,25 @@
+/**
+ * @license Angular v11.0.0-next.5+3.sha-b635f25
+ * (c) 2010-2020 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+
+/**
+ * Platform for testing
+ *
+ * @publicApi
+ */
+export declare const platformServerTesting: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * NgModule for testing.
+ *
+ * @publicApi
+ */
+export declare class ServerTestingModule {
+}
+
+export { }

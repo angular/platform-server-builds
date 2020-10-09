20b86091568dcbc1ef713398baf65f43c1a0b86b
atscott <atscott01@gmail.com>
2020-10-09 16:01:46 +0000

    7f689a291a fix(compiler): incorrectly encapsulating @import containing colons and semicolons (#38716)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.5+14.sha-ca4ef61
+ * @license Angular v11.0.0-next.5+15.sha-7f689a2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -979,7 +979,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.5+14.sha-ca4ef61');
+const VERSION = new Version('11.0.0-next.5+15.sha-7f689a2');
 
 /**
  * @license

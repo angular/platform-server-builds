ccf5d5580b3e73f29dbfb2d8a148d97e6acb522b
atscott <atscott01@gmail.com>
2020-10-14 21:32:35 +0000

    aee2d3f994 docs: fix broken link (#39256)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6
+ * @license Angular v11.0.0-next.6+7.sha-aee2d3f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -979,7 +979,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6');
+const VERSION = new Version('11.0.0-next.6+7.sha-aee2d3f');
 
 /**
  * @license

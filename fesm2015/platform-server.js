89021e900cfd021a84a0d1d685de487d9cf8f9e5
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:42:45 +0000

    a38293d213 release: cut the zone.js-0.11.2 release to fix several new issues (#39298)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+35.sha-492e236
+ * @license Angular v11.0.0-next.6+37.sha-a38293d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -979,7 +979,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+35.sha-492e236');
+const VERSION = new Version('11.0.0-next.6+37.sha-a38293d');
 
 /**
  * @license

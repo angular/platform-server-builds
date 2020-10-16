8fa81bb7a0c1a628ae84474a186ffe7b49aadc1e
Andrew Kushnir <akushnir@google.com>
2020-10-16 00:23:21 +0000

    94d7ef3909 refactor(core): renames checkNoChangesMode to be clearer (#39277)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+14.sha-af17038
+ * @license Angular v10.1.6+16.sha-94d7ef3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -980,7 +980,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.1.6+14.sha-af17038');
+const VERSION = new Version('10.1.6+16.sha-94d7ef3');
 
 /**
  * @license

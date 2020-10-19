52586d59488ac49bc8ff893a4d99832084f80967
Andrew Kushnir <akushnir@google.com>
2020-10-19 19:05:18 +0000

    aba1cb4672 docs: change definition of NgModules in Angular concepts guide (#36179)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+37.sha-726c5ef
+ * @license Angular v11.0.0-next.6+38.sha-aba1cb4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -979,7 +979,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+37.sha-726c5ef');
+const VERSION = new Version('11.0.0-next.6+38.sha-aba1cb4');
 
 /**
  * @license

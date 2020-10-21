ad51eb14b5c4a2e3b39f039457c1f042497fb299
Andrew Kushnir <akushnir@google.com>
2020-10-21 17:38:38 +0000

    71fb99f062 fix(platform-server): Resolve absolute URL from baseUrl (#39334)

---
+++ testing/testing.d.ts
@@ -0,0 +1,25 @@
+/**
+ * @license Angular v10.1.6+33.sha-71fb99f
+ * (c) 2010-2020 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+
+/**
+ * Platform for testing
+ *
+ * @publicApi
+ */
+export declare const platformServerTesting: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * NgModule for testing.
+ *
+ * @publicApi
+ */
+export declare class ServerTestingModule {
+}
+
+export { }

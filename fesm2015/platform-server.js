5f31e22f0eee2079e0bf11eaf96a3208a782f867
Joey Perrott <josephperrott@gmail.com>
2020-10-29 23:52:53 +0000

    90acb9171e docs: tView.preOrderHooks and tView.preOrderCheckHooks docs update (#39497)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+10.sha-fb96c3d
+ * @license Angular v10.2.1+12.sha-90acb91
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -992,7 +992,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.2.1+10.sha-fb96c3d');
+const VERSION = new Version('10.2.1+12.sha-90acb91');
 
 /**
  * @license

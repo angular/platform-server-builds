ddb16db31b1268a9f0558046f4c2cf039da43417
Joey Perrott <josephperrott@gmail.com>
2020-11-03 23:16:10 +0000

    250858a48e feat(dev-infra): provide tooling to check what branches a pr targets (#39504)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+29.sha-aa3bd27
+ * @license Angular v10.2.1+30.sha-250858a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -992,7 +992,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('10.2.1+29.sha-aa3bd27');
+const VERSION = new Version('10.2.1+30.sha-250858a');
 
 /**
  * @license

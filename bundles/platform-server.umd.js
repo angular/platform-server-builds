f921c955060d5fcd4836a923511f049e2c570037
atscott <atscott01@gmail.com>
2020-11-12 17:37:07 +0000

    44763245e1 fix(core): handle !important in style property value (#39603)

--- bundles/platform-server.umd.js
+++ bundles/platform-server.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+237.sha-d1355ca
+ * @license Angular v11.0.0-next.6+238.sha-4476324
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -939,6 +939,9 @@
         DefaultServerRenderer2.prototype.setStyle = function (el, style, value, flags) {
             style = style.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
             var styleMap = _readStyleAttribute(el);
+            if (flags & i0.RendererStyleFlags2.Important) {
+                value += ' !important';
+            }
             styleMap[style] = value == null ? '' : value;
             _writeStyleAttribute(el, styleMap);
         };
@@ -1370,7 +1373,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('11.0.0-next.6+237.sha-d1355ca');
+    var VERSION = new i0.Version('11.0.0-next.6+238.sha-4476324');
 
     /**
      * @license

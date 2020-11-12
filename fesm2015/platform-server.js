7da2d0f6c151de9fc5878e29b47b2c7ce50fb06e
atscott <atscott01@gmail.com>
2020-11-12 22:14:44 +0000

    a87236292e docs: edit two-way binding doc (#38952)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.6+240.sha-b015d3e
+ * @license Angular v11.0.0-next.6+241.sha-a872362
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0-next.6+240.sha-b015d3e');
+const VERSION = new Version('11.0.0-next.6+241.sha-a872362');
 
 /**
  * @license

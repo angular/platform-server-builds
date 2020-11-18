36a9985094e8419432de0011d1653b07ff2767ad
atscott <atscott01@gmail.com>
2020-11-18 19:07:08 +0000

    153b42b01e release: cut the v11.0.1 release

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0+46.sha-5d8b5eb
+ * @license Angular v11.0.1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.0+46.sha-5d8b5eb');
+const VERSION = new Version('11.0.1');
 
 /**
  * @license

a212ec5002af77306363d5d892ca0f667ade22ba
Andrew Kushnir <akushnir@google.com>
2020-11-19 18:17:48 +0000

    81d72a1e37 fix(router): migration incorrectly replacing deprecated key (#39763)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+16.sha-6150640
+ * @license Angular v11.1.0-next.0+17.sha-81d72a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+16.sha-6150640');
+const VERSION = new Version('11.1.0-next.0+17.sha-81d72a1');
 
 /**
  * @license

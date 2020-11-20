ee917839b367b15cd6dabce48cafd3a9e8dfae4a
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:06:25 +0000

    1cba56e11f refactor(core): remove unused fakeAsyncFallback and asyncFallback (#37879)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+38.sha-66d863f
+ * @license Angular v11.1.0-next.0+39.sha-1cba56e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1030,7 +1030,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+38.sha-66d863f');
+const VERSION = new Version('11.1.0-next.0+39.sha-1cba56e');
 
 /**
  * @license

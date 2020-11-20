a87d378f34456a8b5d1d12db987f0c88de839dae
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:24:14 +0000

    50c19a2dde fix(router): correctly handle string command in outlets (#39728)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+17.sha-27152e2
+ * @license Angular v11.0.2+28.sha-50c19a2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.2+17.sha-27152e2');
+const VERSION = new Version('11.0.2+28.sha-50c19a2');
 
 /**
  * @license

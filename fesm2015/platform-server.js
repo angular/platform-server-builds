e0a9650a04e7cb89c3692c1e060d7419a8956cf5
Andrew Kushnir <akushnir@google.com>
2020-11-24 19:39:56 +0000

    9cb12ca62c docs(core): update links to schema.json of @schematics/angular (#39827)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+48.sha-8634611
+ * @license Angular v11.0.2+49.sha-9cb12ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.2+48.sha-8634611');
+const VERSION = new Version('11.0.2+49.sha-9cb12ca');
 
 /**
  * @license

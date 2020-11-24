eda4ac85db9916448e2311d772d60829e8f086b2
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:12:13 +0000

    8634611e37 fix(compiler-cli): report error when a reference target is missing instead of crashing (#39805)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+43.sha-7bf73d7
+ * @license Angular v11.0.2+48.sha-8634611
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.2+43.sha-7bf73d7');
+const VERSION = new Version('11.0.2+48.sha-8634611');
 
 /**
  * @license

1ff5f6a0db49fe68ef2e6ec3b6732aa0a19b4e84
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:37:05 +0000

    20ab5d89a0 docs: fix typo in two-way-binding guide (#39859)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.2+63.sha-01c1bfd
+ * @license Angular v11.0.2+68.sha-20ab5d8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.2+63.sha-01c1bfd');
+const VERSION = new Version('11.0.2+68.sha-20ab5d8');
 
 /**
  * @license

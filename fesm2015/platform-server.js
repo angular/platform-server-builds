ebe073d70663310b50e8663d652bec7d9234298c
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 23:42:20 +0000

    eac85b4e23 test(compiler-cli): migrate view compliance tests (#39914)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.0+125.sha-7695671
+ * @license Angular v11.1.0-next.0+126.sha-eac85b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.0+125.sha-7695671');
+const VERSION = new Version('11.1.0-next.0+126.sha-eac85b4');
 
 /**
  * @license

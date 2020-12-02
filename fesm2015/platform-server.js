c9a6e8465faf4b6a81af9e7fb18e1e789f0458bd
Misko Hevery <misko@hevery.com>
2020-12-02 21:19:53 +0000

    eba185edc8 test(docs-infra): reduce ambiguity in `deploy-to-firebase.js` test descriptions (#39853)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+18.sha-75fc893
+ * @license Angular v11.1.0-next.1+12.sha-eba185e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+18.sha-75fc893');
+const VERSION = new Version('11.1.0-next.1+12.sha-eba185e');
 
 /**
  * @license

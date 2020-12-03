a95e702653cf4e4589966b77ac79d231129e67be
Misko Hevery <misko@hevery.com>
2020-12-03 15:43:40 +0000

    2b84882ab0 fix(language-service): do not return external template that does not exist (#39898)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.1+24.sha-efd0d33
+ * @license Angular v11.1.0-next.1+26.sha-2b84882
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.1+24.sha-efd0d33');
+const VERSION = new Version('11.1.0-next.1+26.sha-2b84882');
 
 /**
  * @license

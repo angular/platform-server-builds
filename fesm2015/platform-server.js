dd7f2021ba4aabd71ff7e7e1b681501b060d9d98
Misko Hevery <misko@hevery.com>
2020-12-07 18:18:37 +0000

    6d629713f4 docs: use double colon for pseudo-elements (#40004)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.3+26.sha-1e3534f
+ * @license Angular v11.0.3+39.sha-6d62971
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.3+26.sha-1e3534f');
+const VERSION = new Version('11.0.3+39.sha-6d62971');
 
 /**
  * @license

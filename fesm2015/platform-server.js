85be8852f38d5cb2634271812a2bca1f0fb5c54f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 19:39:12 +0000

    f5aab2b972 fix(compiler): handle strings inside bindings that contain binding characters (#39826)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.4+1.sha-eba7de6
+ * @license Angular v11.0.4+3.sha-f5aab2b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.4+1.sha-eba7de6');
+const VERSION = new Version('11.0.4+3.sha-f5aab2b');
 
 /**
  * @license

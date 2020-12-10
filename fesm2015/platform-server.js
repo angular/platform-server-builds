8d45081155f4873db080712340b99bb2c4818c3d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:09:03 +0000

    973f797ad5 feat(language-service): enable get references for directive and component from template (#40054)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+13.sha-b4b21bd
+ * @license Angular v11.1.0-next.2+16.sha-973f797
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+13.sha-b4b21bd');
+const VERSION = new Version('11.1.0-next.2+16.sha-973f797');
 
 /**
  * @license

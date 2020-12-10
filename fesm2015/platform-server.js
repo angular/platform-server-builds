8d542661019fff88921d153e3880d6cc14982446
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:55:08 +0000

    b08fd0c18c docs: remove unused CSS rules (#40069)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+23.sha-91fb830
+ * @license Angular v11.1.0-next.2+24.sha-b08fd0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+23.sha-91fb830');
+const VERSION = new Version('11.1.0-next.2+24.sha-b08fd0c');
 
 /**
  * @license

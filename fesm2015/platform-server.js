b3f00ca8f0799bba5ab84383bb7f1d4ee6f0d31f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:00:58 +0000

    35a1975697 build: update to use npm_package.pack rule (#39636)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.2+34.sha-1e05109
+ * @license Angular v11.1.0-next.2+30.sha-35a1975
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.2+34.sha-1e05109');
+const VERSION = new Version('11.1.0-next.2+30.sha-35a1975');
 
 /**
  * @license

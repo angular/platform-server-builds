4ede375fcc82b468397b5445f7ea3c6af020c85b
Joey Perrott <josephperrott@gmail.com>
2020-12-17 20:30:12 +0000

    2075cf79a3 docs: use an explicit product id in the getting started example (#34934)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+2.sha-ab34211
+ * @license Angular v11.0.5+5.sha-2075cf7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.5+2.sha-ab34211');
+const VERSION = new Version('11.0.5+5.sha-2075cf7');
 
 /**
  * @license

7ef3e9d2db8e9374b88aee9449e432318b3bd05c
JoostK <joost.koehoorn@gmail.com>
2020-12-22 18:26:22 +0000

    20112e0f37 fixup! feat(compiler-cli): JIT compilation of directive declarations

---
+++ testing/testing.d.ts
@@ -0,0 +1,25 @@
+/**
+ * @license Angular v11.1.0-next.3+7.sha-20112e0
+ * (c) 2010-2020 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+
+/**
+ * Platform for testing
+ *
+ * @publicApi
+ */
+export declare const platformServerTesting: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * NgModule for testing.
+ *
+ * @publicApi
+ */
+export declare class ServerTestingModule {
+}
+
+export { }

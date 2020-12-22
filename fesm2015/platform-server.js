502ccf938af214cd4e1117a4be27636b9c08c661
Joey Perrott <josephperrott@gmail.com>
2020-12-22 23:20:58 +0000

    ef13e834fe fix(core): error if detectChanges is called at the wrong time under specific circumstances (#40206)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+11.sha-3f4a1d6
+ * @license Angular v11.0.5+23.sha-ef13e83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.5+11.sha-3f4a1d6');
+const VERSION = new Version('11.0.5+23.sha-ef13e83');
 
 /**
  * @license

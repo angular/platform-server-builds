63deb5af028733f51751176a99e19b9b5abd57e8
Joey Perrott <josephperrott@gmail.com>
2020-12-23 18:27:07 +0000

    c83163ecfe docs(router): fix typo (#40244)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+24.sha-9a149d4
+ * @license Angular v11.0.5+26.sha-c83163e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.5+24.sha-9a149d4');
+const VERSION = new Version('11.0.5+26.sha-c83163e');
 
 /**
  * @license

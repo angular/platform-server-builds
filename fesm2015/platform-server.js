1727ec69e171055300038c0c7f210b7d6038b7c9
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:59:17 +0000

    7977509b18 fix(compiler): don't report parse error for interpolation inside string in property binding (#40267)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+33.sha-7e583ab
+ * @license Angular v11.0.5+43.sha-7977509
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.5+33.sha-7e583ab');
+const VERSION = new Version('11.0.5+43.sha-7977509');
 
 /**
  * @license

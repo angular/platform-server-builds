ad46e4085dbb55ad7907ec595443b87aadb47888
Joey Perrott <josephperrott@gmail.com>
2021-01-05 18:25:58 +0000

    ec5b09b6f3 build(docs-infra): upgrade cli command docs sources to 5dd818279 (#40251)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+26.sha-c83163e
+ * @license Angular v11.0.5+31.sha-ec5b09b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.5+26.sha-c83163e');
+const VERSION = new Version('11.0.5+31.sha-ec5b09b');
 
 /**
  * @license

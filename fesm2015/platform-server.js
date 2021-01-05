0eedcc50986a76c07365a41b53d362fe9f4d468b
Joey Perrott <josephperrott@gmail.com>
2021-01-05 20:53:04 +0000

    7e583abfe0 ci: remove autolabeling from pullapprove config (#40314)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.5+32.sha-45838c0
+ * @license Angular v11.0.5+33.sha-7e583ab
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.5+32.sha-45838c0');
+const VERSION = new Version('11.0.5+33.sha-7e583ab');
 
 /**
  * @license

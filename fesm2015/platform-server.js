dad419639ecffa77f5e9c034a2a3b59329180105
Andrew Scott <atscott@google.com>
2021-01-08 18:09:11 +0000

    7a819caef6 fix(dev-infra): delay checking if a closed pr has been merged by 30 seconds (#40181)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+35.sha-e27b920
+ * @license Angular v11.1.0-next.4+37.sha-7a819ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+35.sha-e27b920');
+const VERSION = new Version('11.1.0-next.4+37.sha-7a819ca');
 
 /**
  * @license

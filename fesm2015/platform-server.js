70786d6d1c81e6a2bde597a15dae69328e78c94f
Andrew Scott <atscott@google.com>
2021-01-08 18:20:31 +0000

    e05ac395fc docs: add @christianliebel to list of GDEs (#39788)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+39.sha-4f73820
+ * @license Angular v11.1.0-next.4+43.sha-e05ac39
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+39.sha-4f73820');
+const VERSION = new Version('11.1.0-next.4+43.sha-e05ac39');
 
 /**
  * @license

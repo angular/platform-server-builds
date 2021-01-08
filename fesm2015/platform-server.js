e620025ad1ba30e3128f91732071a50b801e96b0
Andrew Scott <atscott@google.com>
2021-01-08 17:59:37 +0000

    e27b920ac3 refactor(compiler-cli): split up `NodeJSFileSystem` class (#40281)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+22.sha-1045465
+ * @license Angular v11.1.0-next.4+35.sha-e27b920
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+22.sha-1045465');
+const VERSION = new Version('11.1.0-next.4+35.sha-e27b920');
 
 /**
  * @license

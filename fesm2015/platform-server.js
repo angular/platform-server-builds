8d3d6215bc085b471571ed302242b14a79c3a601
atscott <atscott01@gmail.com>
2021-01-11 19:34:59 +0000

    76954688b1 fix(router): better ngZone checking for warning (#25839)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+65.sha-c9de7da
+ * @license Angular v11.1.0-next.4+66.sha-7695468
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+65.sha-c9de7da');
+const VERSION = new Version('11.1.0-next.4+66.sha-7695468');
 
 /**
  * @license

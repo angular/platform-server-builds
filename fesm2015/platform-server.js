2b2c855ac9041ae7abd1de91f8beb70c187265d0
atscott <atscott01@gmail.com>
2021-01-11 19:33:09 +0000

    c9de7daae4 build: update bazel rules_nodejs to 2.3.2 (#40367)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+58.sha-4bb067d
+ * @license Angular v11.1.0-next.4+65.sha-c9de7da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+58.sha-4bb067d');
+const VERSION = new Version('11.1.0-next.4+65.sha-c9de7da');
 
 /**
  * @license

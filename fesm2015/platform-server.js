390d9932cc0427851c9ae232c09e768d67568602
atscott <atscott01@gmail.com>
2021-01-12 00:00:34 +0000

    184d0e568f docs: edit getting started start-forms.md (#39681)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+75.sha-957332e
+ * @license Angular v11.1.0-next.4+77.sha-184d0e5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+75.sha-957332e');
+const VERSION = new Version('11.1.0-next.4+77.sha-184d0e5');
 
 /**
  * @license

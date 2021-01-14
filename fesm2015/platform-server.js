357330f08d9c656c88286c5fb1da7a199bb62e9f
Andrew Scott <atscott@google.com>
2021-01-14 17:46:24 +0000

    f3a96a50b6 build(docs-infra): upgrade cli command docs sources to f67800842 (#40426)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.9+9.sha-5aa264f
+ * @license Angular v11.0.9+11.sha-f3a96a5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.9+9.sha-5aa264f');
+const VERSION = new Version('11.0.9+11.sha-f3a96a5');
 
 /**
  * @license

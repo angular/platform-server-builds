67cbe0cb3fae930b897ec746d9748f76e6f53a68
Andrew Kushnir <akushnir@google.com>
2021-01-14 20:03:11 +0000

    63bf613785 docs: remove duplicated the (#40434)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-rc.0+12.sha-968b2bb
+ * @license Angular v11.1.0-rc.0+19.sha-63bf613
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-rc.0+12.sha-968b2bb');
+const VERSION = new Version('11.1.0-rc.0+19.sha-63bf613');
 
 /**
  * @license

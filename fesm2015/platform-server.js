00a7bf725282c35c4e969e359cacc0487b15d508
Andrew Kushnir <akushnir@google.com>
2021-01-20 17:26:15 +0000

    d684e5a8d3 docs: add embedded videos to error guides (#40453)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.9+26.sha-96e6f20
+ * @license Angular v11.0.9+27.sha-d684e5a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.0.9+26.sha-96e6f20');
+const VERSION = new Version('11.0.9+27.sha-d684e5a');
 
 /**
  * @license

6926d7cfd417b1f104552d37592f21edc0fe75bf
Harun Urhan <harunurhan17@gmail.com>
2021-01-20 22:46:08 +0000

    ddc0a2b54e feat(common): implement appendAll for HttpParams

---
+++ testing/testing.d.ts
@@ -0,0 +1,25 @@
+/**
+ * @license Angular v11.1.0-next.1+50.sha-ddc0a2b
+ * (c) 2010-2020 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+
+/**
+ * Platform for testing
+ *
+ * @publicApi
+ */
+export declare const platformServerTesting: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * NgModule for testing.
+ *
+ * @publicApi
+ */
+export declare class ServerTestingModule {
+}
+
+export { }

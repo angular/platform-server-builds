0d6b2ec9af1e7dc5ea63232674f6bbe08be0f767
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:33:01 +0000

    566206b519 fix(compiler-cli): handle pseudo cycles in inline source-maps (#40435)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+14.sha-69fd942
+ * @license Angular v11.1.0+19.sha-566206b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0+14.sha-69fd942');
+const VERSION = new Version('11.1.0+19.sha-566206b');
 
 /**
  * @license

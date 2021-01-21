b4e3cc0278427fe9346723b1e6627bf53c792746
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 20:46:32 +0000

    f0733d109e fix(router): Fix occasional error when creating url tree in IE 11 and Edge (#40488)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+142.sha-69385f7
+ * @license Angular v11.1.0-next.4+145.sha-f0733d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+142.sha-69385f7');
+const VERSION = new Version('11.1.0-next.4+145.sha-f0733d1');
 
 /**
  * @license

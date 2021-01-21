4b24540ebc30fabf0f42a3c520c4508cba50227a
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:22:33 +0000

    575a2d162c feat(common): implement `appendAll()` method on `HttpParams` (#20930)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+149.sha-c9033d6
+ * @license Angular v11.1.0-next.4+150.sha-575a2d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+149.sha-c9033d6');
+const VERSION = new Version('11.1.0-next.4+150.sha-575a2d1');
 
 /**
  * @license

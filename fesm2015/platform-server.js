951e9881de4a949fd5b8975db482bee67dafc75e
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 18:51:33 +0000

    e25413c6da build: update to @bazel/bazelisk@1.7.3 (#40519)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+155.sha-1e4b51e
+ * @license Angular v11.1.0-next.4+156.sha-e25413c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+155.sha-1e4b51e');
+const VERSION = new Version('11.1.0-next.4+156.sha-e25413c');
 
 /**
  * @license

a90fb21e3273347ad815a4fa055ef4f918f6cb46
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 21:56:19 +0000

    c5599ccd3e docs: fix NG0201 error description (#40500)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+156.sha-e25413c
+ * @license Angular v11.1.0-next.4+157.sha-c5599cc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+156.sha-e25413c');
+const VERSION = new Version('11.1.0-next.4+157.sha-c5599cc');
 
 /**
  * @license

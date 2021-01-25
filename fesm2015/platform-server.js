2f815feca40c6d6e86d80214f9bfb05cb8a1ffb3
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 18:37:40 +0000

    37710b9be3 fix(service-worker): handle error with console.error (#40236)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+26.sha-dfe4200
+ * @license Angular v11.1.0+30.sha-37710b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0+26.sha-dfe4200');
+const VERSION = new Version('11.1.0+30.sha-37710b9');
 
 /**
  * @license

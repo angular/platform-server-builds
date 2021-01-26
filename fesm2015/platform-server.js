68f4cbec94448643ba13bcc1f4f955f1f4100e7a
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 23:32:41 +0000

    cffb00ec11 build: update bazel rules_nodejs to 2.3.3 (#40581)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+182.sha-39b3cbb
+ * @license Angular v11.1.0-next.4+183.sha-cffb00e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+182.sha-39b3cbb');
+const VERSION = new Version('11.1.0-next.4+183.sha-cffb00e');
 
 /**
  * @license

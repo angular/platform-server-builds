100596a039483664b09fbf4134c61f56a0c3777c
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 19:17:29 +0000

    be979c907b perf(compiler-cli): introduce fast path for resource-only updates (#40561)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0-next.4+189.sha-e43cba5
+ * @license Angular v11.1.0-next.4+194.sha-be979c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0-next.4+189.sha-e43cba5');
+const VERSION = new Version('11.1.0-next.4+194.sha-be979c9');
 
 /**
  * @license

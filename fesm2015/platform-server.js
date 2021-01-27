6077a949cfed6608bb3e72ef2febcdd90c9fb1bb
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 23:46:37 +0000

    de124856f6 docs: update release notes for the 11.1.1 release (#40602)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.0+60.sha-63275ae
+ * @license Angular v11.1.1+1.sha-de12485
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.0+60.sha-63275ae');
+const VERSION = new Version('11.1.1+1.sha-de12485');
 
 /**
  * @license

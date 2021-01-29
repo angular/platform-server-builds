c8e844ab3c0a0291f341cd2e224689dc9902e395
Misko Hevery <misko@hevery.com>
2021-01-29 00:24:49 +0000

    cafd4f58c9 fix(common): parse `YYYY-MM` strings as UTC dates (#40620)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.1+10.sha-e706f46
+ * @license Angular v11.1.1+14.sha-cafd4f5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.1+10.sha-e706f46');
+const VERSION = new Version('11.1.1+14.sha-cafd4f5');
 
 /**
  * @license

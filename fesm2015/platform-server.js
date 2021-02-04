5d24c9746d98450d16479ad8d74f582608b69bfb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-04 19:25:44 +0000

    0fc5a1690a docs: add Angular Global Summit to event pages (#40697)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.1.2
+ * @license Angular v11.1.2+4.sha-0fc5a16
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.1.2');
+const VERSION = new Version('11.1.2+4.sha-0fc5a16');
 
 /**
  * @license

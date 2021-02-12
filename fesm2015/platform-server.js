b1819b31c40d18c02750cb70988bd68349ca4544
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:20:37 +0000

    9cb43fb507 refactor(compiler-cli): implement `ɵɵngDeclarePipe()` (#40803)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+26.sha-6425a6d
+ * @license Angular v12.0.0-next.0+27.sha-9cb43fb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.0+26.sha-6425a6d');
+const VERSION = new Version('12.0.0-next.0+27.sha-9cb43fb');
 
 /**
  * @license

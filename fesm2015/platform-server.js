16662ec0e1e5724d34c2c6d501edb7754a54de8d
Joey Perrott <josephperrott@gmail.com>
2021-02-16 21:25:35 +0000

    53865c4517 docs(forms): fix link for browser compatibility of using selects in IE (#40855)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.0+37.sha-7f09945
+ * @license Angular v11.2.0+61.sha-53865c4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.0+37.sha-7f09945');
+const VERSION = new Version('11.2.0+61.sha-53865c4');
 
 /**
  * @license

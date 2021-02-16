95dbdc77ee008a5d975c0763421716fdaedc3984
Joey Perrott <josephperrott@gmail.com>
2021-02-16 16:01:30 +0000

    67d85b3680 docs: capitalize Angular word in upgrade-setup.md file (#40862) PR Close #40862

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+38.sha-5f0c219
+ * @license Angular v12.0.0-next.0+37.sha-67d85b3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.0+38.sha-5f0c219');
+const VERSION = new Version('12.0.0-next.0+37.sha-67d85b3');
 
 /**
  * @license

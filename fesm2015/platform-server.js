d009028782a1a5ad39395aac54674f2b7c994558
GitHub <noreply@github.com>
2021-02-17 19:07:39 +0000

    a9936c22ba docs: release notes for the v11.2.1 release (#40888)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.0+73.sha-322951a
+ * @license Angular v12.0.0-next.0+77.sha-a9936c2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1030,7 +1030,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.0+73.sha-322951a');
+const VERSION = new Version('12.0.0-next.0+77.sha-a9936c2');
 
 /**
  * @license

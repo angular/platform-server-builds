33abf7a72ab6e0daa2b3d3f3473159ab0dfacdb7
atscott <atscott01@gmail.com>
2021-02-19 22:57:30 +0000

    ff86f919ab refactor(core): use RuntimeError to throw provider not found error (#40901)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.1+32.sha-3b884ed
+ * @license Angular v11.2.1+33.sha-ff86f91
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.1+32.sha-3b884ed');
+const VERSION = new Version('11.2.1+33.sha-ff86f91');
 
 /**
  * @license

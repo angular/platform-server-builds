7690575f9f3924523ee3fd982152b95955cf9439
atscott <atscott01@gmail.com>
2021-02-23 19:06:32 +0000

    c6e2234fa6 docs: improve accessibility for toh-pt1 example (#40560)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.2+1.sha-09b92d7
+ * @license Angular v11.2.2+10.sha-c6e2234
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.2+1.sha-09b92d7');
+const VERSION = new Version('11.2.2+10.sha-c6e2234');
 
 /**
  * @license

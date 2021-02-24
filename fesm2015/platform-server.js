9f90932430b0fd7252a140c07f40365523dbf1f9
GitHub <noreply@github.com>
2021-02-24 17:53:33 +0000

    d7133a8e4e release: cut the v11.2.3 release (#40981)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.2+25.sha-3fe759e
+ * @license Angular v11.2.2+26.sha-d7133a8
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.2+25.sha-3fe759e');
+const VERSION = new Version('11.2.2+26.sha-d7133a8');
 
 /**
  * @license

7839f8b740071a20bc41afa042eea8cec6d9d05d
Andrew Kushnir <akushnir@google.com>
2021-03-06 01:06:57 +0000

    4fd5d688ce docs: fix some typos in angular.io examples (#41093)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.4+14.sha-2de7eef
+ * @license Angular v11.2.4+15.sha-4fd5d68
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.4+14.sha-2de7eef');
+const VERSION = new Version('11.2.4+15.sha-4fd5d68');
 
 /**
  * @license

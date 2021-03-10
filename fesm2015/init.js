acc7dd0782941e9fdc9e08e6d93cee839227e68f
Andrew Kushnir <akushnir@google.com>
2021-03-10 20:15:50 +0000

    3c66b100dd perf(common): remove unused methods from DomAdapter (#41102)

--- fesm2015/init.js
+++ fesm2015/init.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.4+3.sha-4c79b8a
+ * @license Angular v12.0.0-next.4+4.sha-3c66b10
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -15,9 +15,6 @@ import { ɵsetRootDomAdapter } from '@angular/common';
  * found in the LICENSE file at https://angular.io/license
  */
 const domino = require('domino');
-function _notImplemented(methodName) {
-    return new Error('This method is not implemented in DominoAdapter: ' + methodName);
-}
 function setDomTypes() {
     // Make all Domino types available in the global env.
     Object.assign(global, domino.impl);
@@ -41,20 +38,13 @@ function serializeDocument(doc) {
  * DOM Adapter for the server platform based on https://github.com/fgnass/domino.
  */
 class DominoAdapter extends ɵBrowserDomAdapter {
+    constructor() {
+        super(...arguments);
+        this.supportsDOMEvents = false;
+    }
     static makeCurrent() {
         setDomTypes();
         ɵsetRootDomAdapter(new DominoAdapter());
-    }
-    log(error) {
-        // tslint:disable-next-line:no-console
-        console.log(error);
-    }
-    logGroup(error) {
-        console.error(error);
-    }
-    logGroupEnd() { }
-    supportsDOMEvents() {
-        return false;
     }
     createHtmlDocument() {
         return parseDocument('<html><head><title>fakeTitle</title></head><body></body></html>');
@@ -71,18 +61,6 @@ class DominoAdapter extends ɵBrowserDomAdapter {
     isShadowRoot(node) {
         return node.shadowRoot == node;
     }
-    getProperty(el, name) {
-        if (name === 'href') {
-            // Domino tries to resolve href-s which we do not want. Just return the
-            // attribute value.
-            return el.getAttribute('href');
-        }
-        else if (name === 'innerText') {
-            // Domino does not support innerText. Just map it to textContent.
-            return el.textContent;
-        }
-        return el[name];
-    }
     getGlobalEventTarget(doc, target) {
         if (target === 'window') {
             return doc.defaultView;
@@ -96,13 +74,9 @@ class DominoAdapter extends ɵBrowserDomAdapter {
         return null;
     }
     getBaseHref(doc) {
-        const base = doc.documentElement.querySelector('base');
-        let href = '';
-        if (base) {
-            href = base.getAttribute('href');
-        }
+        var _a;
         // TODO(alxhub): Need relative path logic from BrowserDomAdapter here?
-        return href;
+        return ((_a = doc.documentElement.querySelector('base')) === null || _a === void 0 ? void 0 : _a.getAttribute('href')) || '';
     }
     dispatchEvent(el, evt) {
         el.dispatchEvent(evt);
@@ -113,23 +87,11 @@ class DominoAdapter extends ɵBrowserDomAdapter {
             win.dispatchEvent(evt);
         }
     }
-    getHistory() {
-        throw _notImplemented('getHistory');
-    }
-    getLocation() {
-        throw _notImplemented('getLocation');
-    }
     getUserAgent() {
         return 'Fake user agent';
     }
-    performanceNow() {
-        return Date.now();
-    }
-    supportsCookies() {
-        return false;
-    }
     getCookie(name) {
-        throw _notImplemented('getCookie');
+        throw new Error('getCookie has not been implemented');
     }
 }
 

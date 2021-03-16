6bfc4e152fb895285c36c35fa0726b866b219182
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-16 16:56:38 +0000

    6414590f8d perf(forms): make `RadioControlRegistry` class tree-shakable (#41126)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.5+22.sha-bec919a
+ * @license Angular v11.2.5+25.sha-6414590
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.5+22.sha-bec919a');
+const VERSION = new Version('11.2.5+25.sha-6414590');
 
 /**
  * @license

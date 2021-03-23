fd5aaf633254dfd283694d09a17bf2ff12fbf340
Joey Perrott <josephperrott@gmail.com>
2021-03-23 17:10:37 +0000

    e340d190bd docs: add some boilerplate button, h3, and hr styles (#41052)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.5+26.sha-31dc483
+ * @license Angular v12.0.0-next.5+27.sha-e340d19
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -958,7 +958,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.5+26.sha-31dc483');
+const VERSION = new Version('12.0.0-next.5+27.sha-e340d19');
 
 /**
  * @license

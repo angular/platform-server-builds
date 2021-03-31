b384b938771f3fc4fdad12df84bb18168d3b7a10
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 17:16:57 +0000

    723bfc717b Revert "feat(bazel): allow setting `compilationMode` in `ng_module` rule (#41366)"

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.7+9.sha-06e5132
+ * @license Angular v11.2.7+13.sha-723bfc7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.7+9.sha-06e5132');
+const VERSION = new Version('11.2.7+13.sha-723bfc7');
 
 /**
  * @license

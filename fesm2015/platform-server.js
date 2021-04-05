9c024ba994893938ed6b0070ef81d3a7b58141ac
atscott <atscott01@gmail.com>
2021-04-05 16:16:47 +0000

    754247fefd feat(dev-infra): prevent `git push` from being called in dryRun mode (#41387)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.8+6.sha-103f05a
+ * @license Angular v11.2.8+10.sha-754247f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.8+6.sha-103f05a');
+const VERSION = new Version('11.2.8+10.sha-754247f');
 
 /**
  * @license

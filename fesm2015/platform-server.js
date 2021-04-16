e677e509195a5ab8efe9d789b403e8b098bd5750
Andrew Kushnir <akushnir@google.com>
2021-04-16 00:00:18 +0000

    21d3084512 docs: edits to remove jargon and abbreviations (#41530)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+8.sha-6c43a94
+ * @license Angular v11.2.10+9.sha-21d3084
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.10+8.sha-6c43a94');
+const VERSION = new Version('11.2.10+9.sha-21d3084');
 
 /**
  * @license

9ca44eee8bca3231ebbb9a845a563a72e2193468
Andrew Kushnir <akushnir@google.com>
2021-04-19 23:18:46 +0000

    a76f07bd13 docs: documentation of descendants property of @ContentChildren (#35927)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.10+31.sha-3631934
+ * @license Angular v11.2.10+35.sha-a76f07b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.10+31.sha-3631934');
+const VERSION = new Version('11.2.10+35.sha-a76f07b');
 
 /**
  * @license

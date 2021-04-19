d5db44ddcb754202d22a58cab194b21541b44735
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:29:17 +0000

    343b9d0ddc build: update jasmine-spec-reporter to version 7.0.0 (#41692)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+159.sha-322db17
+ * @license Angular v12.0.0-next.8+155.sha-343b9d0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -958,7 +958,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+159.sha-322db17');
+const VERSION = new Version('12.0.0-next.8+155.sha-343b9d0');
 
 /**
  * @license

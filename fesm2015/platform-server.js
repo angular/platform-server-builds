97a1b21e8ae1dea2c5469f8543beb9ad6529df42
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:41:39 +0000

    6cdb8fb0aa build: remove entities dependency from root package.json (#41705)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+171.sha-c617f1f
+ * @license Angular v12.0.0-next.8+172.sha-6cdb8fb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -996,7 +996,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+171.sha-c617f1f');
+const VERSION = new Version('12.0.0-next.8+172.sha-6cdb8fb');
 
 /**
  * @license

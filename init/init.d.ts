27624efe18133f45ea577db8793be181517271ee
Zach Arend <zachzach@google.com>
2021-04-21 21:33:32 +0000

    c270c88dfd fix(compiler-cli): add support for partially evaluating types

---
+++ init/init.d.ts
@@ -0,0 +1,8 @@
+/**
+ * @license Angular v12.0.0-next.8+189.sha-c270c88
+ * (c) 2010-2021 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+
+export { }
---
+++ testing/testing.d.ts
@@ -0,0 +1,25 @@
+/**
+ * @license Angular v12.0.0-next.8+189.sha-c270c88
+ * (c) 2010-2021 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+import { PlatformRef } from '@angular/core';
+import { StaticProvider } from '@angular/core';
+
+/**
+ * Platform for testing
+ *
+ * @publicApi
+ */
+export declare const platformServerTesting: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+
+/**
+ * NgModule for testing.
+ *
+ * @publicApi
+ */
+export declare class ServerTestingModule {
+}
+
+export { }

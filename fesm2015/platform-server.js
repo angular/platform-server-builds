f2da7aaf9402fafbd9378ee4f31a6505f0448dbd
GitHub <noreply@github.com>
2021-04-21 17:42:25 +0000

    897d5ad76e release: cut the v12.0.0-rc.0 release (#41752)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.9+88.sha-7bd8741
+ * @license Angular v12.0.0-next.9+89.sha-897d5ad
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -958,7 +958,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.9+88.sha-7bd8741');
+const VERSION = new Version('12.0.0-next.9+89.sha-897d5ad');
 
 /**
  * @license

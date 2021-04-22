faf2408c4ecc5adeb3b4e264aa348bc4218e2c75
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 18:03:42 +0000

    64567d3d39 docs: Remove Protractor test from CI examples (#41733)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+203.sha-e0fb18a
+ * @license Angular v12.0.0-next.8+204.sha-64567d3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -958,7 +958,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+203.sha-e0fb18a');
+const VERSION = new Version('12.0.0-next.8+204.sha-64567d3');
 
 /**
  * @license

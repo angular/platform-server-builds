bfe17c631ad9fe4f9ee86b81297caede0ad7d7f6
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 22:36:47 +0000

    e47ea2959c docs: add chau tran to GDE resources (#41817)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+232.sha-4b4f565
+ * @license Angular v12.0.0-next.8+233.sha-e47ea29
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -996,7 +996,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+232.sha-4b4f565');
+const VERSION = new Version('12.0.0-next.8+233.sha-e47ea29');
 
 /**
  * @license

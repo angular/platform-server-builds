2950dc91e7d9fbdefc1e7540d761bf8abcd9c507
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 19:01:29 +0000

    295889ed2e ci: prevent CI cache from growing indefinitely (#41814)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+226.sha-a938849
+ * @license Angular v12.0.0-next.8+228.sha-295889e
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -958,7 +958,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+226.sha-a938849');
+const VERSION = new Version('12.0.0-next.8+228.sha-295889e');
 
 /**
  * @license

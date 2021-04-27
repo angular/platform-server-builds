3069b266de638c64ded2002354adaa3491b9a1d8
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 20:14:48 +0000

    eedbb758d2 build(docs-infra): update dgeni-packages to fix version list (#41841)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+24.sha-e758658
+ * @license Angular v11.2.11+25.sha-eedbb75
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.11+24.sha-e758658');
+const VERSION = new Version('11.2.11+25.sha-eedbb75');
 
 /**
  * @license

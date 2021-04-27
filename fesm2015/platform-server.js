d4748b54e1181904b24a768f5f90fe547a91d969
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 20:46:03 +0000

    4aafcc06ed Revert "feat(dev-infra): create the prettier formatter for ng-dev format tooling (#41824)" (#41844)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.11+25.sha-eedbb75
+ * @license Angular v11.2.11+27.sha-4aafcc0
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.11+25.sha-eedbb75');
+const VERSION = new Version('11.2.11+27.sha-4aafcc0');
 
 /**
  * @license

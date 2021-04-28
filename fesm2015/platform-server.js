0de3bef5e00e51d481a5a2fb60ea356962cb2bb3
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 19:56:52 +0000

    f555a557a3 docs: remove mention of scheduleTick in markForCheck (#41845)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.12
+ * @license Angular v11.2.12+1.sha-f555a55
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.12');
+const VERSION = new Version('11.2.12+1.sha-f555a55');
 
 /**
  * @license

4f086922272c42ed15d46ae1b4015e46d0e728a2
Misko Hevery <misko@hevery.com>
2021-04-30 21:55:24 +0000

    7e9ef4d257 docs: Update link to Testing Angular online book (#41906)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.2.12+9.sha-5bae16d
+ * @license Angular v11.2.12+12.sha-7e9ef4d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -994,7 +994,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('11.2.12+9.sha-5bae16d');
+const VERSION = new Version('11.2.12+12.sha-7e9ef4d');
 
 /**
  * @license

7ed2f329319488340b874e32899dab34fb589ace
Misko Hevery <misko@hevery.com>
2021-05-03 21:52:26 +0000

    b5b09dcca4 Revert "fix(language-service): only provide template results on reference requests (#41041)" (#41930)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-rc.1+35.sha-d59330b
+ * @license Angular v12.0.0-rc.1+44.sha-b5b09dc
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-rc.1+35.sha-d59330b');
+const VERSION = new Version('12.0.0-rc.1+44.sha-b5b09dc');
 
 /**
  * @license

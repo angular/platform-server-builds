df3e31b4ceda65f7f4d1572ef526e31323b311a0
Misko Hevery <misko@hevery.com>
2021-05-05 21:09:24 +0000

    530e45f093 fix(dev-infra): publish script python check not working on windows (#41943)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+338.sha-931d40b
+ * @license Angular v12.0.0-next.8+336.sha-530e45f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+338.sha-931d40b');
+const VERSION = new Version('12.0.0-next.8+336.sha-530e45f');
 
 /**
  * @license

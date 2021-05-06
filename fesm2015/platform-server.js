871ce1cd5df2f8528b9de56759e36cd258ba322c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 13:53:21 +0000

    abcd4bbfaa fix(compiler): preserve @page rules in encapsulated styles (#41915)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+343.sha-efe8566
+ * @license Angular v12.0.0-next.8+344.sha-abcd4bb
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+343.sha-efe8566');
+const VERSION = new Version('12.0.0-next.8+344.sha-abcd4bb');
 
 /**
  * @license

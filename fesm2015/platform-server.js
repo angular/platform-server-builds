0bcb965152c4b9fb483c9848653d50b53b1b1089
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:56:41 +0000

    a529d4f4f8 fix(core): do not retain dynamically compiled components and modules (#42003)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0-next.8+376.sha-23f6b76
+ * @license Angular v12.0.0-next.8+386.sha-a529d4f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0-next.8+376.sha-23f6b76');
+const VERSION = new Version('12.0.0-next.8+386.sha-a529d4f');
 
 /**
  * @license

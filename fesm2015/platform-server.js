e13db7f8e5fae5f83093432e8048565dbf7f3f49
Andrew Scott <atscott@google.com>
2021-05-18 18:39:27 +0000

    c58e15a4b0 ci: add pullapprove entries for devtools (#42152)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+26.sha-6861911
+ * @license Angular v12.0.0+32.sha-c58e15a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0+26.sha-6861911');
+const VERSION = new Version('12.0.0+32.sha-c58e15a');
 
 /**
  * @license

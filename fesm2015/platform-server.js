f7d50f501cc8dae364da6099af2d7494c498294c
Zach Arend <zachzach@google.com>
2021-05-20 16:27:21 +0000

    390d24b75f test(docs-infra): correctly test URL redirects when the destination URL is also redirected (#42018)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.0+42.sha-43525ad
+ * @license Angular v12.0.1+3.sha-390d24b
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.0+42.sha-43525ad');
+const VERSION = new Version('12.0.1+3.sha-390d24b');
 
 /**
  * @license

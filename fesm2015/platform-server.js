bfc2edd2ba298d1acae656d3f3c97abdee73dacf
Andrew Kushnir <akushnir@google.com>
2021-06-01 19:39:56 +0000

    8bdcca1e08 fix(compiler-cli): better detect classes that are indirectly exported (#42207)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+62.sha-b0293c6
+ * @license Angular v12.0.2+63.sha-8bdcca1
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.2+62.sha-b0293c6');
+const VERSION = new Version('12.0.2+63.sha-8bdcca1');
 
 /**
  * @license

629025a279d4dde02caa56672aafc33b2b1468a3
Andrew Kushnir <akushnir@google.com>
2021-06-02 18:13:11 +0000

    495a2138a0 release: cut the v12.0.3 release

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.2+65.sha-e937f77
+ * @license Angular v12.0.3
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.2+65.sha-e937f77');
+const VERSION = new Version('12.0.3');
 
 /**
  * @license

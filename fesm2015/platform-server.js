b1100abf808f6bb0a4e93c296199baf5dd88d77c
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:14:27 +0000

    2a28a1ddb2 docs: add Angular Rocks podcast (#42078)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+89.sha-0777fac
+ * @license Angular v12.1.0-next.4+88.sha-2a28a1d
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+89.sha-0777fac');
+const VERSION = new Version('12.1.0-next.4+88.sha-2a28a1d');
 
 /**
  * @license

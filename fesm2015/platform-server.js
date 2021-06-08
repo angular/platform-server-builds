63e76757be997de7ce73706d3b0fc13dc89c6d65
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 01:43:03 +0000

    f1d58efdb1 docs: Edits to remove jargon in Reference Observables, Dependency Injection (#42185)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.4+63.sha-b762820
+ * @license Angular v12.1.0-next.4+64.sha-f1d58ef
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.4+63.sha-b762820');
+const VERSION = new Version('12.1.0-next.4+64.sha-f1d58ef');
 
 /**
  * @license

fa0a0c18d64de0c76ca13bbd179750560723a99f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-09 19:34:26 +0000

    785da0f1bf fix(core): ensure that autoRegisterModuleById registration in ɵɵdefineNgModule is not DCE-ed by closure (#42529)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.0.3+66.sha-dfb072a
+ * @license Angular v12.0.4+2.sha-785da0f
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.0.3+66.sha-dfb072a');
+const VERSION = new Version('12.0.4+2.sha-785da0f');
 
 /**
  * @license

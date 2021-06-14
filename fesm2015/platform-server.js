7cc54ddad7f247a5d06863e1d410f8500686de57
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 21:30:48 +0000

    228beeabd1 fix(language-service): Use last child end span for parent without close tag (#42554)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+28.sha-044e022
+ * @license Angular v12.1.0-next.5+30.sha-228beea
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+28.sha-044e022');
+const VERSION = new Version('12.1.0-next.5+30.sha-228beea');
 
 /**
  * @license

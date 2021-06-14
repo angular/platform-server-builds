909ef00087fb5a9cf5aa442d1c5860e3a5fee4fa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:13:30 +0000

    828fde6e0d feat(docs-infra): implement popup to inform about the use of cookies (#42259)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+14.sha-5161084
+ * @license Angular v12.1.0-next.5+18.sha-828fde6
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+14.sha-5161084');
+const VERSION = new Version('12.1.0-next.5+18.sha-828fde6');
 
 /**
  * @license

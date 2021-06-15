eac6ae0d37442358acd77052162705f0cf9b6ba9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-15 20:12:21 +0000

    b52e93543f docs: add Daniel Trevino to contributors file (#42573)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+32.sha-a66dd88
+ * @license Angular v12.1.0-next.5+33.sha-b52e935
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+32.sha-a66dd88');
+const VERSION = new Version('12.1.0-next.5+33.sha-b52e935');
 
 /**
  * @license

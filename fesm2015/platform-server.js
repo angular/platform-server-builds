be42b244eac67ea96a2d52793192b92c3c486bbe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:20:32 +0000

    f3a79878af docs(forms): correct sample code for FormArray.reset (#42477)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+41.sha-62aca30
+ * @license Angular v12.1.0-next.5+42.sha-f3a7987
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+41.sha-62aca30');
+const VERSION = new Version('12.1.0-next.5+42.sha-f3a7987');
 
 /**
  * @license

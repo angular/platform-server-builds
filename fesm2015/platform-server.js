d01c9226b2318ff7050571a2f86b96d3d4937be1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:20:09 +0000

    62aca30286 feat(docs-infra): add support for "special elements" (#41299)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.5+40.sha-2d1347b
+ * @license Angular v12.1.0-next.5+41.sha-62aca30
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.5+40.sha-2d1347b');
+const VERSION = new Version('12.1.0-next.5+41.sha-62aca30');
 
 /**
  * @license

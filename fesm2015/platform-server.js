9ab00a1c0a81271fc6c4f1635614187c7de0e5f5
Dylan Hunn <dylhunn@gmail.com>
2021-06-22 16:46:23 +0000

    75bbcf7c2f docs: improve styles of the roadmap done section (#42616)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+35.sha-cc672f0
+ * @license Angular v12.1.0-next.6+36.sha-75bbcf7
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.6+35.sha-cc672f0');
+const VERSION = new Version('12.1.0-next.6+36.sha-75bbcf7');
 
 /**
  * @license

f961287ba1e5e5bd24e0aeeab3d066fe1d7097b9
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 20:16:22 +0000

    2203217c40 build(docs-infra): disambiguate doc paths for global APIs (#42648)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0-next.6+58.sha-637ac00
+ * @license Angular v12.1.0-next.6+59.sha-2203217
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0-next.6+58.sha-637ac00');
+const VERSION = new Version('12.1.0-next.6+59.sha-2203217');
 
 /**
  * @license

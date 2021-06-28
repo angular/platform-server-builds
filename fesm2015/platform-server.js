bbc9de2a63585586458700c12a0661d6faa9050f
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 18:03:39 +0000

    a1e036cfd1 docs: correct literal string example (#42623)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0+2.sha-f6b828e
+ * @license Angular v12.1.0+11.sha-a1e036c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0+2.sha-f6b828e');
+const VERSION = new Version('12.1.0+11.sha-a1e036c');
 
 /**
  * @license

5bc12f9d2fc6bdd2eafd6c218b041aa28940a296
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 17:59:13 +0000

    9f5cc7c808 feat(compiler): support number separators in templates (#42672)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.0+22.sha-234b5ed
+ * @license Angular v12.2.0-next.0+23.sha-9f5cc7c
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.0+22.sha-234b5ed');
+const VERSION = new Version('12.2.0-next.0+23.sha-9f5cc7c');
 
 /**
  * @license

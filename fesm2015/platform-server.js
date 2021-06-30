8a2f4eb1d0e5c3019bf54a11b88b909c067cc090
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 19:04:09 +0000

    aab41ca42f build: update API goldens to reflect new tool (#42688)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.0+18.sha-d19ddd1
+ * @license Angular v12.1.0+22.sha-aab41ca
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.0+18.sha-d19ddd1');
+const VERSION = new Version('12.1.0+22.sha-aab41ca');
 
 /**
  * @license

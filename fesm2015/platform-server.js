c21be2514c9cd6a252e7f27aee29d129b70b9c1d
Andrew Kushnir <akushnir@google.com>
2021-07-12 22:45:50 +0000

    88b15d572f docs: clarify voting phase for feature requests (#42663)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+98.sha-c1c1cda
+ * @license Angular v12.2.0-next.1+99.sha-88b15d5
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+98.sha-c1c1cda');
+const VERSION = new Version('12.2.0-next.1+99.sha-88b15d5');
 
 /**
  * @license

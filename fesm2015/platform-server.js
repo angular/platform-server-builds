59bac42f1d78c2383dcd023ce5cdfaf860f62ede
Andrew Kushnir <akushnir@google.com>
2021-07-13 23:19:24 +0000

    cd2d82a91a fix(core): associate the NgModule scope for an overridden component (#42817)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+103.sha-51156f3
+ * @license Angular v12.2.0-next.1+104.sha-cd2d82a
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+103.sha-51156f3');
+const VERSION = new Version('12.2.0-next.1+104.sha-cd2d82a');
 
 /**
  * @license

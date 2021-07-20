9a296442e1e6e17596a344ca8b703457d2407460
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 01:05:09 +0000

    e6b7472d11 build(docs-infra): improve @usageNotes message (#42888)

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.1.2+13.sha-97fa1cf
+ * @license Angular v12.1.2+29.sha-e6b7472
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.1.2+13.sha-97fa1cf');
+const VERSION = new Version('12.1.2+29.sha-e6b7472');
 
 /**
  * @license

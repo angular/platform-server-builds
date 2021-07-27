8186cf72f1028ec40d257c56af32ead70dabf636
Dylan Hunn <dylhunn@gmail.com>
2021-07-27 23:53:41 +0000

    0d81b007e4 fix(router): add missing outlet events to RouterOutletContract (#42431) Exposes both activateEvents and deactivateEvents as the original outlet interface did.

--- fesm2015/platform-server.js
+++ fesm2015/platform-server.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v12.2.0-next.1+85.sha-bfc4c3c.with-local-changes
+ * @license Angular v12.2.0-next.1+86.sha-5b5868d.with-local-changes
  * (c) 2010-2021 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -960,7 +960,7 @@ function renderModuleFactory(moduleFactory, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('12.2.0-next.1+85.sha-bfc4c3c.with-local-changes');
+const VERSION = new Version('12.2.0-next.1+86.sha-5b5868d.with-local-changes');
 
 /**
  * @license

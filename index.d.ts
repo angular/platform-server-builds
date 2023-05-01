b5aa08f477853edf795083ddc39e87a113193e0f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 15:13:06 +0000

    d6d347fec5 docs: add developer preview guide for esbuild-based build system (#50068)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v16.0.0-rc.3+sha-6dc3add
+ * @license Angular v16.0.0-rc.3+sha-d6d347f
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -78,6 +78,9 @@ export declare interface PlatformConfig {
 /**
  * The server platform that supports the runtime compiler.
  *
+ * @see `platformServer`
+ * @deprecated add an `import @angular/compiler` and replace the usage with `platformServer`
+ *     instead.
  * @publicApi
  */
 export declare const platformDynamicServer: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;

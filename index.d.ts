f45d82d300ad95cb11566608c31856b1a4167b01
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 10:53:36 +0000

    ea86754 refactor(platform-server): Add private profiler. (#56274)

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v18.0.6+sha-85f77b5
+ * @license Angular v18.0.6+sha-ea86754
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -154,6 +154,15 @@ export declare class ServerModule {
  * @publicApi
  */
 export declare const VERSION: Version;
+
+export declare function ɵdisableSsrProfiling(): void;
+
+/**
+ * This enables an internal performance profiler for SSR apps
+ *
+ * It should not be imported in application code
+ */
+export declare function ɵenableSsrProfiling(): void;
 
 export declare const ɵINTERNAL_SERVER_PLATFORM_PROVIDERS: StaticProvider[];
 

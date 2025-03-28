efda03b2a42325018fe4ea83ef31e0b4f71a90a9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-28 11:58:55 +0000

    958e98e fix(router): Add missing types to transition (#60307)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.4+sha-f4c4b10
+ * @license Angular v19.2.4+sha-958e98e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.4+sha-f4c4b10');
+const VERSION = new Version('19.2.4+sha-958e98e');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

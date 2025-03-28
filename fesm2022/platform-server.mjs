f95099cf5912b58dfa8615b6c4998774a5985f4b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-28 17:31:51 +0000

    d15fda3 Revert "refactor(core): allow multiple DI profilers (#60562)" (#60611)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-e40b5c9
+ * @license Angular v20.0.0-next.4+sha-d15fda3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-e40b5c9');
+const VERSION = new Version('20.0.0-next.4+sha-d15fda3');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

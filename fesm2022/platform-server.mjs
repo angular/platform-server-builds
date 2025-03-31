4f365a54c87694eb351fda432e897dc51c42ca83
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 20:04:31 +0000

    302cb83 docs: remove todo comment (#59928)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-be7c8d4
+ * @license Angular v20.0.0-next.4+sha-302cb83
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-be7c8d4');
+const VERSION = new Version('20.0.0-next.4+sha-302cb83');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

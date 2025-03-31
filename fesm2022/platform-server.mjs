6f11a3d8fc6746d3c1cbac0e1b23577a4fb1fbf2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 21:43:12 +0000

    ea62a4f fix(language-service): Update adapter to log instead of throw errors (#60651)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.4+sha-97556e6
+ * @license Angular v20.0.0-next.4+sha-ea62a4f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.4+sha-97556e6');
+const VERSION = new Version('20.0.0-next.4+sha-ea62a4f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

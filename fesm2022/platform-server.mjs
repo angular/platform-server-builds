cc6a6fbe4c8d3c6db96491e75f8108ca522a90ff
Andrew Scott <atscott@google.com>
2025-04-07 20:41:54 +0000

    35de6f6 build: update io_bazel_rules_sass digest to b4800f3 (#60737)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.5+sha-3441f7b
+ * @license Angular v19.2.5+sha-35de6f6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.5+sha-3441f7b');
+const VERSION = new Version('19.2.5+sha-35de6f6');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

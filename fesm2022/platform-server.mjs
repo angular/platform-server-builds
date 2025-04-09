427206e0c6a14abbebd54714f21309085b963b1e
Andrew Scott <atscott@google.com>
2025-04-09 14:01:12 +0000

    df42976 build: update github/codeql-action action to v3.28.15 (#60780)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.5+sha-b8d9f95
+ * @license Angular v20.0.0-next.5+sha-df42976
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.5+sha-b8d9f95');
+const VERSION = new Version('20.0.0-next.5+sha-df42976');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

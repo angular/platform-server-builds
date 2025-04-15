739c161e5de69219b65392ca0ee58ddbc359cfc6
kirjs <kirjs@users.noreply.github.com>
2025-04-15 13:28:02 +0000

    9171401 refactor(devtools): update release process to commit before publishing (#60646)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.6+sha-9ebe59f
+ * @license Angular v19.2.6+sha-9171401
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.6+sha-9ebe59f');
+const VERSION = new Version('19.2.6+sha-9171401');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

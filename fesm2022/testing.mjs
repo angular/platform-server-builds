7e3a75257435e1e23e12a793490a0eef34bf02c4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 09:21:57 +0000

    b43fe29 docs: update examples for i18n tokens (#60938)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-0162ceb
+ * @license Angular v20.0.0-next.7+sha-b43fe29
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -22,19 +22,21 @@ const INTERNAL_SERVER_DYNAMIC_PLATFORM_TESTING_PROVIDERS = [
  * Platform for testing
  *
  * @publicApi
+ * @deprecated from v20.0.0, use e2e testing to verify SSR behavior.
  */
 const platformServerTesting = createPlatformFactory(platformCore, 'serverTesting', INTERNAL_SERVER_DYNAMIC_PLATFORM_TESTING_PROVIDERS);
 /**
  * NgModule for testing.
  *
  * @publicApi
+ * @deprecated from v20.0.0, use e2e testing to verify SSR behavior.
  */
 class ServerTestingModule {
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.0.0-next.7+sha-0162ceb", ngImport: i0, type: ServerTestingModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
-    static ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "20.0.0-next.7+sha-0162ceb", ngImport: i0, type: ServerTestingModule, exports: [BrowserDynamicTestingModule] });
-    static ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "20.0.0-next.7+sha-0162ceb", ngImport: i0, type: ServerTestingModule, providers: SERVER_RENDER_PROVIDERS, imports: [BrowserDynamicTestingModule] });
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.0.0-next.7+sha-b43fe29", ngImport: i0, type: ServerTestingModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
+    static ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "20.0.0-next.7+sha-b43fe29", ngImport: i0, type: ServerTestingModule, exports: [BrowserDynamicTestingModule] });
+    static ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "20.0.0-next.7+sha-b43fe29", ngImport: i0, type: ServerTestingModule, providers: SERVER_RENDER_PROVIDERS, imports: [BrowserDynamicTestingModule] });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.0.0-next.7+sha-0162ceb", ngImport: i0, type: ServerTestingModule, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.0.0-next.7+sha-b43fe29", ngImport: i0, type: ServerTestingModule, decorators: [{
             type: NgModule,
             args: [{
                     exports: [BrowserDynamicTestingModule],
--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.7+sha-0162ceb
+ * @license Angular v20.0.0-next.7+sha-b43fe29
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -12,12 +12,14 @@ import * as i1 from '@angular/platform-browser-dynamic/testing';
  * Platform for testing
  *
  * @publicApi
+ * @deprecated from v20.0.0, use e2e testing to verify SSR behavior.
  */
 declare const platformServerTesting: (extraProviders?: StaticProvider[]) => i0.PlatformRef;
 /**
  * NgModule for testing.
  *
  * @publicApi
+ * @deprecated from v20.0.0, use e2e testing to verify SSR behavior.
  */
 declare class ServerTestingModule {
     static ɵfac: i0.ɵɵFactoryDeclaration<ServerTestingModule, never>;

7aaeebae28d74379333f4a5610c056b5dfa55313
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 09:17:27 +0000

    5555e9f docs: update docs centered on NgModules (#60933)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.7+sha-763e3ca
+ * @license Angular v19.2.7+sha-5555e9f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.7+sha-763e3ca');
+const VERSION = new Version('19.2.7+sha-5555e9f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

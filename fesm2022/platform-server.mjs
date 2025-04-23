f26cd4bda0346e3632a75071426c37ef78dccdef
Miles Malerba <mmalerba@google.com>
2025-04-23 21:44:43 +0000

    5b0ec4a release: bump Angular DevTools version to 1.0.30 (#60992)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-5696451
+ * @license Angular v19.2.8+sha-5b0ec4a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.8+sha-5696451');
+const VERSION = new Version('19.2.8+sha-5b0ec4a');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

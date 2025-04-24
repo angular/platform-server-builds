44392cdd21bbcc744565c240d23729ca40079443
Miles Malerba <mmalerba@google.com>
2025-04-24 16:51:38 +0000

    e6c14f9 docs: remove old pipes docs. (#60996)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-c6b41e1
+ * @license Angular v20.0.0-next.8+sha-e6c14f9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.8+sha-c6b41e1');
+const VERSION = new Version('20.0.0-next.8+sha-e6c14f9');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

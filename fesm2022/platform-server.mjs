716266693dc2a97c16e736ddfb51863f1c29821e
Miles Malerba <mmalerba@google.com>
2025-04-25 15:57:52 +0000

    05a03d3 docs(docs-infra): fix mat-tab colors (#61000)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.8+sha-d5fd734
+ * @license Angular v20.0.0-next.8+sha-05a03d3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.8+sha-d5fd734');
+const VERSION = new Version('20.0.0-next.8+sha-05a03d3');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

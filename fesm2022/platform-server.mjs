0dba19733e9bda39bbf3564938ccfc5c71f9c98c
Miles Malerba <mmalerba@google.com>
2025-04-28 15:39:11 +0000

    0203e0b Update dependency-injection.md (#61019)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-54465e8
+ * @license Angular v19.2.8+sha-0203e0b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.8+sha-54465e8');
+const VERSION = new Version('19.2.8+sha-0203e0b');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

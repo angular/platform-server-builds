60d8f6f31129621651c777e20b88bd8258eb4175
Miles Malerba <mmalerba@google.com>
2025-04-28 15:27:34 +0000

    54465e8 fix(docs-infra): use proper API for style overrides (#61008)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-aca1f45
+ * @license Angular v19.2.8+sha-54465e8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.8+sha-aca1f45');
+const VERSION = new Version('19.2.8+sha-54465e8');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

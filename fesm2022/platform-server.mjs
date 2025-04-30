4f0ce711cb8fbb819b414a57587e1209f9a2e775
Andrew Kushnir <akushnir@google.com>
2025-04-30 21:26:09 +0000

    b8f80ab refactor(router): Remove unnecessary runOutsideAngular in view transition helper (#61068)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-next.9+sha-59ca539
+ * @license Angular v20.0.0-next.9+sha-b8f80ab
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-next.9+sha-59ca539');
+const VERSION = new Version('20.0.0-next.9+sha-b8f80ab');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

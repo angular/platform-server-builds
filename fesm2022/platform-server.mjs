e989a218935310b7a182694a2895384a035c2dab
Miles Malerba <mmalerba@google.com>
2025-04-30 15:30:40 +0000

    a554208 build: Run browsers tests without `platform-browser-dynamic` (#60937) (#61060)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.8+sha-6b76bca
+ * @license Angular v19.2.8+sha-a554208
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.8+sha-6b76bca');
+const VERSION = new Version('19.2.8+sha-a554208');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

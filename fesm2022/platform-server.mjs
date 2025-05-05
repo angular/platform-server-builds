4b4ecc323074dc36adce05382b24f06a43d42e15
Andrew Kushnir <akushnir@google.com>
2025-05-05 22:43:34 +0000

    235f7c9 build: update rules_angular digest to 42d4791 (#61126)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-2c17145
+ * @license Angular v20.1.0-next.0+sha-235f7c9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-2c17145');
+const VERSION = new Version('20.1.0-next.0+sha-235f7c9');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

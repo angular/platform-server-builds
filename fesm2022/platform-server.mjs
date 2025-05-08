d94c8cb438241ed3b3667faff96f039aeb421fce
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 12:48:54 +0000

    6f4307f ci: remove redundant dash in workflow file (#61206)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-50993be
+ * @license Angular v20.1.0-next.0+sha-6f4307f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-50993be');
+const VERSION = new Version('20.1.0-next.0+sha-6f4307f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

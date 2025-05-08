5cbde7a20bf21f8641b9aefc8deb713529ac8734
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 14:46:28 +0000

    3b9440b ci: use bash syntax for `CURRENT_BRANCH` in GitHub Actions (#61212)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-6255954
+ * @license Angular v19.2.10+sha-3b9440b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-6255954');
+const VERSION = new Version('19.2.10+sha-3b9440b');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

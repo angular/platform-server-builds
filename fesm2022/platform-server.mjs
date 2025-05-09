e23856a058eba01e891a3334959a7e5b2ff51204
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-09 14:33:34 +0000

    83c98ad ci: restore `.npmrc` before executing `postUpgradeTasks` (#61239)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.0+sha-5cecf0d
+ * @license Angular v20.0.0-rc.0+sha-83c98ad
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.0+sha-5cecf0d');
+const VERSION = new Version('20.0.0-rc.0+sha-83c98ad');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

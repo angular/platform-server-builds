495295164305e60c7740b0572dd8b98a82ec37d3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-09 17:32:42 +0000

    223ca9f docs(docs-infra): limit the flexible width of the main content for better UX (#58831)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-f0dd8f8
+ * @license Angular v19.2.10+sha-223ca9f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-f0dd8f8');
+const VERSION = new Version('19.2.10+sha-223ca9f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

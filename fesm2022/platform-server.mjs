2fd09c8c28cb4f00419782275c13724f1997e857
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-12 22:43:44 +0000

    7be6e5d refactor(migrations): remove unused code (#61260)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-223ca9f
+ * @license Angular v19.2.10+sha-7be6e5d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-223ca9f');
+const VERSION = new Version('19.2.10+sha-7be6e5d');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

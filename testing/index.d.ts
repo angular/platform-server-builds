d08dbeced8d7aa0d080f1fe80166bb9a934d13e8
Joey Perrott <josephperrott@gmail.com>
2025-05-13 22:54:57 +0000

    899cb4a refactor: add explicit types for exports relying on inferred call return type (#61316)

--- testing/index.d.ts
+++ testing/index.d.ts
@@ -1,11 +1,11 @@
 /**
- * @license Angular v19.2.10+sha-7be6e5d
+ * @license Angular v19.2.10+sha-899cb4a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
 
 import * as i0 from '@angular/core';
-import { StaticProvider } from '@angular/core';
+import { StaticProvider, PlatformRef } from '@angular/core';
 import * as i1 from '@angular/platform-browser-dynamic/testing';
 
 /**
@@ -13,7 +13,7 @@ import * as i1 from '@angular/platform-browser-dynamic/testing';
  *
  * @publicApi
  */
-declare const platformServerTesting: (extraProviders?: StaticProvider[]) => i0.PlatformRef;
+declare const platformServerTesting: (extraProviders?: StaticProvider[]) => PlatformRef;
 /**
  * NgModule for testing.
  *

b9540121bb46526bcfc1844ffa3c56fd375ee7fd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 15:40:14 +0000

    3047638 build: add script to diff packages and ensure no unexpected changes (#61336)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.10+sha-ac77e30
+ * @license Angular v19.2.10+sha-3047638
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.10+sha-ac77e30');
+const VERSION = new Version('19.2.10+sha-3047638');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

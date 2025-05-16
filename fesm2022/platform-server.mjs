0f689ec8174cb31c9110003b0024db9ae922e84f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 09:45:07 +0000

    57df361 docs: fix incomplete sentence (#61391)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-85e0baf
+ * @license Angular v19.2.11+sha-57df361
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-85e0baf');
+const VERSION = new Version('19.2.11+sha-57df361');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

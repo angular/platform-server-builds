ae3a0e2407de1fc3151cdff0441d578bd89ef001
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 15:17:32 +0000

    8768894 fix(common): prevent reading chunks if app is destroyed (#61354)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-1148b7e
+ * @license Angular v20.1.0-next.0+sha-8768894
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-1148b7e');
+const VERSION = new Version('20.1.0-next.0+sha-8768894');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

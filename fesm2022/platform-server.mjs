e1479486dbfbf0c20fb43a629a27b1c10af1164e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 12:15:07 +0000

    2796f18 test: set the `noCheck` TS compiler option in declaration-only emission compliance tests (#61470)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-2e7032e
+ * @license Angular v20.1.0-next.0+sha-2796f18
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-2e7032e');
+const VERSION = new Version('20.1.0-next.0+sha-2796f18');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

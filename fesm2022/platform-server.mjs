40fcd65c3a712825ad38cfc83684252d0d4c0ea2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 12:49:16 +0000

    f6b4f72 Revert "test: replace `platform-browser-dynamic` with  `platform-browser` (#61495)" (#61510)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-4430423
+ * @license Angular v19.2.11+sha-f6b4f72
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-4430423');
+const VERSION = new Version('19.2.11+sha-f6b4f72');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

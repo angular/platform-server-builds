6ffeb0ef3648178ac1b3b775e04d664ca2ba68a3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 17:02:48 +0000

    f1f3789 docs(docs-infra): display CLI enum values for enums (#61521)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-8768894
+ * @license Angular v20.1.0-next.0+sha-f1f3789
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-8768894');
+const VERSION = new Version('20.1.0-next.0+sha-f1f3789');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

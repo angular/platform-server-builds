7b23d9e4d464b88b110183ab607f006c692620c7
Andrew Scott <atscott@google.com>
2025-05-21 23:12:14 +0000

    2f5912e docs(docs-infra): fix nav items active status is shown for multiple items (#61597)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.12+sha-ac76549
+ * @license Angular v19.2.12+sha-2f5912e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.12+sha-ac76549');
+const VERSION = new Version('19.2.12+sha-2f5912e');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

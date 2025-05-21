97b5f6c67bde86460a01044a4cba75d6ce47c715
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 10:17:24 +0000

    4e5d6de refactor(core): Merge R3TemplateRef implementation and TemplateRef interface (#61455)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.1+sha-3cc0910
+ * @license Angular v20.0.0-rc.1+sha-4e5d6de
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.1+sha-3cc0910');
+const VERSION = new Version('20.0.0-rc.1+sha-4e5d6de');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

489e3f6f6e04fc8b7b67fc73897c96b5f8c5769b
Andrew Scott <atscott01@gmail.com>
2025-05-21 20:19:08 +0000

    7a43963 docs: release notes for the v20.0.0-rc.2 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-833c615
+ * @license Angular v20.1.0-next.0+sha-7a43963
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-833c615');
+const VERSION = new Version('20.1.0-next.0+sha-7a43963');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

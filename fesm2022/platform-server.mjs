29875a0b92b9fe3179d0c77cb4ed745bdf7fc763
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 11:23:31 +0000

    0104a69 ci: temporarily disable saucelabs for push jobs (#61559)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.11+sha-e4943c6
+ * @license Angular v19.2.11+sha-0104a69
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.11+sha-e4943c6');
+const VERSION = new Version('19.2.11+sha-0104a69');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

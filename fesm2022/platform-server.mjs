e74b4eb21c1a946dd0b0c3cc6e33d301af0e5b3c
Andrew Scott <atscott@google.com>
2025-05-22 22:42:43 +0000

    a6d5479 build: migrate platform-server to rules_js (#61619)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.12+sha-e38cc41
+ * @license Angular v19.2.12+sha-a6d5479
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.12+sha-e38cc41');
+const VERSION = new Version('19.2.12+sha-a6d5479');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

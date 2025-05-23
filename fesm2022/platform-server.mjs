46e13c4ae46689f6b7844c80a1008978a5f349f4
Andrew Scott <atscott@google.com>
2025-05-23 18:57:00 +0000

    9701047 test(common): Add circular deps test to 19.2.x (#61651)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.13+sha-517cde4
+ * @license Angular v19.2.13+sha-9701047
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.13+sha-517cde4');
+const VERSION = new Version('19.2.13+sha-9701047');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

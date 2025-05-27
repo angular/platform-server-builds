def9daf921bfa6a1b0c506e7ae78ed9e39e96493
Andrew Scott <atscott01@gmail.com>
2025-05-27 23:07:08 +0000

    0502cf2 release: cut the v20.0.0-rc.3 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0-rc.2+sha-925a41c
+ * @license Angular v20.0.0-rc.3+sha-0502cf2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0-rc.2+sha-925a41c');
+const VERSION = new Version('20.0.0-rc.3+sha-0502cf2');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

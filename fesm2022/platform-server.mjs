c053e8a7bfa5b780c4aebb698c756a9c3d4bc063
Andrew Scott <atscott01@gmail.com>
2025-05-28 13:40:34 +0000

    cb1b167 release: cut the v19.2.14 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.13+sha-73d3e00
+ * @license Angular v19.2.14+sha-cb1b167
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('19.2.13+sha-73d3e00');
+const VERSION = new Version('19.2.14+sha-cb1b167');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

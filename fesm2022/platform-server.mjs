17a4986ce2a50b5ee66f6f83714211373c8dc386
kirjs <kirts@google.com>
2025-06-03 18:06:14 +0000

    c6f470e docs(docs-infra): fix tutorial tooltip (#61782)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-5813dbd
+ * @license Angular v20.1.0-next.0+sha-c6f470e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-5813dbd');
+const VERSION = new Version('20.1.0-next.0+sha-c6f470e');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

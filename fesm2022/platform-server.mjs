1df3e78064c36cc0f1a776a61211a8b79857726b
kirjs <kirts@google.com>
2025-06-04 00:28:38 +0000

    2bf7249 docs: update resource docs (#61846)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.0+sha-b93fa22
+ * @license Angular v20.0.0+sha-2bf7249
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.0+sha-b93fa22');
+const VERSION = new Version('20.0.0+sha-2bf7249');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

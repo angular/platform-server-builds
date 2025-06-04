c27f94de7b02bb5fa2d3829bfbf00c115fc1c5f6
Kirill Cherkashin <kirill@kcherkashin.org>
2025-06-04 21:28:55 +0000

    586be0e release: cut the v20.1.0-next.0 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-b839d08
+ * @license Angular v20.1.0-next.0+sha-586be0e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-b839d08');
+const VERSION = new Version('20.1.0-next.0+sha-586be0e');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

d01e9bc520825a6f9f65d6abdee0301a3ec86404
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-05 09:00:10 +0000

    967c8fe docs(docs-infra): use application builder for adev karma tests (#58291)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-ab5b7df
+ * @license Angular v20.1.0-next.0+sha-967c8fe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-ab5b7df');
+const VERSION = new Version('20.1.0-next.0+sha-967c8fe');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

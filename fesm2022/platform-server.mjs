7fcc259018c2739c4cb6051337c03e866336e212
Andrew Kushnir <akushnir@google.com>
2025-06-06 01:59:06 +0000

    71017f2 fix(docs-infra): fix wrong `visibleLines` numbers in `docs-code` blocks (#61909)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.1+sha-33bd215
+ * @license Angular v20.0.1+sha-71017f2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.1+sha-33bd215');
+const VERSION = new Version('20.0.1+sha-71017f2');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

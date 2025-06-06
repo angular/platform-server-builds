b568ea2386c625c23fc0ea00c67122e04c584762
Andrew Kushnir <akushnir@google.com>
2025-06-06 00:54:24 +0000

    cfb0dc2 fix(docs-infra): Fix import name for house location (#61914)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.0+sha-9354efc
+ * @license Angular v20.1.0-next.0+sha-cfb0dc2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.0+sha-9354efc');
+const VERSION = new Version('20.1.0-next.0+sha-cfb0dc2');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

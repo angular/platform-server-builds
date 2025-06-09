3b62c95e0e8eaebce6ef1f725535dac40f05ca43
Andrew Kushnir <akushnir@google.com>
2025-06-09 18:30:55 +0000

    85e9505 fix(zone.js): waitForAsync should pass args to the test function (#61755)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.2+sha-74b2aa6
+ * @license Angular v20.0.2+sha-85e9505
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.2+sha-74b2aa6');
+const VERSION = new Version('20.0.2+sha-85e9505');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

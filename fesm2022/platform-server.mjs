d101ce90b6c584db269705feb205371d2cf04e34
Joey Perrott <josephperrott@gmail.com>
2025-06-17 23:54:02 +0000

    04e0d4f build: update the ts_integrity expected for the typescript version in WORKSPACE (#62109)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-1c73f54
+ * @license Angular v20.0.3+sha-04e0d4f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-1c73f54');
+const VERSION = new Version('20.0.3+sha-04e0d4f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

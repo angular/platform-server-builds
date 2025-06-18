00fc6f53954a6f432448d5fbac81d2c327c56f4d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-18 16:12:00 +0000

    f201df9 build: migrate to using new jasmine_test (#62106)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.3+sha-b40613f
+ * @license Angular v20.0.3+sha-f201df9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.3+sha-b40613f');
+const VERSION = new Version('20.0.3+sha-f201df9');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

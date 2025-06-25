617f07a79b16d233b2b109b1b2dab4d0b0f42218
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 12:58:49 +0000

    17a8ca3 ci: Update pullapprove ownership for compiler and zonejs (#62261)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.4+sha-c43f632
+ * @license Angular v20.0.4+sha-17a8ca3
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.4+sha-c43f632');
+const VERSION = new Version('20.0.4+sha-17a8ca3');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

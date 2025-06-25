798ccf57903e80e377d74ef2526254153b1c99ab
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 13:32:40 +0000

    36a1931 fix(core): allow to set a resource in an error state (#62253)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.2+sha-1bf1fb2
+ * @license Angular v20.1.0-next.2+sha-36a1931
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.2+sha-1bf1fb2');
+const VERSION = new Version('20.1.0-next.2+sha-36a1931');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

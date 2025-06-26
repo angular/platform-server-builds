277f9e632c16fae701f04d43d8a01e13fb5a627d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 11:55:20 +0000

    349719c build: update cross-repo angular dependencies (#62288)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-cd30c4d
+ * @license Angular v20.1.0-next.3+sha-349719c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-cd30c4d');
+const VERSION = new Version('20.1.0-next.3+sha-349719c');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

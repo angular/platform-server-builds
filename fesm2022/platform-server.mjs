a7670edd355e7937a535683eefd1e73ffc6a43db
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 13:15:58 +0000

    f943505 refactor(devtools): main nav (#62242)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-6ec2900
+ * @license Angular v20.1.0-next.3+sha-f943505
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-6ec2900');
+const VERSION = new Version('20.1.0-next.3+sha-f943505');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

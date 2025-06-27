20cf5a0e67816800cf32b43ba444650ebc659430
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 09:35:08 +0000

    e3103fe build: migrate devtools to use new sass ruleset (#62298)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.0.5+sha-41230e6
+ * @license Angular v20.0.5+sha-e3103fe
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.0.5+sha-41230e6');
+const VERSION = new Version('20.0.5+sha-e3103fe');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

df1e6a65200549b16e346d1486cb167951b5d7c0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 07:51:35 +0000

    08e2d9d build: update rules_browsers digest to 800d91c (#62358)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-059fb06
+ * @license Angular v20.1.0-next.3+sha-08e2d9d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-059fb06');
+const VERSION = new Version('20.1.0-next.3+sha-08e2d9d');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

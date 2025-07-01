17ef67ea8f22e6d5b025e80b1ae1de3bf37f6f7a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-07-01 17:12:54 +0000

    4ac0147 docs: release notes for the v20.1.0-rc.0 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.0-next.3+sha-b8c6cfa
+ * @license Angular v20.2.0-next.0+sha-4ac0147
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.0-next.3+sha-b8c6cfa');
+const VERSION = new Version('20.2.0-next.0+sha-4ac0147');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

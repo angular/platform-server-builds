d8e44db73c34f899049d92f58946842122295cd8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 17:02:02 +0000

    65b9adf docs: update comment (#62482) PR Close #62482

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-633d01b
+ * @license Angular v20.2.0-next.0+sha-65b9adf
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-633d01b');
+const VERSION = new Version('20.2.0-next.0+sha-65b9adf');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

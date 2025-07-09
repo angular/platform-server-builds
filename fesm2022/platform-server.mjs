a0ec64f96dec8c0fc4c7d4f43976c47fc90be5a4
Andrew Kushnir <akushnir@google.com>
2025-07-09 21:13:08 +0000

    9d5e248 docs: update Angular CLI help (#62520)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.0+sha-75a5d08
+ * @license Angular v20.2.0-next.0+sha-9d5e248
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.0+sha-75a5d08');
+const VERSION = new Version('20.2.0-next.0+sha-9d5e248');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

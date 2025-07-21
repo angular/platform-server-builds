2f863c04e9fe95fa7c13f2876f7b1654318c5bd9
Kirill Cherkashin <kirjs@google.com>
2025-07-21 13:45:40 +0000

    cf48b13 docs: fix code snippet (#62720)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-c36de9e
+ * @license Angular v20.1.2+sha-cf48b13
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-c36de9e');
+const VERSION = new Version('20.1.2+sha-cf48b13');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

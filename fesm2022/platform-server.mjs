9a2f55977d2a19996a2573a75d1fa0720b7c18a0
Kirill Cherkashin <kirjs@google.com>
2025-07-21 16:26:02 +0000

    aee5260 fix(devtools): stop reseting currentlyMatchedIndex when a node is selected in the component explorer (#62727)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-70d0c10
+ * @license Angular v20.1.2+sha-aee5260
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-70d0c10');
+const VERSION = new Version('20.1.2+sha-aee5260');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

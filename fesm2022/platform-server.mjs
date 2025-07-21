826dbeaf5e91d42e26a448a1a17cba62e3ff7a23
Kirill Cherkashin <kirjs@google.com>
2025-07-21 16:14:48 +0000

    70d0c10 docs: update Angular CLI help (#62730)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-bca8a3c
+ * @license Angular v20.1.2+sha-70d0c10
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-bca8a3c');
+const VERSION = new Version('20.1.2+sha-70d0c10');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

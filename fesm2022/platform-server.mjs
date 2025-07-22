900f015581f6c123a8cd7d7bf1226a44892f449b
Kirill Cherkashin <kirjs@google.com>
2025-07-22 14:22:35 +0000

    db8f7ce build: move generating doc files for @angular/ssr to using an intermediate typescript file (#62734)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-38b7ab2
+ * @license Angular v20.1.2+sha-db8f7ce
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-38b7ab2');
+const VERSION = new Version('20.1.2+sha-db8f7ce');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

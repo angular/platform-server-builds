5ad2c2b7abe9139c19a6b77822f2f18a4d7b0964
Kirill Cherkashin <kirjs@google.com>
2025-07-23 18:08:42 +0000

    155dbee docs: guard a resource error with `hasValue()` (#62708)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.2+sha-6398baa
+ * @license Angular v20.1.2+sha-155dbee
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.2+sha-6398baa');
+const VERSION = new Version('20.1.2+sha-155dbee');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

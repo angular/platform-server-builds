d9e8ee5f096674d27a4fe9b378a613823cbc2bbe
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 14:07:52 +0000

    24e6f4d docs: add initial documentation for MCP server tools (#62777)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.2+sha-8ebc1e7
+ * @license Angular v20.2.0-next.2+sha-24e6f4d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.2+sha-8ebc1e7');
+const VERSION = new Version('20.2.0-next.2+sha-24e6f4d');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

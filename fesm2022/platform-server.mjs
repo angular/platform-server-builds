b62754c99b6b85b7a8e2661c42dead72a2b015be
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-29 17:00:17 +0000

    ea1afec build: move http_server and generate_api_docs into defaults2.bzl (#62878)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.3+sha-0b9f53a
+ * @license Angular v20.1.3+sha-ea1afec
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.3+sha-0b9f53a');
+const VERSION = new Version('20.1.3+sha-ea1afec');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

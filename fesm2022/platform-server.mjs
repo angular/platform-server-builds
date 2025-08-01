b3919a44a9b10690cc4a44e6819a0e355fc10549
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 14:27:15 +0000

    c2b48be build: move to pnpm version 9.15.9 (#62951)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.4+sha-f700bd9
+ * @license Angular v20.1.4+sha-c2b48be
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.4+sha-f700bd9');
+const VERSION = new Version('20.1.4+sha-c2b48be');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

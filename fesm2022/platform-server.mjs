af34039320d53ade4f865a39bf8b9d1273eeffd0
Andrew Scott <atscott@google.com>
2025-08-07 16:16:28 +0000

    8f234ff docs(devtools): update `pnpm` install command (#63040)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-0430a56
+ * @license Angular v20.1.6+sha-8f234ff
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.6+sha-0430a56');
+const VERSION = new Version('20.1.6+sha-8f234ff');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

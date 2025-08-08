071e3ff7c46d584a8e8f2449b3111f023043e7fc
Andrew Scott <atscott@google.com>
2025-08-08 18:12:11 +0000

    3b214d2 feat(service-worker): Logs unhandled promise rejections in service worker (#63059)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-next.6+sha-6a11846
+ * @license Angular v20.2.0-next.6+sha-3b214d2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-next.6+sha-6a11846');
+const VERSION = new Version('20.2.0-next.6+sha-3b214d2');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

24fe91f403b3ff0c48c808cd64fceceb63e8da9c
Andrew Scott <atscott@google.com>
2025-08-12 18:10:08 +0000

    4635095 fix(docs-infra): put content in `<main>` element (#63121)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0-rc.0+sha-31df4d2
+ * @license Angular v20.2.0-rc.0+sha-4635095
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0-rc.0+sha-31df4d2');
+const VERSION = new Version('20.2.0-rc.0+sha-4635095');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

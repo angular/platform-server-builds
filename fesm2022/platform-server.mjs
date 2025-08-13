74c46ac01e26c8b2c44377cc3def50fe1384cf59
Andrew Scott <atscott01@gmail.com>
2025-08-13 16:34:48 +0000

    774b225 docs: add rxjs-interop section for `rxResource` (#63125)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.1.6+sha-2c12f2a
+ * @license Angular v20.1.6+sha-774b225
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.1.6+sha-2c12f2a');
+const VERSION = new Version('20.1.6+sha-774b225');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

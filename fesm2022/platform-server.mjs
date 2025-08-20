dfa995169b09962cbbd2b153adf7388d790ed2b8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 09:57:28 +0000

    086ae5e build: update all non-major dependencies (#63264)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-eeeaadc
+ * @license Angular v21.0.0-next.0+sha-086ae5e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-eeeaadc');
+const VERSION = new Version('21.0.0-next.0+sha-086ae5e');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

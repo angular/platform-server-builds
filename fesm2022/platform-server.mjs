b0fcc6021061c185f3e05da123ca2a9ad4cd1668
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-21 10:26:43 +0000

    a28672f fix(compiler): Keep paraenthesis in Nullish + Boolean expression. (#63292)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.0+sha-65c5a73
+ * @license Angular v20.2.0+sha-a28672f
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.0+sha-65c5a73');
+const VERSION = new Version('20.2.0+sha-a28672f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

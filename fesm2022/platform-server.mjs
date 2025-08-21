33a4ef9ccd77bdeb2f1e9577209cea3d45e3bfc5
Andrew Kushnir <akushnir@google.com>
2025-08-21 21:17:52 +0000

    6f541b8 build: update dependency rollup to v4.47.1 (#63304)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-69df2c0
+ * @license Angular v21.0.0-next.0+sha-6f541b8
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-69df2c0');
+const VERSION = new Version('21.0.0-next.0+sha-6f541b8');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

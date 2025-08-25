ba086ca94c1a1f3cc99bab9860fd221cf87bf435
Andrew Kushnir <akushnir@google.com>
2025-08-25 22:42:42 +0000

    a26d94d build: enable strict deps enforcement for ts_project (#63376)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.1+sha-454a269
+ * @license Angular v20.2.1+sha-a26d94d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.1+sha-454a269');
+const VERSION = new Version('20.2.1+sha-a26d94d');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

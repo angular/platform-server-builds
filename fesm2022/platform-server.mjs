c83025b706be4f503922afb1f106666b3e27cb53
Andrew Kushnir <akushnir@google.com>
2025-08-25 23:33:12 +0000

    67d4449 build: update cross-repo angular dependencies (#63379)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.0+sha-2fcafb6
+ * @license Angular v21.0.0-next.0+sha-67d4449
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.0+sha-2fcafb6');
+const VERSION = new Version('21.0.0-next.0+sha-67d4449');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

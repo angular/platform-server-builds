4e125ee24e7693ac4029be4db6732f06f2abb5c0
Andrew Kushnir <akushnir@google.com>
2025-08-27 18:46:44 +0000

    1bb30c7 docs(docs-infra): remove dependency on `html-entities` (#63416)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-3a3bd36
+ * @license Angular v21.0.0-next.1+sha-1bb30c7
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-3a3bd36');
+const VERSION = new Version('21.0.0-next.1+sha-1bb30c7');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

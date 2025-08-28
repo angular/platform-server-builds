04b740b753e5bd408ed9b7bd49341220bd3a7e7f
Miles Malerba <mmalerba@google.com>
2025-08-28 16:58:05 +0000

    c9b215b build: update cross-repo angular dependencies (#63443)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-2f5f975
+ * @license Angular v21.0.0-next.1+sha-c9b215b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-2f5f975');
+const VERSION = new Version('21.0.0-next.1+sha-c9b215b');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

fe8097e2b834bb53c0186edccd7583bba58354c0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 10:13:39 +0000

    fe533cd docs: update Angular CLI help (#63420)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-421edfe
+ * @license Angular v20.2.2+sha-fe533cd
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-421edfe');
+const VERSION = new Version('20.2.2+sha-fe533cd');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

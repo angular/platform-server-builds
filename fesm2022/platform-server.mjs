11808535d028b0947e2fa6fab2b1efbab56edb1d
Miles Malerba <mmalerba@google.com>
2025-08-28 16:46:51 +0000

    2f5f975 refactor(forms): fix dependencies (#63451)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-1f4c5f7
+ * @license Angular v21.0.0-next.1+sha-2f5f975
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-1f4c5f7');
+const VERSION = new Version('21.0.0-next.1+sha-2f5f975');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

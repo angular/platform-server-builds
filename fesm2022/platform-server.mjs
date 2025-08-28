712dad4f71909cfca209263b05ebfe75cabb8a46
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 14:16:59 +0000

    114906d fix(core): Fix cancellation of animation enter classes (#63442)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-2e05dea
+ * @license Angular v20.2.2+sha-114906d
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-2e05dea');
+const VERSION = new Version('20.2.2+sha-114906d');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

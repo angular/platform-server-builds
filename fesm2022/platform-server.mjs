0ae43773a859e1ae8aef0014b6f4ba14c4927930
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 10:24:57 +0000

    690405b docs: Add docs service worker to new options (#63436)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.2+sha-fe533cd
+ * @license Angular v20.2.2+sha-690405b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.2+sha-fe533cd');
+const VERSION = new Version('20.2.2+sha-690405b');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

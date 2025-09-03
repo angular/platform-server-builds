c756562effe05efb773c66d2d9b821ab205b875c
Miles Malerba <mmalerba@google.com>
2025-09-03 16:37:41 +0000

    8738b81 build: use common jasmine rule from dev-infra (#63568)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.1+sha-49d5de4
+ * @license Angular v21.0.0-next.1+sha-8738b81
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.1+sha-49d5de4');
+const VERSION = new Version('21.0.0-next.1+sha-8738b81');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

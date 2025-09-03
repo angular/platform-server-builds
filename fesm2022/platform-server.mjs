e9932e7385c97a8f4ec3eed8cbe661b627f22960
Andrew Scott <atscott@google.com>
2025-09-03 16:56:54 +0000

    320f1d6 build: update @angular/ng-dev digest to 435c59b (#63571)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.2.3+sha-991785f
+ * @license Angular v20.2.3+sha-320f1d6
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.2.3+sha-991785f');
+const VERSION = new Version('20.2.3+sha-320f1d6');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

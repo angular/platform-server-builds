2036bed8b8fc121818081e358360eec1442897f7
Andrew Scott <atscott@google.com>
2025-09-08 18:21:50 +0000

    160e065 feat(devtools): store last selected tab in settings (#63594)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.2+sha-e7fbee0
+ * @license Angular v21.0.0-next.2+sha-160e065
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -301,7 +301,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.2+sha-e7fbee0');
+const VERSION = new Version('21.0.0-next.2+sha-160e065');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

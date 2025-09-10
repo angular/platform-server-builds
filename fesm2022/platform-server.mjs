39fdf798d7b5cf2624253d36be60559aef788dfe
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-10 23:13:07 +0000

    c7fd7f9 docs: update mcp guide (#63664)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-5f5828f
+ * @license Angular v20.3.0+sha-c7fd7f9
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-5f5828f');
+const VERSION = new Version('20.3.0+sha-c7fd7f9');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

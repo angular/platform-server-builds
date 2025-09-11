1024e46b365ae901abdce7ff4fb1e19e6077da20
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 16:36:02 +0000

    5a1c7ee ci: fallback to last known branch when branch does not exist downstream (#63733)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-663f48c
+ * @license Angular v21.0.0-next.3+sha-5a1c7ee
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-663f48c');
+const VERSION = new Version('21.0.0-next.3+sha-5a1c7ee');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

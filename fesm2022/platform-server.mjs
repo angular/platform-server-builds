8eb92d2b73b7c61f969ee6bd5c750f224cfc4df4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 17:07:56 +0000

    cd36189 ci: update pullapprove public-api rules (#63737)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-38c9921
+ * @license Angular v20.3.0+sha-cd36189
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-38c9921');
+const VERSION = new Version('20.3.0+sha-cd36189');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

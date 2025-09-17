e59229a3cefa1f2f595c5df06380256dde257e0c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 21:22:27 +0000

    8ad26ed refactor(platform-browser): Remove zone provideres from BrowserTestingModule (#63872)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-95207b6
+ * @license Angular v21.0.0-next.4+sha-8ad26ed
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-95207b6');
+const VERSION = new Version('21.0.0-next.4+sha-8ad26ed');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

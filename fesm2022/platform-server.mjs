976343276ea4109ea00b4f28bd978dc8354cad53
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 14:56:47 +0000

    1c7106a docs: fix pluralization error in templates guide (#63854)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.0+sha-8cd2d8b
+ * @license Angular v20.3.0+sha-1c7106a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.0+sha-8cd2d8b');
+const VERSION = new Version('20.3.0+sha-1c7106a');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

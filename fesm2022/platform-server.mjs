6cde58c2743270b8ed6dc7b262a73aefa455d5bd
Jan Martin <jan.krems@gmail.com>
2025-09-17 18:01:46 +0000

    7d3919e release: bump Angular DevTools version to 1.2.1 (#63879)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.3+sha-dee6ef8
+ * @license Angular v21.0.0-next.3+sha-7d3919e
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.3+sha-dee6ef8');
+const VERSION = new Version('21.0.0-next.3+sha-7d3919e');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

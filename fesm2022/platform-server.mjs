1ab06579a0a3501be0db87d31340b7592451ba9b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-18 20:20:28 +0000

    d40867c docs: re-add ai event banner on homepage (#63922)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-96a070e
+ * @license Angular v20.3.1+sha-d40867c
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-96a070e');
+const VERSION = new Version('20.3.1+sha-d40867c');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

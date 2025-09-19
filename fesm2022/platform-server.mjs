d95e69364f902765673a1f45289a067e5b46501a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 15:06:05 +0000

    fec7c28 fix(core): Error on invalid APP_ID (#63252)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-4c091ab
+ * @license Angular v21.0.0-next.4+sha-fec7c28
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-4c091ab');
+const VERSION = new Version('21.0.0-next.4+sha-fec7c28');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

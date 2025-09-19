85821435d76b17480fb186254e347f2e8dc041da
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 20:31:35 +0000

    56cb093 refactor(core): drop the usage of `PromiseSettledResult<void>` (#63955)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.4+sha-e835c35
+ * @license Angular v21.0.0-next.4+sha-56cb093
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.4+sha-e835c35');
+const VERSION = new Version('21.0.0-next.4+sha-56cb093');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

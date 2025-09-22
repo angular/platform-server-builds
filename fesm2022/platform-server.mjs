47441f68c317522f34787d3be65b2e77b9765df7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-22 16:59:43 +0000

    a67d822 refactor(core): use version>0 instead of hasRun (#62467)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-1d4f81c
+ * @license Angular v20.3.1+sha-a67d822
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-1d4f81c');
+const VERSION = new Version('20.3.1+sha-a67d822');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

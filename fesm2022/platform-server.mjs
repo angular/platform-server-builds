12601fe58506d1ba82e9784f194b78f45fa0dc23
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 14:55:11 +0000

    9f0c6bc ci: use release config when deploying adev (#64030)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-301155b
+ * @license Angular v20.3.1+sha-9f0c6bc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-301155b');
+const VERSION = new Version('20.3.1+sha-9f0c6bc');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

1ffb68d49e43f55d60cee156e61a4f7025a67da4
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-09-24 23:26:54 +0000

    31f2030 release: cut the v20.3.2 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.1+sha-c525a5a
+ * @license Angular v20.3.2+sha-31f2030
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.1+sha-c525a5a');
+const VERSION = new Version('20.3.2+sha-31f2030');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

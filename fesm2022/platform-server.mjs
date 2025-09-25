8a30cd1800582b2a0690b4265a75e53ed3520da7
kirjs <kirts@google.com>
2025-09-25 19:44:41 +0000

    7d720d5 build: update cross-repo angular dependencies (#64077)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-36c4b3f
+ * @license Angular v20.3.2+sha-7d720d5
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.2+sha-36c4b3f');
+const VERSION = new Version('20.3.2+sha-7d720d5');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

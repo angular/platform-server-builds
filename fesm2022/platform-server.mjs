f7f389c0278ccba9ec9307f54bf62e6b16d5f670
kirjs <kirts@google.com>
2025-09-25 18:26:41 +0000

    cd23761 build: mark lsp integration test as flaky (#64085)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-1dee0d3
+ * @license Angular v21.0.0-next.5+sha-cd23761
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-1dee0d3');
+const VERSION = new Version('21.0.0-next.5+sha-cd23761');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

9d8f9f3d1f232c5900bde292842474cb2667c194
kirjs <kirts@google.com>
2025-09-26 13:37:36 +0000

    8ff536b docs: update text (#64095)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-14d5b0e
+ * @license Angular v21.0.0-next.5+sha-8ff536b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-14d5b0e');
+const VERSION = new Version('21.0.0-next.5+sha-8ff536b');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

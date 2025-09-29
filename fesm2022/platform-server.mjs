ddfa4425973c2d93b09c56df18882443ef23f888
kirjs <kirts@google.com>
2025-09-29 17:36:19 +0000

    aa28aa2 docs: Adds guide for custom HTTP parameter encoding (#64119)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-4b3f7a0
+ * @license Angular v20.3.2+sha-aa28aa2
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.2+sha-4b3f7a0');
+const VERSION = new Version('20.3.2+sha-aa28aa2');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

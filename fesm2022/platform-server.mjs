aad26a51c57bc8dffba308a939fc21f69f9eeffc
kirjs <kirts@google.com>
2025-09-29 20:52:44 +0000

    9a913dc docs(docs-infra): clean up code and use inferred types where possible (#64139)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-04462ed
+ * @license Angular v21.0.0-next.5+sha-9a913dc
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-04462ed');
+const VERSION = new Version('21.0.0-next.5+sha-9a913dc');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

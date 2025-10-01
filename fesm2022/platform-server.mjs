9683be6441ef09981a5945393f586b8c0b8cf111
kirjs <kirts@google.com>
2025-10-01 16:49:26 +0000

    0537083 Revert "feat(common):  Support of optional keys for the KeyValue pipe (#48814)" (#64179)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-1b102c2
+ * @license Angular v21.0.0-next.5+sha-0537083
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-1b102c2');
+const VERSION = new Version('21.0.0-next.5+sha-0537083');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

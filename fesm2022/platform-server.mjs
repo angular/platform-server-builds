2fc6b34ad303a3f54f6ef68af1b717e8fc8dc26e
kirjs <kirts@google.com>
2025-10-01 13:15:42 +0000

    0e928fb fix(core): Fixes animations in conjunction with content projection (#63776)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.2+sha-24c979d
+ * @license Angular v20.3.2+sha-0e928fb
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.2+sha-24c979d');
+const VERSION = new Version('20.3.2+sha-0e928fb');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

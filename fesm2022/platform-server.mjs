8ed8675fc046a6107b01615a54a9ac2a14c53c4e
Miles Malerba <mmalerba@google.com>
2025-10-01 22:47:01 +0000

    d892aab Revert "fix(core): Fixes animations in conjunction with content projection (#63776)" (#64189)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-be0455a
+ * @license Angular v21.0.0-next.5+sha-d892aab
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-be0455a');
+const VERSION = new Version('21.0.0-next.5+sha-d892aab');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

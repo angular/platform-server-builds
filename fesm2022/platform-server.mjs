652e87bfdec894c41a3f20fcc7aaadeb153e409e
kirjs <kirts@google.com>
2025-10-01 14:40:27 +0000

    5319177 build: update dependency ng-packagr to v21.0.0-next.4 (#64170)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-3bd4362
+ * @license Angular v21.0.0-next.5+sha-5319177
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-3bd4362');
+const VERSION = new Version('21.0.0-next.5+sha-5319177');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

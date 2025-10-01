06464fbbc9f8f54adbd199b3c9c769ce4474ee5c
kirjs <kirts@google.com>
2025-10-01 13:07:27 +0000

    f866132 docs: add allowed-host mention (#64145)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-159be56
+ * @license Angular v21.0.0-next.5+sha-f866132
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-159be56');
+const VERSION = new Version('21.0.0-next.5+sha-f866132');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

54b982273a9b5cd72a2d9058ffc0cbefe78485f1
kirjs <kirts@google.com>
2025-10-01 17:18:05 +0000

    ee7d49a docs: update Angular CLI help (#64174)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-0537083
+ * @license Angular v21.0.0-next.5+sha-ee7d49a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-0537083');
+const VERSION = new Version('21.0.0-next.5+sha-ee7d49a');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

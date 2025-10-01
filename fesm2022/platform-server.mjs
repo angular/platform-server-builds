5a5b481439b1fee36505ac0c53339eb7f2ff0c40
kirjs <kirts@google.com>
2025-10-01 17:57:21 +0000

    be0455a fix(elements): return value on signal input getter (#62113)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-c0e1c41
+ * @license Angular v21.0.0-next.5+sha-be0455a
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-c0e1c41');
+const VERSION = new Version('21.0.0-next.5+sha-be0455a');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

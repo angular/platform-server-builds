847519a24b5f331b5a3f4f739ce5f30889244001
Kirill Cherkashin <kirill@kcherkashin.org>
2025-10-02 12:28:50 +0000

    732635b release: cut the v21.0.0-next.6 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.5+sha-4699ccf
+ * @license Angular v21.0.0-next.6+sha-732635b
  * (c) 2010-2025 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.5+sha-4699ccf');
+const VERSION = new Version('21.0.0-next.6+sha-732635b');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

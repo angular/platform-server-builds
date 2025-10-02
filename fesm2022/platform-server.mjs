52fe318c505b7b6286c7140429296bd794267d6b
Miles Malerba <mmalerba@google.com>
2025-10-02 15:05:47 +0000

    b687d9c docs: update website URL in license banners  (#64183)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,6 +1,6 @@
 /**
- * @license Angular v20.3.3+sha-c314ccb
- * (c) 2010-2025 Google LLC. https://angular.io/
+ * @license Angular v20.3.3+sha-b687d9c
+ * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.3+sha-c314ccb');
+const VERSION = new Version('20.3.3+sha-b687d9c');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

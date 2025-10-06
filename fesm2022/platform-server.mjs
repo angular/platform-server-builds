2c60f7f3a16c6de342b9137557e2b1e16119e887
Miles Malerba <mmalerba@google.com>
2025-10-06 19:02:46 +0000

    6e5be38 build: update pnpm to v10.18.0 (#64233)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.3+sha-853ed16
+ * @license Angular v20.3.3+sha-6e5be38
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.3+sha-853ed16');
+const VERSION = new Version('20.3.3+sha-6e5be38');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

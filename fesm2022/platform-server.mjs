b885bee795932f3b209262850041ffe89953749e
Miles Malerba <mmalerba@google.com>
2025-10-08 14:55:08 +0000

    28b0d53 release: cut the v20.3.4 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.3+sha-11d83d1
+ * @license Angular v20.3.4+sha-28b0d53
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.3+sha-11d83d1');
+const VERSION = new Version('20.3.4+sha-28b0d53');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

60695e7730312fa787bc93e2e8ed341c949e4ae8
Andrew Kushnir <akushnir@google.com>
2025-10-13 17:05:30 +0000

    eee8eab docs: improve clarity of self-closing tag migration example (#64388)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.7+sha-09a927a
+ * @license Angular v21.0.0-next.7+sha-eee8eab
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.7+sha-09a927a');
+const VERSION = new Version('21.0.0-next.7+sha-eee8eab');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

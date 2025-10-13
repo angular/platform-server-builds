db4cd2b362055c75fc45fed5e256ab0684b78672
Andrew Kushnir <akushnir@google.com>
2025-10-13 23:02:52 +0000

    fdd0856 docs: Removes standalone flag from drag-drop examples (#64395)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.4+sha-0c58b5f
+ * @license Angular v20.3.4+sha-fdd0856
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.4+sha-0c58b5f');
+const VERSION = new Version('20.3.4+sha-fdd0856');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

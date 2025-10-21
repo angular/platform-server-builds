9b8bac3de69dd11c269c6c473fbc6e8e5ec21c37
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-21 15:29:06 +0000

    e568ddb docs: Standardizes code example formatting in Angular Tools and fixed highlight (#64554)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.6+sha-cfd8ed3
+ * @license Angular v20.3.6+sha-e568ddb
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.6+sha-cfd8ed3');
+const VERSION = new Version('20.3.6+sha-e568ddb');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

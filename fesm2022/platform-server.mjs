7d31c0d6cbcc9e423a0baeba68b7532e68eb3be2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 16:46:21 +0000

    1f389b8 fix(compiler-cli): missingStructuralDirective diagnostic produces false negatives (#64579)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.8+sha-9d48e53
+ * @license Angular v21.0.0-next.8+sha-1f389b8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.0.0-next.8+sha-9d48e53');
+const VERSION = new Version('21.0.0-next.8+sha-1f389b8');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

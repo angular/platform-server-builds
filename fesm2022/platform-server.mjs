ea22f2f32ef1e819cecefb59fb8dab4c8b0947a7
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-23 14:07:02 +0000

    6a3e881 build: update cross-repo angular dependencies (#64616)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-3ace778
+ * @license Angular v21.1.0-next.0+sha-6a3e881
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('21.1.0-next.0+sha-3ace778');
+const VERSION = new Version('21.1.0-next.0+sha-6a3e881');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

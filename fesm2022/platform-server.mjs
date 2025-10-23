8d9f065b66a96aacbd16cc49c30a9ce77da5257b
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-23 10:40:36 +0000

    50d2670 docs: Add router config options (#64529)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.7+sha-2a249b7
+ * @license Angular v20.3.7+sha-50d2670
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.7+sha-2a249b7');
+const VERSION = new Version('20.3.7+sha-50d2670');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

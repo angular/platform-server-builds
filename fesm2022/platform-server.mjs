7a71a54b948137317a7e0703742a041337549110
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-28 09:36:15 +0000

    81b8765 build: lock file maintenance

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-next.9+sha-54ad230
+ * @license Angular v21.0.0-next.9+sha-81b8765
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = new Version('21.0.0-next.9+sha-54ad230');
+const VERSION = new Version('21.0.0-next.9+sha-81b8765');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

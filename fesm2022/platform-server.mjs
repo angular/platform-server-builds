74406f8ba3f3ebd35f635a2bc4b7d52007e226af
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-29 17:39:20 +0000

    b517c9c release: cut the v20.3.9 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.8+sha-77bc6ee
+ * @license Angular v20.3.9+sha-b517c9c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.8+sha-77bc6ee');
+const VERSION = new Version('20.3.9+sha-b517c9c');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

b9c2f6a87993b6fa3ae9287928bb64e9455cb2c7
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:21:37 +0000

    90452f5 docs: update Angular Aria apis (#64804)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.0+sha-60cc3cb
+ * @license Angular v21.0.0-rc.0+sha-90452f5
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = new Version('21.0.0-rc.0+sha-60cc3cb');
+const VERSION = new Version('21.0.0-rc.0+sha-90452f5');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

d8676d332544dda82951ad6e33421286f9f6d443
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:29:46 +0000

    040585d ci: make the github action fail when a firebase command fails (#64801)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-54ad3f6
+ * @license Angular v20.3.9+sha-040585d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-54ad3f6');
+const VERSION = new Version('20.3.9+sha-040585d');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

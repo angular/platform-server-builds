c21a894b3aa46f11e04eb62021ee0697194a8b71
Andrew Scott <atscott@google.com>
2025-11-03 22:35:56 +0000

    a57c03d docs(docs-infra): Improve font loading performance with preconnect

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-88952b5
+ * @license Angular v20.3.9+sha-a57c03d
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-88952b5');
+const VERSION = new Version('20.3.9+sha-a57c03d');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

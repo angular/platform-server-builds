15dcee78cb1add3c5bcb19ded26e84560d989ff8
Andrew Scott <atscott@google.com>
2025-11-04 00:38:54 +0000

    4f7b97c docs: simplify tutorial signals with self-closing component tags

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.9+sha-3d6a206
+ * @license Angular v20.3.9+sha-4f7b97c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = new Version('20.3.9+sha-3d6a206');
+const VERSION = new Version('20.3.9+sha-4f7b97c');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

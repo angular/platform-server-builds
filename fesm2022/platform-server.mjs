01526f8598cd41124ccc14a2dfe6d2d4ec811e2f
Andrew Scott <atscott01@gmail.com>
2025-11-05 19:03:42 +0000

    aa104c9 docs: release notes for the v20.3.10 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-7f44345
+ * @license Angular v21.1.0-next.0+sha-aa104c9
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-7f44345');
+const VERSION = new Version('21.1.0-next.0+sha-aa104c9');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

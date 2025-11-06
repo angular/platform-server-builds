a5bec064068302b077ecb9fe4a5324d960118b98
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-06 16:46:04 +0000

    ca3ef38 refactor(common): Removes unused imports to clean up dependencies

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-e87f423
+ * @license Angular v21.1.0-next.0+sha-ca3ef38
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = new Version('21.1.0-next.0+sha-e87f423');
+const VERSION = new Version('21.1.0-next.0+sha-ca3ef38');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

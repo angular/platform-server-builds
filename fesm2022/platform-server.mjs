cf1317288e3d8f0a51dc27af22de3d5bf1b6892d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-06 20:13:12 +0000

    acaa1e6 build: restrain visibility to compiler-cli subpackages (#64732)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-e90f9e4
+ * @license Angular v21.0.0-rc.1+sha-acaa1e6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-e90f9e4');
+const VERSION = new Version('21.0.0-rc.1+sha-acaa1e6');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

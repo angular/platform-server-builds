c408b68b906a8f538ccdf4d2efb795e9d4c19955
Andrew Kushnir <akushnir@google.com>
2025-11-07 18:51:41 +0000

    f54cc4f fix(compiler): support commas in :host() argument

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-0552e72
+ * @license Angular v21.0.0-rc.1+sha-f54cc4f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = new Version('21.0.0-rc.1+sha-0552e72');
+const VERSION = new Version('21.0.0-rc.1+sha-f54cc4f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

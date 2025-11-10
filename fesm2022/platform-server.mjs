b603d4bf7bf1ab0ba7c3ee3b0888bc4ebcd3b009
Andrew Kushnir <akushnir@google.com>
2025-11-10 22:21:12 +0000

    2019940 docs: add links to SDUI example and livestream

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.1+sha-ab98e71
+ * @license Angular v21.0.0-rc.1+sha-2019940
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-ab98e71');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.1+sha-2019940');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

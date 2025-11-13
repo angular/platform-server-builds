6c92b63605b84aa2166691000d03a24c05351277
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 22:10:02 +0000

    70da36f docs(docs-infra): improve theme picker accessibility and add animations

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.11+sha-06bc146
+ * @license Angular v20.3.11+sha-70da36f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-06bc146');
+const VERSION = /* @__PURE__ */ new Version('20.3.11+sha-70da36f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

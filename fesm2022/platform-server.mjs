f7eac2403a655b55f2d05660b67aa6659950859a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 22:50:10 +0000

    91de094 fix(devtools): refine when signal graph button is shown

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0-rc.3+sha-f279aab
+ * @license Angular v21.0.0-rc.3+sha-91de094
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-f279aab');
+const VERSION = /* @__PURE__ */new Version('21.0.0-rc.3+sha-91de094');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

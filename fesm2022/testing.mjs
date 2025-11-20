44f070e2af1c970964860dc4e2ce5d09a3d469e6
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 22:09:30 +0000

    1ab461c refactor(router): Store route injector on ActivatedRoute instance

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-2ecdb22
+ * @license Angular v21.1.0-next.0+sha-1ab461c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -18,7 +18,7 @@ const platformServerTesting = createPlatformFactory(platformCore, 'serverTesting
 class ServerTestingModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-2ecdb22",
+    version: "21.1.0-next.0+sha-1ab461c",
     ngImport: i0,
     type: ServerTestingModule,
     deps: [],
@@ -26,14 +26,14 @@ class ServerTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.1.0-next.0+sha-2ecdb22",
+    version: "21.1.0-next.0+sha-1ab461c",
     ngImport: i0,
     type: ServerTestingModule,
     exports: [BrowserDynamicTestingModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.1.0-next.0+sha-2ecdb22",
+    version: "21.1.0-next.0+sha-1ab461c",
     ngImport: i0,
     type: ServerTestingModule,
     providers: SERVER_RENDER_PROVIDERS,
@@ -42,7 +42,7 @@ class ServerTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.1.0-next.0+sha-2ecdb22",
+  version: "21.1.0-next.0+sha-1ab461c",
   ngImport: i0,
   type: ServerTestingModule,
   decorators: [{

01201a388d59b812b70bb8929a83c3b718c24240
kirjs <kirts@google.com>
2025-11-20 22:08:25 +0000

    70fb1d6 ci: try to resolve vscode extension e2e flakes

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-da97099
+ * @license Angular v21.0.0+sha-70fb1d6
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-da97099');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-70fb1d6');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

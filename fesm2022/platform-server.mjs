c91c59ef3265a3e81e145f3846f7dbeaab5f78c7
kirjs <kirts@google.com>
2025-11-20 20:06:04 +0000

    b9181f1 docs: prioritize ng add for Tailwind CSS and clarify manual steps

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-82c085d
+ * @license Angular v21.0.0+sha-b9181f1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-82c085d');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-b9181f1');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

466db4afedbaa4f98b399f874334d286dc65ae1d
kirjs <kirts@google.com>
2025-11-20 15:40:04 +0000

    cf7579f docs: add signal forms custom controls guide

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-f31fbdd
+ * @license Angular v21.0.0+sha-cf7579f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-f31fbdd');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-cf7579f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

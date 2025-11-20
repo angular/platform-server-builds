a5cb84245b6ef1f686e11c26ef41e394895ef5b1
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-20 16:50:59 +0000

    183d194 docs: add 20 to 21 update guide

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-29185a8
+ * @license Angular v21.1.0-next.0+sha-183d194
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-29185a8');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-183d194');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

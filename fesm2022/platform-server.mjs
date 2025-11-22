a78966490cfb283412f298f41134d7a23bba98ab
kirjs <kirts@google.com>
2025-11-22 14:15:32 +0000

    696aaa1 docs: fix router event subscription in error handler example

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-a70a191
+ * @license Angular v21.0.0+sha-696aaa1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-a70a191');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-696aaa1');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

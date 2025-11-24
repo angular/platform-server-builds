08be49e00d60ecb103669ff503c8827a5497a23a
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 17:23:39 +0000

    9dd7cc1 ci: adding myself as optional to the compiler scope

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-0f4b11c
+ * @license Angular v21.1.0-next.0+sha-9dd7cc1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-0f4b11c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-9dd7cc1');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

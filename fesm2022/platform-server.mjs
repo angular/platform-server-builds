96986d2f61ffbd1ab1110099fa0c8772ce4803a2
kirjs <kirts@google.com>
2025-11-24 20:35:23 +0000

    70507b8 fix(core): debug data causing memory leak for root effects

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-4845a33
+ * @license Angular v21.0.0+sha-70507b8
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-4845a33');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-70507b8');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

e04c56a85e51ddd7e9527d4416e3ff6d2b97c7f3
kirjs <kirts@google.com>
2025-11-24 17:45:38 +0000

    f394215 fix(migrations): detect structural ngTemplateOutlet and ngComponentOutlet

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.0+sha-70bc311
+ * @license Angular v21.0.0+sha-f394215
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.0+sha-70bc311');
+const VERSION = /* @__PURE__ */new Version('21.0.0+sha-f394215');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

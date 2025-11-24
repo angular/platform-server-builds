c951eb1daa23f0f196fc6c54342990a7ed1d25cb
kirjs <kirts@google.com>
2025-11-24 16:35:49 +0000

    0f4b11c refactor(compiler-cli): add a resource debugName transform (#64172)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.0+sha-b5ae62c
+ * @license Angular v21.1.0-next.0+sha-0f4b11c
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-b5ae62c');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.0+sha-0f4b11c');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

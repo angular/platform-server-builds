3e7aca6a622b41d91a19775d46eadcc899cb06d3
kirjs <kirts@google.com>
2025-11-26 18:16:36 +0000

    1260d1a refactor(devtools): Use the same error message as the signal custom formatter for signal errors

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.1+sha-0ae14cb
+ * @license Angular v21.0.1+sha-1260d1a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -18,7 +18,7 @@ const platformServerTesting = createPlatformFactory(platformCore, 'serverTesting
 class ServerTestingModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.0.1+sha-0ae14cb",
+    version: "21.0.1+sha-1260d1a",
     ngImport: i0,
     type: ServerTestingModule,
     deps: [],
@@ -26,14 +26,14 @@ class ServerTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.0.1+sha-0ae14cb",
+    version: "21.0.1+sha-1260d1a",
     ngImport: i0,
     type: ServerTestingModule,
     exports: [BrowserDynamicTestingModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.0.1+sha-0ae14cb",
+    version: "21.0.1+sha-1260d1a",
     ngImport: i0,
     type: ServerTestingModule,
     providers: SERVER_RENDER_PROVIDERS,
@@ -42,7 +42,7 @@ class ServerTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.0.1+sha-0ae14cb",
+  version: "21.0.1+sha-1260d1a",
   ngImport: i0,
   type: ServerTestingModule,
   decorators: [{

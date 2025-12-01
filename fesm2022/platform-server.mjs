1477f335868a40f16564515a2e28303921f26c45
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-01 18:25:42 +0000

    8ab7594 docs: format typescript examples and replace <docs-code> with fenced code blocks

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.2+sha-ed32e8c
+ * @license Angular v21.0.2+sha-8ab7594
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.2+sha-ed32e8c');
+const VERSION = /* @__PURE__ */new Version('21.0.2+sha-8ab7594');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

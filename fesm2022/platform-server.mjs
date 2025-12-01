3943aa7f09cca44e5ab2afe7b8cbae3e6593c561
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-12-01 11:56:03 +0000

    8c4a152 release: cut the v21.0.2 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.1+sha-78fd159
+ * @license Angular v21.0.2+sha-8c4a152
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.1+sha-78fd159');
+const VERSION = /* @__PURE__ */new Version('21.0.2+sha-8c4a152');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

29180d7fee1a8996cd1c2853967329201185a71f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 09:52:51 +0000

    2935fef build: update cross-repo angular dependencies

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.2+sha-893fbdf
+ * @license Angular v21.0.2+sha-2935fef
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.2+sha-893fbdf');
+const VERSION = /* @__PURE__ */new Version('21.0.2+sha-2935fef');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

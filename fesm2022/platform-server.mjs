f03560ca0e54baa8b69e6c16455119fc9ffb54be
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 15:24:00 +0000

    c355ffd release: cut the v21.0.3 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.2+sha-6b4ab87
+ * @license Angular v21.0.3+sha-c355ffd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.2+sha-6b4ab87');
+const VERSION = /* @__PURE__ */new Version('21.0.3+sha-c355ffd');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

dbe82b6749c5bb98b94c805d6544a7dda856da3d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-03 19:30:27 +0000

    eebcaf4 ci: use defined ng-dev config for deploying docs site

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-7f8ffa3
+ * @license Angular v21.1.0-next.1+sha-eebcaf4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-7f8ffa3');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-eebcaf4');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

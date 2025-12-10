574b1da78202e3eb70c57027ff099aaf5e1da467
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 20:38:13 +0000

    af6c01f build: update pnpm to v10.25.0

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-954c546
+ * @license Angular v21.1.0-next.1+sha-af6c01f
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-954c546');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-af6c01f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

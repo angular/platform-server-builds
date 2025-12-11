cfdc3c3228e48788d188018609ea6e867fc78604
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 00:48:55 +0000

    a52c834 build: update ng-dev to latest version

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-d0c3806
+ * @license Angular v21.1.0-next.1+sha-a52c834
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-d0c3806');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-a52c834');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

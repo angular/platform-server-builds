41a3c349001db90d35d25d78d12c51330e21628e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 23:46:55 +0000

    5a21465 release: cut the v21.0.5 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.4+sha-4fd2b72
+ * @license Angular v21.0.5+sha-5a21465
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.4+sha-4fd2b72');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-5a21465');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

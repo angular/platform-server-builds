93d3583c22b2488023b270d62675b15aa91c5012
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 00:55:35 +0000

    d343ff3 docs: release notes for the v21.0.4 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.1+sha-a52c834
+ * @license Angular v21.1.0-next.1+sha-d343ff3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-a52c834');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.1+sha-d343ff3');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

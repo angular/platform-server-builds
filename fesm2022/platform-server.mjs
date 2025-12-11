be542fc85a5ad4a746b4405391cc102d044ca992
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 23:38:27 +0000

    4fd2b72 fix(forms): fix signal forms type error

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.4+sha-69d243a
+ * @license Angular v21.0.4+sha-4fd2b72
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.4+sha-69d243a');
+const VERSION = /* @__PURE__ */new Version('21.0.4+sha-4fd2b72');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

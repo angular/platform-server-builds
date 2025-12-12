9d2db8ba86722a6331a3ce1d3a4fb840557fa4f2
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-12 01:23:51 +0000

    87d20fd build: update cross-repo angular dependencies

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-25f8007
+ * @license Angular v21.0.5+sha-87d20fd
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-25f8007');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-87d20fd');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

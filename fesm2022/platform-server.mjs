2c9b4851e377d52f1635c9cbabbd00ffb3a092ca
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-12 18:15:48 +0000

    be5dbf4 docs: update `guidelines.md` to match `best-practices.md`

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-480c4dc
+ * @license Angular v21.1.0-next.3+sha-be5dbf4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-480c4dc');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-be5dbf4');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

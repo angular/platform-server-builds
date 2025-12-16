0010acfa18bb921d9377ea3a894622099946c7e7
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-16 22:49:52 +0000

    6270bba ci: reformat files

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.3+sha-06be803
+ * @license Angular v21.1.0-next.3+sha-6270bba
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-06be803');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.3+sha-6270bba');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

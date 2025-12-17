b1d2b3c3505a05797ff6feebf774b73a3eaad4b5
Andrew Kushnir <akushnir@google.com>
2025-12-17 22:32:36 +0000

    c8d14d7 docs: fix incorrect line highlighting in tutorials

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-eaa68c4
+ * @license Angular v21.0.6+sha-c8d14d7
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-eaa68c4');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-c8d14d7');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

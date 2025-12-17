30532833a09491c66fb32537559597ce34b7b61e
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 23:49:43 +0000

    99ad18a feat(core): Add stability debugging utility

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-e2a9938
+ * @license Angular v21.1.0-next.4+sha-99ad18a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-e2a9938');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-99ad18a');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

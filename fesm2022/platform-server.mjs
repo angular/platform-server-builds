6e724218bb415123430f19472f8fc726d575fe52
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 18:06:14 +0000

    a27bdd3 build: update bazel dependencies

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.5+sha-3b3b982
+ * @license Angular v21.0.5+sha-a27bdd3
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.5+sha-3b3b982');
+const VERSION = /* @__PURE__ */new Version('21.0.5+sha-a27bdd3');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

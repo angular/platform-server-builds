d5f5435a2a80bf5a1bb6be51f20e7293c030c5de
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-05 17:30:19 +0000

    6a94300 refactor(devtools): use `whenStable` instead of `detectChanges`

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-821d565
+ * @license Angular v21.1.0-next.4+sha-6a94300
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-821d565');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-6a94300');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

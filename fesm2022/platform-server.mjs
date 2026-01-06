2f65074656019ca442c185c2094fb5507df9afed
GitHub <noreply@github.com>
2026-01-06 20:59:32 +0000

    c2c2b4a fix(core): sanitize sensitive attributes on SVG script elements

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.15+sha-4755bbd
+ * @license Angular v20.3.15+sha-c2c2b4a
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -311,7 +311,7 @@ async function renderApplication(bootstrap, options) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.15+sha-4755bbd');
+const VERSION = /* @__PURE__ */ new Version('20.3.15+sha-c2c2b4a');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

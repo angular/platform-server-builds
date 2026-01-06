46f80ef6242b9a0a8f69b0e863c1a37069e7005d
kirjs <kirts@google.com>
2026-01-06 00:43:57 +0000

    a119427 docs: rewrite testing docs

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-b16c2f5
+ * @license Angular v21.0.6+sha-a119427
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-b16c2f5');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-a119427');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

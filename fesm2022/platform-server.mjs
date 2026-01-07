ffdecf4cd995d4eac4ab5a028e995f7809b8325e
kirjs <kirts@google.com>
2026-01-07 14:40:53 +0000

    7738eb9 docs(docs-infra): Use category for signal forms guides

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.6+sha-c1e9627
+ * @license Angular v21.0.6+sha-7738eb9
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.6+sha-c1e9627');
+const VERSION = /* @__PURE__ */new Version('21.0.6+sha-7738eb9');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

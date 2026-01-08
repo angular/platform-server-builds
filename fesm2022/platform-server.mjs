a219bc04db330eee302e44341e08e1ed495eeb01
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-08 18:08:13 +0000

    bd5997c docs(forms): make onSubmit method async for form submission

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-next.4+sha-71149ef
+ * @license Angular v21.1.0-next.4+sha-bd5997c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-71149ef');
+const VERSION = /* @__PURE__ */new Version('21.1.0-next.4+sha-bd5997c');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

ab0921ca3c867c0423ba33443e1ec9d9203be77a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-08 19:26:42 +0000

    8ff2776 fix(docs-infra): improve dark mode button hover contrast for WCAG compliance

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.7+sha-0dead0f
+ * @license Angular v21.0.7+sha-8ff2776
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.7+sha-0dead0f');
+const VERSION = /* @__PURE__ */new Version('21.0.7+sha-8ff2776');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

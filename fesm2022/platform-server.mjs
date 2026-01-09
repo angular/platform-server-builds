af3aa3ab1d436e774d56066ffbc20c20fa8034d9
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 17:01:18 +0000

    24fa4a2 docs(forms): Clarify returning errors from submit functions

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-491a156
+ * @license Angular v21.0.8+sha-24fa4a2
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-491a156');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-24fa4a2');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

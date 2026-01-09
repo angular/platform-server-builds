79dfa4a24eef6667f8787a99e9bcec3ddf8efce0
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 18:46:54 +0000

    a201d33 ci: add pnpm and typescript exclusions to a Renovate package rule.

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-0875dea
+ * @license Angular v21.1.0-rc.0+sha-a201d33
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-0875dea');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-a201d33');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

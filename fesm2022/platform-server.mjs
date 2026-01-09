8639001745c64e7c67b407287e9b172739bfd001
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-09 17:00:00 +0000

    7d985ce docs(forms): Clarify returning errors from submit functions

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-e66aeac
+ * @license Angular v21.2.0-next.0+sha-7d985ce
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-e66aeac');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-7d985ce');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

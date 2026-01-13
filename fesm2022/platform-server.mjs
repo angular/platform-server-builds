ea9b3be9eda088dfcf4596c36b4cb683f573890b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-13 21:13:30 +0000

    227acdd build: Update vscode release script

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-cb5879f
+ * @license Angular v21.2.0-next.0+sha-227acdd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-cb5879f');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-227acdd');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

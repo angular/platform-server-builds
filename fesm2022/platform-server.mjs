d9fc3016b190ef1e987036253a897254bec9a9c6
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 21:14:00 +0000

    6d3b093 build: Update vscode release script

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0-rc.0+sha-9a764e0
+ * @license Angular v21.1.0-rc.0+sha-6d3b093
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-9a764e0');
+const VERSION = /* @__PURE__ */new Version('21.1.0-rc.0+sha-6d3b093');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

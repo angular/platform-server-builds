5ec05fbf88f043f0ff7a6cb08421e2a7447af258
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 19:25:51 +0000

    99b10e4 build: update cherrypick changelog to stash

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.0.8+sha-01e5bab
+ * @license Angular v21.0.8+sha-99b10e4
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.0.8+sha-01e5bab');
+const VERSION = /* @__PURE__ */new Version('21.0.8+sha-99b10e4');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

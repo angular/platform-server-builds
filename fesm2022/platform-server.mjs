245472fad545d8d017538aa157bf097dce0dc2e3
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-14 19:50:57 +0000

    0159268 docs: release notes for the v21.1.0 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-d12e6f0
+ * @license Angular v21.2.0-next.0+sha-0159268
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-d12e6f0');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-0159268');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

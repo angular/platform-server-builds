4d497f931e4248ef6d0346e4542d31fd3fea061d
Alon Mishne <amishne@users.noreply.github.com>
2026-01-15 17:04:54 +0000

    c0446da docs(docs-infra): prevent heading from linking symbols

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-ae4e2e0
+ * @license Angular v21.1.0+sha-c0446da
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-ae4e2e0');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-c0446da');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

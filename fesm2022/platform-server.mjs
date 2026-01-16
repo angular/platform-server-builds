e5c1bff1902dd92ba3f4e15a773eff4f5656665c
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-16 21:33:37 +0000

    5d46554 docs: Add section on `animate.leave` for clarity

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-e55260f-with-local-changes
+ * @license Angular v21.2.0-next.0+sha-5d46554-with-local-changes
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-e55260f-with-local-changes');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-5d46554-with-local-changes');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

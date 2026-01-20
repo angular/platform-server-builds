f26f590dbd787a60ad8f04e193ddbc5e37c52f8e
Alon Mishne <amishne@users.noreply.github.com>
2026-01-20 18:30:02 +0000

    e664249 ci: add template for vscode dedicated issues

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-9cc0f4d
+ * @license Angular v21.1.0+sha-e664249
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-9cc0f4d');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-e664249');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

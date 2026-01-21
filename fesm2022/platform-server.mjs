99c227f6c1e62819b69eb4d9655cf18cc2898bce
Alon Mishne <amishne@users.noreply.github.com>
2026-01-21 18:43:45 +0000

    223ff74 build: don't substitute binary files

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.0+sha-b9c995e
+ * @license Angular v21.1.0+sha-223ff74
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.0+sha-b9c995e');
+const VERSION = /* @__PURE__ */new Version('21.1.0+sha-223ff74');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

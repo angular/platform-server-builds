91fd09014f4618b35e3a0684aefa609a1662ac76
Andrew Scott <atscott01@gmail.com>
2026-01-23 19:51:22 +0000

    85122cb docs: update `bootstrapApplication` docs

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-ddd3198
+ * @license Angular v21.2.0-next.0+sha-85122cb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-ddd3198');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-85122cb');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

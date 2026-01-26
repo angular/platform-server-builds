2f4f2625cd87b2ad76db391134e580c79beb24f4
Andrew Scott <atscott01@gmail.com>
2026-01-26 23:41:11 +0000

    b51bab5 feat(router): Add partial ActivatedRouteSnapshot information to `canMatch` params

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.0+sha-899df39
+ * @license Angular v21.2.0-next.0+sha-b51bab5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-899df39');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.0+sha-b51bab5');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

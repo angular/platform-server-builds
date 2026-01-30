cb4565d661d93500a43e7c9721cf427e2926f6c1
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-30 22:30:55 +0000

    8d5210c feat(core): add ChangeDetectionStrategy.Eager alias for Default

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.1+sha-424ecf8
+ * @license Angular v21.2.0-next.1+sha-8d5210c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-424ecf8');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.1+sha-8d5210c');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

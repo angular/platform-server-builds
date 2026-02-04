9a0fc24c50ca98347a656993c214f5a5706a3216
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-04 22:31:25 +0000

    04d518b refactor(forms): work around internal issue

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.2+sha-2cf4da0
+ * @license Angular v21.1.2+sha-04d518b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-2cf4da0');
+const VERSION = /* @__PURE__ */new Version('21.1.2+sha-04d518b');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

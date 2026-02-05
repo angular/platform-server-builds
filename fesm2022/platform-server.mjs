47f48e809c7a49e000914dcc6e3984f981035391
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-05 00:13:14 +0000

    70668be release: cut the v21.1.3 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.2+sha-767c6b4
+ * @license Angular v21.1.3+sha-70668be
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.2+sha-767c6b4');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-70668be');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

7b8c3e25be6f667e80f4ffbbdac2403800e575da
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 17:54:30 +0000

    15c71fb build: update all non-major dependencies

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-24c0c5a
+ * @license Angular v21.2.0-next.2+sha-15c71fb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-24c0c5a');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-15c71fb');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

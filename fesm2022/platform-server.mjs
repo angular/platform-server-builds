4663b3ca67a73dfc6797bc4abef40d980bebaced
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-09 22:21:45 +0000

    e53c8ab fix(core): Fix flakey test due to document injection

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-cb1163e
+ * @license Angular v21.2.0-next.2+sha-e53c8ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-cb1163e');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-e53c8ab');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

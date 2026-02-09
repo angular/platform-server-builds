1a896bbeb0ba507f3e0ad57174bc8e39b4aa27a6
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-09 20:41:14 +0000

    cb1163e fix(http): correctly parse ArrayBuffer and Blob in transfer cache

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-4b3b149
+ * @license Angular v21.2.0-next.2+sha-cb1163e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-4b3b149');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-cb1163e');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

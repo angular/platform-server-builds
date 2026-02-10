74e0530aa8c7e2af56d3ad93787593073aebf9ff
Andrew Kushnir <akushnir@google.com>
2026-02-10 15:54:48 +0000

    2c3f3cc refactor(forms): use optional chaining for safer method calls in form directives

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.3+sha-c6bbf40
+ * @license Angular v21.1.3+sha-2c3f3cc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.3+sha-c6bbf40');
+const VERSION = /* @__PURE__ */new Version('21.1.3+sha-2c3f3cc');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

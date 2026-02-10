bfd0d846b1efdc2cb400c372946e8e2ff11886f5
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-10 15:53:36 +0000

    0f47fda test(router): move timeout and autoTick helpers to shared testing utilities

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-760910e
+ * @license Angular v21.2.0-next.2+sha-0f47fda
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-760910e');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-0f47fda');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

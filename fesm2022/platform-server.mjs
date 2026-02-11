a06081488d156a2d872f32b2268330cc8023bebc
Matthew Beck (Berry) <me@mattrb.com>
2026-02-11 22:50:01 +0000

    7150df2 build: update cross-repo angular dependencies to v21.1.4

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-6060aad
+ * @license Angular v21.1.4+sha-7150df2
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-6060aad');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-7150df2');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

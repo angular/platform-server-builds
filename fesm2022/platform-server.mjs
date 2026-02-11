b0755da647cd81f90836f9fd06170e0c04a14916
Matthew Beck (Berry) <me@mattrb.com>
2026-02-11 23:44:35 +0000

    937e98f build: update pnpm to v10.29.2

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.4+sha-4ef0f1f
+ * @license Angular v21.1.4+sha-937e98f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.4+sha-4ef0f1f');
+const VERSION = /* @__PURE__ */new Version('21.1.4+sha-937e98f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

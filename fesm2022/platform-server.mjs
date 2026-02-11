271d2959601a02dca7126fabad501acff33b7557
GitHub <noreply@github.com>
2026-02-11 19:52:02 +0000

    3606902 refactor(forms): relax `[formField]` input type from `FieldTree` to `Field`

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-39cff9c
+ * @license Angular v21.2.0-next.3+sha-3606902
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-39cff9c');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-3606902');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

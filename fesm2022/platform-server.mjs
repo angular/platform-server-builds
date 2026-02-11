d5910ae87c6a063880df02ed404a629c27eee04c
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-11 17:58:38 +0000

    ad81510 docs: release notes for the v21.1.4 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.2+sha-ba009b6
+ * @license Angular v21.2.0-next.2+sha-ad81510
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-ba009b6');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.2+sha-ad81510');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

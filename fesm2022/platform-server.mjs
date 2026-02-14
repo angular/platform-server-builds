559fe05c3d192a333c5d34aa9b8f628e2229d470
GitHub <noreply@github.com>
2026-02-14 00:55:44 +0000

    815e1a0 refactor(compiler-cli): Add skeleton tests around source->source compiler transform mode

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-a37b6c5
+ * @license Angular v21.2.0-next.3+sha-815e1a0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-a37b6c5');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-815e1a0');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

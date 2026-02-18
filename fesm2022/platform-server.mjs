8dada5069d276d6ef2fdfc580aefa48050e5af76
Matthew Beck (Berry) <me@mattrb.com>
2026-02-18 22:16:21 +0000

    567f292 fix(forms): support custom controls as host directives

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-next.3+sha-9a9f4e7
+ * @license Angular v21.2.0-next.3+sha-567f292
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-9a9f4e7');
+const VERSION = /* @__PURE__ */new Version('21.2.0-next.3+sha-567f292');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

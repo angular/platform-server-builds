f6b2d00a51b3259ad05984ecb67cdfed3d91a360
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-19 20:47:59 +0000

    9943cb7 ci: add `zone.js` to `minimumReleaseAgeExclude`

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-77fc7b9
+ * @license Angular v21.1.5+sha-9943cb7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-77fc7b9');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-9943cb7');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

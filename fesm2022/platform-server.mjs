d1d314155046b4f9708f58647161d963a74e33d1
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 17:22:56 +0000

    3b64e7d refactor(common): log a warning when a JsonPipe receives a signal

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-24b578c
+ * @license Angular v21.1.5+sha-3b64e7d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-24b578c');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-3b64e7d');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

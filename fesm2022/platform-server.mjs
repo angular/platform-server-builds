af72a7fe6d92d9fe770a9255a9b4561a885e4a40
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 21:35:05 +0000

    d3479f1 docs: update and reformat the roadmap

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.1.5+sha-a8dcb28
+ * @license Angular v21.1.5+sha-d3479f1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.1.5+sha-a8dcb28');
+const VERSION = /* @__PURE__ */new Version('21.1.5+sha-d3479f1');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

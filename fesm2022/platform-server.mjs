004d0188d64610302d5cb9d21b184df4e43110f8
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-24 22:17:34 +0000

    bb2745d build: update bazel dependencies

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-db11e74
+ * @license Angular v22.0.0-next.0+sha-bb2745d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-db11e74');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-bb2745d');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

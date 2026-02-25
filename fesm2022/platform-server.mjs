db62e895c167b8292301e7111df5c2b4678c5ca4
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 17:00:08 +0000

    d3ab1ea build: lock file maintenance

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0-rc.0+sha-fe7b26e
+ * @license Angular v21.2.0-rc.0+sha-d3ab1ea
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-fe7b26e');
+const VERSION = /* @__PURE__ */new Version('21.2.0-rc.0+sha-d3ab1ea');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

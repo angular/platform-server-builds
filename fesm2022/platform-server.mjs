78056783a119b65b225f4e597f6695f1caaa04f4
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-26 16:00:49 +0000

    f23f3e7 docs: add mention of identity tracking for field iterations

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-661e800
+ * @license Angular v21.2.0+sha-f23f3e7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-661e800');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-f23f3e7');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

c5ad84a061686ac98524b1e89878452362a56590
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-26 18:11:10 +0000

    9441818 ci: set up linting for upcoming agent skills

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-3d3e6cb
+ * @license Angular v21.2.0+sha-9441818
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-3d3e6cb');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-9441818');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

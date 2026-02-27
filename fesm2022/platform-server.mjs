d705a7766707caa8084a75b56705e19372497690
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-27 17:22:24 +0000

    498d9cd build: update cross-repo angular dependencies to 9cc4778

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-4e9648d
+ * @license Angular v21.2.0+sha-498d9cd
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-4e9648d');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-498d9cd');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

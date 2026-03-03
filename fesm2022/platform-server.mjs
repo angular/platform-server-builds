124a0df07baf9daab69d0b9058abfc68d08e863b
GitHub <noreply@github.com>
2026-03-03 17:20:14 +0000

    b8fb51c build(dev-infra): add release automation script for zone.js

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.0+sha-dd77363
+ * @license Angular v22.0.0-next.0+sha-b8fb51c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-dd77363');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.0+sha-b8fb51c');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

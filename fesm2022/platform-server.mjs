babb23361c81b3f7b20ce4bc64e77e0edcf9a7a4
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-04 18:25:03 +0000

    6d7446b refactor(core): account for shadow roots in animations

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.0+sha-f4160c2
+ * @license Angular v21.2.0+sha-6d7446b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.0+sha-f4160c2');
+const VERSION = /* @__PURE__ */new Version('21.2.0+sha-6d7446b');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

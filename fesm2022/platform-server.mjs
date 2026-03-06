17d6ae8c278b3234d54aa061c422da71d94162a9
Andrew Scott <atscott01@gmail.com>
2026-03-06 23:41:48 +0000

    276c12a build: update dependency node to v22.22.1

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-104ac11
+ * @license Angular v21.2.1+sha-276c12a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-104ac11');
+const VERSION = /* @__PURE__ */new Version('21.2.1+sha-276c12a');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

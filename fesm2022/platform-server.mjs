c545bad51a997ad06cc33161d3af51f2c02c6fdc
Andrew Scott <atscott@google.com>
2026-03-09 17:53:54 +0000

    bd72571 refactor(devtools): prevent spamming the message bus

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.1+sha-628f3f9
+ * @license Angular v21.2.1+sha-bd72571
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.1+sha-628f3f9');
+const VERSION = /* @__PURE__ */new Version('21.2.1+sha-bd72571');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

f91a7be58953e97b6dfbfb857338a9774f47aa9f
Andrew Scott <atscott01@gmail.com>
2026-03-10 18:10:54 +0000

    1866bf5 refactor(compiler-cli): remove more unused code

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.1+sha-1969338
+ * @license Angular v22.0.0-next.1+sha-1866bf5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-1969338');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.1+sha-1866bf5');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

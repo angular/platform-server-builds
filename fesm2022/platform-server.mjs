f3706c96422b64210118647a54c65e7343fc7c10
Andrew Scott <atscott01@gmail.com>
2026-03-10 00:09:26 +0000

    024e307 build: update pnpm to v10.31.0

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.2+sha-4a9b715
+ * @license Angular v21.2.2+sha-024e307
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.2+sha-4a9b715');
+const VERSION = /* @__PURE__ */new Version('21.2.2+sha-024e307');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

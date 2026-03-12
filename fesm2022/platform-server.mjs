47c5bc9ce475edf9f8edaf6f32ac78c92dde9db0
Matthew Beck <me@mattrb.com>
2026-03-12 17:11:58 +0000

    ed2d324 fix(compiler): disallow translations of iframe src

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.3+sha-c06e3a0
+ * @license Angular v21.2.3+sha-ed2d324
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.3+sha-c06e3a0');
+const VERSION = /* @__PURE__ */new Version('21.2.3+sha-ed2d324');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

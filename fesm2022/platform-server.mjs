f13400cb957be7e54c4a76166590188fafc3a99f
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 19:21:43 +0000

    4cbc1a1 release: cut the v22.0.0-next.3 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.2+sha-5b66252
+ * @license Angular v22.0.0-next.3+sha-4cbc1a1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.2+sha-5b66252');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-4cbc1a1');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

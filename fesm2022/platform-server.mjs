3aa64edbe1c17f513fdd65846b0666c415592de2
Matthew Beck <me@mattrb.com>
2026-03-17 20:07:44 +0000

    a8f80c1 refactor(core): declare explicit reactive node prototypes types

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.4+sha-67e0ba7
+ * @license Angular v21.2.4+sha-a8f80c1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.4+sha-67e0ba7');
+const VERSION = /* @__PURE__ */new Version('21.2.4+sha-a8f80c1');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

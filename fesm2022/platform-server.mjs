dfb1ea0bee7eb7efa3367fdc05758dfca42c8a21
Matthew Beck (Berry) <me@mattrb.com>
2026-03-19 02:19:16 +0000

    557f6e7 docs: release notes for the v21.2.5 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.3+sha-785735b
+ * @license Angular v22.0.0-next.3+sha-557f6e7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-785735b');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.3+sha-557f6e7');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

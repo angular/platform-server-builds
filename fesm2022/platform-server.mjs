ad9ef36411c79aff370d8b6e41fb9bec9d5796f1
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-23 18:32:22 +0000

    2615f35 docs: fix js doc of signal forms ignoreValidators option.

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.4+sha-293ac66
+ * @license Angular v22.0.0-next.4+sha-2615f35
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-293ac66');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.4+sha-2615f35');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

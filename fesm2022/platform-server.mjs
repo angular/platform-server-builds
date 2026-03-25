d186b3461240ba1bc7a7b4b5d7613b6ab11d0e04
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-25 20:15:42 +0000

    a6c49e0 refactor(language-service): Export template target from API

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.5+sha-2c7d3b5
+ * @license Angular v21.2.5+sha-a6c49e0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -179,7 +179,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.5+sha-2c7d3b5');
+const VERSION = /* @__PURE__ */new Version('21.2.5+sha-a6c49e0');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

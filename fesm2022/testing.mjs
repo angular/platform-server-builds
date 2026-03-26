ecedce1f4f118b37b57a789f7e490bcf36c9a136
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-26 17:45:53 +0000

    d268df6 build: remove unused `@nginfra/angular-linking` dependency

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.5+sha-d4c085d
+ * @license Angular v22.0.0-next.5+sha-d268df6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -19,7 +19,7 @@ const platformServerTesting = createPlatformFactory(platformCore, 'serverTesting
 class ServerTestingModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.5+sha-d4c085d",
+    version: "22.0.0-next.5+sha-d268df6",
     ngImport: i0,
     type: ServerTestingModule,
     deps: [],
@@ -27,14 +27,14 @@ class ServerTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "22.0.0-next.5+sha-d4c085d",
+    version: "22.0.0-next.5+sha-d268df6",
     ngImport: i0,
     type: ServerTestingModule,
     exports: [BrowserDynamicTestingModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "22.0.0-next.5+sha-d4c085d",
+    version: "22.0.0-next.5+sha-d268df6",
     ngImport: i0,
     type: ServerTestingModule,
     providers: SERVER_RENDER_PROVIDERS,
@@ -43,7 +43,7 @@ class ServerTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.5+sha-d4c085d",
+  version: "22.0.0-next.5+sha-d268df6",
   ngImport: i0,
   type: ServerTestingModule,
   decorators: [{

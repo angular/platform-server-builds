c73b2780da69ce11f004c9de94e41a8a9fcd304e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-31 11:50:11 +0000

    5fd4f0f build: lock file maintenance

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.6+sha-429513b
+ * @license Angular v21.2.6+sha-5fd4f0f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -180,7 +180,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.6+sha-429513b');
+const VERSION = /* @__PURE__ */new Version('21.2.6+sha-5fd4f0f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

26f18ef4ffe2fea7a31abbac385f5e901d8c96d7
Andrew Scott <atscott01@gmail.com>
2026-04-03 18:02:08 +0000

    24c5400 build: update dependency undici to v8

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.6+sha-394ad0c
+ * @license Angular v22.0.0-next.6+sha-24c5400
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -180,7 +180,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-394ad0c');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-24c5400');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

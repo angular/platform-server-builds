406942680662334944ef4d02ff8d960970525e99
Andrew Scott <atscott01@gmail.com>
2026-04-06 16:28:57 +0000

    ecae525 Revert "refactor(core): remove ComponentFactoryResolver & ComponentFactory from the api surface"

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.6+sha-4a174b8
+ * @license Angular v22.0.0-next.6+sha-ecae525
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -180,7 +180,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-4a174b8');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-ecae525');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

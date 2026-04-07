e1f275f66c9c291e23da8e3ca5453598c5a6d928
Andrew Scott <atscott01@gmail.com>
2026-04-07 21:35:05 +0000

    de56d74 fix(forms): align FormField CVA selection priority with standard forms

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.6+sha-a0aa830
+ * @license Angular v22.0.0-next.6+sha-de56d74
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -180,7 +180,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-a0aa830');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.6+sha-de56d74');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

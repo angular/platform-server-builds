780a09029453b2a83b37d3ed0025b3e223bd070d
Andrew Scott <atscott01@gmail.com>
2026-04-07 23:48:19 +0000

    a385743 build: update cross-repo angular dependencies

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.7+sha-c9f8f3a-with-local-changes
+ * @license Angular v21.2.7+sha-a385743
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -19,7 +19,7 @@ const platformServerTesting = createPlatformFactory(platformCore, 'serverTesting
 class ServerTestingModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "21.2.7+sha-c9f8f3a-with-local-changes",
+    version: "21.2.7+sha-a385743",
     ngImport: i0,
     type: ServerTestingModule,
     deps: [],
@@ -27,14 +27,14 @@ class ServerTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "21.2.7+sha-c9f8f3a-with-local-changes",
+    version: "21.2.7+sha-a385743",
     ngImport: i0,
     type: ServerTestingModule,
     exports: [BrowserDynamicTestingModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "21.2.7+sha-c9f8f3a-with-local-changes",
+    version: "21.2.7+sha-a385743",
     ngImport: i0,
     type: ServerTestingModule,
     providers: SERVER_RENDER_PROVIDERS,
@@ -43,7 +43,7 @@ class ServerTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "21.2.7+sha-c9f8f3a-with-local-changes",
+  version: "21.2.7+sha-a385743",
   ngImport: i0,
   type: ServerTestingModule,
   decorators: [{

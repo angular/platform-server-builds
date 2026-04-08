fc78dad9004adf36e9aa1e7128f52eb96fad1e7e
Andrew Scott <atscott01@gmail.com>
2026-04-08 19:39:12 +0000

    6aafd87 release: cut the v21.2.8 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.7+sha-d4c8a9a
+ * @license Angular v21.2.8+sha-6aafd87
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -180,7 +180,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.7+sha-d4c8a9a');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-6aafd87');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

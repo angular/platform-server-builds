59777eab7678e65d1cf89dc41c1cfdba2141f0a9
kirjs <kirts@google.com>
2026-04-13 10:31:40 +0000

    b72b6b4 docs(forms): update signal forms migration guide

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.8+sha-3ed14c6
+ * @license Angular v21.2.8+sha-b72b6b4
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -180,7 +180,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.8+sha-3ed14c6');
+const VERSION = /* @__PURE__ */new Version('21.2.8+sha-b72b6b4');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

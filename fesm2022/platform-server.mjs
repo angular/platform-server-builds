42d84351f6cd899be1b651124dc99bf6385f3e47
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 08:29:30 +0000

    973ede6 refactor: Mobile layout api reference Fix mobile layout shift in API reference

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-59513b7
+ * @license Angular v22.0.0-next.7+sha-973ede6
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -180,7 +180,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-59513b7');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-973ede6');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

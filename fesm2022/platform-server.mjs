0e645e4b16ba0d25ab82065a373baeb76d625461
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 17:35:35 +0000

    034b12f build: update cross-repo angular dependencies

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.7+sha-281a2db
+ * @license Angular v22.0.0-next.7+sha-034b12f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -180,7 +180,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-281a2db');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.7+sha-034b12f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

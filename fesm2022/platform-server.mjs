e326e850620d7b24809d09af741a0ed9d5e600ba
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 16:47:38 +0000

    ce883d9 build: update peer dependencies and bump version

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.9+sha-be74909
+ * @license Angular v21.2.9+sha-ce883d9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -180,7 +180,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.9+sha-be74909');
+const VERSION = /* @__PURE__ */new Version('21.2.9+sha-ce883d9');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

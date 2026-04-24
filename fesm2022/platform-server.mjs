0ac5b2892d7df85d7c18b682d3ccd9f518186630
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-24 20:37:38 +0000

    ac9babe docs: update cross-repo adev docs

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.9+sha-29081f7
+ * @license Angular v22.0.0-next.9+sha-ac9babe
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -180,7 +180,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-29081f7');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.9+sha-ac9babe');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

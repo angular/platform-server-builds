5f22c0b15668dc76156a2e3dccd58f6728e13f17
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-29 17:11:16 +0000

    37ec63e refactor(compiler): introduce default constant for legacyOptionalChaining flag

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.9+sha-2896c93
+ * @license Angular v22.0.0-next.9+sha-37ec63e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -19,7 +19,7 @@ const platformServerTesting = createPlatformFactory(platformCore, 'serverTesting
 class ServerTestingModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.9+sha-2896c93",
+    version: "22.0.0-next.9+sha-37ec63e",
     ngImport: i0,
     type: ServerTestingModule,
     deps: [],
@@ -27,14 +27,14 @@ class ServerTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "22.0.0-next.9+sha-2896c93",
+    version: "22.0.0-next.9+sha-37ec63e",
     ngImport: i0,
     type: ServerTestingModule,
     exports: [BrowserDynamicTestingModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "22.0.0-next.9+sha-2896c93",
+    version: "22.0.0-next.9+sha-37ec63e",
     ngImport: i0,
     type: ServerTestingModule,
     providers: SERVER_RENDER_PROVIDERS,
@@ -43,7 +43,7 @@ class ServerTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.9+sha-2896c93",
+  version: "22.0.0-next.9+sha-37ec63e",
   ngImport: i0,
   type: ServerTestingModule,
   decorators: [{

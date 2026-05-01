ecd6401898fa8a6f90c230263301932e8f18183d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-01 18:13:34 +0000

    a4145de Revert "fix(language-service): Add support for `@Input` with transforms"

--- fesm2022/_server-chunk.mjs
+++ fesm2022/_server-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.10+sha-2b62458
+ * @license Angular v22.0.0-next.10+sha-a4145de
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -109,7 +109,7 @@ class PlatformState {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.10+sha-2b62458",
+    version: "22.0.0-next.10+sha-a4145de",
     ngImport: i0,
     type: PlatformState,
     deps: [{
@@ -119,14 +119,14 @@ class PlatformState {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.10+sha-2b62458",
+    version: "22.0.0-next.10+sha-a4145de",
     ngImport: i0,
     type: PlatformState
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.10+sha-2b62458",
+  version: "22.0.0-next.10+sha-a4145de",
   ngImport: i0,
   type: PlatformState,
   decorators: [{
@@ -163,7 +163,7 @@ class ServerXhr {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.10+sha-2b62458",
+    version: "22.0.0-next.10+sha-a4145de",
     ngImport: i0,
     type: ServerXhr,
     deps: [],
@@ -171,14 +171,14 @@ class ServerXhr {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.10+sha-2b62458",
+    version: "22.0.0-next.10+sha-a4145de",
     ngImport: i0,
     type: ServerXhr
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.10+sha-2b62458",
+  version: "22.0.0-next.10+sha-a4145de",
   ngImport: i0,
   type: ServerXhr,
   decorators: [{
@@ -317,7 +317,7 @@ class ServerPlatformLocation {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.10+sha-2b62458",
+    version: "22.0.0-next.10+sha-a4145de",
     ngImport: i0,
     type: ServerPlatformLocation,
     deps: [],
@@ -325,14 +325,14 @@ class ServerPlatformLocation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.10+sha-2b62458",
+    version: "22.0.0-next.10+sha-a4145de",
     ngImport: i0,
     type: ServerPlatformLocation
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.10+sha-2b62458",
+  version: "22.0.0-next.10+sha-a4145de",
   ngImport: i0,
   type: ServerPlatformLocation,
   decorators: [{
@@ -355,7 +355,7 @@ class ServerEventManagerPlugin extends EventManagerPlugin {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.10+sha-2b62458",
+    version: "22.0.0-next.10+sha-a4145de",
     ngImport: i0,
     type: ServerEventManagerPlugin,
     deps: [{
@@ -365,14 +365,14 @@ class ServerEventManagerPlugin extends EventManagerPlugin {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.0.0-next.10+sha-2b62458",
+    version: "22.0.0-next.10+sha-a4145de",
     ngImport: i0,
     type: ServerEventManagerPlugin
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.10+sha-2b62458",
+  version: "22.0.0-next.10+sha-a4145de",
   ngImport: i0,
   type: ServerEventManagerPlugin,
   decorators: [{
@@ -482,7 +482,7 @@ const PLATFORM_SERVER_PROVIDERS = [TRANSFER_STATE_SERIALIZATION_PROVIDERS, SERVE
 class ServerModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.0-next.10+sha-2b62458",
+    version: "22.0.0-next.10+sha-a4145de",
     ngImport: i0,
     type: ServerModule,
     deps: [],
@@ -490,14 +490,14 @@ class ServerModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "22.0.0-next.10+sha-2b62458",
+    version: "22.0.0-next.10+sha-a4145de",
     ngImport: i0,
     type: ServerModule,
     exports: [BrowserModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "22.0.0-next.10+sha-2b62458",
+    version: "22.0.0-next.10+sha-a4145de",
     ngImport: i0,
     type: ServerModule,
     providers: PLATFORM_SERVER_PROVIDERS,
@@ -506,7 +506,7 @@ class ServerModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.0-next.10+sha-2b62458",
+  version: "22.0.0-next.10+sha-a4145de",
   ngImport: i0,
   type: ServerModule,
   decorators: [{

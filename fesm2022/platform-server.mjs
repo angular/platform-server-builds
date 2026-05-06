542d8439fd31b49820dccee494d317edae8c2f8d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 22:56:58 +0000

    5316753 release: cut the v21.2.12 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.11+sha-9e38ed7
+ * @license Angular v21.2.12+sha-5316753
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -180,7 +180,7 @@ async function renderApplication(bootstrap, options) {
   }
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.11+sha-9e38ed7');
+const VERSION = /* @__PURE__ */new Version('21.2.12+sha-5316753');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

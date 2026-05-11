9d06d82c288ca56d0a45a741c7f5dc474ab5fd81
Matthew Beck <me@mattrb.com>
2026-05-11 19:52:41 +0000

    49a133a refactor(compiler-cli): drop @ts-ignore around jsDocParsingMode

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.12+sha-c93d158
+ * @license Angular v21.2.12+sha-49a133a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -216,7 +216,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.12+sha-c93d158');
+const VERSION = /* @__PURE__ */new Version('21.2.12+sha-49a133a');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

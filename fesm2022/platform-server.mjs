c7e2c57ae99f738f604fb6a3b12de90379ec0a5c
Matthew Beck (Berry) <me@mattrb.com>
2026-05-12 23:50:03 +0000

    f05d675 build: update cross-repo angular dependencies (main)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-d42fdf1
+ * @license Angular v22.1.0-next.0+sha-f05d675
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -216,7 +216,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-d42fdf1');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-f05d675');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

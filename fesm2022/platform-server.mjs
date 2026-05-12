c1f73dbae0362be6cf00054e8474e310f574fb2b
Matthew Beck (Berry) <me@mattrb.com>
2026-05-12 17:22:05 +0000

    62710ad docs: release notes for the v20.3.21 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-1f287b9
+ * @license Angular v22.1.0-next.0+sha-62710ad
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -216,7 +216,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-1f287b9');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-62710ad');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

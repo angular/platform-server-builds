439eb936bc2b29cfc42abe53de52e050dafa0291
Matthew Beck <me@mattrb.com>
2026-05-12 17:56:36 +0000

    7ab78d5 ci: mark devversion as unavailable

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.12+sha-49a133a
+ * @license Angular v21.2.12+sha-7ab78d5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -216,7 +216,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.12+sha-49a133a');
+const VERSION = /* @__PURE__ */new Version('21.2.12+sha-7ab78d5');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

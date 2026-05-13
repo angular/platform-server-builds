d29f9f9e3b9315a2d8d78864d8860130f306f8c6
Matthew Beck <me@mattrb.com>
2026-05-13 18:39:43 +0000

    1e079a8 docs: add clarification around plain object models

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.12+sha-7ab78d5
+ * @license Angular v21.2.12+sha-1e079a8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -216,7 +216,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.12+sha-7ab78d5');
+const VERSION = /* @__PURE__ */new Version('21.2.12+sha-1e079a8');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

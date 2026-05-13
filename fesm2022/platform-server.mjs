99e984d016036db5f8c6e7297ca8b862e3ebed07
Matthew Beck <me@mattrb.com>
2026-05-13 19:30:57 +0000

    1f0c1ab refactor(core): patch special provider classes with __NG_ELEMENT_ID__

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.12+sha-ccb7d42
+ * @license Angular v22.0.0-next.12+sha-1f0c1ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -216,7 +216,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-ccb7d42');
+const VERSION = /* @__PURE__ */new Version('22.0.0-next.12+sha-1f0c1ab');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

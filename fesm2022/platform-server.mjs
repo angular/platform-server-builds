27328b6b5ce7ea66479ef0ded7abcc64174fbdc0
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-18 20:38:00 +0000

    16fe27b fix(core): do not insert todo when migrating void @Output

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-9a7dedc
+ * @license Angular v22.1.0-next.0+sha-16fe27b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -216,7 +216,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-9a7dedc');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-16fe27b');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

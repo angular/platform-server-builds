55454efb3e28b7539ee897a129a134bb3cbe18cd
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-19 20:40:16 +0000

    b53fa3c build: lock file maintenance

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-16fe27b
+ * @license Angular v22.1.0-next.0+sha-b53fa3c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -216,7 +216,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-16fe27b');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-b53fa3c');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

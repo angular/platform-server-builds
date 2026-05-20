89f0e2a57a6104f90dfcef5d82bb4f46eda6c3ae
leonsenft <leonsenft@users.noreply.github.com>
2026-05-20 18:31:15 +0000

    1d6e71d docs: clarify ngDoCheck invocation behavior with OnPush strategy

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.13+sha-0c7f70e
+ * @license Angular v21.2.13+sha-1d6e71d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -216,7 +216,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.13+sha-0c7f70e');
+const VERSION = /* @__PURE__ */new Version('21.2.13+sha-1d6e71d');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

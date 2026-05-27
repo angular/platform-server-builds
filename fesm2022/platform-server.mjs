f7f98c22ce8141b7fe36cab74f44f78140431dfa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 23:06:18 +0000

    a68a33e docs: release notes for the v20.3.22 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-161c79d
+ * @license Angular v22.1.0-next.0+sha-a68a33e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -219,7 +219,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-161c79d');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-a68a33e');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

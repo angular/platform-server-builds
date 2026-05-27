bfb43dd27f139cd93a9eace9e67d997104323d1b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 18:05:57 +0000

    2b17b2d refactor(language-server): drop duplicate isAngularCore helpers in session

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.14+sha-37e8aad
+ * @license Angular v21.2.14+sha-2b17b2d
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -219,7 +219,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.14+sha-37e8aad');
+const VERSION = /* @__PURE__ */new Version('21.2.14+sha-2b17b2d');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

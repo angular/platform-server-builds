728c2cf7ffe17142f5bec03c3e0275f2c6186033
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 21:26:52 +0000

    34090cb fix(http): exclude withCredentials requests from transfer cache

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-6388675
+ * @license Angular v22.1.0-next.0+sha-34090cb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -219,7 +219,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-6388675');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-34090cb');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

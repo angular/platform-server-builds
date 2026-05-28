5124a411214eb487f9773cff3bab02f94e3479cf
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 11:54:10 +0000

    8538bdc docs: fix grammar issues in resource guide

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.14+sha-582a417
+ * @license Angular v21.2.14+sha-8538bdc
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -219,7 +219,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.14+sha-582a417');
+const VERSION = /* @__PURE__ */new Version('21.2.14+sha-8538bdc');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

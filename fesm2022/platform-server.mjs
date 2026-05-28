634be6bf5fb3e2e9f4ebe21ffe01b88bfd803ad0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 14:15:43 +0000

    4f79cfb build: update all github actions

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-ec138c3
+ * @license Angular v22.1.0-next.0+sha-4f79cfb
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-ec138c3');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-4f79cfb');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

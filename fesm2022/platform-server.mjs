9ba059075d59ad3afbe29e4c81c6bbbbb7f9dabe
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 14:15:22 +0000

    6b8202e refactor(docs-infra): extract magic 27 in navigation-list tooltip threshold

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.14+sha-eb1cbbf
+ * @license Angular v21.2.14+sha-6b8202e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -219,7 +219,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.14+sha-eb1cbbf');
+const VERSION = /* @__PURE__ */new Version('21.2.14+sha-6b8202e');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

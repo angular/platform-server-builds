9af206e7b787531c8bc571738ae8f5aa52b52a96
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 12:14:37 +0000

    94d520f fix(compiler): prevent namespaced SVG <style> elements from being stripped

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.1+sha-7b4b067
+ * @license Angular v22.0.0-rc.1+sha-94d520f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-7b4b067');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.1+sha-94d520f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

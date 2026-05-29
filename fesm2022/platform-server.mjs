6910ba2f06520cff852b116f1d1f5b85c6e161eb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 10:28:02 +0000

    5f0c9f8 docs: update cross-repo adev docs

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.2+sha-c2153b1
+ * @license Angular v22.0.0-rc.2+sha-5f0c9f8
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.2+sha-c2153b1');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.2+sha-5f0c9f8');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

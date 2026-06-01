acd8aed846d12fae0af443f848cb98260eb7d102
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 20:15:03 +0000

    3cc9e2b docs: release notes for the v22.0.0-rc.3 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-0b19c92
+ * @license Angular v22.1.0-next.0+sha-3cc9e2b
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-0b19c92');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-3cc9e2b');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

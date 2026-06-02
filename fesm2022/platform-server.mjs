2cbe5e424744754c7da4184fc0ec6edd9906069c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-02 09:31:32 +0000

    04028c7 docs: add testing guides to Angular Aria

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-rc.3+sha-7584e35
+ * @license Angular v22.0.0-rc.3+sha-04028c7
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0-rc.3+sha-7584e35');
+const VERSION = /* @__PURE__ */new Version('22.0.0-rc.3+sha-04028c7');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

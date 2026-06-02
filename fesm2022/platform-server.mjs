1b151909efc432460f71356ad45b22675cd72e62
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-02 12:23:51 +0000

    736c4ab refactor(core): fix broken unit test.

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.15+sha-3fd6897
+ * @license Angular v21.2.15+sha-736c4ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -219,7 +219,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.15+sha-3fd6897');
+const VERSION = /* @__PURE__ */new Version('21.2.15+sha-736c4ab');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

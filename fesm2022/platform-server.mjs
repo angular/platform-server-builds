7564dc6fdc115044d6dc2cb4a372158be2e6ceb3
Andrew Scott <atscott01@gmail.com>
2026-06-03 23:11:16 +0000

    6bde84f fix(core): harden TransferState restoration against DOM clobbering

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-7896e74
+ * @license Angular v22.1.0-next.0+sha-6bde84f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-7896e74');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-6bde84f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

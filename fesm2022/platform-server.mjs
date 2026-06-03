8d59e08362bd74fdfe8c246eb6ff4a68bbaab3c5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 09:12:01 +0000

    ac30e48 docs: update signal forms conditional rules

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-e695379
+ * @license Angular v22.1.0-next.0+sha-ac30e48
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-e695379');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-ac30e48');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

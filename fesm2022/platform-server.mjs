71d0891c62cc34bf823d326e3a6b44662433e087
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 16:23:10 +0000

    c1b2c58 docs: align dev skills examples with style guide

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-2052495
+ * @license Angular v22.1.0-next.0+sha-c1b2c58
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-2052495');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-c1b2c58');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

46124ac90a34319a4622b822e3e2dd364120f018
Andrew Scott <atscott01@gmail.com>
2026-06-04 22:35:48 +0000

    af04e26 refactor(http): deprecate jsonp support

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-df77e42
+ * @license Angular v22.1.0-next.0+sha-af04e26
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-df77e42');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-af04e26');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

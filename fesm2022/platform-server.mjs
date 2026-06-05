1c9623673e858ebb0725790ddbcebb5319307be2
Andrew Scott <atscott01@gmail.com>
2026-06-05 22:21:33 +0000

    255151a fix(http): Rejects non-HTTP(S) URLs in JSONP requests

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-79e5d5d
+ * @license Angular v22.1.0-next.0+sha-255151a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-79e5d5d');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-255151a');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

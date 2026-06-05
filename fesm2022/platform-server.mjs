30e152e1687663651ca2d45bfc2248f73dd8a7f6
Andrew Scott <atscott01@gmail.com>
2026-06-05 16:56:19 +0000

    bc55749 fix(common): use cryptographically secure SHA-256 for transfer cache key generation

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v21.2.16+sha-d846326
+ * @license Angular v21.2.16+sha-bc55749
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -219,7 +219,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('21.2.16+sha-d846326');
+const VERSION = /* @__PURE__ */new Version('21.2.16+sha-bc55749');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

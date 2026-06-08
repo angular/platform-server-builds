28d165cda4e8533172587793486c093c42f368ea
Andrew Scott <atscott01@gmail.com>
2026-06-08 21:32:07 +0000

    e7dae12 fix(docs-infra): avoid code copy button overlap

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-167bd4c
+ * @license Angular v22.0.0+sha-e7dae12
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-167bd4c');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-e7dae12');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

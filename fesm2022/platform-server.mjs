8c2deacae577e22d800b8740d3c9a23c85ce307c
Andrew Scott <atscott01@gmail.com>
2026-06-09 18:22:49 +0000

    43edc84 fix(router): use native URL object for navigation boundary and comparison

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-c4b5fa3
+ * @license Angular v22.0.0+sha-43edc84
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-c4b5fa3');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-43edc84');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

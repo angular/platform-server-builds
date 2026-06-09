80c5698e10093eabe084371cb62aeccb808dca8c
Andrew Scott <atscott01@gmail.com>
2026-06-09 17:12:58 +0000

    f908a3e refactor(core): harden change store access

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-8ddda4d
+ * @license Angular v22.0.0+sha-f908a3e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-8ddda4d');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-f908a3e');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

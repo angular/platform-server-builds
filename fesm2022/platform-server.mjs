4f9dd4d344c077831eafd1ec3a6d2b2c0557a30e
Andrew Scott <atscott01@gmail.com>
2026-06-10 20:22:59 +0000

    8b32559 release: cut the v22.0.1 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-2dd65d2
+ * @license Angular v22.0.1+sha-8b32559
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-2dd65d2');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-8b32559');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

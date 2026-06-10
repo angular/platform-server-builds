3e69c29f2bebbcf89c94bf14e922074706cbf6a5
Andrew Scott <atscott01@gmail.com>
2026-06-10 17:07:30 +0000

    e51ad37 fix(forms): remove animationstart listener on component destroy to prevent memory leak

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0+sha-85d2d10
+ * @license Angular v22.0.0+sha-e51ad37
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.0+sha-85d2d10');
+const VERSION = /* @__PURE__ */new Version('22.0.0+sha-e51ad37');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

6120491bcd1638f2c1b9e5ad2284cbdd0da3d362
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-11 17:09:08 +0000

    a6c7fc5 fix(http): distinguish repeated transfer cache params

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-fd7c2da
+ * @license Angular v22.1.0-next.0+sha-a6c7fc5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-fd7c2da');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-a6c7fc5');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

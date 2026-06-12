f607c8b2a89425f806345df2d99636573b1d7798
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-12 15:25:51 +0000

    e74b884 build: update cross-repo angular dependencies to 11ee1f5

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-0f68807
+ * @license Angular v22.0.1+sha-e74b884
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-0f68807');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-e74b884');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

1383fcf921b2eb6f3687e7f2f8371bc193daaa55
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-15 19:03:36 +0000

    83622ee refactor(compiler-cli): Export indexer API for use in hybrid analysis

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-01e99d8
+ * @license Angular v22.1.0-next.0+sha-83622ee
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-01e99d8');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-83622ee');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

5f06cc18b9a45005ff9ff52393b69452dd73b1e2
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-16 17:35:57 +0000

    86ade07 refactor(compiler): add support for @Input transforms under isolatedDeclarations

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-0152e3c
+ * @license Angular v22.1.0-next.0+sha-86ade07
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-0152e3c');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-86ade07');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

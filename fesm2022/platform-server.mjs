980cf4cf51efb57735bca00d57eaf509eb82333c
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-17 20:17:50 +0000

    45192ba ci: update dev-infra reference to new commit SHA

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.0+sha-1e79dd3
+ * @license Angular v22.1.0-next.0+sha-45192ba
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-1e79dd3');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.0+sha-45192ba');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

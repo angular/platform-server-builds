c5fa4408e0381555d6507e47d01cf72936aef529
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-17 17:40:29 +0000

    e3d392f ci: add id-token: write permission to release workflow

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.1+sha-74f2b91
+ * @license Angular v22.0.1+sha-e3d392f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.1+sha-74f2b91');
+const VERSION = /* @__PURE__ */new Version('22.0.1+sha-e3d392f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

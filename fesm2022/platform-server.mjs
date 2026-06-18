4387c0e15a759cb61f7bd5ffd712f8f4ff6596e6
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-06-18 13:35:35 +0000

    291d574 docs(docs-infra): remove deprecated ts flags

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.2+sha-aac8577
+ * @license Angular v22.0.2+sha-291d574
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.2+sha-aac8577');
+const VERSION = /* @__PURE__ */new Version('22.0.2+sha-291d574');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

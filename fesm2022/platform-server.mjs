e4d3709f6d73412f455b7755d6dde1963ff84a07
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-18 16:40:16 +0000

    a849b6f Revert "fix(core): escape overlapping comment delimiters in escapeCommentText"

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.1+sha-1566442
+ * @license Angular v22.1.0-next.1+sha-a849b6f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-1566442');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.1+sha-a849b6f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

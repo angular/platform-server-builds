dbda2def48b5935ec4970584a6f8bb79567c3bef
GitHub <noreply@github.com>
2026-06-29 21:39:39 +0000

    1659952 fix(compiler-cli): include toSignal in debugName transform

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.3+sha-d109cc5
+ * @license Angular v22.1.0-next.3+sha-1659952
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.3+sha-d109cc5');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.3+sha-1659952');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

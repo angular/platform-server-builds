b9bbc7e1ad57635ff3d526f9ec71a9e56c814194
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-29 23:13:26 +0000

    b5ea340 refactor(compiler): remove `visitAttributeComment`

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.4+sha-ca76b2c
+ * @license Angular v22.0.4+sha-b5ea340
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.4+sha-ca76b2c');
+const VERSION = /* @__PURE__ */new Version('22.0.4+sha-b5ea340');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

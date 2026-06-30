3c782eb095f8e03a544dd5a7c1e3cc6b67a05671
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-30 20:34:04 +0000

    af7edd9 Revert "fix(compiler-cli): include toSignal in debugName transform"

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.4+sha-b5ea340
+ * @license Angular v22.0.4+sha-af7edd9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.4+sha-b5ea340');
+const VERSION = /* @__PURE__ */new Version('22.0.4+sha-af7edd9');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

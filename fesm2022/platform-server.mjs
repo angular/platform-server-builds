41701d2d2bd49cc02ef990cd6ad0b546a1a6c479
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-01 00:37:32 +0000

    ced0180 fix(core): reject dynamic script host elements

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.4+sha-af7edd9
+ * @license Angular v22.0.4+sha-ced0180
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.4+sha-af7edd9');
+const VERSION = /* @__PURE__ */new Version('22.0.4+sha-ced0180');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

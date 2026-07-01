1b5d7da86ebf0fc96ef3ce51621aa8e55522a28c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-01 20:56:02 +0000

    2b3277f release: cut the v22.0.5 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.4+sha-0554bf5
+ * @license Angular v22.0.5+sha-2b3277f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.4+sha-0554bf5');
+const VERSION = /* @__PURE__ */new Version('22.0.5+sha-2b3277f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

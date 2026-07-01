2281187a645ad2df6cb679efaaafc2e93a416f2a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-01 01:08:13 +0000

    c4cb66e docs: improve introductory resource example

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.3+sha-d7f7061
+ * @license Angular v22.1.0-next.3+sha-c4cb66e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.3+sha-d7f7061');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.3+sha-c4cb66e');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

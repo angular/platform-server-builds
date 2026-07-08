c5c3bf94acb6847728983b2e86ac18be21689f5d
leonsenft <leonsenft@users.noreply.github.com>
2026-07-08 18:35:07 +0000

    ab18f5f refactor(core): cleanup Meta service

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.5+sha-2d52d53
+ * @license Angular v22.0.5+sha-ab18f5f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.5+sha-2d52d53');
+const VERSION = /* @__PURE__ */new Version('22.0.5+sha-ab18f5f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

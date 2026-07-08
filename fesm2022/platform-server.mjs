b25b9d3e23833130dbd1bd4c592e9e975aafc086
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 21:45:03 +0000

    89e2c77 release: cut the v22.0.6 release

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.5+sha-d1aaf1a
+ * @license Angular v22.0.6+sha-89e2c77
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.5+sha-d1aaf1a');
+const VERSION = /* @__PURE__ */new Version('22.0.6+sha-89e2c77');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

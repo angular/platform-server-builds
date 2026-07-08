7f6c68eb0f59cf67e24b28185bac4aab20f44f5c
leonsenft <leonsenft@users.noreply.github.com>
2026-07-08 17:52:44 +0000

    2d52d53 docs: update optional chaining behavior

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.5+sha-748faa4
+ * @license Angular v22.0.5+sha-2d52d53
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.5+sha-748faa4');
+const VERSION = /* @__PURE__ */new Version('22.0.5+sha-2d52d53');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

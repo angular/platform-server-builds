5366d5964af438156b829fb3db04a2e91997db59
leonsenft <leonsenft@users.noreply.github.com>
2026-07-08 19:27:37 +0000

    2cb034f docs: clarify runtime component comparison in elements guide

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.5+sha-ab18f5f
+ * @license Angular v22.0.5+sha-2cb034f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.5+sha-ab18f5f');
+const VERSION = /* @__PURE__ */new Version('22.0.5+sha-2cb034f');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

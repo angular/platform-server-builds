926da88102e050388e2e070399207e1f7e6e1f32
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 08:54:33 +0000

    2aecfb0 refactor(devtools): format code

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.6+sha-f8c9e9d
+ * @license Angular v22.1.0-next.6+sha-2aecfb0
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-f8c9e9d');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-2aecfb0');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

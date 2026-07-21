a974b37e19126bec8b7a1ca2866e60810cc34b72
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-21 11:54:15 +0000

    d302c7a fix(forms):  ensure `pending` status propagates to the root form in signal forms

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.7+sha-a79eb0d
+ * @license Angular v22.0.7+sha-d302c7a
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.0.7+sha-a79eb0d');
+const VERSION = /* @__PURE__ */new Version('22.0.7+sha-d302c7a');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

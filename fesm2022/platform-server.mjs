dfac4b1e7d1407fe356bd3315fdc3d7d10cfbb04
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 10:07:06 +0000

    6557df5 docs: update stale Twitter reference to X in the URL matcher guide

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-next.6+sha-6371f0b
+ * @license Angular v22.1.0-next.6+sha-6557df5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-6371f0b');
+const VERSION = /* @__PURE__ */new Version('22.1.0-next.6+sha-6557df5');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

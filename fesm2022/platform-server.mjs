95c86763fcccb04b0ec8881d8455474f3aa40eb8
Douglas Parker <dgp1130@users.noreply.github.com>
2026-07-23 19:09:27 +0000

    e779309 release: bump Angular DevTools version to 1.18.0

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.0+sha-3bf2430
+ * @license Angular v22.2.0-next.0+sha-e779309
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-3bf2430');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-e779309');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

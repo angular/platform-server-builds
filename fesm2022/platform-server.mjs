4cafe696736adefaac213bde9c1206532dfeff41
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 17:42:16 +0000

    1784530 docs: modernize directives guides (#69822)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.0+sha-28d59e8
+ * @license Angular v22.2.0-next.0+sha-1784530
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-28d59e8');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-1784530');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

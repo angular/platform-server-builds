a34fe7e3e0183179d04297070700717bbde9832c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 17:43:44 +0000

    0650966 docs: modernize directives guides (#69822)

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.8+sha-433276b
+ * @license Angular v22.0.8+sha-0650966
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -19,7 +19,7 @@ const platformServerTesting = createPlatformFactory(platformCore, 'serverTesting
 class ServerTestingModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.0.8+sha-433276b",
+    version: "22.0.8+sha-0650966",
     ngImport: i0,
     type: ServerTestingModule,
     deps: [],
@@ -27,14 +27,14 @@ class ServerTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "22.0.8+sha-433276b",
+    version: "22.0.8+sha-0650966",
     ngImport: i0,
     type: ServerTestingModule,
     exports: [BrowserDynamicTestingModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "22.0.8+sha-433276b",
+    version: "22.0.8+sha-0650966",
     ngImport: i0,
     type: ServerTestingModule,
     providers: SERVER_RENDER_PROVIDERS,
@@ -43,7 +43,7 @@ class ServerTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.0.8+sha-433276b",
+  version: "22.0.8+sha-0650966",
   ngImport: i0,
   type: ServerTestingModule,
   decorators: [{

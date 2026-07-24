8c82a2d00c101d32a322918d72f941ff53e5e28a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 20:53:00 +0000

    e606a02 fix(language-service): account for strictTemplates being enabled by default

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.0+sha-1784530
+ * @license Angular v22.2.0-next.0+sha-e606a02
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-1784530');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-e606a02');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

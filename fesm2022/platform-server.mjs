83f8a487a836d30fa403a78611e39d6d42a5ed29
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 15:34:19 +0000

    125a794 docs: use https for external links in adev

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-rc.0+sha-1b85b3c
+ * @license Angular v22.1.0-rc.0+sha-125a794
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-rc.0+sha-1b85b3c');
+const VERSION = /* @__PURE__ */new Version('22.1.0-rc.0+sha-125a794');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

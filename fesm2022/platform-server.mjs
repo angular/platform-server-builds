8d5ac90c9ba037c93e2557916478be22ac31bb1c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 17:08:43 +0000

    7825e60 docs: invoke signal inputs in adev examples and tutorials

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0-rc.0+sha-098d28f
+ * @license Angular v22.1.0-rc.0+sha-7825e60
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0-rc.0+sha-098d28f');
+const VERSION = /* @__PURE__ */new Version('22.1.0-rc.0+sha-7825e60');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

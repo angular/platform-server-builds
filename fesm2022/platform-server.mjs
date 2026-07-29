d44a26d51a137e677cc378ba4a17a301b101165f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 16:00:18 +0000

    db0d4a1 fix(compiler): restrict possible event handler check to property names longer than 2 characters

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.26+sha-4d62760
+ * @license Angular v20.3.26+sha-db0d4a1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -359,7 +359,7 @@ function isHostAllowed(hostname, allowedHosts) {
 /**
  * @publicApi
  */
-const VERSION = /* @__PURE__ */ new Version('20.3.26+sha-4d62760');
+const VERSION = /* @__PURE__ */ new Version('20.3.26+sha-db0d4a1');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map
--- fesm2022/server.mjs
+++ fesm2022/server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v20.3.26+sha-4d62760
+ * @license Angular v20.3.26+sha-db0d4a1
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -762,6 +762,15 @@ function requireNodeUtils () {
 	  PLAINTEXT: true
 	};
 
+	var hasRawContentFallback = {
+	  // Text in these fallback raw-content elements is inert for browser parsing,
+	  // but downstream SSR post-processing may reparse it without raw-text state.
+	  IFRAME: true,
+	  NOEMBED: true,
+	  NOSCRIPT: true,
+	  NOFRAMES: true
+	};
+
 	var emptyElements = {
 	  area: true,
 	  base: true,
@@ -862,7 +871,7 @@ function requireNodeUtils () {
 	 * and unsafe behavior after de-serialization.
 	 */
 	function escapeMatchingClosingTag(rawText, parentTag) {
-	  const parentClosingTag = '</' + parentTag;
+	  const parentClosingTag = ('</' + parentTag).toLowerCase();
 	  if (!rawText.toLowerCase().includes(parentClosingTag)) {
 	    return rawText; // fast path
 	  }
@@ -872,9 +881,51 @@ function requireNodeUtils () {
 	  // would otherwise shift the replacement and leave a real `</tag>`
 	  // break-out in the output.
 	  return rawText.replace(
-	    new RegExp(parentClosingTag, 'ig'),
+	    new RegExp(escapeRegExp(parentClosingTag), 'ig'),
 	    (m) => '&lt;' + m.slice(1)
 	  );
+	}
+
+	function escapeRegExp(text) {
+	  return text.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+	}
+
+	function escapeFallbackRawText(rawText, parentTag) {
+	  var result = '';
+	  var index = 0;
+
+	  while (index < rawText.length) {
+	    var commentStart = rawText.indexOf('<!--', index);
+	    if (commentStart === -1) {
+	      result += escape(rawText.slice(index));
+	      break;
+	    }
+
+	    result += escape(rawText.slice(index, commentStart));
+
+	    var commentEnd = findCommentEnd(rawText, commentStart + 4);
+	    if (commentEnd === -1) {
+	      result += escapeMatchingClosingTag(rawText.slice(commentStart), parentTag);
+	      break;
+	    }
+
+	    // A complete HTML comment remains inert if downstream tooling reparses
+	    // fallback raw text as normal HTML, so preserve its comment semantics.
+	    result += escapeMatchingClosingTag(rawText.slice(commentStart, commentEnd), parentTag);
+	    index = commentEnd;
+	  }
+
+	  return result;
+	}
+
+	function findCommentEnd(rawText, index) {
+	  if (rawText.charAt(index) === '>')
+	    return index + 1;
+	  if (rawText.charAt(index) === '-' && rawText.charAt(index + 1) === '>')
+	    return index + 2;
+
+	  var match = CLOSING_COMMENT_REGEXP.exec(rawText.slice(index));
+	  return match ? index + match.index + match[0].length : -1;
 	}
 
 	const CLOSING_COMMENT_REGEXP = /--!?>/;
@@ -924,7 +975,7 @@ function requireNodeUtils () {
 	        // If an element can have raw content, this content may
 	        // potentially require escaping to avoid XSS.
 	        var upperTag = tagname.toUpperCase();
-	        if (hasRawContent[upperTag]) {
+	        if (hasRawContent[upperTag] && !hasRawContentFallback[upperTag]) {
 	          ss = escapeMatchingClosingTag(ss, tagname);
 	        }
 	        if (html && extraNewLine[tagname] && ss.charAt(0)==='\n') s += '\n';
@@ -943,7 +994,9 @@ function requireNodeUtils () {
 	        parenttag = '';
 
 	      if (hasRawContent[parenttag]) {
-	        s += kid.data;
+	        // Preserve actual child element markup in fallback elements such as
+	        // <noscript>, but do not emit text-node payloads as raw HTML.
+	        s += hasRawContentFallback[parenttag] ? escapeFallbackRawText(kid.data, parent.localName) : kid.data;
 	      } else {
 	        s += escape(kid.data);
 	      }
@@ -17238,10 +17291,10 @@ class PlatformState {
     getDocument() {
         return this._doc;
     }
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.26+sha-4d62760", ngImport: i0, type: PlatformState, deps: [{ token: DOCUMENT }], target: i0.ɵɵFactoryTarget.Injectable });
-    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "20.3.26+sha-4d62760", ngImport: i0, type: PlatformState });
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.26+sha-db0d4a1", ngImport: i0, type: PlatformState, deps: [{ token: DOCUMENT }], target: i0.ɵɵFactoryTarget.Injectable });
+    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "20.3.26+sha-db0d4a1", ngImport: i0, type: PlatformState });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.26+sha-4d62760", ngImport: i0, type: PlatformState, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.26+sha-db0d4a1", ngImport: i0, type: PlatformState, decorators: [{
             type: Injectable
         }], ctorParameters: () => [{ type: undefined, decorators: [{
                     type: Inject,
@@ -17361,10 +17414,10 @@ class ServerXhr {
         }
         return new impl.XMLHttpRequest();
     }
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.26+sha-4d62760", ngImport: i0, type: ServerXhr, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
-    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "20.3.26+sha-4d62760", ngImport: i0, type: ServerXhr });
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.26+sha-db0d4a1", ngImport: i0, type: ServerXhr, deps: [], target: i0.ɵɵFactoryTarget.Injectable });
+    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "20.3.26+sha-db0d4a1", ngImport: i0, type: ServerXhr });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.26+sha-4d62760", ngImport: i0, type: ServerXhr, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.26+sha-db0d4a1", ngImport: i0, type: ServerXhr, decorators: [{
             type: Injectable
         }] });
 /**
@@ -17488,10 +17541,10 @@ class ServerPlatformLocation {
     getState() {
         return undefined;
     }
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.26+sha-4d62760", ngImport: i0, type: ServerPlatformLocation, deps: [{ token: DOCUMENT }, { token: INITIAL_CONFIG, optional: true }], target: i0.ɵɵFactoryTarget.Injectable });
-    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "20.3.26+sha-4d62760", ngImport: i0, type: ServerPlatformLocation });
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.26+sha-db0d4a1", ngImport: i0, type: ServerPlatformLocation, deps: [{ token: DOCUMENT }, { token: INITIAL_CONFIG, optional: true }], target: i0.ɵɵFactoryTarget.Injectable });
+    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "20.3.26+sha-db0d4a1", ngImport: i0, type: ServerPlatformLocation });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.26+sha-4d62760", ngImport: i0, type: ServerPlatformLocation, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.26+sha-db0d4a1", ngImport: i0, type: ServerPlatformLocation, decorators: [{
             type: Injectable
         }], ctorParameters: () => [{ type: undefined, decorators: [{
                     type: Inject,
@@ -17516,10 +17569,10 @@ class ServerEventManagerPlugin extends EventManagerPlugin {
     addEventListener(element, eventName, handler, options) {
         return _getDOM().onAndCancel(element, eventName, handler, options);
     }
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.26+sha-4d62760", ngImport: i0, type: ServerEventManagerPlugin, deps: [{ token: DOCUMENT }], target: i0.ɵɵFactoryTarget.Injectable });
-    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "20.3.26+sha-4d62760", ngImport: i0, type: ServerEventManagerPlugin });
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.26+sha-db0d4a1", ngImport: i0, type: ServerEventManagerPlugin, deps: [{ token: DOCUMENT }], target: i0.ɵɵFactoryTarget.Injectable });
+    static ɵprov = i0.ɵɵngDeclareInjectable({ minVersion: "12.0.0", version: "20.3.26+sha-db0d4a1", ngImport: i0, type: ServerEventManagerPlugin });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.26+sha-4d62760", ngImport: i0, type: ServerEventManagerPlugin, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.26+sha-db0d4a1", ngImport: i0, type: ServerEventManagerPlugin, decorators: [{
             type: Injectable
         }], ctorParameters: () => [{ type: undefined, decorators: [{
                     type: Inject,
@@ -17632,11 +17685,11 @@ const PLATFORM_SERVER_PROVIDERS = [
  * @publicApi
  */
 class ServerModule {
-    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.26+sha-4d62760", ngImport: i0, type: ServerModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
-    static ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "20.3.26+sha-4d62760", ngImport: i0, type: ServerModule, exports: [BrowserModule] });
-    static ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "20.3.26+sha-4d62760", ngImport: i0, type: ServerModule, providers: PLATFORM_SERVER_PROVIDERS, imports: [BrowserModule] });
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.26+sha-db0d4a1", ngImport: i0, type: ServerModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule });
+    static ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "20.3.26+sha-db0d4a1", ngImport: i0, type: ServerModule, exports: [BrowserModule] });
+    static ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "20.3.26+sha-db0d4a1", ngImport: i0, type: ServerModule, providers: PLATFORM_SERVER_PROVIDERS, imports: [BrowserModule] });
 }
-i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.26+sha-4d62760", ngImport: i0, type: ServerModule, decorators: [{
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.26+sha-db0d4a1", ngImport: i0, type: ServerModule, decorators: [{
             type: NgModule,
             args: [{
                     exports: [BrowserModule],

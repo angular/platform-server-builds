4839e7f2c548d2f977f45bc9350517aed970ac03
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-07-30 17:07:30 +0000

    b63b903 build: update all non-major dependencies

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.0+sha-9e5dc74
+ * @license Angular v22.2.0-next.0+sha-b63b903
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-9e5dc74');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.0+sha-b63b903');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

c19a4b6f1e158620e4f0c4c7ad1b0f66525c849f
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-03 16:19:52 +0000

    6b643ab refactor(common): modernize directive tests to rely on whenStable

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0+sha-79a8193
+ * @license Angular v22.1.0+sha-6b643ab
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-79a8193');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-6b643ab');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

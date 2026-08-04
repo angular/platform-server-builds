b870959492b79dd4d261b2d445876e8121b002d7
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-04 15:36:06 +0000

    32af9b5 fix(http): strip RFC 6265 DQUOTE characters and handle URIError in parseCookieValue

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.0+sha-3478df7
+ * @license Angular v22.1.0+sha-32af9b5
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.0+sha-3478df7');
+const VERSION = /* @__PURE__ */new Version('22.1.0+sha-32af9b5');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

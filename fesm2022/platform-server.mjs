2204cfd0f354f7197f706db7e8e93c98486ad685
Alon Mishne <amishne@users.noreply.github.com>
2026-08-10 22:44:14 +0000

    0df9d5e fix(core): ensure i18n_util hasOwnProperty checks are safe for property renaming

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-596356e
+ * @license Angular v22.1.1+sha-0df9d5e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-596356e');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-0df9d5e');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

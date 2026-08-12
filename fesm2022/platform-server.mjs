51afe9ec6ca1e8369d8c1d096e496f7fd7ab7730
Alon Mishne <amishne@users.noreply.github.com>
2026-08-12 23:24:47 +0000

    e9660b1 fix(compiler-cli): correctly resolve symbol for SafePropertyRead in chained optional navigation

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-3032e7f
+ * @license Angular v22.1.1+sha-e9660b1
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-3032e7f');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-e9660b1');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

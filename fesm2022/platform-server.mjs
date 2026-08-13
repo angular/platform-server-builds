e73f1f8fa366f188f1993772410076118be5e76d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-13 16:05:08 +0000

    0a708e9 docs: note SafeResourceUrl audio src change

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.1+sha-910f391
+ * @license Angular v22.1.1+sha-0a708e9
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.1+sha-910f391');
+const VERSION = /* @__PURE__ */new Version('22.1.1+sha-0a708e9');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

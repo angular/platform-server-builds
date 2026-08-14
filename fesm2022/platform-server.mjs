bc5dfd240d4d65f2e90889ce069c1b220cf1c56d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-14 15:40:10 +0000

    c04931c fix(core): throw a descriptive error instead of crashing when a hydration node is missing

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.2+sha-0cd243d
+ * @license Angular v22.1.2+sha-c04931c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.2+sha-0cd243d');
+const VERSION = /* @__PURE__ */new Version('22.1.2+sha-c04931c');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

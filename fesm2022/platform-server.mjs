798c4f0fb5e1a21b5792b247777654ee9e97fea4
leonsenft <leonsenft@users.noreply.github.com>
2026-08-25 17:44:34 +0000

    07605ba feat(devtools): implement CD analyzer (#70254)

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.3+sha-90fc947
+ * @license Angular v22.2.0-next.3+sha-07605ba
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-90fc947');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.3+sha-07605ba');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

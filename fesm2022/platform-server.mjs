687692846f5589cff36fa560842cd8f00353c563
leonsenft <leonsenft@users.noreply.github.com>
2026-08-25 23:37:41 +0000

    b199bdf fix(core): explicitly reject foreign components in JIT mode

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.3+sha-bf69e85
+ * @license Angular v22.1.3+sha-b199bdf
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.3+sha-bf69e85');
+const VERSION = /* @__PURE__ */new Version('22.1.3+sha-b199bdf');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

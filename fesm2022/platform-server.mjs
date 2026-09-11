ac92bd7dc77f1a0f8e7feea6a528ba395d5ea725
Andrew Scott <atscott01@gmail.com>
2026-09-11 18:09:16 +0000

    d0ed76c docs: fix the broken housing location template in the first-app tutorial

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-next.7+sha-01d091b
+ * @license Angular v22.2.0-next.7+sha-d0ed76c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-01d091b');
+const VERSION = /* @__PURE__ */new Version('22.2.0-next.7+sha-d0ed76c');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

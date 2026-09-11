3ab04c7cb154796beff3555fd77843dd8cd3f082
Andrew Scott <atscott01@gmail.com>
2026-09-11 22:24:56 +0000

    376b71e docs(forms): document what required() considers empty

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.6+sha-4b5571c
+ * @license Angular v22.1.6+sha-376b71e
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-4b5571c');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-376b71e');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

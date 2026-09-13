baafe72fd9e2c8076fdab3c99d43e2b67efac42a
Andrew Scott <atscott01@gmail.com>
2026-09-13 21:27:39 +0000

    16c8064 test(core): reduce fakeAsync usage in animation tests

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.6+sha-8509045
+ * @license Angular v22.1.6+sha-16c8064
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.6+sha-8509045');
+const VERSION = /* @__PURE__ */new Version('22.1.6+sha-16c8064');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

2d175938c3f7eec6ab1539422e2a9bfd556021ec
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-17 21:17:21 +0000

    ab0cfda refactor(platform-browser): type shadow DOM renderer fields

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.2.0-rc.0+sha-af2c7e3
+ * @license Angular v22.2.0-rc.0+sha-ab0cfda
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-af2c7e3');
+const VERSION = /* @__PURE__ */new Version('22.2.0-rc.0+sha-ab0cfda');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

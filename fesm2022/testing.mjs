10e5e312d08d59bd554edc9149c48958b365f982
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-17 17:54:05 +0000

    03754b3 docs: use the remote-cache config in the bazel caching guide

--- fesm2022/testing.mjs
+++ fesm2022/testing.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-c0ed0ad
+ * @license Angular v22.1.7+sha-03754b3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -19,7 +19,7 @@ const platformServerTesting = createPlatformFactory(platformCore, 'serverTesting
 class ServerTestingModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.1.7+sha-c0ed0ad",
+    version: "22.1.7+sha-03754b3",
     ngImport: i0,
     type: ServerTestingModule,
     deps: [],
@@ -27,14 +27,14 @@ class ServerTestingModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "22.1.7+sha-c0ed0ad",
+    version: "22.1.7+sha-03754b3",
     ngImport: i0,
     type: ServerTestingModule,
     exports: [BrowserDynamicTestingModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "22.1.7+sha-c0ed0ad",
+    version: "22.1.7+sha-03754b3",
     ngImport: i0,
     type: ServerTestingModule,
     providers: SERVER_RENDER_PROVIDERS,
@@ -43,7 +43,7 @@ class ServerTestingModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.1.7+sha-c0ed0ad",
+  version: "22.1.7+sha-03754b3",
   ngImport: i0,
   type: ServerTestingModule,
   decorators: [{

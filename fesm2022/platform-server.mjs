a07c0ed843d8872639d8ce775cf55f0aeabaf42d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 17:36:57 +0000

    78c7ab3 fix(forms): create controls before listeners

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.1.7+sha-aa2cabb
+ * @license Angular v22.1.7+sha-78c7ab3
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.1.7+sha-aa2cabb');
+const VERSION = /* @__PURE__ */new Version('22.1.7+sha-78c7ab3');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

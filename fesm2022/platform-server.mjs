7458cac7beb0e0ce9e20302e53ad536071642b60
Alan Agius <alan.agius4@gmail.com>
2026-09-22 18:05:22 +0000

    6cae42c build: update all non-major dependencies

--- fesm2022/platform-server.mjs
+++ fesm2022/platform-server.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-aa1cc47
+ * @license Angular v22.3.0-next.0+sha-6cae42c
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -226,7 +226,7 @@ function isHostAllowed(hostname, allowedHosts) {
   return false;
 }
 
-const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-aa1cc47');
+const VERSION = /* @__PURE__ */new Version('22.3.0-next.0+sha-6cae42c');
 
 export { BEFORE_APP_SERIALIZED, INITIAL_CONFIG, PlatformState, VERSION, platformServer, provideServerRendering, renderApplication, renderModule, SERVER_CONTEXT as ɵSERVER_CONTEXT, isHostAllowed as ɵisHostAllowed, renderInternal as ɵrenderInternal };
 //# sourceMappingURL=platform-server.mjs.map

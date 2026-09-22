ab5c09ddb07d0fcfd50c99df66fca9368dc023ea
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-22 21:25:20 +0000

    249f55f docs: correct the adev writing guide skill

--- fesm2022/_server-chunk.mjs
+++ fesm2022/_server-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.3.0-next.0+sha-6cae42c
+ * @license Angular v22.3.0-next.0+sha-249f55f
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -109,7 +109,7 @@ class PlatformState {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.3.0-next.0+sha-6cae42c",
+    version: "22.3.0-next.0+sha-249f55f",
     ngImport: i0,
     type: PlatformState,
     deps: [{
@@ -119,14 +119,14 @@ class PlatformState {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.3.0-next.0+sha-6cae42c",
+    version: "22.3.0-next.0+sha-249f55f",
     ngImport: i0,
     type: PlatformState
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.3.0-next.0+sha-6cae42c",
+  version: "22.3.0-next.0+sha-249f55f",
   ngImport: i0,
   type: PlatformState,
   decorators: [{
@@ -220,7 +220,7 @@ class ServerXhr {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.3.0-next.0+sha-6cae42c",
+    version: "22.3.0-next.0+sha-249f55f",
     ngImport: i0,
     type: ServerXhr,
     deps: [],
@@ -228,14 +228,14 @@ class ServerXhr {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.3.0-next.0+sha-6cae42c",
+    version: "22.3.0-next.0+sha-249f55f",
     ngImport: i0,
     type: ServerXhr
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.3.0-next.0+sha-6cae42c",
+  version: "22.3.0-next.0+sha-249f55f",
   ngImport: i0,
   type: ServerXhr,
   decorators: [{
@@ -244,8 +244,7 @@ i0.ɵɵngDeclareClassMetadata({
 });
 const URL_SCHEMA_REGEXP = /^(?:[a-zA-Z][a-zA-Z0-9+\-.]*:)/;
 function relativeUrlsTransformerInterceptorFn(request, next) {
-  const trimmedUrl = request.url.trim();
-  if (URL_SCHEMA_REGEXP.test(trimmedUrl) && !HTTP_OR_HTTPS_NO_AUTHORITY_REGEXP.test(trimmedUrl)) {
+  if (URL_SCHEMA_REGEXP.test(request.url) && !HTTP_OR_HTTPS_NO_AUTHORITY_REGEXP.test(request.url)) {
     return next(request);
   }
   const platformLocation = inject(PlatformLocation);
@@ -377,7 +376,7 @@ class ServerPlatformLocation {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.3.0-next.0+sha-6cae42c",
+    version: "22.3.0-next.0+sha-249f55f",
     ngImport: i0,
     type: ServerPlatformLocation,
     deps: [],
@@ -385,14 +384,14 @@ class ServerPlatformLocation {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.3.0-next.0+sha-6cae42c",
+    version: "22.3.0-next.0+sha-249f55f",
     ngImport: i0,
     type: ServerPlatformLocation
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.3.0-next.0+sha-6cae42c",
+  version: "22.3.0-next.0+sha-249f55f",
   ngImport: i0,
   type: ServerPlatformLocation,
   decorators: [{
@@ -415,7 +414,7 @@ class ServerEventManagerPlugin extends EventManagerPlugin {
   }
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.3.0-next.0+sha-6cae42c",
+    version: "22.3.0-next.0+sha-249f55f",
     ngImport: i0,
     type: ServerEventManagerPlugin,
     deps: [{
@@ -425,14 +424,14 @@ class ServerEventManagerPlugin extends EventManagerPlugin {
   });
   static ɵprov = i0.ɵɵngDeclareInjectable({
     minVersion: "12.0.0",
-    version: "22.3.0-next.0+sha-6cae42c",
+    version: "22.3.0-next.0+sha-249f55f",
     ngImport: i0,
     type: ServerEventManagerPlugin
   });
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.3.0-next.0+sha-6cae42c",
+  version: "22.3.0-next.0+sha-249f55f",
   ngImport: i0,
   type: ServerEventManagerPlugin,
   decorators: [{
@@ -542,7 +541,7 @@ const PLATFORM_SERVER_PROVIDERS = [TRANSFER_STATE_SERIALIZATION_PROVIDERS, SERVE
 class ServerModule {
   static ɵfac = i0.ɵɵngDeclareFactory({
     minVersion: "12.0.0",
-    version: "22.3.0-next.0+sha-6cae42c",
+    version: "22.3.0-next.0+sha-249f55f",
     ngImport: i0,
     type: ServerModule,
     deps: [],
@@ -550,14 +549,14 @@ class ServerModule {
   });
   static ɵmod = i0.ɵɵngDeclareNgModule({
     minVersion: "14.0.0",
-    version: "22.3.0-next.0+sha-6cae42c",
+    version: "22.3.0-next.0+sha-249f55f",
     ngImport: i0,
     type: ServerModule,
     exports: [BrowserModule]
   });
   static ɵinj = i0.ɵɵngDeclareInjector({
     minVersion: "12.0.0",
-    version: "22.3.0-next.0+sha-6cae42c",
+    version: "22.3.0-next.0+sha-249f55f",
     ngImport: i0,
     type: ServerModule,
     providers: PLATFORM_SERVER_PROVIDERS,
@@ -566,7 +565,7 @@ class ServerModule {
 }
 i0.ɵɵngDeclareClassMetadata({
   minVersion: "12.0.0",
-  version: "22.3.0-next.0+sha-6cae42c",
+  version: "22.3.0-next.0+sha-249f55f",
   ngImport: i0,
   type: ServerModule,
   decorators: [{
